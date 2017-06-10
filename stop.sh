646aa3d9ee152ff0bc1f20c7cb23ccd0e97ea8e1
eriknordmark <nordmark@sonic.net>
2017-06-09 17:18:29 -0700

    Clarify output

--- stop.sh
+++ stop.sh
@@ -13,13 +13,13 @@ eid=`grep "eid-prefix = fd" lisp.config | awk '{print $3}' | awk -F/ '{print $1}
 # Take the first one for now
 ms=`grep dns-name $LISPDIR/lisp.config | awk '{print $3}' | sort -u`
 for m in $ms; do
-    echo ms $ms
+    # echo ms $ms
     ips=`getent hosts $m | awk '{print $1}' | sort -u`
     # Could get multiple ips
     for ip in $ips; do
-	echo ip $ip
+	# echo ip $ip
 	rt=`ip route get $ip`
-	echo rt $rt
+	# echo rt $rt
 	intf=`echo $rt | sed 's/.* dev \([^ ]*\) .*/\1/'`
 	if [ "$intf" != "" ]; then
 	    break
@@ -29,8 +29,7 @@ for m in $ms; do
 	break
     fi
 done
-echo "EID: $eid"
-echo "INTF: $intf"
+echo "Removing config for EID $eid on $intf"
 
 sudo /sbin/ifconfig lo inet6 del $eid
 sudo ip route del fd00::/8 via fe80::1 src $eid dev $intf
