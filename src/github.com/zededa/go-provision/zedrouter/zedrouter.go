d320b64c6420f6aeed7988d02231c68884e43040
eriknordmark <nordmark@sonic.net>
2017-06-15 23:49:43 -0700

    Making progress on handleCreate and handleDelete

--- src/github.com/zededa/go-provision/zedrouter/zedrouter.go
+++ src/github.com/zededa/go-provision/zedrouter/zedrouter.go
@@ -14,6 +14,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/exec"
 	"path"
 	"strconv"
 	"strings"
@@ -43,14 +44,13 @@ func WatchConfigStatus(configDir string, statusDir string,
 				// log.Println("event:", event)
 				// We get create events when file is moved into
 				// the watched directory.
-				if event.Op&
+				if event.Op &
 					(fsnotify.Write|fsnotify.Create) != 0 {
-					// log.Println("modified", baseName
+					// log.Println("modified", baseName)
 					fileChanges <- "M " + baseName
-
-				} else if event.Op&
+				} else if event.Op &
 					(fsnotify.Rename|fsnotify.Remove) != 0 {
-					// log.Println("deleted", baseName
+					// log.Println("deleted", baseName)
 					fileChanges <- "D " + baseName
 				}
 			case err := <-w.Errors:
@@ -276,37 +276,115 @@ func handleCreate(statusFilename string, config types.AppNetworkConfig) {
 		PendingAdd:     true,
 		OlNum:          len(config.OverlayNetworkList),
 		UlNum:          len(config.UnderlayNetworkList),
+		DisplayName:    config.DisplayName,
 		IsZedmanager:   config.IsZedmanager,
 	}
-	writeAppNetworkStatus(&status, statusFilename)
 
+	if config.IsZedmanager {
+		if len(config.OverlayNetworkList) != 1 ||
+		   len(config.UnderlayNetworkList) != 0 {
+			log.Println("Malformed IsZedmanager config; ignored")
+			return
+		}
+		writeAppNetworkStatus(&status, statusFilename)
+		
+		// Configure the EID on loopback and set up a default route
+		// for all
+		//    ip addr add ${EID}/128 dev lo
+		EID := config.OverlayNetworkList[0].EID
+		addr, err := netlink.ParseAddr(EID.String() + "/128")
+		if err != nil {
+			fmt.Printf("ParseAddr %s failed: %s\n", EID, err)
+			return
+		}
+		lo, err := netlink.LinkByName("lo")
+		if err != nil {
+			fmt.Printf("LinkByname(lo) failed: %s\n", err)
+			return
+		}
+		if err := netlink.AddrAdd(lo, addr); err != nil {
+			fmt.Printf("AddrAdd %s failed: %s\n", EID, err)
+		}
+
+		//    ip route add fd00::/8 via fe80::1 src $eid dev $intf
+		upLink, err := netlink.LinkByName(globalConfig.Uplink)
+		if err != nil {
+			fmt.Printf("LinkByname(%s) failed: %s\n",
+				globalConfig.Uplink, err)
+		}
+		index := upLink.Attrs().Index
+		_, ipnet, err := net.ParseCIDR("fd00::/8")
+		if err != nil {
+			log.Fatal("ParseCIDR fd00::/8 failed:\n", err)
+		}
+		via := net.ParseIP("fe80::1")
+
+		if via == nil {
+			log.Fatal("ParseIP fe80::1 failed: ", err)
+		}
+		//    ip nei add fe80::1 lladdr 0:0:0:0:0:1 dev $intf
+		//    ip nei change fe80::1 lladdr 0:0:0:0:0:1 dev $intf
+		hw, err := net.ParseMAC("00:00:00:00:00:01")
+		if err != nil {
+			log.Fatal("ParseMAC failed: ", err)
+		}
+		neigh := netlink.Neigh{LinkIndex: index, IP: via,
+			HardwareAddr: hw, State: netlink.NUD_PERMANENT}
+		if err := netlink.NeighAdd(&neigh); err != nil {
+			fmt.Printf("NeighAdd fe80::1 failed: %s\n", err)
+		}		
+		if err := netlink.NeighSet(&neigh); err != nil {
+			fmt.Printf("NeighSet fe80::1 failed: %s\n", err)
+		}		
+
+		// XXX needed fix in library for Src to work
+		// /home/nordmark/gocode/src/github.com/vishvananda/netlink/route_linux.go
+		// Replaced RTA_PREFSRC with RTA_SRC
+		rt := netlink.Route{Dst: ipnet, LinkIndex: index,
+			Gw: via, Src: EID}
+		if err := netlink.RouteAdd(&rt); err != nil {
+			fmt.Printf("RouteAdd fd00::/8 failed: %s\n", err)
+		}
+
+		// XXX ACLs for IsZedmanager? Apply to input/output
+		// XXX use an IpSet for the EIDs; overlay.$IID?
+		// Implies an input/output drop for fd00::/8 but that will
+		// affect application overlays unless applied to uplink only.
+
+		// XXX NameToEids to /etc/host? XXX easier in separate domU!
+		
+		status.OverlayNetworkList = config.OverlayNetworkList
+		status.PendingAdd = false
+		writeAppNetworkStatus(&status, statusFilename)
+		return
+	}
+	writeAppNetworkStatus(&status, statusFilename)
+	
 	for i, olConfig := range config.OverlayNetworkList {
 		olNum := i + 1
 		fmt.Printf("olNum %d ACLs %v\n", olNum, olConfig.ACLs)
 
-		olAddr2 := olConfig.EID
+		EID := olConfig.EID
 		olIfname := "bo" + strconv.Itoa(olNum) + "_" +
 			strconv.Itoa(appNum)
 		fmt.Printf("olIfname %s\n", olIfname)
 		olAddr1 := "fd00::" + strconv.FormatInt(int64(olNum), 16) +
 			":" + strconv.FormatInt(int64(appNum), 16)
-		fmt.Printf("olAddr1 %s olAddr2 %s\n", olAddr1, olAddr2)
+		fmt.Printf("olAddr1 %s EID %s\n", olAddr1, EID)
 		olMac := "00:16:3e:1:" + strconv.FormatInt(int64(olNum), 16) +
 			":" + strconv.FormatInt(int64(appNum), 16)
 		fmt.Printf("olMac %s\n", olMac)
 
+		// Start clean
 		attrs := netlink.NewLinkAttrs()
 		attrs.Name = olIfname
 		oLink := &netlink.Bridge{LinkAttrs: attrs}
-
-		// Start clean
 		netlink.LinkDel(oLink)
 
+		//    ip link add ${olIfname} type bridge
 		attrs = netlink.NewLinkAttrs()
 		attrs.Name = olIfname
 		oLink = &netlink.Bridge{LinkAttrs: attrs}
-
-		//    ip link add ${olIfname} type bridge
 		if err := netlink.LinkAdd(oLink); err != nil {
 			fmt.Printf("LinkAdd on %s failed: %s\n", olIfname, err)
 		}
@@ -325,31 +403,63 @@ func handleCreate(statusFilename string, config types.AppNetworkConfig) {
 			fmt.Printf("AddrAdd %s failed: %s\n", olAddr1, err)
 		}
 
-		//    ip -6 route add ${olAddr2}/128 dev ${olIfname}
-		_, ipnet, err := net.ParseCIDR(olAddr2.String() + "/128")
+		//    ip -6 route add ${EID}/128 dev ${olIfname}
+		_, ipnet, err := net.ParseCIDR(EID.String() + "/128")
 		if err != nil {
-			fmt.Printf("ParseCIDR %s failed: %v\n", olAddr2, err)
+			fmt.Printf("ParseCIDR %s failed: %v\n", EID, err)
 		}
 		fmt.Printf("oLink.Index %d\n", oLink.Index)
 		rt := netlink.Route{Dst: ipnet, LinkIndex: oLink.Index}
 		if err := netlink.RouteAdd(&rt); err != nil {
-			fmt.Printf("RouteAdd %s failed: %s\n", olAddr2, err)
+			fmt.Printf("RouteAdd %s failed: %s\n", EID, err)
 		}
 		// Write radvd configlet; start radvd
-		//    Start clean
-		//    pkill -u radvd -f radvd.${OLIFNAME}.conf
-		//    Enable radvd on interface
-		//    cat <<EOF >>/etc/radvd.${OLIFNAME}.conf
-		//interface ${OLIFNAME} {
-		//	IgnoreIfMissing on;
-		//	AdvSendAdvert on;
-		//	MaxRtrAdvInterval 1800;
-		//	AdvManagedFlag on;
-		//};
-		//EOF
-		//    radvd -u radvd -C /etc/radvd.${OLIFNAME}.conf -p /var/run/radvd.${OLIFNAME}.pid
+		// XXX TODO define as a function
+		cfgFilename := "radvd." + olIfname + ".conf"
+		cfgPathname := "/etc/" + cfgFilename
+		pidPathname := "/var/run/radvd." + olIfname + ".pid"
 
+		//    Start clean; kill just in case
+		//    pkill -u radvd -f radvd.${OLIFNAME}.conf
+		pkillUserArgs("radvd", cfgFilename, false)
+		
+		file, err := os.Create(cfgPathname)
+		if err != nil {
+			log.Fatal("os.Create for ", cfgPathname, err)
+		}
+		defer file.Close()
+		multiLine := `
+interface %s {
+	IgnoreIfMissing on;
+	AdvSendAdvert on;
+	MaxRtrAdvInterval 1800;
+	AdvManagedFlag on;
+};
+`
+		file.WriteString(fmt.Sprintf(multiLine, olIfname))
+		//    radvd -u radvd -C /etc/radvd.${OLIFNAME}.conf -p /var/run/radvd.${OLIFNAME}.pid
+		cmd := "nohup"
+		args := []string{
+			"radvd",
+			"-u",
+			"radvd",
+			"-C",
+			cfgPathname,
+			"-p",
+			pidPathname,
+		}
+		go exec.Command(cmd, args...).Output()
+
+		// Create a hosts file for the overlay based on NameToEids
+		// XXX TODO define as a function
+		
+		// Start clean
+		cfgFilename = "dnsmasq." + olIfname + ".conf"
+		pkillUserArgs("nobody", cfgFilename, false)
+
+		// XXX add start; keep configdir for ipset? directory for addn-hosts?
 	}
+
 	for i, ulConfig := range config.UnderlayNetworkList {
 		ulNum := i + 1
 		if ulNum != 1 {
@@ -369,17 +479,16 @@ func handleCreate(statusFilename string, config types.AppNetworkConfig) {
 		ulMac := "00:16:3e:0:0:" + strconv.FormatInt(int64(appNum), 16)
 		fmt.Printf("ulMac %s\n", ulMac)
 
+		// Start clean
 		attrs := netlink.NewLinkAttrs()
 		attrs.Name = ulIfname
 		uLink := &netlink.Bridge{LinkAttrs: attrs}
-
-		// Start clean
 		netlink.LinkDel(uLink)
 
+		//    ip link add ${ulIfname} type bridge
 		attrs = netlink.NewLinkAttrs()
 		attrs.Name = ulIfname
 		uLink = &netlink.Bridge{LinkAttrs: attrs}
-		//    ip link add ${ulIfname} type bridge
 		if err := netlink.LinkAdd(uLink); err != nil {
 			fmt.Printf("LinkAdd on %s failed: %s\n", ulIfname, err)
 		}
@@ -395,6 +504,10 @@ func handleCreate(statusFilename string, config types.AppNetworkConfig) {
 		if err := netlink.AddrAdd(uLink, addr); err != nil {
 			fmt.Printf("AddrAdd %s failed: %s\n", ulAddr1, err)
 		}
+
+		// Start clean
+		cfgFilename := "dnsmasq." + ulIfname + ".conf"
+		pkillUserArgs("nobody", cfgFilename, false)
 	}
 	// Write out what we created to AppNetworkStatus
 	// XXX TBD to handle core dumps before this point? Cleanup based
@@ -413,6 +526,9 @@ func handleModify(statusFilename string, config types.AppNetworkConfig,
 	appNum := status.AppNum
 	fmt.Printf("handleModify appNum %d\n", appNum)
 
+	status.PendingModify = true
+	writeAppNetworkStatus(&status, statusFilename)
+
 	// Look for ACL and NametoEids changes in overlay
 	// XXX flag others as errors; need lastError in status?
 	// XXX flag change in olNum as error
@@ -429,6 +545,9 @@ func handleModify(statusFilename string, config types.AppNetworkConfig,
 	}
 	// Write out what we modified to AppNetworkStatus
 	// XXX todo
+	status.UUIDandVersion = config.UUIDandVersion
+	status.PendingModify = false
+	writeAppNetworkStatus(&status, statusFilename)
 }
 
 // Need the olNum and ulNum to delete and EID route to delete
@@ -437,39 +556,127 @@ func handleDelete(statusFilename string, status types.AppNetworkStatus) {
 		status.UUIDandVersion, status.DisplayName)
 
 	appNum := status.AppNum
-	fmt.Printf("handleDelete appNum %d\n", appNum)
+	maxOlNum := status.OlNum
+	maxUlNum := status.UlNum
+	fmt.Printf("handleDelete appNum %d maxOlNum %d maxUlNum %d\n",
+		appNum, maxOlNum, maxUlNum)
 
-	// Delete everything for overlay
-	// XXX need EID for route deletion? Only for IsZedmanager case
-	for i, _ := range status.OverlayNetworkList {
-		olNum := i + 1
-		fmt.Printf("handleDelete olNum %d\n", olNum)
-		olIfname := "bo" + strconv.Itoa(olNum) + "_" +
-			strconv.Itoa(appNum)
-		fmt.Printf("olIfname %s\n", olIfname)
-		attrs := netlink.NewLinkAttrs()
-		attrs.Name = olIfname
-		oLink := &netlink.Bridge{LinkAttrs: attrs}
+	status.PendingDelete = true
+	writeAppNetworkStatus(&status, statusFilename)
 
-		// Start clean
-		netlink.LinkDel(oLink)
-	}
-	// Look for ACL changes in underlay
-	// XXX flag others as errors; need lastError in status?
-	// XXX flag change in olNum as error
-	for i, _ := range status.UnderlayNetworkList {
-		ulNum := i + 1
-		fmt.Printf("handleDelete ulNum %d\n", ulNum)
-		ulIfname := "bu" + strconv.Itoa(appNum)
-		fmt.Printf("ulIfname %s\n", ulIfname)
+	if status.IsZedmanager {
+		if len(status.OverlayNetworkList) != 1 ||
+		   len(status.UnderlayNetworkList) != 0 {
+			log.Println("Malformed IsZedmanager status; ignored")
+			return
+		}
+		// Delete the address from loopback
+		// Delete fd00::/8 route
+		// Delete fe80::1 neighbor
 
-		attrs := netlink.NewLinkAttrs()
-		attrs.Name = ulIfname
-		uLink := &netlink.Bridge{LinkAttrs: attrs}
-		netlink.LinkDel(uLink)
+		//    ip addr del ${EID}/128 dev lo
+		EID := status.OverlayNetworkList[0].EID
+		addr, err := netlink.ParseAddr(EID.String() + "/128")
+		if err != nil {
+			fmt.Printf("ParseAddr %s failed: %s\n", EID, err)
+			return
+		}
+		lo, err := netlink.LinkByName("lo")
+		if err != nil {
+			fmt.Printf("LinkByname(lo) failed: %s\n", err)
+			return
+		}
+		if err := netlink.AddrDel(lo, addr); err != nil {
+			fmt.Printf("AddrDel %s failed: %s\n", EID, err)
+		}
+
+		//    ip route del fd00::/8 via fe80::1 src $eid dev $intf
+		upLink, err := netlink.LinkByName(globalConfig.Uplink)
+		if err != nil {
+			fmt.Printf("LinkByname(%s) failed: %s\n",
+				globalConfig.Uplink, err)
+		}
+		index := upLink.Attrs().Index
+		_, ipnet, err := net.ParseCIDR("fd00::/8")
+		if err != nil {
+			log.Fatal("ParseCIDR fd00::/8 failed:\n", err)
+		}
+		via := net.ParseIP("fe80::1")
+		if via == nil {
+			log.Fatal("ParseIP fe80::1 failed: ", err)
+		}
+		rt := netlink.Route{Dst: ipnet, LinkIndex: index,
+			Gw: via, Src: EID}
+		if err := netlink.RouteDel(&rt); err != nil {
+			fmt.Printf("RouteDel fd00::/8 failed: %s\n", err)
+		}
+		//    ip nei del fe80::1 lladdr 0:0:0:0:0:1 dev $intf
+		neigh := netlink.Neigh{LinkIndex: index, IP: via}
+		if err := netlink.NeighDel(&neigh); err != nil {
+			fmt.Printf("NeighDel fe80::1 failed: %s\n", err)
+		}		
+
+		// XXX delete ACLs?
+		// XXX delete hosts from /etc/host?
+	} else {
+		// Delete everything for overlay
+		// XXX need EID for route deletion? Only for IsZedmanager case
+		for olNum := 1; olNum <= maxOlNum; olNum++ {
+			fmt.Printf("handleDelete olNum %d\n", olNum)
+			olIfname := "bo" + strconv.Itoa(olNum) + "_" +
+				strconv.Itoa(appNum)
+			fmt.Printf("Deleting olIfname %s\n", olIfname)
+			attrs := netlink.NewLinkAttrs()
+			attrs.Name = olIfname
+			oLink := &netlink.Bridge{LinkAttrs: attrs}
+	
+			// Start clean
+			netlink.LinkDel(oLink)
+
+			cfgFilename := "radvd." + olIfname + ".conf"
+			cfgPathname := "/etc/" + cfgFilename
+
+			pkillUserArgs("radvd", cfgFilename, true)
+			cmd := "rm"
+			args := []string{
+				"-f",
+				cfgPathname,
+				}
+			_, err := exec.Command(cmd, args...).Output()
+			if err != nil {
+				fmt.Printf("Command %v %v failed: %s\n",
+					cmd, args, err)
+			}
+		}
+
+		// Delete everything in underlay
+		for ulNum := 1; ulNum <= maxUlNum; ulNum++ {
+			fmt.Printf("handleDelete ulNum %d\n", ulNum)
+			ulIfname := "bu" + strconv.Itoa(appNum)
+			fmt.Printf("Deleting ulIfname %s\n", ulIfname)
+	
+			attrs := netlink.NewLinkAttrs()
+			attrs.Name = ulIfname
+			uLink := &netlink.Bridge{LinkAttrs: attrs}
+			netlink.LinkDel(uLink)
+		}
 	}
 	// Write out what we modified to AppNetworkStatus aka delete
 	if err := os.Remove(statusFilename); err != nil {
 		log.Println("Failed to remove", statusFilename, err)
+	}
+}
+
+func pkillUserArgs(userName string, match string, printOnError bool) {
+	cmd := "pkill"
+	args := []string{
+		"-u",
+		userName,
+		"-f",
+		match,
+	}
+	_, err := exec.Command(cmd, args...).Output()
+	if err != nil && printOnError {
+		fmt.Printf("Command %v %v failed: %s\n", cmd, args, err)
 	}
 }
