591ac77c10af013d18d8f98e54fe8cd6d144678e
Roman Shaposhnik <rvs@zededa.com>
2017-11-07 10:09:30 -0800

    Adding dom0-ztools

--- parse-pkgs.sh
+++ parse-pkgs.sh
@@ -13,9 +13,11 @@ DNSMASQ_TAG=$(./findtag.sh pkg/dnsmasq)-$ARCH
 TESTCERT_TAG=$(./findtag.sh pkg/test-cert)-$ARCH
 TESTMSVCS_TAG=$(./findtag.sh pkg/test-microsvcs)-$ARCH
 ZEDEDA_TAG=$(./findtag.sh zededa-container)-$ARCH
+DOM0ZTOOLS_TAG=$(./findtag.sh pkg/dom0-ztools)-$ARCH
 
 sed -e "s#KERNEL_TAG#"$KERNEL_TAG"#" \
     -e "s#XENTOOLS_TAG#"$XENTOOLS_TAG"#" \
+    -e "s#DOM0ZTOOLS_TAG#"$DOM0ZTOOLS_TAG"#" \
     -e "s#XEN_TAG#"$XEN_TAG"#" \
     -e "s#DNSMASQ_TAG#"$DNSMASQ_TAG"#" \
     -e "s#TESTCERT_TAG#"$TESTCERT_TAG"#" \
