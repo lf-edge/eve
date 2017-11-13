2efe4638143aee4c8406e1638d29ceb94e057b4a
saurabh-newera <saurabh@zededa.com>
2017-11-13 20:44:46 +0530

    merging changes with GG/Mirage testing

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -280,6 +280,8 @@ func main() {
 				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256},
 			// TLS 1.2 because we can
+			// XXX:FIXME needed while testing
+			//InsecureSkipVerify: true,
 			MinVersion: tls.VersionTLS12,
 		}
 		tlsConfig.BuildNameToCertificate()
