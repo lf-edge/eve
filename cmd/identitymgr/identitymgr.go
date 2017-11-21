4f5cb0e2f26faf1699cb3bb16ad1d6d729d7d0ec
eriknordmark <erik@zededa.com>
2017-11-21 21:01:23 +0530

    Defining dirnames as const

--- cmd/identitymgr/identitymgr.go
+++ cmd/identitymgr/identitymgr.go
@@ -31,17 +31,19 @@ import (
 	"time"
 )
 
+// Keeping status in /var/run to be clean after a crash/reboot
+const (
+	baseDirname = "/var/tmp/identitymgr"
+	runDirname = "/var/run/identitymgr"
+	configDirname = baseDirname + "/config"
+	statusDirname = runDirname + "/status"
+)
+
 func main() {
 	log.SetOutput(os.Stdout)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.LUTC)
 	log.Printf("Starting identitymgr\n")
 	watch.CleanupRestarted("identitymgr")
-
-	// Keeping status in /var/run to be clean after a crash/reboot
-	baseDirname := "/var/tmp/identitymgr"
-	runDirname := "/var/run/identitymgr"
-	configDirname := baseDirname + "/config"
-	statusDirname := runDirname + "/status"
 
 	if _, err := os.Stat(baseDirname); err != nil {
 		if err := os.Mkdir(baseDirname, 0700); err != nil {
