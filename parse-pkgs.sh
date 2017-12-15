4d8a1f8220c41de12ac45b93e92e6de60b5073fe
Gianluca Guida <gianluca@zededa.com>
2017-12-15 21:01:53 +0100

    parse-pkgs.sh: rationalise find tag
    Make findtag a bash function as plugintag.

--- parse-pkgs.sh
+++ parse-pkgs.sh
@@ -4,6 +4,11 @@
 #
 # [1] A poor man is a man on a deadline.
 #
+
+linuxkit_tag() {
+    linuxkit pkg show-tag $1
+}
+
 plugin_tag() {
   if (docker inspect "$1" || docker pull "$1") > /dev/null 2>&1 ; then
     echo $1
@@ -15,14 +20,14 @@ plugin_tag() {
 
 ARCH=amd64
 
-KERNEL_TAG=$(./findtag.sh pkg/kernel)-$ARCH
-XENTOOLS_TAG=$(./findtag.sh pkg/xen-tools)-$ARCH
-XEN_TAG=$(./findtag.sh pkg/xen)-$ARCH
-DNSMASQ_TAG=$(./findtag.sh pkg/dnsmasq)-$ARCH
-TESTCERT_TAG=$(./findtag.sh pkg/test-cert)-$ARCH
-TESTMSVCS_TAG=$(./findtag.sh pkg/test-microsvcs)-$ARCH
-ZEDEDA_TAG=$(./findtag.sh zededa-container)-$ARCH
-DOM0ZTOOLS_TAG=$(./findtag.sh pkg/dom0-ztools)-$ARCH
+KERNEL_TAG=$(linuxkit_tag pkg/kernel)-$ARCH
+XENTOOLS_TAG=$(linuxkit_tag pkg/xen-tools)-$ARCH
+XEN_TAG=$(linuxkit_tag pkg/xen)-$ARCH
+DNSMASQ_TAG=$(linuxkit_tag pkg/dnsmasq)-$ARCH
+TESTCERT_TAG=$(linuxkit_tag pkg/test-cert)-$ARCH
+TESTMSVCS_TAG=$(linuxkit_tag pkg/test-microsvcs)-$ARCH
+ZEDEDA_TAG=$(linuxkit_tag zededa-container)-$ARCH
+DOM0ZTOOLS_TAG=$(linuxkit_tag pkg/dom0-ztools)-$ARCH
 
 # Plugin tags: the following tags will default to
 # 'scratch' Docker container if not available.
@@ -30,7 +35,7 @@ DOM0ZTOOLS_TAG=$(./findtag.sh pkg/dom0-ztools)-$ARCH
 # our build easier. WARNING: it also means if you're
 # not logged into the Docker hub you may see final
 # images lacking functionality.
-ZTOOLS_TAG=${ZTOOLS_TAG:-`plugin_tag zededa/ztools:latest`}
+ZTOOLS_TAG=${ZTOOLS_TAG:-$(plugin_tag zededa/ztools:latest)}
 
 sed -e "s#KERNEL_TAG#"$KERNEL_TAG"#" \
     -e "s#XENTOOLS_TAG#"$XENTOOLS_TAG"#" \
