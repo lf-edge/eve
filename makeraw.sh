cf1b67fc80a43899c08b38c54af5f7cb1d1ffecf
Gianluca Guida <gianluca@zededa.com>
2017-12-15 21:00:23 +0100

    linuxkit: make it official
    Use 'linuxkit build' instead of 'moby'

--- makeraw.sh
+++ makeraw.sh
@@ -5,4 +5,4 @@
 #
 MKIMAGE_TAG="$(linuxkit pkg show-tag pkg/mkimage-raw-efi)-amd64"
 
-moby build -o - $1 | docker run -v /dev:/dev --privileged -i ${MKIMAGE_TAG} > $2
+linuxkit build -o - $1 | docker run -v /dev:/dev --privileged -i ${MKIMAGE_TAG} > $2
