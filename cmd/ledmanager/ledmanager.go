7ff6498e40f24da0cbe95b4c50fdc4b62200398f
saurabh-newera <saurabh@zededa.com>
2018-01-23 16:47:52 +0530

    write blinkCounter in a file

--- cmd/ledmanager/ledmanager.go
+++ cmd/ledmanager/ledmanager.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"log"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"log"
+	"time"
 )
 
 const (
 	ledStatusDirName = "/home/saurabh/examples/ledmanager/status"
 )
+
 func main() {
 	ledChanges := make(chan string)
 	var w Watcher
 	go w.LedWatcher(ledStatusDirName, ledChanges)
 	log.Println("called watcher...")
-	done := false
-	for !done {
+	for {
 		select {
 		case change := <-ledChanges:
 			{
@@ -27,7 +28,8 @@ func main() {
 }
 
 func HandleLedBlink() {
-	ledStatusFileName := ledStatusDirName+"/ledstatus.json"
+	ledStatusFileName := ledStatusDirName + "/ledstatus.json"
+	testFile := "/home/saurabh/examples/ledmanager/blink.json"
 	var watch = Watcher{}
 	cb, err := ioutil.ReadFile(ledStatusFileName)
 	if err != nil {
@@ -35,8 +37,17 @@ func HandleLedBlink() {
 	}
 	if err := json.Unmarshal(cb, &watch); err != nil {
 		log.Printf("%s %T file: %s\n",
-			err, watch,ledStatusFileName)
+			err, watch, ledStatusFileName)
 	}
 	blinkCount := watch.BlinkCounter
-	log.Println("blinkCount: ",blinkCount)
+	log.Println("blinkCount: ", blinkCount)
+	b, err := json.Marshal(watch)
+	if err != nil {
+		log.Fatal(err, "json Marshal ledwatcher")
+	}
+	err = ioutil.WriteFile(testFile, b, 0644)
+	if err != nil {
+		log.Fatal("err: ", err, testFile)
+	}
+	time.Sleep(time.Second * 1)
 }
