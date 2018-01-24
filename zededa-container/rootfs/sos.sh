5d8834bcdf7e3b0cb6fefb861ff12ce036e8246d
eriknordmark <erik@zededa.com>
2018-01-23 18:14:43 -0800

    Adjusted morse code timing

--- zededa-container/rootfs/sos.sh
+++ zededa-container/rootfs/sos.sh
@@ -17,16 +17,16 @@ while [ $repeat != 0 ] ; do
 	dd if=/dev/sda of=/dev/null bs=4M count=11 2>1 >/dev/null
 	usleep 100000
     done
-    usleep 200000
+    usleep 300000
     for i in `seq 1 3`; do
 	dd if=/dev/sda of=/dev/null bs=4M count=33 2>1 >/dev/null
 	usleep 100000
     done
-    usleep 200000
+    usleep 300000
     for i in `seq 1 3`; do
 	dd if=/dev/sda of=/dev/null bs=4M count=11 2>1 >/dev/null
 	usleep 100000
     done
-    usleep 800000
+    usleep 700000
     repeat=`expr $repeat - 1`
 done
