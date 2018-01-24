012a05865bca14011e447e35d5887b9750baa015
eriknordmark <erik@zededa.com>
2018-01-23 23:09:36 -0800

    Fixed race in pbr resulting in no ip rules for some source addresses

--- cmd/zedrouter/pbr.go
+++ cmd/zedrouter/pbr.go
@@ -200,7 +200,7 @@ func PbrLinkChange(change netlink.LinkUpdate) {
 			// flushRoutesTable(FreeTable, ifindex)
 			// MyTable := FreeTable + ifindex
 			// flushRoutesTable(MyTable, 0)
-			flushRules(ifindex)
+			// flushRules(ifindex)
 
 			if isFreeUplink(ifname) {
 				log.Printf("PbrLinkChange moving to FreeTable %s\n",
