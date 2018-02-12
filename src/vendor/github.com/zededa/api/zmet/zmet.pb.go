57a7b5c978365682b85134c0f1ba346866b48fc2
eriknordmark <erik@zededa.com>
2018-02-12 09:41:17 -0800

    Updated vendors zededa/api

--- src/vendor/github.com/zededa/api/zmet/zmet.pb.go
+++ src/vendor/github.com/zededa/api/zmet/zmet.pb.go
@@ -6,6 +6,7 @@ Package zmet is a generated protocol buffer package.
 
 It is generated from these files:
 	zmet.proto
+	zregister.proto
 
 It has these top-level messages:
 	ZInfoManufacturer
@@ -28,6 +29,8 @@ It has these top-level messages:
 	DiskMetric
 	AppMetric
 	ZMetricMsg
+	ZRegisterResp
+	ZRegisterMsg
 */
 package zmet
 
