9fd4647b8449eaf05026b8afc83ef3185060d6e4
eriknordmark <erik@zededa.com>
2018-02-23 10:37:29 -0800

    scripts/buildit.sh

--- scripts/buildit.sh
+++ scripts/buildit.sh
@@ -4,27 +4,10 @@
 
 DIR=`pwd`
 
-GIT_TAG=`git tag`
-BUILD_DATE=`date -u +"%Y-%m-%d-%H:%M"`
-GIT_VERSION=`git describe --match v --abbrev=8 --always --dirty`
-BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
-VERSION=${GIT_TAG}
-
-# XXX note that if PROD is changed things to not get rebuilt
-if [ ! -z ${PROD+x} ]; then
-	EXTRA_VERSION=""
-else
-	EXTRA_VERSION=-${GIT_VERSION}-${BUILD_DATE}
-fi
-
-if [ ${BRANCH_NAME} = "master" ]; then
-	BUILD_VERSION=${VERSION}${EXTRA_VERSION}
-else
-	BUILD_VERSION=${VERSION}-${GIT_BRANCH}${EXTRA_VERSION}
-fi
+BUILD_VERSION=`scripts/getversion.sh`
 echo "Building version ${BUILD_VERSION}"
+
 mkdir -p var/tmp/zededa/
-echo "all: ${BUILD_VERSION}" >var/tmp/zededa/version_tag
 
 [ -d bin ] || mkdir bin
 [ -d bin/linux_x86_64 ] || mkdir bin/linux_x86_64
