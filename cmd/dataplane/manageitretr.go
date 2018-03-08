12f9ff3e9bd31541b17c9848942e3a39849341e0
Gopi krishna Kodali <gkodali@zededa.com>
2018-03-08 14:13:12 +0530

    moving control code to dataplane directory

--- cmd/dataplane/manageitretr.go
+++ cmd/dataplane/manageitretr.go
@@ -1,1 +1,97 @@
-control/manageitretr.go
+// Copyright (c) 2017 Zededa, Inc.
+// All rights reserved.
+
+// Maintains maps for storing ITR/ETR thread information. Also takes care of
+// creating new threads and destroying stale ITR/ETR threads.
+
+package main
+
+import (
+	"github.com/google/gopacket/pfring"
+	"github.com/zededa/go-provision/dataplane/etr"
+	"github.com/zededa/go-provision/dataplane/itr"
+	"github.com/zededa/go-provision/types"
+	"log"
+)
+
+type ThreadEntry struct {
+	killChannel chan bool
+	ring        *pfring.Ring
+}
+
+var threadTable map[string]ThreadEntry
+
+func InitThreadTable() {
+	threadTable = make(map[string]ThreadEntry)
+}
+
+func DumpThreadTable() {
+	for name, _ := range threadTable {
+		log.Println(name)
+	}
+}
+
+// Find the difference between running ITR threads and the threads
+// that need to be running according to new configuration.
+//
+// Kill the ITR threads that are no longer needed and create
+// newly required threads.
+func ManageItrThreads(interfaces Interfaces) {
+	tmpMap := make(map[string]bool)
+
+	// Build a map of threads needed according to new configuration
+	for _, iface := range interfaces.Interfaces {
+		tmpMap[iface.Interface] = true
+	}
+
+	// Kill ITR threads that are not needed with new configuration
+	for name, entry := range threadTable {
+		// Check if this thread is needed with new configuration and send
+		// a kill signal if not.
+		if _, ok := tmpMap[name]; !ok {
+			// This thread has to die, break the bad news to it
+			log.Println("ManageItrThreads: Sending kill signal to", name)
+			entry.killChannel <- true
+
+			// XXX
+			// ITR threads use pf_ring for packet capture.
+			// pf_ring packet read calls are blocking. If a thread is blocked
+			// and there are no packets coming in from the corresponding interface,
+			// it can never get unblocked and process messages from kill channel.
+			//
+			// We delete the pf_ring socket for now, so that the ITR thread blocking
+			// calls returns with error.
+			//
+			// We'll retain the kill channel mechanism for future optimizations.
+			close(entry.killChannel)
+			entry.ring.Disable()
+			entry.ring.Close()
+			delete(threadTable, name)
+		}
+	}
+
+	// Create new threads that do not already exist
+	for name, _ := range tmpMap {
+		if _, ok := threadTable[name]; !ok {
+			// This ITR thread has to be given birth to. Find a mom!!
+			killChannel := make(chan bool, 1)
+
+			// Start the go thread here
+			ring := itr.SetupPacketCapture(name, 65536)
+			log.Println("ManageItrThreads: Creating new ITR thread for", name)
+			threadTable[name] = ThreadEntry{
+				killChannel: killChannel,
+				ring:        ring,
+			}
+			go itr.StartItrThread(name, ring, killChannel, puntChannel)
+		}
+	}
+}
+
+func ManageETRThread(port int) {
+	etr.HandleEtrEphPort(port)
+}
+
+func ManageEtrDNS(deviceNetworkStatus types.DeviceNetworkStatus) {
+	etr.HandleDeviceNetworkChange(deviceNetworkStatus)
+}
--- cmd/dataplane/types.go
+++ cmd/dataplane/types.go
@@ -1,1 +1,81 @@
-control/types.go
+// Copyright (c) 2017 Zededa, Inc.
+// All rights reserved.
+
+// Defines json structures for control messages from lispers.net
+
+package main
+
+const CRYPTO_KEY_LEN = 32
+
+// These message type definitions should be in sync with lispers.net
+const (
+	MAPCACHETYPE         = "map-cache"
+	ENTIREMAPCACHE       = "entire-map-cache"
+	DATABASEMAPPINGSTYPE = "database-mappings"
+	INTERFACESTYPE       = "interfaces"
+	DECAPKEYSTYPE        = "decap-keys"
+	ETRNATPORT           = "etr-nat-port"
+)
+
+type Type struct {
+	Type string `json:"type"`
+}
+
+type Key struct {
+	KeyId  string `json:"key-id"`
+	EncKey string `json:"encrypt-key"`
+	IcvKey string `json:"icv-key"`
+}
+
+type DKey struct {
+	KeyId  string `json:"key-id"`
+	DecKey string `json:"decrypt-key"`
+	IcvKey string `json:"icv-key"`
+}
+
+type Rloc struct {
+	Rloc     string `json:"rloc"`
+	Priority string `json:"priority"`
+	Weight   string `json:"weight"`
+	Keys     []Key  `json:"keys"`
+}
+
+type MapCacheEntry struct {
+	Opcode     string `json:"opcode"`
+	InstanceId string `json:"instance-id"`
+	EidPrefix  string `json:"eid-prefix"`
+	Rlocs      []Rloc `json:"rlocs"`
+}
+
+type EntireMapCache struct {
+	MapCaches []MapCacheEntry
+}
+
+type DatabaseMap struct {
+	InstanceId string `json:"instance-id"`
+	EidPrefix  string `json:"eid-prefix"`
+}
+
+type DatabaseMappings struct {
+	Mappings []DatabaseMap `json:"database-mappings"`
+}
+
+type Interface struct {
+	Interface  string `json:"interface"`
+	InstanceId string `json:"instance-id"`
+	//InstanceId int `json:"instance-id"`
+}
+
+type Interfaces struct {
+	Interfaces []Interface `json:"interfaces"`
+}
+
+type DecapKeys struct {
+	Rloc string `json:"rloc"`
+	Keys []DKey `json:"keys"`
+}
+
+type EtrNatPort struct {
+	Type string `json:"type"`
+	Port int    `json:"port"`
+}
