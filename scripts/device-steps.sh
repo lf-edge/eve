37cc4e85524d8072ec4824bebd8b87c6883b4bdb
eriknordmark <erik@zededa.com>
2018-03-22 08:09:42 +0000

    run watchdog(8)

--- scripts/device-steps.sh
+++ scripts/device-steps.sh
@@ -65,10 +65,6 @@ if [ -f $TMPDIR/uuid ]; then
     fi
 fi
 
-echo "Configuration from factory/install:"
-(cd $CONFIGDIR; ls -l)
-echo
-
 echo "Handling restart case at" `date`
 # XXX should we check if zedmanager is running?
 
@@ -216,6 +212,36 @@ fi
 
 echo "Handling restart done at" `date`
 
+echo "Configuration from factory/install:"
+(cd $CONFIGDIR; ls -l)
+echo
+
+CURPART=`zboot curpart`
+if [ $? != 0 ]; then
+    CURPART="IMGA"
+fi
+
+# Create config file for watchdog(8)
+cat >$TMPDIR/watchdog.conf <<EOF
+admin =
+realtime = yes
+priority = 1
+interval = 10
+logtick  = 60
+EOF
+echo "log-dir = /persist/$CURPART/log/" >>$TMPDIR/watchdog.conf
+echo "pidfile = /var/run/ledmanager.pid" >>$TMPDIR/watchdog.conf
+
+# The client should start soon
+cp $TMPDIR/watchdog.conf $TMPDIR/watchdogc.conf
+echo "pidfile = /var/run/zedclient.pid" >>$TMPDIR/watchdogc.conf
+
+if [ -f /var/run/watchdog.pid ]; then
+    kill `cat /var/run/watchdog.pid`
+fi
+# Make sure client.go doesn't fail
+/usr/sbin/watchdog -c /var/tmp/zededa/watchdogc.conf -F -s &
+
 if [ ! -d $LOGDIRA ]; then
     echo "Creating $LOGDIRA"
     mkdir -p $LOGDIRA
@@ -226,10 +252,6 @@ if [ ! -d $LOGDIRB ]; then
 fi
 
 echo "Set up log capture"
-CURPART=`zboot curpart`
-if [ $? != 0 ]; then
-    CURPART="IMGA"
-fi
 DOM0LOGFILES="dhcpcd.err.log ntpd.err.log wlan.err.log wwan.err.log zededa-tools.err.log dhcpcd.out.log ntpd.out.log wlan.out.log wwan.out.log zededa-tools.out.log"
 # XXX note that these tail and dmesg processes are not killed when
 # device-steps.sh is re-run
@@ -266,6 +288,7 @@ if [ $? != 0 ]; then
     fi
 fi
 
+# XXX could ntp take more than 60 seconds??
 # We need to try our best to setup time *before* we generate the certifiacte.
 # Otherwise it may have start date in the future
 echo "Check for NTP config"
@@ -474,6 +497,14 @@ echo '{"MaxSpace":2000000}' >/var/tmp/downloader/config/global
 
 rm -f /var/run/verifier/*/status/restarted
 rm -f /var/tmp/zedrouter/config/restart
+
+for AGENT in $AGENTS; do
+    echo "pidfile = /var/run/$AGENT.pid" >>$TMPDIR/watchdog.conf
+done
+if [ -f /var/run/watchdog.pid ]; then
+    kill `cat /var/run/watchdog.pid`
+fi
+/usr/sbin/watchdog -c /var/tmp/zededa/watchdog.conf -F -s &
 
 echo "Starting verifier at" `date`
 verifier &
