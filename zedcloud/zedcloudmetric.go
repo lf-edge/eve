76819b554c05ecd13b1267c1ade02a7a7401b609
eriknordmark <erik@zededa.com>
2018-03-28 16:01:29 +0200

    Introduce metricsMap type Hack to Cast to correct type.

--- zedcloud/zedcloudmetric.go
+++ zedcloud/zedcloudmetric.go
@@ -8,6 +8,7 @@
 package zedcloud
 
 import (
+	"encoding/json"
 	"log"
 	"time"
 )
@@ -30,12 +31,14 @@ type urlcloudMetrics struct {
 }
 
 // Key is ifname string
-var metrics map[string]zedcloudMetric
+type metricsMap map[string]zedcloudMetric
+
+var metrics metricsMap
 
 func maybeInit(ifname string) {
 	if metrics == nil {
 		log.Printf("create zedcloudmetric map\n")
-		metrics = make(map[string]zedcloudMetric)
+		metrics = make(metricsMap)
 	}
 	if _, ok := metrics[ifname]; !ok {
 		log.Printf("create zedcloudmetric for %s\n", ifname)
@@ -83,6 +86,36 @@ func ZedCloudSuccess(ifname string, url string, reqLen int64, respLen int64) {
 	metrics[ifname] = m
 }
 
-func GetCloudMetrics() map[string]zedcloudMetric {
+func GetCloudMetrics() metricsMap {
 	return metrics
+}
+
+// XXX this works but ugly as ...
+// Alternative seems to be a deep walk with type assertions in order
+// to produce the map of map of map with the correct type.
+func CastCloudMetrics(in interface{}) metricsMap {
+	b, err := json.Marshal(in)
+	if err != nil {
+		log.Fatal(err, "json Marshal in CastCloudMetrics")
+	}
+	var output metricsMap
+	if err := json.Unmarshal(b, &output); err != nil {
+		log.Fatal(err, "json Unmarshal in CastCloudMetrics")
+	}
+	return output
+}
+
+// XXX Need to walk and do type assertions for each map member
+// XXX incomplete; remove or fix
+func CastCloudMetrics2(in interface{}) metricsMap {
+	in1 := *(in.(*interface{}))
+	in2 := in1.(map[string]interface{})
+	o1 := make(metricsMap)
+	for k1, e1 := range in2 {
+		log.Printf("Cast: %s: %v %t\n", k1, e1, e1)
+		// XXX blows up on next line; need to do a deep type assertions
+		elem := e1.(zedcloudMetric)
+		o1[k1] = elem
+	}
+	return o1
 }
