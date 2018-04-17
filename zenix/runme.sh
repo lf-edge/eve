b8a247f10b0f64b2092c66532e0dce25da42bfc0
Roman Shaposhnik <rvs@zededa.com>
2018-04-16 18:03:03 -0700

    Adding a quick way of publishing all the artifacts produced

--- zenix/runme.sh
+++ zenix/runme.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ] ; then
    echo "Usage: $0 [-|shell_script]"
    echo "       specifying - instead of a shell script will generate tarball.gz on stdout"
 elif [ "$1" = "-" ] ; then
-   tar -C bits -czf - .
+   tar -C /bits -czf - .
 else
    bash -c "$*"
 fi
