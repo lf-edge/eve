93c46ea3ee10cefffa5faf5e44f9f7b2a244c1fd
eriknordmark <erik@zededa.com>
2018-05-11 16:25:24 -0700

    Added initial support for Supermicro.SYS-5018D-FN8T

--- cmd/ledmanager/ledmanager.go
+++ cmd/ledmanager/ledmanager.go
@@ -62,6 +62,9 @@ var mToF = []modelToFuncs{
 		model:     "Supermicro.SYS-E300-8D",
 		blinkFunc: ExecuteDDCmd},
 	modelToFuncs{
+		model:     "Supermicro.SYS-5018D-FN8T",
+		blinkFunc: ExecuteDDCmd},
+	modelToFuncs{
 		model:     "hisilicon,hikey.hisilicon,hi6220.",
 		initFunc:  InitWifiLedCmd,
 		blinkFunc: ExecuteWifiLedCmd},
