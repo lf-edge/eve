67dfaa81c12cdf89535cd6581d7ba95c73f8a28c
eriknordmark <erik@zededa.com>
2018-06-13 21:47:51 -0700

    finished service structure

--- cmd/zedrouter/service.go
+++ cmd/zedrouter/service.go
@@ -6,8 +6,11 @@
 package zedrouter
 
 import (
+	"errors"
+	"fmt"
 	"github.com/zededa/go-provision/types"
 	"log"
+	"time"
 )
 
 func handleNetworkServiceModify(ctxArg interface{}, key string, configArg interface{}) {
@@ -46,11 +49,22 @@ func handleNetworkServiceCreate(ctx *zedrouterContext, key string, config types.
 	}
 	status.PendingAdd = true
 	pub.Publish(key, status)
-	doCreate(config, &status)
+	err := doCreate(config, &status)
+	if err != nil {
+		status.Error = err.Error()
+		status.ErrorTime = time.Now()
+		status.PendingAdd = false
+		pub.Publish(key, status)
+		return
+	}
 	pub.Publish(key, status)
 	if config.Activate {
-		doActivate(config, &status)
-		status.Activated = true
+		err := doActivate(config, &status)
+		if err != nil {
+			// XXX report error
+		} else {
+			status.Activated = true
+		}
 	}
 	status.PendingAdd = false
 	pub.Publish(key, status)
@@ -82,12 +96,30 @@ func handleNetworkServiceDelete(ctxArg interface{}, key string) {
 	pub.Unpublish(key)
 }
 
-func doCreate(config types.NetworkService, status *types.NetworkServiceStatus) {
-	log.Printf("XXX doCreate NetworkService key %s\n", config.UUID)
+func doCreate(config types.NetworkService, status *types.NetworkServiceStatus) error {
+	log.Printf("doCreate NetworkService key %s type %d\n",
+		config.UUID, config.Type)
+
+	var err error
 	// Validate that the objects exists
 	switch config.Type {
-	case 1:
+	case types.NST_STRONGSWAN:
+		err = strongswanCreate(config, status)
+	case types.NST_LISP:
+		err = lispCreate(config, status)
+	case types.NST_BRIDGE:
+		err = bridgeCreate(config, status)
+	case types.NST_NAT:
+		err = natCreate(config, status)
+	case types.NST_LB:
+		errStr := "doCreate NetworkService LB not yet supported"
+		err = errors.New(errStr)
+	default:
+		errStr := fmt.Sprintf("doCreate NetworkService %d not yet supported",
+			config.Type)
+		err = errors.New(errStr)
 	}
+	return err
 }
 
 func doModify(config types.NetworkService, status *types.NetworkServiceStatus) {
@@ -102,23 +134,165 @@ func doModify(config types.NetworkService, status *types.NetworkServiceStatus) {
 	}
 
 	if config.Activate && !status.Activated {
-		doActivate(config, status)
-		status.Activated = true
+		err := doActivate(config, status)
+		if err != nil {
+			// XXX report error
+		} else {
+			status.Activated = true
+		}
 	} else if status.Activated && !config.Activate {
 		doInactivate(status)
 		status.Activated = false
 	}
 }
 
-func doActivate(config types.NetworkService, status *types.NetworkServiceStatus) {
-	log.Printf("XXX doActivate NetworkService key %s\n", config.UUID)
+func doActivate(config types.NetworkService, status *types.NetworkServiceStatus) error {
+	log.Printf("doActivate NetworkService key %s type %d\n",
+		config.UUID, config.Type)
+
+	var err error
+	switch config.Type {
+	case types.NST_STRONGSWAN:
+		err = strongswanActivate(config, status)
+	case types.NST_LISP:
+		err = lispActivate(config, status)
+	case types.NST_BRIDGE:
+		err = bridgeActivate(config, status)
+	case types.NST_NAT:
+		err = natActivate(config, status)
+	case types.NST_LB:
+		errStr := "doActivate NetworkService LB not yet supported"
+		err = errors.New(errStr)
+	default:
+		errStr := fmt.Sprintf("doActivate NetworkService %d not yet supported",
+			config.Type)
+		err = errors.New(errStr)
+	}
+	return err
 }
 
 func doInactivate(status *types.NetworkServiceStatus) {
-	log.Printf("XXX doInactivate NetworkService key %s\n", status.UUID)
+	log.Printf("doInactivate NetworkService key %s type %d\n",
+		status.UUID, status.Type)
+
+	switch status.Type {
+	case types.NST_STRONGSWAN:
+		strongswanInactivate(status)
+	case types.NST_LISP:
+		lispInactivate(status)
+	case types.NST_BRIDGE:
+		bridgeInactivate(status)
+	case types.NST_NAT:
+		natInactivate(status)
+	case types.NST_LB:
+		errStr := "doInactivate NetworkService LB not yet supported"
+		log.Println(errStr)
+	default:
+		errStr := fmt.Sprintf("doInactivate NetworkService %d not yet supported",
+			status.Type)
+		log.Println(errStr)
+	}
 }
 
 func doDelete(status *types.NetworkServiceStatus) {
-	log.Printf("XXX doDelete NetworkService key %s\n", status.UUID)
+	log.Printf("doDelete NetworkService key %s type %d\n",
+		status.UUID, status.Type)
 	// Anything to do except the inactivate already done?
+	switch status.Type {
+	case types.NST_STRONGSWAN:
+		strongswanDelete(status)
+	case types.NST_LISP:
+		lispDelete(status)
+	case types.NST_BRIDGE:
+		bridgeDelete(status)
+	case types.NST_NAT:
+		natDelete(status)
+	case types.NST_LB:
+		errStr := "doDelete NetworkService LB not yet supported"
+		log.Println(errStr)
+	default:
+		errStr := fmt.Sprintf("doDelete NetworkService %d not yet supported",
+			status.Type)
+		log.Println(errStr)
+	}
+}
+
+// ==== StrongSwan
+
+func strongswanCreate(config types.NetworkService,
+	status *types.NetworkServiceStatus) error {
+
+	return nil
+}
+
+func strongswanActivate(config types.NetworkService,
+	status *types.NetworkServiceStatus) error {
+
+	return nil
+}
+
+func strongswanInactivate(status *types.NetworkServiceStatus) {
+}
+
+func strongswanDelete(status *types.NetworkServiceStatus) {
+}
+
+// ==== Lisp
+
+func lispCreate(config types.NetworkService,
+	status *types.NetworkServiceStatus) error {
+
+	return nil
+}
+
+func lispActivate(config types.NetworkService,
+	status *types.NetworkServiceStatus) error {
+
+	return nil
+}
+
+func lispInactivate(status *types.NetworkServiceStatus) {
+}
+
+func lispDelete(status *types.NetworkServiceStatus) {
+}
+
+// ==== Bridge
+
+func bridgeCreate(config types.NetworkService,
+	status *types.NetworkServiceStatus) error {
+
+	return nil
+}
+
+func bridgeActivate(config types.NetworkService,
+	status *types.NetworkServiceStatus) error {
+
+	return nil
+}
+
+func bridgeInactivate(status *types.NetworkServiceStatus) {
+}
+
+func bridgeDelete(status *types.NetworkServiceStatus) {
+}
+
+// ==== Nat
+
+func natCreate(config types.NetworkService,
+	status *types.NetworkServiceStatus) error {
+
+	return nil
+}
+
+func natActivate(config types.NetworkService,
+	status *types.NetworkServiceStatus) error {
+
+	return nil
+}
+
+func natInactivate(status *types.NetworkServiceStatus) {
+}
+
+func natDelete(status *types.NetworkServiceStatus) {
 }
