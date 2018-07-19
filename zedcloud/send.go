a5ada6ea243b6b2acc6ab6bc8afc5499bb605970
eriknordmark <erik@zededa.com>
2018-07-19 19:07:03 -0400

    turn on/off proxy for testing

--- zedcloud/send.go
+++ zedcloud/send.go
@@ -16,7 +16,11 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptrace"
+	"os"
 )
+
+// XXX needs to match device-steps.sh
+const proxyFile = "/config/proxy"
 
 // XXX should we add some Init() function to create this?
 type ZedCloudContext struct {
@@ -105,10 +109,14 @@ func sendOnIntf(ctx ZedCloudContext, url string, intf string, reqlen int64, b *b
 		}
 		d := net.Dialer{LocalAddr: &localTCPAddr}
 		transport := &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
 			TLSClientConfig: ctx.TlsConfig,
 			Dial:            d.Dial,
 		}
+		// XXX Enable debugging by looking for the file at runtime
+		if proxyFileExists() {
+			transport.Proxy = http.ProxyFromEnvironment
+		}
+
 		client := &http.Client{Transport: transport}
 		var req *http.Request
 		if b != nil {
@@ -216,4 +224,12 @@ func sendOnIntf(ctx ZedCloudContext, url string, intf string, reqlen int64, b *b
 		url, intf)
 	log.Println(errStr)
 	return nil, nil, errors.New(errStr)
+}
+
+func proxyFileExists() bool {
+	if _, err := os.Stat(proxyFile); err == nil {
+		return true
+	} else {
+		return false
+	}
 }
