aa84a32f115766ed9571c509507c1c9e0bb9fadc
eriknordmark <erik@zededa.com>
2018-07-26 01:47:09 -0700

    read /config/hardwaremodel if exists

--- hardware/model.go
+++ hardware/model.go
@@ -26,11 +26,18 @@ import (
 
 const (
 	compatibleFile = "/proc/device-tree/compatible"
+	overrideFile   = "/config/hardwaremodel"
 )
 
 // XXX Note that this function (and the ones below) log if there is an
 // error. That's impolite for a library to do.
 func GetHardwareModel() string {
+	model := getOverride(overrideFile)
+	if model != "" {
+		// XXX remove
+		log.Printf("GetHardwareModel override <%s>\n", model)
+		return model
+	}
 	product := ""
 	manufacturer := ""
 
@@ -75,6 +82,19 @@ func FormatModel(manufacturer, product, compatible string) string {
 	// Make sure it can be used as a filename by removing any '/'
 	safename := strings.Replace(model, "/", "_ ", -1)
 	return safename
+}
+
+// If the file exists return its content
+func getOverride(filename string) string {
+	if _, err := os.Stat(filename); err != nil {
+		return ""
+	}
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	return string(contents)
 }
 
 const controlChars = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f"
