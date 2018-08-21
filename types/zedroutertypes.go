67adb28b21fc18c5c4efe3ce32df45238c53593f
eriknordmark <erik@zededa.com>
2018-08-20 22:55:12 -0700

    iptables counters

--- types/zedroutertypes.go
+++ types/zedroutertypes.go
@@ -468,7 +468,9 @@ const (
 	NT_IPV4      NetworkType = 4
 	NT_IPV6                  = 6
 	NT_CryptoEID             = 14
+	// XXX IPV4 EIDs?
 	// XXX Do we need a NT_DUAL/NT_IPV46? Implies two subnets/dhcp ranges?
+	// XXX how do we represent a bridge? NT_L2??
 )
 
 // Extracted from the protobuf NetworkConfig
@@ -515,6 +517,10 @@ type NetworkObjectStatus struct {
 
 	// Union of all ipsets fed to dnsmasq for the linux bridge
 	BridgeIPSets []string
+
+	// Set of vifs on this bridge
+	VifNames []string
+
 	// Any errrors from provisioning the network
 	Error     string
 	ErrorTime time.Time
