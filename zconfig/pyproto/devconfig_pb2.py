cec03761e556c448f0e026b4447047cb8ff3798c
Vijay Tapaskar <vijay@zededa.com>
2018-09-25 22:57:17 -0700

    enterprise proxy configuration support

--- zconfig/pyproto/devconfig_pb2.py
+++ zconfig/pyproto/devconfig_pb2.py
@@ -26,10 +26,45 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='devconfig.proto',
   package='',
   syntax='proto3',
-  serialized_pb=_b('\n\x0f\x64\x65vconfig.proto\x1a\x0f\x64\x65vcommon.proto\x1a\x0f\x61ppconfig.proto\x1a\x12\x62\x61seosconfig.proto\x1a\x0fnetconfig.proto\x1a\rstorage.proto\x1a\rservice.proto\"1\n\tMapServer\x12\x10\n\x08NameOrIp\x18\x01 \x01(\t\x12\x12\n\nCredential\x18\x02 \x01(\t\"*\n\tZedServer\x12\x10\n\x08HostName\x18\x01 \x01(\t\x12\x0b\n\x03\x45ID\x18\x02 \x03(\t\"\xf5\x01\n\x11\x44\x65viceLispDetails\x12\"\n\x0eLispMapServers\x18\x01 \x03(\x0b\x32\n.MapServer\x12\x14\n\x0cLispInstance\x18\x02 \x01(\r\x12\x0b\n\x03\x45ID\x18\x04 \x01(\t\x12\x12\n\nEIDHashLen\x18\x05 \x01(\r\x12\x1e\n\nZedServers\x18\x06 \x03(\x0b\x32\n.ZedServer\x12\x1b\n\x13\x45idAllocationPrefix\x18\x08 \x01(\x0c\x12\x1e\n\x16\x45idAllocationPrefixLen\x18\t \x01(\r\x12\x12\n\nClientAddr\x18\n \x01(\t\x12\x14\n\x0c\x45xperimental\x18\x14 \x01(\x08\"F\n\x0c\x44\x65viceOpsCmd\x12\x0f\n\x07\x63ounter\x18\x02 \x01(\r\x12\x14\n\x0c\x64\x65siredState\x18\x03 \x01(\x08\x12\x0f\n\x07opsTime\x18\x04 \x01(\t\"n\n\x0fsWAdapterParams\x12\x1d\n\x05\x61Type\x18\x01 \x01(\x0e\x32\x0e.sWAdapterType\x12\x19\n\x11underlayInterface\x18\x08 \x01(\t\x12\x0e\n\x06vlanId\x18\t \x01(\r\x12\x11\n\tbondgroup\x18\n \x03(\t\"\x8c\x01\n\rSystemAdapter\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\x0c\x61llocDetails\x18\x14 \x01(\x0b\x32\x10.sWAdapterParams\x12\x12\n\nfreeUplink\x18\x02 \x01(\x08\x12\x0e\n\x06uplink\x18\x03 \x01(\x08\x12\x13\n\x0bnetworkUUID\x18\x04 \x01(\t\x12\x0c\n\x04\x61\x64\x64r\x18\x05 \x01(\t\"\xee\x03\n\rEdgeDevConfig\x12\x1b\n\x02id\x18\x01 \x01(\x0b\x32\x0f.UUIDandVersion\x12\x17\n\x0f\x64\x65vConfigSha256\x18\x02 \x01(\x0c\x12\x1a\n\x12\x64\x65vConfigSignature\x18\x03 \x01(\x0c\x12 \n\x04\x61pps\x18\x04 \x03(\x0b\x32\x12.AppInstanceConfig\x12 \n\x08networks\x18\x05 \x03(\x0b\x32\x0e.NetworkConfig\x12$\n\ndatastores\x18\x06 \x03(\x0b\x32\x10.DatastoreConfig\x12$\n\x08lispInfo\x18\x07 \x01(\x0b\x32\x12.DeviceLispDetails\x12\x1b\n\x04\x62\x61se\x18\x08 \x03(\x0b\x32\r.BaseOSConfig\x12\x1d\n\x06reboot\x18\t \x01(\x0b\x32\r.DeviceOpsCmd\x12\x1d\n\x06\x62\x61\x63kup\x18\n \x01(\x0b\x32\r.DeviceOpsCmd\x12 \n\x0b\x63onfigItems\x18\x0b \x03(\x0b\x32\x0b.ConfigItem\x12)\n\x11systemAdapterList\x18\x0c \x03(\x0b\x32\x0e.SystemAdapter\x12(\n\x08services\x18\r \x03(\x0b\x32\x16.ServiceInstanceConfig\x12\x14\n\x0cmanufacturer\x18\x0e \x01(\t\x12\x13\n\x0bproductName\x18\x0f \x01(\t\"\x9c\x01\n\nConfigItem\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x13\n\tboolValue\x18\x03 \x01(\x08H\x00\x12\x15\n\x0buint32Value\x18\x04 \x01(\rH\x00\x12\x15\n\x0buint64Value\x18\x05 \x01(\x04H\x00\x12\x14\n\nfloatValue\x18\x06 \x01(\x02H\x00\x12\x15\n\x0bstringValue\x18\x07 \x01(\tH\x00\x42\x11\n\x0f\x63onfigItemValue\"#\n\rConfigRequest\x12\x12\n\nconfigHash\x18\x01 \x01(\t\"D\n\x0e\x43onfigResponse\x12\x1e\n\x06\x63onfig\x18\x01 \x01(\x0b\x32\x0e.EdgeDevConfig\x12\x12\n\nconfigHash\x18\x02 \x01(\t*/\n\rsWAdapterType\x12\n\n\x06IGNORE\x10\x00\x12\x08\n\x04VLAN\x10\x01\x12\x08\n\x04\x42OND\x10\x02\x42@\n\x1f\x63om.zededa.cloud.uservice.protoZ\x1dgithub.com/zededa/api/zconfigb\x06proto3')
+  serialized_pb=_b('\n\x0f\x64\x65vconfig.proto\x1a\x0f\x64\x65vcommon.proto\x1a\x0f\x61ppconfig.proto\x1a\x12\x62\x61seosconfig.proto\x1a\x0fnetconfig.proto\x1a\rstorage.proto\x1a\rservice.proto\"1\n\tMapServer\x12\x10\n\x08NameOrIp\x18\x01 \x01(\t\x12\x12\n\nCredential\x18\x02 \x01(\t\"*\n\tZedServer\x12\x10\n\x08HostName\x18\x01 \x01(\t\x12\x0b\n\x03\x45ID\x18\x02 \x03(\t\"\xf5\x01\n\x11\x44\x65viceLispDetails\x12\"\n\x0eLispMapServers\x18\x01 \x03(\x0b\x32\n.MapServer\x12\x14\n\x0cLispInstance\x18\x02 \x01(\r\x12\x0b\n\x03\x45ID\x18\x04 \x01(\t\x12\x12\n\nEIDHashLen\x18\x05 \x01(\r\x12\x1e\n\nZedServers\x18\x06 \x03(\x0b\x32\n.ZedServer\x12\x1b\n\x13\x45idAllocationPrefix\x18\x08 \x01(\x0c\x12\x1e\n\x16\x45idAllocationPrefixLen\x18\t \x01(\r\x12\x12\n\nClientAddr\x18\n \x01(\t\x12\x14\n\x0c\x45xperimental\x18\x14 \x01(\x08\"F\n\x0c\x44\x65viceOpsCmd\x12\x0f\n\x07\x63ounter\x18\x02 \x01(\r\x12\x14\n\x0c\x64\x65siredState\x18\x03 \x01(\x08\x12\x0f\n\x07opsTime\x18\x04 \x01(\t\"G\n\x0bProxyServer\x12\x1a\n\x05proto\x18\x01 \x01(\x0e\x32\x0b.proxyProto\x12\x0e\n\x06server\x18\x02 \x01(\t\x12\x0c\n\x04port\x18\x03 \x01(\r\"Q\n\x0bProxyConfig\x12\x1d\n\x07proxies\x18\x01 \x03(\x0b\x32\x0c.ProxyServer\x12\x12\n\nexceptions\x18\x02 \x01(\t\x12\x0f\n\x07pacfile\x18\x03 \x01(\t\"n\n\x0fsWAdapterParams\x12\x1d\n\x05\x61Type\x18\x01 \x01(\x0e\x32\x0e.sWAdapterType\x12\x19\n\x11underlayInterface\x18\x08 \x01(\t\x12\x0e\n\x06vlanId\x18\t \x01(\r\x12\x11\n\tbondgroup\x18\n \x03(\t\"\xac\x01\n\rSystemAdapter\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\x0c\x61llocDetails\x18\x14 \x01(\x0b\x32\x10.sWAdapterParams\x12\x12\n\nfreeUplink\x18\x02 \x01(\x08\x12\x0e\n\x06uplink\x18\x03 \x01(\x08\x12\x13\n\x0bnetworkUUID\x18\x04 \x01(\t\x12\x0c\n\x04\x61\x64\x64r\x18\x05 \x01(\t\x12\x1e\n\x08\x65ntProxy\x18\x06 \x01(\x0b\x32\x0c.ProxyConfig\"\xee\x03\n\rEdgeDevConfig\x12\x1b\n\x02id\x18\x01 \x01(\x0b\x32\x0f.UUIDandVersion\x12\x17\n\x0f\x64\x65vConfigSha256\x18\x02 \x01(\x0c\x12\x1a\n\x12\x64\x65vConfigSignature\x18\x03 \x01(\x0c\x12 \n\x04\x61pps\x18\x04 \x03(\x0b\x32\x12.AppInstanceConfig\x12 \n\x08networks\x18\x05 \x03(\x0b\x32\x0e.NetworkConfig\x12$\n\ndatastores\x18\x06 \x03(\x0b\x32\x10.DatastoreConfig\x12$\n\x08lispInfo\x18\x07 \x01(\x0b\x32\x12.DeviceLispDetails\x12\x1b\n\x04\x62\x61se\x18\x08 \x03(\x0b\x32\r.BaseOSConfig\x12\x1d\n\x06reboot\x18\t \x01(\x0b\x32\r.DeviceOpsCmd\x12\x1d\n\x06\x62\x61\x63kup\x18\n \x01(\x0b\x32\r.DeviceOpsCmd\x12 \n\x0b\x63onfigItems\x18\x0b \x03(\x0b\x32\x0b.ConfigItem\x12)\n\x11systemAdapterList\x18\x0c \x03(\x0b\x32\x0e.SystemAdapter\x12(\n\x08services\x18\r \x03(\x0b\x32\x16.ServiceInstanceConfig\x12\x14\n\x0cmanufacturer\x18\x0e \x01(\t\x12\x13\n\x0bproductName\x18\x0f \x01(\t\"\x9c\x01\n\nConfigItem\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x13\n\tboolValue\x18\x03 \x01(\x08H\x00\x12\x15\n\x0buint32Value\x18\x04 \x01(\rH\x00\x12\x15\n\x0buint64Value\x18\x05 \x01(\x04H\x00\x12\x14\n\nfloatValue\x18\x06 \x01(\x02H\x00\x12\x15\n\x0bstringValue\x18\x07 \x01(\tH\x00\x42\x11\n\x0f\x63onfigItemValue\"#\n\rConfigRequest\x12\x12\n\nconfigHash\x18\x01 \x01(\t\"D\n\x0e\x43onfigResponse\x12\x1e\n\x06\x63onfig\x18\x01 \x01(\x0b\x32\x0e.EdgeDevConfig\x12\x12\n\nconfigHash\x18\x02 \x01(\t*_\n\nproxyProto\x12\x0e\n\nPROXY_HTTP\x10\x00\x12\x0f\n\x0bPROXY_HTTPS\x10\x01\x12\x0f\n\x0bPROXY_SOCKS\x10\x02\x12\r\n\tPROXY_FTP\x10\x03\x12\x10\n\x0bPROXY_OTHER\x10\xff\x01*/\n\rsWAdapterType\x12\n\n\x06IGNORE\x10\x00\x12\x08\n\x04VLAN\x10\x01\x12\x08\n\x04\x42OND\x10\x02\x42@\n\x1f\x63om.zededa.cloud.uservice.protoZ\x1dgithub.com/zededa/api/zconfigb\x06proto3')
   ,
   dependencies=[devcommon__pb2.DESCRIPTOR,appconfig__pb2.DESCRIPTOR,baseosconfig__pb2.DESCRIPTOR,netconfig__pb2.DESCRIPTOR,storage__pb2.DESCRIPTOR,service__pb2.DESCRIPTOR,])
 
+_PROXYPROTO = _descriptor.EnumDescriptor(
+  name='proxyProto',
+  full_name='proxyProto',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='PROXY_HTTP', index=0, number=0,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='PROXY_HTTPS', index=1, number=1,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='PROXY_SOCKS', index=2, number=2,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='PROXY_FTP', index=3, number=3,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='PROXY_OTHER', index=4, number=255,
+      options=None,
+      type=None),
+  ],
+  containing_type=None,
+  options=None,
+  serialized_start=1741,
+  serialized_end=1836,
+)
+_sym_db.RegisterEnumDescriptor(_PROXYPROTO)
+
+proxyProto = enum_type_wrapper.EnumTypeWrapper(_PROXYPROTO)
 _SWADAPTERTYPE = _descriptor.EnumDescriptor(
   name='sWAdapterType',
   full_name='sWAdapterType',
@@ -51,12 +86,17 @@ _SWADAPTERTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   options=None,
-  serialized_start=1553,
-  serialized_end=1600,
+  serialized_start=1838,
+  serialized_end=1885,
 )
 _sym_db.RegisterEnumDescriptor(_SWADAPTERTYPE)
 
 sWAdapterType = enum_type_wrapper.EnumTypeWrapper(_SWADAPTERTYPE)
+PROXY_HTTP = 0
+PROXY_HTTPS = 1
+PROXY_SOCKS = 2
+PROXY_FTP = 3
+PROXY_OTHER = 255
 IGNORE = 0
 VLAN = 1
 BOND = 2
@@ -271,6 +311,96 @@ _DEVICEOPSCMD = _descriptor.Descriptor(
 )
 
 
+_PROXYSERVER = _descriptor.Descriptor(
+  name='ProxyServer',
+  full_name='ProxyServer',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='proto', full_name='ProxyServer.proto', index=0,
+      number=1, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='server', full_name='ProxyServer.server', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='port', full_name='ProxyServer.port', index=2,
+      number=3, type=13, cpp_type=3, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=535,
+  serialized_end=606,
+)
+
+
+_PROXYCONFIG = _descriptor.Descriptor(
+  name='ProxyConfig',
+  full_name='ProxyConfig',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='proxies', full_name='ProxyConfig.proxies', index=0,
+      number=1, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='exceptions', full_name='ProxyConfig.exceptions', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='pacfile', full_name='ProxyConfig.pacfile', index=2,
+      number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=608,
+  serialized_end=689,
+)
+
+
 _SWADAPTERPARAMS = _descriptor.Descriptor(
   name='sWAdapterParams',
   full_name='sWAdapterParams',
@@ -318,8 +448,8 @@ _SWADAPTERPARAMS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=535,
-  serialized_end=645,
+  serialized_start=691,
+  serialized_end=801,
 )
 
 
@@ -372,6 +502,13 @@ _SYSTEMADAPTER = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='entProxy', full_name='SystemAdapter.entProxy', index=6,
+      number=6, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -384,8 +521,8 @@ _SYSTEMADAPTER = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=648,
-  serialized_end=788,
+  serialized_start=804,
+  serialized_end=976,
 )
 
 
@@ -513,8 +650,8 @@ _EDGEDEVCONFIG = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=791,
-  serialized_end=1285,
+  serialized_start=979,
+  serialized_end=1473,
 )
 
 
@@ -582,8 +719,8 @@ _CONFIGITEM = _descriptor.Descriptor(
       name='configItemValue', full_name='ConfigItem.configItemValue',
       index=0, containing_type=None, fields=[]),
   ],
-  serialized_start=1288,
-  serialized_end=1444,
+  serialized_start=1476,
+  serialized_end=1632,
 )
 
 
@@ -613,8 +750,8 @@ _CONFIGREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1446,
-  serialized_end=1481,
+  serialized_start=1634,
+  serialized_end=1669,
 )
 
 
@@ -651,14 +788,17 @@ _CONFIGRESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1483,
-  serialized_end=1551,
+  serialized_start=1671,
+  serialized_end=1739,
 )
 
 _DEVICELISPDETAILS.fields_by_name['LispMapServers'].message_type = _MAPSERVER
 _DEVICELISPDETAILS.fields_by_name['ZedServers'].message_type = _ZEDSERVER
+_PROXYSERVER.fields_by_name['proto'].enum_type = _PROXYPROTO
+_PROXYCONFIG.fields_by_name['proxies'].message_type = _PROXYSERVER
 _SWADAPTERPARAMS.fields_by_name['aType'].enum_type = _SWADAPTERTYPE
 _SYSTEMADAPTER.fields_by_name['allocDetails'].message_type = _SWADAPTERPARAMS
+_SYSTEMADAPTER.fields_by_name['entProxy'].message_type = _PROXYCONFIG
 _EDGEDEVCONFIG.fields_by_name['id'].message_type = devcommon__pb2._UUIDANDVERSION
 _EDGEDEVCONFIG.fields_by_name['apps'].message_type = appconfig__pb2._APPINSTANCECONFIG
 _EDGEDEVCONFIG.fields_by_name['networks'].message_type = netconfig__pb2._NETWORKCONFIG
@@ -690,12 +830,15 @@ DESCRIPTOR.message_types_by_name['MapServer'] = _MAPSERVER
 DESCRIPTOR.message_types_by_name['ZedServer'] = _ZEDSERVER
 DESCRIPTOR.message_types_by_name['DeviceLispDetails'] = _DEVICELISPDETAILS
 DESCRIPTOR.message_types_by_name['DeviceOpsCmd'] = _DEVICEOPSCMD
+DESCRIPTOR.message_types_by_name['ProxyServer'] = _PROXYSERVER
+DESCRIPTOR.message_types_by_name['ProxyConfig'] = _PROXYCONFIG
 DESCRIPTOR.message_types_by_name['sWAdapterParams'] = _SWADAPTERPARAMS
 DESCRIPTOR.message_types_by_name['SystemAdapter'] = _SYSTEMADAPTER
 DESCRIPTOR.message_types_by_name['EdgeDevConfig'] = _EDGEDEVCONFIG
 DESCRIPTOR.message_types_by_name['ConfigItem'] = _CONFIGITEM
 DESCRIPTOR.message_types_by_name['ConfigRequest'] = _CONFIGREQUEST
 DESCRIPTOR.message_types_by_name['ConfigResponse'] = _CONFIGRESPONSE
+DESCRIPTOR.enum_types_by_name['proxyProto'] = _PROXYPROTO
 DESCRIPTOR.enum_types_by_name['sWAdapterType'] = _SWADAPTERTYPE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -726,6 +869,20 @@ DeviceOpsCmd = _reflection.GeneratedProtocolMessageType('DeviceOpsCmd', (_messag
   # @@protoc_insertion_point(class_scope:DeviceOpsCmd)
   ))
 _sym_db.RegisterMessage(DeviceOpsCmd)
+
+ProxyServer = _reflection.GeneratedProtocolMessageType('ProxyServer', (_message.Message,), dict(
+  DESCRIPTOR = _PROXYSERVER,
+  __module__ = 'devconfig_pb2'
+  # @@protoc_insertion_point(class_scope:ProxyServer)
+  ))
+_sym_db.RegisterMessage(ProxyServer)
+
+ProxyConfig = _reflection.GeneratedProtocolMessageType('ProxyConfig', (_message.Message,), dict(
+  DESCRIPTOR = _PROXYCONFIG,
+  __module__ = 'devconfig_pb2'
+  # @@protoc_insertion_point(class_scope:ProxyConfig)
+  ))
+_sym_db.RegisterMessage(ProxyConfig)
 
 sWAdapterParams = _reflection.GeneratedProtocolMessageType('sWAdapterParams', (_message.Message,), dict(
   DESCRIPTOR = _SWADAPTERPARAMS,
