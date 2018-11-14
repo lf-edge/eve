811b64f7eba212cd4ba27b50b43c290c1a306ef6
eriknordmark <erik@zededa.com>
2018-11-13 19:40:25 -0800

    added DomainName walker code

--- devicenetwork/wpad.go
+++ devicenetwork/wpad.go
@@ -9,6 +9,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/zededa/go-provision/types"
 	"github.com/zededa/go-provision/zedcloud"
+	"strings"
 )
 
 // Caller should loop over interfaces
@@ -16,35 +17,79 @@ import (
 func CheckAndGetNetworkProxy(status *types.DeviceNetworkStatus,
 	proxyConfig *types.ProxyConfig) error {
 
-	log.Infof("CheckAndGetNetworkProxy: enable %v, url %s, %s, %s\n",
-		proxyConfig.NetworkProxyEnable, proxyConfig.NetworkProxyURL)
-
 	// XXX make per interface
 	ifname := "eth0"
+
+	log.Infof("CheckAndGetNetworkProxy(%s): enable %v, url %s, %s, %s\n",
+		ifname, proxyConfig.NetworkProxyEnable,
+		proxyConfig.NetworkProxyURL)
+
 	if proxyConfig.Pacfile != "" {
-		log.Infof("CheckAndGetNetworkProxy: already have Pacfile\n")
+		log.Infof("CheckAndGetNetworkProxy(%s): already have Pacfile\n",
+			ifname)
 		return nil
 	}
 	if !proxyConfig.NetworkProxyEnable {
-		log.Infof("CheckAndGetNetworkProxy: not enabled\n")
+		log.Infof("CheckAndGetNetworkProxy(%s): not enabled\n",
+			ifname)
 		return nil
 	}
 	if proxyConfig.NetworkProxyURL != "" {
 		pac, err := getFile(status, proxyConfig.NetworkProxyURL, ifname)
 		if err != nil {
-			errStr := fmt.Sprintf("Failed to fetch %s: %s",
-				proxyConfig.NetworkProxyURL, err)
+			errStr := fmt.Sprintf("Failed to fetch %s for %s: %s",
+				proxyConfig.NetworkProxyURL, ifname, err)
 			log.Errorln(errStr)
 			return errors.New(errStr)
 		}
-		log.Infof("CheckAndGetNetworkProxy: fetched from URL %s: %s\n",
-			proxyConfig.NetworkProxyURL, pac)
+		log.Infof("CheckAndGetNetworkProxy(%s): fetched from URL %s: %s\n",
+			ifname, proxyConfig.NetworkProxyURL, pac)
 		proxyConfig.Pacfile = pac
 		return nil
 	}
-	// XXX try http://wpad.%s/wpad.dat", dn
-	// starting with DomainName and truncating it
-	return nil
+	// XXX need the DomainName for a specific interface
+	dn := "" // XXX from interface's DomainName
+	if dn == "" {
+		errStr := fmt.Sprintf("NetworkProxyEnable for %s but neither a NetworkProxyURL nor a DomainName",
+			ifname)
+		log.Errorln(errStr)
+		return errors.New(errStr)
+	}
+	log.Infof("CheckAndGetNetworkProxy(%s): DomainName %s\n",
+		ifname, dn)
+	// Try http://wpad.%s/wpad.dat", dn where we the leading labels
+	// in DomainName until we succeed
+	for {
+		url := fmt.Sprintf("http://wpad.%s/wpad.dat", dn)
+		pac, err := getFile(status, proxyConfig.NetworkProxyURL, ifname)
+		if err == nil {
+			log.Infof("CheckAndGetNetworkProxy(%s): fetched from URL %s: %s\n",
+				ifname, url, pac)
+			proxyConfig.Pacfile = pac
+			return nil
+		}
+		errStr := fmt.Sprintf("Failed to fetch %s for %s: %s",
+			url, ifname, err)
+		log.Warnln(errStr)
+		i := strings.Index(dn, ".")
+		if i == -1 {
+			log.Infof("CheckAndGetNetworkProxy(%s): no dots in DomainName %s\n",
+				ifname, dn)
+			log.Errorln(errStr)
+			return errors.New(errStr)
+		}
+		b := []byte(dn)
+		dn = string(b[i:])
+		// How many dots left? End when we have a TLD i.e., not dots
+		// since wpad.com isn't a useful place to look
+		count := strings.Count(dn, ".")
+		if count == 0 {
+			log.Infof("CheckAndGetNetworkProxy(%s): got to TLD in DomainName %s\n",
+				ifname, dn)
+			log.Errorln(errStr)
+			return errors.New(errStr)
+		}
+	}
 }
 
 var ctx = zedcloud.ZedCloudContext{
