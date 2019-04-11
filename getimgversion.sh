9b7a07332be494ed404ec815eece7ffdf6dda5c5
eriknordmark <erik@zededa.com>
2019-04-11 15:06:44 -0700

    add -a option

--- getimgversion.sh
+++ getimgversion.sh
@@ -2,12 +2,26 @@
 # Run against a rootfs.img
 
 ROOTFS=rootfs.img
+PRINTALL=0
 
-if [ $# = 1 ]; then
-    ROOTFS=$1
-fi
+while [ $# != 0 ]; do
+    if [ $1 = "-a" ]; then
+	PRINTALL=1
+    else
+	ROOTFS=$1
+    fi
+    shift
+done
 
 sudo mount -o loop ${ROOTFS} /mnt
 VERS=`sudo cat /mnt/containers/services/zededa-tools/lower/opt/zededa/bin/versioninfo`
+VERS1=`sudo cat /mnt/containers/services/zededa-tools/lower/opt/zededa/bin/versioninfo.1`
+ZA=`sudo file -L /mnt/containers/services/zededa-tools/lower/opt/zededa/bin/zedagent`
+LZ=`sudo file /mnt/containers/services/zededa-tools/lower/opt/zededa/bin/lisp-ztr`
 sudo umount /mnt
 echo $VERS
+if [ $PRINTALL = 1 ]; then
+    echo "version.1: $VERS1"
+    echo "file zedagent: $ZA"
+    echo "file lisp-ztr: $LZ"
+fi
