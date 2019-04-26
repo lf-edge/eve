094382492a59e31856da3e726fba323337ea58db
eriknordmark <erik@zededa.com>
2019-04-25 17:42:12 -0700

    Support ssh authorized_keys being set from the debug.ssh.enable configItem.
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/pillar/rootfs/init.sh
+++ pkg/pillar/rootfs/init.sh
@@ -11,15 +11,6 @@ for i in `cd /sys/class/net ; echo eth*` ; do
   ethtool -K $i sg off
 done
 
-# constructing /run/authorized_keys from a static one
-# At some point we may also consider extracting key material
-# from x509 certificates...
-#   bash -c 'ssh-keygen -f <(openssl x509 -in /config/onboard.cert.pem -pubkey -noout) -i -mPKCS8' >> /run/authorized_keys
-# ...or using CA signed ssh keys
-#   TrustedUserCAKeys /path/to/server_ca.pub
-sed -e '/^[a-z][a-z]*/s#^#command="ctr --namespace services.linuxkit t exec -t --exec-id $(basename $(mktemp)) pillar sh" #' < /config/authorized_keys > /run/authorized_keys
-chmod 600 /run/authorized_keys
-
 # Need this for logrotate
 /usr/sbin/crond
 
