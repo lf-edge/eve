f81ef037c1d2332da41a95e00f3b958c978063ab
Hariharasubramanian C S <cshari@zededa.com>
2019-05-08 12:05:22 +0530

    Correcting gofmt errors in owntpm.go
    Signed-off-by: Hariharasubramanian C S <cshari@zededa.com>

--- pkg/pillar/cmd/owntpm/owntpm.go
+++ pkg/pillar/cmd/owntpm/owntpm.go
@@ -4,21 +4,21 @@ import "fmt"
 import "github.com/google/go-tpm/tpm2"
 
 var (
-        TpmDevicePath = "/dev/tpm0"
+	TpmDevicePath = "/dev/tpm0"
 )
 
-func ownTpm() (error) {
+func owntpm() error {
 	rw, err := tpm2.OpenTPM(TpmDevicePath)
-	if (err != nil) {
+	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	defer rw.Close()
-        //Put an info print for now.
-        fmt.Println("Successfully opened handle to tpm device")
-        return nil
+	//Put an info print for now.
+	fmt.Println("Successfully opened handle to tpm device")
+	return nil
 }
 
 func Run() {
-	ownTpm()
+	owntpm()
 }
