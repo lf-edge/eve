115bb2be0c9184d5d230ac6ec759261c46d05b9b
Vijay Tapaskar <vijay@zededa.com>
2019-05-23 03:22:20 -0700

    fixing review comment
    Signed-off-by: Vijay Tapaskar <vijay@zededa.com>

--- api/python/config/devmodel_pb2.py
+++ api/python/config/devmodel_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=_b('\n\037com.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/config'),
-  serialized_pb=_b('\n\x0e\x64\x65vmodel.proto\"n\n\x0fsWAdapterParams\x12\x1d\n\x05\x61Type\x18\x01 \x01(\x0e\x32\x0e.sWAdapterType\x12\x19\n\x11underlayInterface\x18\x08 \x01(\t\x12\x0e\n\x06vlanId\x18\t \x01(\r\x12\x11\n\tbondgroup\x18\n \x03(\t\"\xa1\x01\n\rSystemAdapter\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\x0c\x61llocDetails\x18\x14 \x01(\x0b\x32\x10.sWAdapterParams\x12\x12\n\nfreeUplink\x18\x02 \x01(\x08\x12\x0e\n\x06uplink\x18\x03 \x01(\x08\x12\x13\n\x0bnetworkUUID\x18\x04 \x01(\t\x12\x0c\n\x04\x61\x64\x64r\x18\x05 \x01(\t\x12\x13\n\x0blogicalName\x18\x06 \x01(\t\"\xba\x02\n\nPhysicalIO\x12\x19\n\x05ptype\x18\x64 \x01(\x0e\x32\n.PhyIoType\x12\x10\n\x08phylabel\x18\x65 \x01(\t\x12+\n\x08phyaddrs\x18\x66 \x03(\x0b\x32\x19.PhysicalIO.PhyaddrsEntry\x12\x14\n\x0clogicallabel\x18g \x01(\t\x12\x11\n\tassigngrp\x18h \x01(\t\x12 \n\x05usage\x18i \x01(\x0e\x32\x11.PhyIoMemberUsage\x12\'\n\x06\x63\x62\x61ttr\x18j \x03(\x0b\x32\x17.PhysicalIO.CbattrEntry\x1a/\n\rPhyaddrsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x1a-\n\x0b\x43\x62\x61ttrEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01*\\\n\x08ZCioType\x12\x0b\n\x07ZCioNop\x10\x00\x12\x0b\n\x07ZCioEth\x10\x01\x12\x0b\n\x07ZCioUSB\x10\x02\x12\x0b\n\x07ZCioCOM\x10\x03\x12\x0c\n\x08ZCioHDMI\x10\x04\x12\x0e\n\tZCioOther\x10\xff\x01*/\n\rsWAdapterType\x12\n\n\x06IGNORE\x10\x00\x12\x08\n\x04VLAN\x10\x01\x12\x08\n\x04\x42OND\x10\x02*U\n\tPhyIoType\x12\r\n\tPhyIoNoop\x10\x00\x12\x0c\n\x08PhyIoEth\x10\x01\x12\x0c\n\x08PhyIoUSB\x10\x02\x12\x0c\n\x08PhyIoCOM\x10\x03\x12\x0f\n\nPhyIoOther\x10\xff\x01*i\n\x10PhyIoMemberUsage\x12\x12\n\x0ePhyIoUsageNone\x10\x00\x12\x12\n\x0ePhyIoUsageMgmt\x10\x01\x12\x14\n\x10PhyIoUsageShared\x10\x02\x12\x17\n\x13PhyIoUsageDedicated\x10\x03\x42G\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
+  serialized_pb=_b('\n\x0e\x64\x65vmodel.proto\"n\n\x0fsWAdapterParams\x12\x1d\n\x05\x61Type\x18\x01 \x01(\x0e\x32\x0e.sWAdapterType\x12\x19\n\x11underlayInterface\x18\x08 \x01(\t\x12\x0e\n\x06vlanId\x18\t \x01(\r\x12\x11\n\tbondgroup\x18\n \x03(\t\"\xa1\x01\n\rSystemAdapter\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\x0c\x61llocDetails\x18\x14 \x01(\x0b\x32\x10.sWAdapterParams\x12\x12\n\nfreeUplink\x18\x02 \x01(\x08\x12\x0e\n\x06uplink\x18\x03 \x01(\x08\x12\x13\n\x0bnetworkUUID\x18\x04 \x01(\t\x12\x0c\n\x04\x61\x64\x64r\x18\x05 \x01(\t\x12\x13\n\x0blogicalName\x18\x06 \x01(\t\"\xba\x02\n\nPhysicalIO\x12\x19\n\x05ptype\x18\x01 \x01(\x0e\x32\n.PhyIoType\x12\x10\n\x08phylabel\x18\x02 \x01(\t\x12+\n\x08phyaddrs\x18\x03 \x03(\x0b\x32\x19.PhysicalIO.PhyaddrsEntry\x12\x14\n\x0clogicallabel\x18\x04 \x01(\t\x12\x11\n\tassigngrp\x18\x05 \x01(\t\x12 \n\x05usage\x18\x06 \x01(\x0e\x32\x11.PhyIoMemberUsage\x12\'\n\x06\x63\x62\x61ttr\x18\x07 \x03(\x0b\x32\x17.PhysicalIO.CbattrEntry\x1a/\n\rPhyaddrsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x1a-\n\x0b\x43\x62\x61ttrEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01*\\\n\x08ZCioType\x12\x0b\n\x07ZCioNop\x10\x00\x12\x0b\n\x07ZCioEth\x10\x01\x12\x0b\n\x07ZCioUSB\x10\x02\x12\x0b\n\x07ZCioCOM\x10\x03\x12\x0c\n\x08ZCioHDMI\x10\x04\x12\x0e\n\tZCioOther\x10\xff\x01*/\n\rsWAdapterType\x12\n\n\x06IGNORE\x10\x00\x12\x08\n\x04VLAN\x10\x01\x12\x08\n\x04\x42OND\x10\x02*\x92\x01\n\tPhyIoType\x12\r\n\tPhyIoNoop\x10\x00\x12\x0c\n\x08PhyIoEth\x10\x01\x12\x0c\n\x08PhyIoUSB\x10\x02\x12\x0c\n\x08PhyIoCOM\x10\x03\x12\x0e\n\nPhyIoAudio\x10\x04\x12\r\n\tPhyIoWLAN\x10\x05\x12\r\n\tPhyIoWWAN\x10\x06\x12\r\n\tPhyIoHDMI\x10\x07\x12\x0f\n\nPhyIoOther\x10\xff\x01*i\n\x10PhyIoMemberUsage\x12\x12\n\x0ePhyIoUsageNone\x10\x00\x12\x12\n\x0ePhyIoUsageMgmt\x10\x01\x12\x14\n\x10PhyIoUsageShared\x10\x02\x12\x17\n\x13PhyIoUsageDedicated\x10\x03\x42G\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
 )
 
 _ZCIOTYPE = _descriptor.EnumDescriptor(
@@ -112,14 +112,30 @@ _PHYIOTYPE = _descriptor.EnumDescriptor(
       serialized_options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
-      name='PhyIoOther', index=4, number=255,
+      name='PhyIoAudio', index=4, number=4,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='PhyIoWLAN', index=5, number=5,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='PhyIoWWAN', index=6, number=6,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='PhyIoHDMI', index=7, number=7,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='PhyIoOther', index=8, number=255,
       serialized_options=None,
       type=None),
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=754,
-  serialized_end=839,
+  serialized_start=755,
+  serialized_end=901,
 )
 _sym_db.RegisterEnumDescriptor(_PHYIOTYPE)
 
@@ -149,8 +165,8 @@ _PHYIOMEMBERUSAGE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=841,
-  serialized_end=946,
+  serialized_start=903,
+  serialized_end=1008,
 )
 _sym_db.RegisterEnumDescriptor(_PHYIOMEMBERUSAGE)
 
@@ -168,6 +184,10 @@ PhyIoNoop = 0
 PhyIoEth = 1
 PhyIoUSB = 2
 PhyIoCOM = 3
+PhyIoAudio = 4
+PhyIoWLAN = 5
+PhyIoWWAN = 6
+PhyIoHDMI = 7
 PhyIoOther = 255
 PhyIoUsageNone = 0
 PhyIoUsageMgmt = 1
@@ -384,49 +404,49 @@ _PHYSICALIO = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='ptype', full_name='PhysicalIO.ptype', index=0,
-      number=100, type=14, cpp_type=8, label=1,
+      number=1, type=14, cpp_type=8, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='phylabel', full_name='PhysicalIO.phylabel', index=1,
-      number=101, type=9, cpp_type=9, label=1,
+      number=2, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='phyaddrs', full_name='PhysicalIO.phyaddrs', index=2,
-      number=102, type=11, cpp_type=10, label=3,
+      number=3, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='logicallabel', full_name='PhysicalIO.logicallabel', index=3,
-      number=103, type=9, cpp_type=9, label=1,
+      number=4, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='assigngrp', full_name='PhysicalIO.assigngrp', index=4,
-      number=104, type=9, cpp_type=9, label=1,
+      number=5, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='usage', full_name='PhysicalIO.usage', index=5,
-      number=105, type=14, cpp_type=8, label=1,
+      number=6, type=14, cpp_type=8, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='cbattr', full_name='PhysicalIO.cbattr', index=6,
-      number=106, type=11, cpp_type=10, label=3,
+      number=7, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
