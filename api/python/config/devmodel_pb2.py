2aef30c125518460950b164d98cc2ea0ebdc55ec
Vijay Tapaskar <vijay@zededa.com>
2019-05-30 06:55:06 -0700

    address more review comments, whitespaces
    Signed-off-by: Vijay Tapaskar <vijay@zededa.com>

--- api/python/config/devmodel_pb2.py
+++ api/python/config/devmodel_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=_b('\n\037com.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/config'),
-  serialized_pb=_b('\n\x0e\x64\x65vmodel.proto\"n\n\x0fsWAdapterParams\x12\x1d\n\x05\x61Type\x18\x01 \x01(\x0e\x32\x0e.sWAdapterType\x12\x19\n\x11underlayInterface\x18\x08 \x01(\t\x12\x0e\n\x06vlanId\x18\t \x01(\r\x12\x11\n\tbondgroup\x18\n \x03(\t\"\xa1\x01\n\rSystemAdapter\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\x0c\x61llocDetails\x18\x14 \x01(\x0b\x32\x10.sWAdapterParams\x12\x12\n\nfreeUplink\x18\x02 \x01(\x08\x12\x0e\n\x06uplink\x18\x03 \x01(\x08\x12\x13\n\x0bnetworkUUID\x18\x04 \x01(\t\x12\x0c\n\x04\x61\x64\x64r\x18\x05 \x01(\t\x12\x13\n\x0blogicalName\x18\x06 \x01(\t\"\xba\x02\n\nPhysicalIO\x12\x19\n\x05ptype\x18\x01 \x01(\x0e\x32\n.PhyIoType\x12\x10\n\x08phylabel\x18\x02 \x01(\t\x12+\n\x08phyaddrs\x18\x03 \x03(\x0b\x32\x19.PhysicalIO.PhyaddrsEntry\x12\x14\n\x0clogicallabel\x18\x04 \x01(\t\x12\x11\n\tassigngrp\x18\x05 \x01(\t\x12 \n\x05usage\x18\x06 \x01(\x0e\x32\x11.PhyIoMemberUsage\x12\'\n\x06\x63\x62\x61ttr\x18\x07 \x03(\x0b\x32\x17.PhysicalIO.CbattrEntry\x1a/\n\rPhyaddrsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x1a-\n\x0b\x43\x62\x61ttrEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01*\\\n\x08ZCioType\x12\x0b\n\x07ZCioNop\x10\x00\x12\x0b\n\x07ZCioEth\x10\x01\x12\x0b\n\x07ZCioUSB\x10\x02\x12\x0b\n\x07ZCioCOM\x10\x03\x12\x0c\n\x08ZCioHDMI\x10\x04\x12\x0e\n\tZCioOther\x10\xff\x01*/\n\rsWAdapterType\x12\n\n\x06IGNORE\x10\x00\x12\x08\n\x04VLAN\x10\x01\x12\x08\n\x04\x42OND\x10\x02*\x92\x01\n\tPhyIoType\x12\r\n\tPhyIoNoop\x10\x00\x12\x0c\n\x08PhyIoEth\x10\x01\x12\x0c\n\x08PhyIoUSB\x10\x02\x12\x0c\n\x08PhyIoCOM\x10\x03\x12\x0e\n\nPhyIoAudio\x10\x04\x12\r\n\tPhyIoWLAN\x10\x05\x12\r\n\tPhyIoWWAN\x10\x06\x12\r\n\tPhyIoHDMI\x10\x07\x12\x0f\n\nPhyIoOther\x10\xff\x01*i\n\x10PhyIoMemberUsage\x12\x12\n\x0ePhyIoUsageNone\x10\x00\x12\x12\n\x0ePhyIoUsageMgmt\x10\x01\x12\x14\n\x10PhyIoUsageShared\x10\x02\x12\x17\n\x13PhyIoUsageDedicated\x10\x03\x42G\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
+  serialized_pb=_b('\n\x0e\x64\x65vmodel.proto\"n\n\x0fsWAdapterParams\x12\x1d\n\x05\x61Type\x18\x01 \x01(\x0e\x32\x0e.sWAdapterType\x12\x19\n\x11underlayInterface\x18\x08 \x01(\t\x12\x0e\n\x06vlanId\x18\t \x01(\r\x12\x11\n\tbondgroup\x18\n \x03(\t\"\xa1\x01\n\rSystemAdapter\x12\x0c\n\x04name\x18\x01 \x01(\t\x12&\n\x0c\x61llocDetails\x18\x14 \x01(\x0b\x32\x10.sWAdapterParams\x12\x12\n\nfreeUplink\x18\x02 \x01(\x08\x12\x0e\n\x06uplink\x18\x03 \x01(\x08\x12\x13\n\x0bnetworkUUID\x18\x04 \x01(\t\x12\x0c\n\x04\x61\x64\x64r\x18\x05 \x01(\t\x12\x13\n\x0blogicalName\x18\x06 \x01(\t\"&\n\x10PhyIOUsagePolicy\x12\x12\n\nfreeUplink\x18\x01 \x01(\x08\"\xe2\x02\n\nPhysicalIO\x12\x19\n\x05ptype\x18\x01 \x01(\x0e\x32\n.PhyIoType\x12\x10\n\x08phylabel\x18\x02 \x01(\t\x12+\n\x08phyaddrs\x18\x03 \x03(\x0b\x32\x19.PhysicalIO.PhyaddrsEntry\x12\x14\n\x0clogicallabel\x18\x04 \x01(\t\x12\x11\n\tassigngrp\x18\x05 \x01(\t\x12 \n\x05usage\x18\x06 \x01(\x0e\x32\x11.PhyIoMemberUsage\x12&\n\x0busagePolicy\x18\x07 \x01(\x0b\x32\x11.PhyIOUsagePolicy\x12\'\n\x06\x63\x62\x61ttr\x18\x08 \x03(\x0b\x32\x17.PhysicalIO.CbattrEntry\x1a/\n\rPhyaddrsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x1a-\n\x0b\x43\x62\x61ttrEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01*\\\n\x08ZCioType\x12\x0b\n\x07ZCioNop\x10\x00\x12\x0b\n\x07ZCioEth\x10\x01\x12\x0b\n\x07ZCioUSB\x10\x02\x12\x0b\n\x07ZCioCOM\x10\x03\x12\x0c\n\x08ZCioHDMI\x10\x04\x12\x0e\n\tZCioOther\x10\xff\x01*/\n\rsWAdapterType\x12\n\n\x06IGNORE\x10\x00\x12\x08\n\x04VLAN\x10\x01\x12\x08\n\x04\x42OND\x10\x02*\x9b\x01\n\tPhyIoType\x12\r\n\tPhyIoNoop\x10\x00\x12\x0f\n\x0bPhyIoNetEth\x10\x01\x12\x0c\n\x08PhyIoUSB\x10\x02\x12\x0c\n\x08PhyIoCOM\x10\x03\x12\x0e\n\nPhyIoAudio\x10\x04\x12\x10\n\x0cPhyIoNetWLAN\x10\x05\x12\x10\n\x0cPhyIoNetWWAN\x10\x06\x12\r\n\tPhyIoHDMI\x10\x07\x12\x0f\n\nPhyIoOther\x10\xff\x01*i\n\x10PhyIoMemberUsage\x12\x12\n\x0ePhyIoUsageNone\x10\x00\x12\x12\n\x0ePhyIoUsageMgmt\x10\x01\x12\x14\n\x10PhyIoUsageShared\x10\x02\x12\x17\n\x13PhyIoUsageDedicated\x10\x03\x42G\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
 )
 
 _ZCIOTYPE = _descriptor.EnumDescriptor(
@@ -56,8 +56,8 @@ _ZCIOTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=611,
-  serialized_end=703,
+  serialized_start=691,
+  serialized_end=783,
 )
 _sym_db.RegisterEnumDescriptor(_ZCIOTYPE)
 
@@ -83,8 +83,8 @@ _SWADAPTERTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=705,
-  serialized_end=752,
+  serialized_start=785,
+  serialized_end=832,
 )
 _sym_db.RegisterEnumDescriptor(_SWADAPTERTYPE)
 
@@ -100,7 +100,7 @@ _PHYIOTYPE = _descriptor.EnumDescriptor(
       serialized_options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
-      name='PhyIoEth', index=1, number=1,
+      name='PhyIoNetEth', index=1, number=1,
       serialized_options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
@@ -116,11 +116,11 @@ _PHYIOTYPE = _descriptor.EnumDescriptor(
       serialized_options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
-      name='PhyIoWLAN', index=5, number=5,
+      name='PhyIoNetWLAN', index=5, number=5,
       serialized_options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
-      name='PhyIoWWAN', index=6, number=6,
+      name='PhyIoNetWWAN', index=6, number=6,
       serialized_options=None,
       type=None),
     _descriptor.EnumValueDescriptor(
@@ -134,8 +134,8 @@ _PHYIOTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=755,
-  serialized_end=901,
+  serialized_start=835,
+  serialized_end=990,
 )
 _sym_db.RegisterEnumDescriptor(_PHYIOTYPE)
 
@@ -165,8 +165,8 @@ _PHYIOMEMBERUSAGE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=903,
-  serialized_end=1008,
+  serialized_start=992,
+  serialized_end=1097,
 )
 _sym_db.RegisterEnumDescriptor(_PHYIOMEMBERUSAGE)
 
@@ -181,12 +181,12 @@ IGNORE = 0
 VLAN = 1
 BOND = 2
 PhyIoNoop = 0
-PhyIoEth = 1
+PhyIoNetEth = 1
 PhyIoUSB = 2
 PhyIoCOM = 3
 PhyIoAudio = 4
-PhyIoWLAN = 5
-PhyIoWWAN = 6
+PhyIoNetWLAN = 5
+PhyIoNetWWAN = 6
 PhyIoHDMI = 7
 PhyIoOther = 255
 PhyIoUsageNone = 0
@@ -321,6 +321,37 @@ _SYSTEMADAPTER = _descriptor.Descriptor(
 )
 
 
+_PHYIOUSAGEPOLICY = _descriptor.Descriptor(
+  name='PhyIOUsagePolicy',
+  full_name='PhyIOUsagePolicy',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='freeUplink', full_name='PhyIOUsagePolicy.freeUplink', index=0,
+      number=1, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=294,
+  serialized_end=332,
+)
+
+
 _PHYSICALIO_PHYADDRSENTRY = _descriptor.Descriptor(
   name='PhyaddrsEntry',
   full_name='PhysicalIO.PhyaddrsEntry',
@@ -354,8 +385,8 @@ _PHYSICALIO_PHYADDRSENTRY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=515,
-  serialized_end=562,
+  serialized_start=595,
+  serialized_end=642,
 )
 
 _PHYSICALIO_CBATTRENTRY = _descriptor.Descriptor(
@@ -391,8 +422,8 @@ _PHYSICALIO_CBATTRENTRY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=564,
-  serialized_end=609,
+  serialized_start=644,
+  serialized_end=689,
 )
 
 _PHYSICALIO = _descriptor.Descriptor(
@@ -445,8 +476,15 @@ _PHYSICALIO = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='cbattr', full_name='PhysicalIO.cbattr', index=6,
-      number=7, type=11, cpp_type=10, label=3,
+      name='usagePolicy', full_name='PhysicalIO.usagePolicy', index=6,
+      number=7, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='cbattr', full_name='PhysicalIO.cbattr', index=7,
+      number=8, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -463,8 +501,8 @@ _PHYSICALIO = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=295,
-  serialized_end=609,
+  serialized_start=335,
+  serialized_end=689,
 )
 
 _SWADAPTERPARAMS.fields_by_name['aType'].enum_type = _SWADAPTERTYPE
@@ -474,9 +512,11 @@ _PHYSICALIO_CBATTRENTRY.containing_type = _PHYSICALIO
 _PHYSICALIO.fields_by_name['ptype'].enum_type = _PHYIOTYPE
 _PHYSICALIO.fields_by_name['phyaddrs'].message_type = _PHYSICALIO_PHYADDRSENTRY
 _PHYSICALIO.fields_by_name['usage'].enum_type = _PHYIOMEMBERUSAGE
+_PHYSICALIO.fields_by_name['usagePolicy'].message_type = _PHYIOUSAGEPOLICY
 _PHYSICALIO.fields_by_name['cbattr'].message_type = _PHYSICALIO_CBATTRENTRY
 DESCRIPTOR.message_types_by_name['sWAdapterParams'] = _SWADAPTERPARAMS
 DESCRIPTOR.message_types_by_name['SystemAdapter'] = _SYSTEMADAPTER
+DESCRIPTOR.message_types_by_name['PhyIOUsagePolicy'] = _PHYIOUSAGEPOLICY
 DESCRIPTOR.message_types_by_name['PhysicalIO'] = _PHYSICALIO
 DESCRIPTOR.enum_types_by_name['ZCioType'] = _ZCIOTYPE
 DESCRIPTOR.enum_types_by_name['sWAdapterType'] = _SWADAPTERTYPE
@@ -497,6 +537,13 @@ SystemAdapter = _reflection.GeneratedProtocolMessageType('SystemAdapter', (_mess
   # @@protoc_insertion_point(class_scope:SystemAdapter)
   ))
 _sym_db.RegisterMessage(SystemAdapter)
+
+PhyIOUsagePolicy = _reflection.GeneratedProtocolMessageType('PhyIOUsagePolicy', (_message.Message,), dict(
+  DESCRIPTOR = _PHYIOUSAGEPOLICY,
+  __module__ = 'devmodel_pb2'
+  # @@protoc_insertion_point(class_scope:PhyIOUsagePolicy)
+  ))
+_sym_db.RegisterMessage(PhyIOUsagePolicy)
 
 PhysicalIO = _reflection.GeneratedProtocolMessageType('PhysicalIO', (_message.Message,), dict(
 
