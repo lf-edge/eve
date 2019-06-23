f7af2bb291e9b221e0d5710e4a1aa29369303475
Bharani Chadalavada <bharani@zededa.com>
2019-06-23 10:56:04 -0700

    API generated python code.
    Signed-off-by: Bharani Chadalavada <bharani@zededa.com>

--- api/python/config/fw_pb2.py
+++ api/python/config/fw_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=_b('\n\037com.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/config'),
-  serialized_pb=_b('\n\x08\x66w.proto\"\'\n\x08\x41\x43\x45Match\x12\x0c\n\x04type\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\"\x84\x01\n\tACEAction\x12\x0c\n\x04\x64rop\x18\x01 \x01(\x08\x12\r\n\x05limit\x18\x02 \x01(\x08\x12\x11\n\tlimitrate\x18\x03 \x01(\r\x12\x11\n\tlimitunit\x18\x04 \x01(\t\x12\x12\n\nlimitburst\x18\x05 \x01(\r\x12\x0f\n\x07portmap\x18\x06 \x01(\x08\x12\x0f\n\x07\x61ppPort\x18\x07 \x01(\r\"t\n\x03\x41\x43\x45\x12\x1a\n\x07matches\x18\x01 \x03(\x0b\x32\t.ACEMatch\x12\x1b\n\x07\x61\x63tions\x18\x02 \x03(\x0b\x32\n.ACEAction\x12\x0c\n\x04name\x18\x03 \x01(\t\x12\n\n\x02id\x18\x04 \x01(\t\x12\x1a\n\x03\x64ir\x18\x05 \x01(\x0e\x32\r.ACEDirection*1\n\x0c\x41\x43\x45\x44irection\x12\x08\n\x04\x42OTH\x10\x00\x12\x0b\n\x07INGRESS\x10\x01\x12\n\n\x06\x45GRESS\x10\x02\x42G\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
+  serialized_pb=_b('\n\x08\x66w.proto\"\'\n\x08\x41\x43\x45Match\x12\x0c\n\x04type\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\"\x84\x01\n\tACEAction\x12\x0c\n\x04\x64rop\x18\x01 \x01(\x08\x12\r\n\x05limit\x18\x02 \x01(\x08\x12\x11\n\tlimitrate\x18\x03 \x01(\r\x12\x11\n\tlimitunit\x18\x04 \x01(\t\x12\x12\n\nlimitburst\x18\x05 \x01(\r\x12\x0f\n\x07portmap\x18\x06 \x01(\x08\x12\x0f\n\x07\x61ppPort\x18\x07 \x01(\r\"t\n\x03\x41\x43\x45\x12\x1a\n\x07matches\x18\x01 \x03(\x0b\x32\t.ACEMatch\x12\x1b\n\x07\x61\x63tions\x18\x02 \x03(\x0b\x32\n.ACEAction\x12\x0c\n\x04name\x18\x03 \x01(\t\x12\n\n\x02id\x18\x04 \x01(\x05\x12\x1a\n\x03\x64ir\x18\x05 \x01(\x0e\x32\r.ACEDirection*1\n\x0c\x41\x43\x45\x44irection\x12\x08\n\x04\x42OTH\x10\x00\x12\x0b\n\x07INGRESS\x10\x01\x12\n\n\x06\x45GRESS\x10\x02\x42G\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
 )
 
 _ACEDIRECTION = _descriptor.EnumDescriptor(
@@ -197,8 +197,8 @@ _ACE = _descriptor.Descriptor(
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
       name='id', full_name='ACE.id', index=3,
-      number=4, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
+      number=4, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
