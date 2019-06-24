e676f70a123be78d8f82c885a99e711440e34d02
Archishmaan Peyyety <archishou@gmail.com>
2019-06-24 09:45:00 -0400

    added test to function getOverlayConfig from zedroutertypes.go
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- pkg/pillar/types/zedroutertypes_test.go
+++ pkg/pillar/types/zedroutertypes_test.go
@@ -45,3 +45,26 @@ func TestIsIPv6(t *testing.T) {
 		assert.IsType(t, test.expectedValue, isIPv6)
 	}
 }
+func TestGetOverlayConfig(t *testing.T) {
+	overlayUUID := uuid.UUID{0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1,
+		0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8}
+	config := AppNetworkConfig{
+		OverlayNetworkList: []OverlayNetworkConfig{
+			OverlayNetworkConfig{Network: overlayUUID},
+		},
+	}
+	testMatrix := map[string]struct {
+		network uuid.UUID
+		config  AppNetworkConfig
+	}{
+		"Overlay UUID": {
+			network: overlayUUID,
+			config:  config,
+		},
+	}
+	for testname, test := range testMatrix {
+		t.Logf("Running test case %s", testname)
+		config := test.config.getOverlayConfig(overlayUUID)
+		assert.IsType(t, test.config.OverlayNetworkList[0], *config)
+	}
+}
