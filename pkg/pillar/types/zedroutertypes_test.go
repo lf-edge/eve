3d438f32be61bfe03f192360411eb714aae03384
Archishmaan Peyyety <archishou@gmail.com>
2019-06-24 09:48:12 -0400

    added test to function getUnderlayConfig from zedroutertypes.go
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- pkg/pillar/types/zedroutertypes_test.go
+++ pkg/pillar/types/zedroutertypes_test.go
@@ -69,3 +69,26 @@ func TestGetOverlayConfig(t *testing.T) {
 		assert.IsType(t, test.config.OverlayNetworkList[0], *config)
 	}
 }
+func TestGetUnderlayConfig(t *testing.T) {
+	underlayUUID := uuid.UUID{0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1,
+		0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8}
+	config := AppNetworkConfig{
+		UnderlayNetworkList: []UnderlayNetworkConfig{
+			UnderlayNetworkConfig{Network: underlayUUID},
+		},
+	}
+	testMatrix := map[string]struct {
+		network uuid.UUID
+		config  AppNetworkConfig
+	}{
+		"Underlay UUID": {
+			network: underlayUUID,
+			config:  config,
+		},
+	}
+	for testname, test := range testMatrix {
+		t.Logf("Running test case %s", testname)
+		config := test.config.getUnderlayConfig(underlayUUID)
+		assert.IsType(t, test.config.UnderlayNetworkList[0], *config)
+	}
+}
