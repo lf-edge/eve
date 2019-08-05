f62778dd05ee7981d1549dc91387ce6aafddfa9b
Srinibas Maharana <srinibas@zededa.com>
2019-08-05 21:27:08 +0530

    first cut for datastore configuration handling into downloader
    Signed-off-by: Srinibas Maharana <srinibas@zededa.com>

--- pkg/pillar/cmd/downloader/downloader.go
+++ pkg/pillar/cmd/downloader/downloader.go
@@ -28,6 +28,7 @@ import (
 	"github.com/lf-edge/eve/pkg/pillar/types"
 	"github.com/lf-edge/eve/pkg/pillar/zedUpload"
 	"github.com/lf-edge/eve/pkg/pillar/zedcloud"
+	"github.com/satori/go.uuid"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -48,6 +49,7 @@ var (
 
 // Set from Makefile
 var Version = "No version specified"
+var nilUUID uuid.UUID
 
 type downloaderContext struct {
 	dCtx                    *zedUpload.DronaCtx
@@ -60,6 +62,7 @@ type downloaderContext struct {
 	pubCertObjStatus        *pubsub.Publication
 	subGlobalDownloadConfig *pubsub.Subscription
 	pubGlobalDownloadStatus *pubsub.Publication
+	subDatastoreConfig      *pubsub.Subscription
 	deviceNetworkStatus     types.DeviceNetworkStatus
 	globalConfig            types.GlobalDownloadConfig
 	globalStatusLock        sync.Mutex
@@ -214,6 +217,17 @@ func Run() {
 	ctx.subCertObjConfig = subCertObjConfig
 	subCertObjConfig.Activate()
 
+	// Look for DatastoreConfig from zedagent
+	subDatastoreConfig, err := pubsub.SubscribeScope("zedagent",
+		baseOsObj, types.DatastoreConfig{}, false, &ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	subDatastoreConfig.ModifyHandler = handleDatastoreConfigModify
+	subDatastoreConfig.DeleteHandler = handleDatastoreConfigDelete
+	ctx.subDatastoreConfig = subDatastoreConfig
+	subDatastoreConfig.Activate()
+
 	pubAppImgStatus.SignalRestarted()
 	pubBaseOsStatus.SignalRestarted()
 	pubCertObjStatus.SignalRestarted()
@@ -267,6 +281,9 @@ func Run() {
 		case change := <-subBaseOsConfig.C:
 			subBaseOsConfig.ProcessChange(change)
 
+		case change := <-subDatastoreConfig.C:
+			subDatastoreConfig.ProcessChange(change)
+
 		case change := <-subGlobalDownloadConfig.C:
 			subGlobalDownloadConfig.ProcessChange(change)
 
@@ -281,6 +298,42 @@ func Run() {
 
 		case <-stillRunning.C:
 			agentlog.StillRunning(agentName)
+		}
+	}
+}
+
+func handleDatastoreConfigModify(ctxArg interface{}, key string,
+	configArg interface{}) {
+
+	ctx := ctxArg.(*downloaderContext)
+	config := cast.CastDatastoreConfig(configArg)
+	log.Infof("handleDatastoreConfigModify for %s\n", key)
+	checkAndUpdateDownloadableObjects(ctx, config.UUID)
+	log.Infof("handleDatastoreConfigModify for %s, done\n", key)
+}
+
+func handleDatastoreConfigDelete(ctxArg interface{}, key string,
+	configArg interface{}) {
+	log.Infof("handleDatastoreConfigDelete for %s\n", key)
+}
+
+// handle the datastore modification
+func checkAndUpdateDownloadableObjects(ctx *downloaderContext, dsid uuid.UUID) {
+	publications := []*pubsub.Publication{
+		ctx.pubAppImgStatus,
+		ctx.pubBaseOsStatus,
+		ctx.pubCertObjStatus,
+	}
+	for _, pub := range publications {
+		items := pub.GetAll()
+		for _, st := range items {
+			status := cast.CastDownloaderStatus(st)
+			if status.DatastoreId == dsid {
+				config := lookupDownloaderConfig(ctx, status.ObjType, status.Key())
+				if config != nil {
+					handleDownloaderModify(ctx, status.ObjType, status.Key(), config)
+				}
+			}
 		}
 	}
 }
@@ -484,14 +537,36 @@ func maybeRetryDownload(ctx *downloaderContext,
 	status.RetryCount += 1
 	// XXX do we need to adjust reservedspace??
 
-	handleSyncOp(ctx, status.Key(), *config, status)
+	dst, err := lookupDatastoreConfig(ctx, config.DatastoreId, config.Name)
+	if dst == nil {
+		log.Warnf("datastore(%s), is still not available, %s\n", config.DatastoreId, err)
+		return
+	}
+	dpath := dst.Dpath
+	if status.ObjType == certObj {
+		dpath = strings.Replace(dpath, "-images", "-certs", 1)
+	}
+	downloadURL := config.Name
+	if !config.NameIsURL {
+		downloadURL = dst.Fqdn + "/" + dpath + "/" + config.Name
+	}
+
+	dsCtx := types.DatastoreContext{
+		DownloadURL: downloadURL,
+		Dpath:       dpath,
+		ApiKey:      dst.ApiKey,
+		Password:    dst.Password,
+		Region:      dst.Region,
+		FinalObjDir: config.FinalObjDir,
+	}
+	handleSyncOp(ctx, status.Key(), *config, status, dsCtx)
 }
 
 func handleCreate(ctx *downloaderContext, objType string,
 	config types.DownloaderConfig, key string) {
 
 	log.Infof("handleCreate(%v) objType %s for %s\n",
-		config.Safename, objType, config.DownloadURL)
+		config.Safename, objType, config.Name)
 
 	if objType == "" {
 		log.Fatalf("handleCreate: No ObjType for %s\n",
@@ -499,11 +574,12 @@ func handleCreate(ctx *downloaderContext, objType string,
 	}
 	// Start by marking with PendingAdd
 	status := types.DownloaderStatus{
+		DatastoreId:      config.DatastoreId,
 		Safename:         config.Safename,
+		Name:             config.Name,
 		ObjType:          objType,
 		RefCount:         config.RefCount,
 		LastUse:          time.Now(),
-		DownloadURL:      config.DownloadURL,
 		UseFreeMgmtPorts: config.UseFreeMgmtPorts,
 		ImageSha256:      config.ImageSha256,
 		PendingAdd:       true,
@@ -524,7 +600,7 @@ func handleCreate(ctx *downloaderContext, objType string,
 		status.LastErrTime = time.Now()
 		status.RetryCount += 1
 		publishDownloaderStatus(ctx, &status)
-		log.Errorf("handleCreate failed for %s\n", config.DownloadURL)
+		log.Errorf("handleCreate failed for %s\n", config.Name)
 		return
 	}
 
@@ -532,7 +608,7 @@ func handleCreate(ctx *downloaderContext, objType string,
 	if config.RefCount == 0 {
 		// XXX odd to treat as error.
 		errString := fmt.Sprintf("RefCount==0; download deferred for %s\n",
-			config.DownloadURL)
+			config.Name)
 		log.Errorln(errString)
 		status.PendingAdd = false
 		status.Size = 0
@@ -540,11 +616,32 @@ func handleCreate(ctx *downloaderContext, objType string,
 		status.LastErrTime = time.Now()
 		status.RetryCount += 1
 		publishDownloaderStatus(ctx, &status)
-		log.Errorf("handleCreate deferred for %s\n", config.DownloadURL)
+		log.Errorf("handleCreate deferred for %s\n", config.Name)
 		return
 	}
 
-	handleSyncOp(ctx, key, config, &status)
+	dst, err := lookupDatastoreConfig(ctx, config.DatastoreId, config.Name)
+	if dst == nil {
+		log.Warnf("datastore(%s) is still not available, %s\n", config.DatastoreId, err)
+		return
+	}
+	dpath := dst.Dpath
+	if status.ObjType == certObj {
+		dpath = strings.Replace(dpath, "-images", "-certs", 1)
+	}
+	downloadURL := config.Name
+	if !config.NameIsURL {
+		downloadURL = dst.Fqdn + "/" + dpath + "/" + config.Name
+	}
+	dsCtx := types.DatastoreContext{
+		DownloadURL: downloadURL,
+		Dpath:       dpath,
+		ApiKey:      dst.ApiKey,
+		Password:    dst.Password,
+		Region:      dst.Region,
+		FinalObjDir: config.FinalObjDir,
+	}
+	handleSyncOp(ctx, key, config, &status, dsCtx)
 }
 
 // XXX Allow to cancel by setting RefCount = 0? Such a change
@@ -555,7 +652,7 @@ func handleModify(ctx *downloaderContext, key string,
 	config types.DownloaderConfig, status *types.DownloaderStatus) {
 
 	log.Infof("handleModify(%v) objType %s for %s\n",
-		status.Safename, status.ObjType, status.DownloadURL)
+		status.Safename, status.ObjType, status.Name)
 
 	if status.ObjType == "" {
 		log.Fatalf("handleModify: No ObjType for %s\n",
@@ -563,9 +660,9 @@ func handleModify(ctx *downloaderContext, key string,
 	}
 	locDirname := objectDownloadDirname + "/" + status.ObjType
 
-	if config.DownloadURL != status.DownloadURL {
+	if config.Name != status.Name {
 		log.Errorf("URL changed - not allowed %s -> %s\n",
-			config.DownloadURL, status.DownloadURL)
+			config.Name, status.Name)
 		return
 	}
 	// If the sha changes, we treat it as a delete and recreate.
@@ -578,22 +675,21 @@ func handleModify(ctx *downloaderContext, key string,
 		} else {
 			reason = "recovering from previous error"
 		}
-		log.Errorf("handleModify %s for %s\n",
-			reason, config.DownloadURL)
+		log.Errorf("handleModify %s for %s\n", reason, config.Name)
 		doDelete(ctx, key, locDirname, status)
 		handleCreate(ctx, status.ObjType, config, key)
-		log.Infof("handleModify done for %s\n", config.DownloadURL)
+		log.Infof("handleModify done for %s\n", config.Name)
 		return
 	}
 
 	log.Infof("handleModify(%v) RefCount %d to %d, Expired %v for %s\n",
 		status.Safename, status.RefCount, config.RefCount,
-		status.Expired, status.DownloadURL)
+		status.Expired, status.Name)
 
 	// If RefCount from zero to non-zero then do install
 	if status.RefCount == 0 && config.RefCount != 0 {
 		status.PendingModify = true
-		log.Infof("handleModify installing %s\n", config.DownloadURL)
+		log.Infof("handleModify installing %s\n", config.Name)
 		handleCreate(ctx, status.ObjType, config, key)
 		status.RefCount = config.RefCount
 		status.LastUse = time.Now()
@@ -610,13 +706,13 @@ func handleModify(ctx *downloaderContext, key string,
 		status.PendingModify = false
 		publishDownloaderStatus(ctx, status)
 	}
-	log.Infof("handleModify done for %s\n", config.DownloadURL)
+	log.Infof("handleModify done for %s\n", config.Name)
 }
 
 func doDelete(ctx *downloaderContext, key string, locDirname string,
 	status *types.DownloaderStatus) {
 
-	log.Infof("doDelete(%v) for %s\n", status.Safename, status.DownloadURL)
+	log.Infof("doDelete(%v) for %s\n", status.Safename, status.Name)
 
 	deletefile(locDirname+"/pending", status)
 
@@ -651,7 +747,7 @@ func handleDelete(ctx *downloaderContext, key string,
 	status *types.DownloaderStatus) {
 
 	log.Infof("handleDelete(%v) objType %s for %s RefCount %d LastUse %v Expired %v\n",
-		status.Safename, status.ObjType, status.DownloadURL,
+		status.Safename, status.ObjType, status.Name,
 		status.RefCount, status.LastUse, status.Expired)
 
 	if status.ObjType == "" {
@@ -675,7 +771,7 @@ func handleDelete(ctx *downloaderContext, key string,
 
 	// Write out what we modified to DownloaderStatus aka delete
 	unpublishDownloaderStatus(ctx, status)
-	log.Infof("handleDelete done for %s, %s\n", status.DownloadURL,
+	log.Infof("handleDelete done for %s, %s\n", status.Name,
 		locDirname)
 }
 
@@ -1177,9 +1273,8 @@ func doSftp(ctx *downloaderContext, status *types.DownloaderStatus,
 }
 
 // Drona APIs for object Download
-
 func handleSyncOp(ctx *downloaderContext, key string,
-	config types.DownloaderConfig, status *types.DownloaderStatus) {
+	config types.DownloaderConfig, status *types.DownloaderStatus, dsCtx types.DatastoreContext) {
 	var err error
 	var errStr string
 	var locFilename string
@@ -1213,7 +1308,7 @@ func handleSyncOp(ctx *downloaderContext, key string,
 	locFilename = locFilename + "/" + config.Safename
 
 	log.Infof("Downloading <%s> to <%s> using %v free management port\n",
-		config.DownloadURL, locFilename, config.UseFreeMgmtPorts)
+		config.Name, locFilename, config.UseFreeMgmtPorts)
 
 	var addrCount int
 	if config.UseFreeMgmtPorts {
@@ -1228,10 +1323,10 @@ func handleSyncOp(ctx *downloaderContext, key string,
 	if addrCount == 0 {
 		errStr = err.Error()
 	}
-	metricsUrl := config.DownloadURL
-	if config.TransportMethod == zconfig.DsType_DsS3.String() {
+	metricsUrl := dsCtx.DownloadURL
+	if dsCtx.TransportMethod == zconfig.DsType_DsS3.String() {
 		// fake URL for metrics
-		metricsUrl = fmt.Sprintf("S3:%s/%s", config.Dpath, filename)
+		metricsUrl = fmt.Sprintf("S3:%s/%s", dsCtx.Dpath, filename)
 	}
 
 	// Loop through all interfaces until a success
@@ -1252,11 +1347,11 @@ func handleSyncOp(ctx *downloaderContext, key string,
 		}
 		ifname := types.GetMgmtPortFromAddr(ctx.deviceNetworkStatus, ipSrc)
 		log.Infof("Using IP source %v if %s transport %v\n",
-			ipSrc, ifname, config.TransportMethod)
-		switch config.TransportMethod {
+			ipSrc, ifname, dsCtx.TransportMethod)
+		switch dsCtx.TransportMethod {
 		case zconfig.DsType_DsS3.String():
-			err = doS3(ctx, status, syncOp, config.DownloadURL, config.ApiKey,
-				config.Password, config.Dpath, config.Region,
+			err = doS3(ctx, status, syncOp, dsCtx.DownloadURL, dsCtx.ApiKey,
+				dsCtx.Password, dsCtx.Dpath, dsCtx.Region,
 				config.Size, ifname, ipSrc, filename, locFilename)
 			if err != nil {
 				log.Errorf("Source IP %s failed: %s\n",
@@ -1282,9 +1377,9 @@ func handleSyncOp(ctx *downloaderContext, key string,
 				return
 			}
 		case zconfig.DsType_DsSFTP.String():
-			serverUrl := getServerUrl(config, filename)
-			err = doSftp(ctx, status, syncOp, config.ApiKey,
-				config.Password, serverUrl, config.Dpath,
+			serverUrl := getServerUrl(dsCtx, filename)
+			err = doSftp(ctx, status, syncOp, dsCtx.ApiKey,
+				dsCtx.Password, serverUrl, dsCtx.Dpath,
 				config.Size, ipSrc, filename, locFilename)
 			if err != nil {
 				log.Errorf("Source IP %s failed: %s\n",
@@ -1310,8 +1405,8 @@ func handleSyncOp(ctx *downloaderContext, key string,
 				return
 			}
 		case zconfig.DsType_DsHttp.String(), zconfig.DsType_DsHttps.String(), "":
-			serverUrl := getServerUrl(config, filename)
-			err = doHttp(ctx, status, syncOp, serverUrl, config.Dpath,
+			serverUrl := getServerUrl(dsCtx, filename)
+			err = doHttp(ctx, status, syncOp, serverUrl, dsCtx.Dpath,
 				config.Size, ifname, ipSrc, filename, locFilename)
 			if err != nil {
 				log.Errorf("Source IP %s failed: %s\n",
@@ -1345,12 +1440,12 @@ func handleSyncOp(ctx *downloaderContext, key string,
 
 // DownloadURL format : http://<serverURL>/dpath/filename
 // XXX why can't we parse URL from font? This only works when filename starts with "/"
-func getServerUrl(config types.DownloaderConfig, filename string) string {
-	if config.Dpath != "" {
-		return strings.TrimSuffix(config.DownloadURL,
-			"/"+config.Dpath+"/"+filename)
+func getServerUrl(dsCtx types.DatastoreContext, filename string) string {
+	if dsCtx.Dpath != "" {
+		return strings.TrimSuffix(dsCtx.DownloadURL,
+			"/"+dsCtx.Dpath+"/"+filename)
 	} else {
-		return strings.TrimSuffix(config.DownloadURL,
+		return strings.TrimSuffix(dsCtx.DownloadURL,
 			"/"+filename)
 	}
 }
@@ -1374,14 +1469,14 @@ func handleSyncOpResponse(ctx *downloaderContext, config types.DownloaderConfig,
 		status.RetryCount += 1
 		publishDownloaderStatus(ctx, status)
 		log.Errorf("handleSyncOpResponse failed for %s, <%s>\n",
-			status.DownloadURL, errStr)
+			status.Name, errStr)
 		return
 	}
 
 	info, err := os.Stat(locFilename)
 	if err != nil {
 		log.Errorf("handleSyncOpResponse Stat failed for %s <%s>\n",
-			status.DownloadURL, err)
+			status.Name, err)
 		// Delete file
 		doDelete(ctx, key, locDirname, status)
 		status.PendingAdd = false
@@ -1398,7 +1493,7 @@ func handleSyncOpResponse(ctx *downloaderContext, config types.DownloaderConfig,
 	unreserveSpace(ctx, status)
 
 	log.Infof("handleSyncOpResponse successful <%s> <%s>\n",
-		config.DownloadURL, locFilename)
+		config.Name, locFilename)
 	// We do not clear any status.RetryCount, LastErr, etc. The caller
 	// should look at State == DOWNLOADED to determine it is done.
 
@@ -1477,4 +1572,26 @@ func handleGlobalConfigDelete(ctxArg interface{}, key string,
 	debug, _ = agentlog.HandleGlobalConfig(ctx.subGlobalConfig, agentName,
 		debugOverride)
 	log.Infof("handleGlobalConfigDelete done for %s\n", key)
+}
+
+// Check for nil UUID (an indication the drive was missing in parseconfig)
+// and a missing datastore id.
+func lookupDatastoreConfig(ctx *downloaderContext,
+	datastoreId uuid.UUID, name string) (*types.DatastoreConfig, error) {
+
+	if datastoreId == nilUUID {
+		errStr := fmt.Sprintf("lookupDatastoreConfig(%s) for %s: No datastore ID",
+			datastoreId.String(), name)
+		log.Errorln(errStr)
+		return nil, errors.New(errStr)
+	}
+	cfg, err := ctx.subDatastoreConfig.Get(datastoreId.String())
+	if err != nil {
+		errStr := fmt.Sprintf("lookupDatastoreConfig(%s) for %s: %v",
+			datastoreId.String(), name, err)
+		log.Errorln(errStr)
+		return nil, errors.New(errStr)
+	}
+	dst := cast.CastDatastoreConfig(cfg)
+	return &dst, nil
 }
