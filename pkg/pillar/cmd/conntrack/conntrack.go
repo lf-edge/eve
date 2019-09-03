d2b4567b4fe10f9ad8500b4fd81fd0e77560020a
Naiming Shen <naiming@zededa.com>
2019-09-03 16:58:26 -0700

    add conntrack del options and update netlink/vendor
    Signed-off-by: Naiming Shen <naiming@zededa.com>

--- pkg/pillar/cmd/conntrack/conntrack.go
+++ pkg/pillar/cmd/conntrack/conntrack.go
@@ -8,12 +8,55 @@ import (
 	"fmt"
 	"github.com/eriknordmark/netlink"
 	log "github.com/sirupsen/logrus"
+	"net"
 	"syscall"
 )
 
 func Run() {
 	// XXX curpartPtr := flag.String("c", "", "Current partition")
+	delFlow := flag.Bool("D", false, "Delete flow")
+	delSrcIP := flag.String("s", "", "Delete flow with Srouce IP")
+	delProto := flag.Int("p", 0, "Delete flow with protocol ID")
+	delFamily := flag.String("f", "", "Delete flow with ipv6")
+	delPort := flag.Int("P", 0, "Delete flow with port number")
+	delMark := flag.Int("m", 0, "Delete flow with Mark number")
 	flag.Parse()
+
+	// conntrack [-D <-s address> [-p proto][-P port][-m Mark]]
+	if *delFlow {
+		if *delSrcIP != "" {
+			var proto uint8
+			var src net.IP
+			var port uint16
+			var family netlink.InetFamily
+			var mark uint32
+
+			family = syscall.AF_INET
+			src = net.ParseIP(*delSrcIP)
+			if *delProto != 0 {
+				proto = uint8(*delProto)
+			}
+			if *delFamily == "ipv6" {
+				family = syscall.AF_INET6
+			}
+			if *delPort != 0 {
+				port = uint16(*delPort)
+			}
+			if *delMark != 0 {
+				mark = uint32(*delMark)
+			}
+
+			number, err := netlink.ConntrackDeleteIPSrc(netlink.ConntrackTable, family, src, proto, port, mark, true)
+			if err != nil {
+				log.Println("ConntrackDeleteIPSrc error:", err)
+			} else {
+				fmt.Printf("ConntrackDeleteIPSrc: deleted %d flow\n", number)
+			}
+			return
+		}
+		fmt.Println("Usage: Conntrack -D <-s IP-Address> [-p Protocol][-P port][-m Mark][-f ipv6]")
+		return
+	}
 	// XXX args := flag.Args()
 	// XXX curpart := *curpartPtr
 	res, err := netlink.ConntrackTableList(netlink.ConntrackTable, syscall.AF_INET)
