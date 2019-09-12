9a1be064882ed443b37cbb5b4076c27471b6cf48
GopiKrishna Kodali <gkodali@zededa.com>
2019-09-12 09:21:42 -0700

    Go vendored the latest netlink changes
    Signed-off-by: GopiKrishna Kodali <gkodali@zededa.com>

--- pkg/pillar/vendor/github.com/eriknordmark/netlink/conntrack_linux.go
+++ pkg/pillar/vendor/github.com/eriknordmark/netlink/conntrack_linux.go
@@ -140,8 +140,9 @@ func (h *Handle) ConntrackDeleteIPSrc(table ConntrackTableType,
 	var flownum int
 	for _, dataRaw := range res {
 		flow := parseRawData(dataRaw)
-		if (strings.Compare(flow.Forward.SrcIP.String(), addr.String()) == 0 ||
-			strings.Compare(flow.Reverse.SrcIP.String(), addr.String()) == 0) &&
+		if (addr.IsUnspecified() ||
+			addr.Equal(flow.Forward.SrcIP) ||
+			addr.Equal(flow.Reverse.SrcIP)) &&
 			(proto == 0 || flow.Forward.Protocol == proto) &&
 			(mark & markMask == 0 || flow.Mark & markMask == mark & markMask) &&
 			(port == 0 || flow.Forward.SrcPort == port || flow.Reverse.SrcPort == port) {
