1b78040a892b8d42f2cd91d34a7d1f78353c37d3
Hariharasubramanian C S <cshari@zededa.com>
2019-09-16 18:38:49 +0530

    Export status of Data Security At Rest from EVE to EVC
    Signed-off-by: Hariharasubramanian C S <cshari@zededa.com>

--- pkg/pillar/cmd/vaultmgr/vaultmgr.go
+++ pkg/pillar/cmd/vaultmgr/vaultmgr.go
@@ -12,6 +12,7 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	"github.com/lf-edge/eve/api/go/info"
 	"github.com/lf-edge/eve/pkg/pillar/agentlog"
 	"github.com/lf-edge/eve/pkg/pillar/cmd/tpmmgr"
 	log "github.com/sirupsen/logrus"
@@ -19,6 +20,7 @@ import (
 
 const (
 	fscryptPath     = "/opt/zededa/bin/fscrypt"
+	fscryptConfFile = "/etc/fscrypt.conf"
 	keyctlPath      = "/bin/keyctl"
 	mountPoint      = "/persist/"
 	defaultImgVault = "/persist/img"
@@ -267,6 +269,29 @@ func setupFscryptEnv() error {
 		return handleFirstUse()
 	}
 	return nil
+}
+
+//GetOperInfo gets the current operational state of fscrypt
+func GetOperInfo() (info.DataSecAtRestStatus, string) {
+	_, err := os.Stat(fscryptConfFile)
+	if err == nil {
+		if _, _, err := execCmd(fscryptPath, statusParams...); err != nil {
+			//fscrypt is setup, but not being used
+			log.Debug("Setting status to Error")
+			return info.DataSecAtRestStatus_DATASEC_AT_REST_ERROR,
+				"Disk Encryption Initialization failure"
+		} else {
+			//fscrypt is setup, and being used on /persist
+			log.Debug("Setting status to Enabled")
+			return info.DataSecAtRestStatus_DATASEC_AT_REST_ENABLED,
+				"Disk Encryption is Active"
+		}
+	} else {
+		//This is due to ext3 partition
+		log.Debug("Setting status to Disabled")
+		return info.DataSecAtRestStatus_DATASEC_AT_REST_DISABLED,
+			"File system is incompatible - Needs a disruptive upgrade"
+	}
 }
 
 //Run is the entrypoint for running vaultmgr as a standalone program
