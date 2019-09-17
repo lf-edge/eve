ab0c4d84abdc2de6baacbadd4b7d768b1bf2618b
Archishmaan Peyyety <archishou@gmail.com>
2019-09-17 16:37:45 -0400

    added base type
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- pkg/pillar/pubsubtypes/verifer_types.go
+++ pkg/pillar/pubsubtypes/verifer_types.go
@@ -1,5 +1,37 @@
+package pubsubtypes
+
+import (
+	"github.com/lf-edge/eve/api/go/config"
+	"github.com/lf-edge/eve/pkg/pillar/pubsub"
+	"github.com/lf-edge/eve/pkg/pillar/types"
+)
+
+type CallbackFn func() bool
 
 type AppInstanceConfigPubSubBase struct {
 	agentName string
-	db        map[string]AppInstanceConfig
+	db        map[string]types.AppInstanceConfig
+}
+
+func (a AppInstanceConfigPubSubBase) GetItem(string key) {
+	return db[key]
+}
+
+func (a AppInstanceConfigPubSubBase) SetItem(string key, types.AppInstanceConfig appInstanceConfig) {
+	db[key] = appInstanceConfig
+	pubAppInstanceConfig, err := pubsub.Publish(agentName,
+  	types.AppInstanceConfig{})
+ 	if err != nil {
+  		log.Fatal(err)
+  	}
+}
+
+
+func (a AppInstanceConfigPubSubBase) IterateDb(func CallbackFn)
+	for key,value range := a.db {
+	   retval := CallbackFn( value)
+	   If retval == false {
+	      break
+	   }
+	}
 }
