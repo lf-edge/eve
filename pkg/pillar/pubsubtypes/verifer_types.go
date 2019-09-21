a722b0a660b2a63df263210fd96d6676bebe2c49
Archishmaan Peyyety <archishou@gmail.com>
2019-09-21 16:38:28 -0400

    fixed most of the go vet issues with /pubsubtypes
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- pkg/pillar/pubsubtypes/verifer_types.go
+++ pkg/pillar/pubsubtypes/verifer_types.go
@@ -3,6 +3,7 @@ package pubsubtypes
 import (
 	"github.com/lf-edge/eve/pkg/pillar/pubsub"
 	"github.com/lf-edge/eve/pkg/pillar/types"
+	"log"
 )
 
 type CallbackFn func(types.AppInstanceConfig) bool
@@ -12,25 +13,32 @@ type AppInstanceConfigPubSubBase struct {
 	db        map[string]types.AppInstanceConfig
 }
 
-func (a AppInstanceConfigPubSubBase) GetItem(string key) {
-	return db[key]
+func (a AppInstanceConfigPubSubBase) GetItem(key string) types.AppInstanceConfig {
+	return a.db[key]
 }
 
-func (a AppInstanceConfigPubSubBase) SetItem(string key, types.AppInstanceConfig appInstanceConfig) {
-	db[key] = appInstanceConfig
-	pubAppInstanceConfig, err := pubsub.Publish(agentName,
+func (a AppInstanceConfigPubSubBase) SetItem(key string, appInstanceConfig types.AppInstanceConfig) {
+	a.db[key] = appInstanceConfig
+	pubAppInstanceConfig, err := pubsub.Publish(a.agentName,
   	types.AppInstanceConfig{})
  	if err != nil {
   		log.Fatal(err)
   	}
 }
 
-
-func (a AppInstanceConfigPubSubBase) IterateDb(func CallbackFn)
-	for key,value range := a.db {
-	   retval := CallbackFn(value)
+func (a AppInstanceConfigPubSubBase) IterateDb(fn CallbackFn) {
+	for _, value := range a.db {
+	   retval := fn(value)
 	   if retval == false {
 	      break
 	   }
 	}
+}
+
+type AppInstanceConfigPub struct {
+	base 	AppInstanceConfigPubSubBase
+}
+
+func (pub AppInstanceConfigPub) Publish() {
+	pubsub.Publish(pub.base.agentName, types.AppInstanceConfig{})
 }
