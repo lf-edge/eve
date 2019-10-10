3e904fc5a390ab61495f3247a5aa7d0ed9a6ae3b
Archishmaan Peyyety <archishou@gmail.com>
2019-10-10 11:35:05 -0400

    resolved comments
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com> (+4 squashed commits)
    Squashed commits:
    [cc05c8f] fixed comments
    
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>
    [39bed0b] fix yetus errors
    
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>
    [03084af] gofmt
    
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>
    [f11eaef] removed changes to .gitignore
    
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- pkg/pillar/pubsubtypes/verifer_types.go
+++ pkg/pillar/pubsubtypes/verifer_types.go
@@ -6,38 +6,49 @@ import (
 	"log"
 )
 
+//CallbackFn : used in the iterate function to perform a function on the data
 type CallbackFn func(types.AppInstanceConfig) bool
 
+//AppInstanceConfigPubSubBase : stores data that both the pub and sub structures will need to use
 type AppInstanceConfigPubSubBase struct {
 	agentName string
 	db        map[string]types.AppInstanceConfig
 }
 
-func (config * AppInstanceConfigPubSubBase) Get(key string) types.AppInstanceConfig {
-	return config.db[key]
-}
-
-func (config * AppInstanceConfigPubSubBase) Set(key string, appInstanceConfig types.AppInstanceConfig) {
-	config.db[key] = appInstanceConfig
-	_, err := pubsub.Publish(config.agentName, types.AppInstanceConfig{})
- 	if err != nil {
-  		log.Fatal(err)
-  	}
-}
-
-func (config * AppInstanceConfigPubSubBase) IterateDb(fn CallbackFn) {
-	for _, value := range config.db {
-	   retval := fn(value)
-	   if retval == false {
-	      break
-	   }
+//Get : returns the data associated by a specific key in the db
+func (config *AppInstanceConfigPubSubBase) Get(key string) types.AppInstanceConfig {
+	if res, ok := config.db[key]; ok {
+		return res
+	} else {
+		return types.AppInstanceConfig{}
 	}
 }
 
-type AppInstanceConfigPub struct {
-	base 	AppInstanceConfigPubSubBase
+//Set : set a key to be associated with an appInstanceConfig
+func (config *AppInstanceConfigPubSubBase) Set(key string, appInstanceConfig types.AppInstanceConfig) {
+	config.db[key] = appInstanceConfig
+	_, err := pubsub.Publish(config.agentName, types.AppInstanceConfig{})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func (pub * AppInstanceConfigPub) Publish() {
+//IterateDb : perfroms CallbackFn on the dataset
+func (config *AppInstanceConfigPubSubBase) IterateDb(fn CallbackFn) {
+	for _, value := range config.db {
+		retval := fn(value)
+		if retval == false {
+			break
+		}
+	}
+}
+
+//AppInstanceConfigPub : has a publish function to publish AppInstanceConfig
+type AppInstanceConfigPub struct {
+	base AppInstanceConfigPubSubBase
+}
+
+//AppInstanceConfigPub : the publish function which takes data from base and publishes it
+func (pub *AppInstanceConfigPub) Publish() {
 	pubsub.Publish(pub.base.agentName, types.AppInstanceConfig{})
 }
