e0135b2dd047e66144d43c8c545fd3e3bc1caca7
Archishmaan Peyyety <archishou@gmail.com>
2019-10-10 11:29:32 -0400

    go fmt
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- pkg/pillar/pubsubtypes/verifer_types.go
+++ pkg/pillar/pubsubtypes/verifer_types.go
@@ -13,31 +13,31 @@ type AppInstanceConfigPubSubBase struct {
 	db        map[string]types.AppInstanceConfig
 }
 
-func (config * AppInstanceConfigPubSubBase) Get(key string) types.AppInstanceConfig {
+func (config *AppInstanceConfigPubSubBase) Get(key string) types.AppInstanceConfig {
 	return config.db[key]
 }
 
-func (config * AppInstanceConfigPubSubBase) Set(key string, appInstanceConfig types.AppInstanceConfig) {
+func (config *AppInstanceConfigPubSubBase) Set(key string, appInstanceConfig types.AppInstanceConfig) {
 	config.db[key] = appInstanceConfig
 	_, err := pubsub.Publish(config.agentName, types.AppInstanceConfig{})
- 	if err != nil {
-  		log.Fatal(err)
-  	}
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func (config * AppInstanceConfigPubSubBase) IterateDb(fn CallbackFn) {
+func (config *AppInstanceConfigPubSubBase) IterateDb(fn CallbackFn) {
 	for _, value := range config.db {
-	   retval := fn(value)
-	   if retval == false {
-	      break
-	   }
+		retval := fn(value)
+		if retval == false {
+			break
+		}
 	}
 }
 
 type AppInstanceConfigPub struct {
-	base 	AppInstanceConfigPubSubBase
+	base AppInstanceConfigPubSubBase
 }
 
-func (pub * AppInstanceConfigPub) Publish() {
+func (pub *AppInstanceConfigPub) Publish() {
 	pubsub.Publish(pub.base.agentName, types.AppInstanceConfig{})
 }
