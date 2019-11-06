76f407f9a3071adb373b5fdaf2c8ecbdc4f00002
Roman Shaposhnik <rvs@zededa.com>
2019-11-06 13:35:38 -0800

    Making the script more reliable (esp. on Linux)
    Signed-off-by: Roman Shaposhnik <rvs@zededa.com>

--- tools/makeusbconf.sh
+++ tools/makeusbconf.sh
@@ -19,6 +19,12 @@ bail() {
     exit 1
 }
 
+silent() {
+    if ! OUT=$("$@" 2>&1) ; then
+       bail "ERROR: $* $OUT"
+    fi
+}
+
 SIZE=204800
 TMPDIR=$(mktemp -d)
 
@@ -34,9 +40,17 @@ done
 
 shift $((OPTIND-1))
 [ $# != 1 ] && bail "$USAGE"
+[ -z "$(ls -A "$TMPDIR")" ] && bail "ERROR: one of the -d -i or -f has to be given"
+
 
 IMAGE="$1"
-[ -e "$IMAGE" ] || dd if=/dev/zero of="$IMAGE" seek="$SIZE" bs=1024 count=0
+if [ -b "$IMAGE" ] ; then
+   [ "$(uname -s)" = Linux ] || bail "ERROR: writing directly to the device is only supported on Linux"
+   IMAGE_BIND_OPT="--device"
+else
+   [ -e "$IMAGE" ] || silent dd if=/dev/zero of="$IMAGE" seek="$SIZE" bs=1024 count=0
+   IMAGE_BIND_OPT="-v"
+fi
 
 # Docker, for unknown reasons, decides whether a passed bind mount is
 # a file or a directory based on whether is a absolute pathname or a
@@ -50,8 +64,4 @@ case "$IMAGE" in
     *) IMAGE="$PWD/$IMAGE";;
 esac
 
-if OUT=$( (cd "$TMPDIR" || exit 1; tar cvf - ./*) | docker run -v "$IMAGE:/output.img" -i "${MKFLASH_TAG}" /output.img usb_conf 2>&1) ; then
-   cleanup
-else
-   bail "ERROR: $OUT"
-fi
+(cd "$TMPDIR" || exit 1; tar cf - ./*) | docker run "$IMAGE_BIND_OPT" "$IMAGE:/output.img" -i "${MKFLASH_TAG}" /output.img usb_conf
