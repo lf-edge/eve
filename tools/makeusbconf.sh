cf8c593c71f0f1059ba5c43e2dc3eae66e60e8e1
eriknordmark <erik@zededa.com>
2019-11-07 18:43:01 +0100

    made makeusbconf work against /dev/ on Linux
    Signed-off-by: eriknordmark <erik@zededa.com>

--- tools/makeusbconf.sh
+++ tools/makeusbconf.sh
@@ -5,7 +5,8 @@
 #
 USAGE="Usage: $0 [-d] [-i] [-s <size in Kb>] [-f <file> ] <output.img>"
 
-MKFLASH_TAG="$(linuxkit pkg show-tag pkg/mkimage-raw-efi)"
+LINUXKIT=$(pwd)/build-tools/bin/linuxkit
+MKFLASH_TAG="$($LINUXKIT pkg show-tag pkg/mkimage-raw-efi)"
 
 cleanup() {
     rm "$TMPDIR"/* 2>/dev/null
