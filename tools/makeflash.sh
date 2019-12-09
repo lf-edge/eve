7f762a871fa5c33bec6d40a117ab3ca76e1be68f
Roman Shaposhnik <rvs@zededa.com>
2019-12-09 15:01:41 -0800

    More yetus feedback
    Signed-off-by: Roman Shaposhnik <rvs@zededa.com>

--- tools/makeflash.sh
+++ tools/makeflash.sh
@@ -6,16 +6,16 @@
 MKFLASH_TAG="$(linuxkit pkg show-tag pkg/mkimage-raw-efi)"
 
 if [ "$1" = "-C" ]; then
-    SIZE=$2
-    dd if=/dev/zero of=$4 seek=$(( $SIZE * 1024 * 1024 - 1)) bs=1 count=1
+    SIZE="$2"
+    dd if=/dev/zero of="$4" seek=$(( SIZE * 1024 * 1024 - 1)) bs=1 count=1
     # If we're a non-root user, the bind mount gets permissions sensitive.
     # So we go docker^Wcowboy style
-    chmod ugo+w $4
+    chmod ugo+w "$4"
     shift 2
 fi
 
-SOURCE="$(cd $1 && pwd)"
-IMAGE="$(cd $(dirname $2) && pwd)/$(basename $2)"
+SOURCE="$(cd "$1" && pwd)"
+IMAGE="$(cd "$(dirname "$2")" && pwd)/$(basename "$2")"
 shift 2
 
-docker run -v "$SOURCE:/bits" -v $IMAGE:/output.img ${MKFLASH_TAG} /output.img "$@"
+docker run -v "$SOURCE:/bits" -v "$IMAGE:/output.img" "$MKFLASH_TAG" /output.img "$@"
