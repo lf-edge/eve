3d947d7cd1af6d0c23458cdcba3f5ffb94bad953
Roman Shaposhnik <rvs@zededa.com>
2019-12-09 15:01:41 -0800

    More yetus feedback
    Signed-off-by: Roman Shaposhnik <rvs@zededa.com>

--- tools/makeiso.sh
+++ tools/makeiso.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 MKIMAGE_TAG="$(linuxkit pkg show-tag pkg/mkimage-iso-efi)"
-SOURCE="$(cd $1 && pwd)"
-ISO="$(cd $(dirname $2) && pwd)/$(basename $2)"
+SOURCE="$(cd "$1" && pwd)"
+ISO="$(cd "$(dirname "$2")" && pwd)/$(basename "$2")"
 
-if [ ! -d "$SOURCE" -o $# -ne 2 ]; then
+if [ ! -d "$SOURCE" ] || [ $# -ne 2 ]; then
    echo "Usage: $0 <input dir> <output iso image file>"
    exit 1
 fi
 
 touch "$ISO"
-docker run -t -v "$SOURCE:/bits" -v "$ISO:/output.iso" -i ${MKIMAGE_TAG}
+docker run -t -v "$SOURCE:/bits" -v "$ISO:/output.iso" -i "$MKIMAGE_TAG"
