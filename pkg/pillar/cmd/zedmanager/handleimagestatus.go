8e12f508b728b1c50fa416064487b7f62e7f27a2
eriknordmark <erik@zededa.com>
2020-01-08 13:14:09 +0100

    fixed mismerge
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/pillar/cmd/zedmanager/handleimagestatus.go
+++ pkg/pillar/cmd/zedmanager/handleimagestatus.go
@@ -4,7 +4,6 @@
 package zedmanager
 
 import (
-	"github.com/lf-edge/eve/pkg/pillar/cast"
 	"github.com/lf-edge/eve/pkg/pillar/types"
 	uuid "github.com/satori/go.uuid"
 	log "github.com/sirupsen/logrus"
@@ -16,7 +15,7 @@ func lookupImageStatusForApp(
 
 	imageStatusList := ctx.subImageStatus.GetAll()
 	for _, item := range imageStatusList {
-		status := cast.CastImageStatus(item)
+		status := item.(types.ImageStatus)
 		if status.AppInstUUID == appUUID && status.ImageSha256 == imageSha {
 			log.Debugf("lookupImageStatusForApp: IS found. appUUID: %s, "+
 				"imageSha: %s", appUUID.String(), imageSha)
