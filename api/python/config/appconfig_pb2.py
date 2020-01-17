d364b2e5446c7516565afd0e5e4677b38cfcfa64
Kalyan Nidumolu <kalyan@zadeda.com>
2020-01-17 15:20:25 -0800

    Generated files for ImageRefresh command in App Instance Config.
    Signed-off-by: Kalyan Nidumolu <kalyan@zadeda.com>

--- api/python/config/appconfig_pb2.py
+++ api/python/config/appconfig_pb2.py
@@ -23,7 +23,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=_b('\n\037com.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/config'),
-  serialized_pb=_b('\n\x0f\x61ppconfig.proto\x1a\x0f\x64\x65vcommon.proto\x1a\rstorage.proto\x1a\x08vm.proto\x1a\x0fnetconfig.proto\"2\n\x0eInstanceOpsCmd\x12\x0f\n\x07\x63ounter\x18\x02 \x01(\r\x12\x0f\n\x07opsTime\x18\x04 \x01(\t\"\xca\x02\n\x11\x41ppInstanceConfig\x12\'\n\x0euuidandversion\x18\x01 \x01(\x0b\x32\x0f.UUIDandVersion\x12\x13\n\x0b\x64isplayname\x18\x02 \x01(\t\x12!\n\x0e\x66ixedresources\x18\x03 \x01(\x0b\x32\t.VmConfig\x12\x16\n\x06\x64rives\x18\x04 \x03(\x0b\x32\x06.Drive\x12\x10\n\x08\x61\x63tivate\x18\x05 \x01(\x08\x12#\n\ninterfaces\x18\x06 \x03(\x0b\x32\x0f.NetworkAdapter\x12\x1a\n\x08\x61\x64\x61pters\x18\x07 \x03(\x0b\x32\x08.Adapter\x12 \n\x07restart\x18\t \x01(\x0b\x32\x0f.InstanceOpsCmd\x12\x1e\n\x05purge\x18\n \x01(\x0b\x32\x0f.InstanceOpsCmd\x12\x10\n\x08userData\x18\x0b \x01(\t\x12\x15\n\rremoteConsole\x18\x0c \x01(\x08\x42G\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
+  serialized_pb=_b('\n\x0f\x61ppconfig.proto\x1a\x0f\x64\x65vcommon.proto\x1a\rstorage.proto\x1a\x08vm.proto\x1a\x0fnetconfig.proto\"2\n\x0eInstanceOpsCmd\x12\x0f\n\x07\x63ounter\x18\x02 \x01(\r\x12\x0f\n\x07opsTime\x18\x04 \x01(\t\"\xf1\x02\n\x11\x41ppInstanceConfig\x12\'\n\x0euuidandversion\x18\x01 \x01(\x0b\x32\x0f.UUIDandVersion\x12\x13\n\x0b\x64isplayname\x18\x02 \x01(\t\x12!\n\x0e\x66ixedresources\x18\x03 \x01(\x0b\x32\t.VmConfig\x12\x16\n\x06\x64rives\x18\x04 \x03(\x0b\x32\x06.Drive\x12\x10\n\x08\x61\x63tivate\x18\x05 \x01(\x08\x12#\n\ninterfaces\x18\x06 \x03(\x0b\x32\x0f.NetworkAdapter\x12\x1a\n\x08\x61\x64\x61pters\x18\x07 \x03(\x0b\x32\x08.Adapter\x12 \n\x07restart\x18\t \x01(\x0b\x32\x0f.InstanceOpsCmd\x12\x1e\n\x05purge\x18\n \x01(\x0b\x32\x0f.InstanceOpsCmd\x12\x10\n\x08userData\x18\x0b \x01(\t\x12\x15\n\rremoteConsole\x18\x0c \x01(\x08\x12%\n\x0crefreshImage\x18\r \x01(\x0b\x32\x0f.InstanceOpsCmdBG\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
   ,
   dependencies=[devcommon__pb2.DESCRIPTOR,storage__pb2.DESCRIPTOR,vm__pb2.DESCRIPTOR,netconfig__pb2.DESCRIPTOR,])
 
@@ -152,6 +152,13 @@ _APPINSTANCECONFIG = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='refreshImage', full_name='AppInstanceConfig.refreshImage', index=11,
+      number=13, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -165,7 +172,7 @@ _APPINSTANCECONFIG = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=131,
-  serialized_end=461,
+  serialized_end=500,
 )
 
 _APPINSTANCECONFIG.fields_by_name['uuidandversion'].message_type = devcommon__pb2._UUIDANDVERSION
@@ -175,6 +182,7 @@ _APPINSTANCECONFIG.fields_by_name['interfaces'].message_type = netconfig__pb2._N
 _APPINSTANCECONFIG.fields_by_name['adapters'].message_type = devcommon__pb2._ADAPTER
 _APPINSTANCECONFIG.fields_by_name['restart'].message_type = _INSTANCEOPSCMD
 _APPINSTANCECONFIG.fields_by_name['purge'].message_type = _INSTANCEOPSCMD
+_APPINSTANCECONFIG.fields_by_name['refreshImage'].message_type = _INSTANCEOPSCMD
 DESCRIPTOR.message_types_by_name['InstanceOpsCmd'] = _INSTANCEOPSCMD
 DESCRIPTOR.message_types_by_name['AppInstanceConfig'] = _APPINSTANCECONFIG
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
