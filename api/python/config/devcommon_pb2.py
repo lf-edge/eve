d11e07ccd227c2c2df1220df2c9fb9037f592adb
Srinibas Maharana <srinibas@zededa.com>
2020-01-19 20:59:20 +0530

    yetus fixes
    Signed-off-by: Srinibas Maharana <srinibas@zededa.com>

--- api/python/config/devcommon_pb2.py
+++ api/python/config/devcommon_pb2.py
@@ -3,6 +3,7 @@
 
 import sys
 _b=sys.version_info[0]<3 and (lambda x:x) or (lambda x:x.encode('latin1'))
+from google.protobuf.internal import enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
@@ -20,10 +21,60 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=_b('\n\037com.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/config'),
-  serialized_pb=_b('\n\x0f\x64\x65vcommon.proto\x1a\x0e\x64\x65vmodel.proto\"/\n\x0eUUIDandVersion\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\"F\n\x0c\x44\x65viceOpsCmd\x12\x0f\n\x07\x63ounter\x18\x02 \x01(\r\x12\x14\n\x0c\x64\x65siredState\x18\x03 \x01(\x08\x12\x0f\n\x07opsTime\x18\x04 \x01(\t\"(\n\nConfigItem\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\"1\n\x07\x41\x64\x61pter\x12\x18\n\x04type\x18\x01 \x01(\x0e\x32\n.PhyIoType\x12\x0c\n\x04name\x18\x02 \x01(\tBG\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
+  serialized_pb=_b('\n\x0f\x64\x65vcommon.proto\x1a\x0e\x64\x65vmodel.proto\"/\n\x0eUUIDandVersion\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\"F\n\x0c\x44\x65viceOpsCmd\x12\x0f\n\x07\x63ounter\x18\x02 \x01(\r\x12\x14\n\x0c\x64\x65siredState\x18\x03 \x01(\x08\x12\x0f\n\x07opsTime\x18\x04 \x01(\t\"(\n\nConfigItem\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\"1\n\x07\x41\x64\x61pter\x12\x18\n\x04type\x18\x01 \x01(\x0e\x32\n.PhyIoType\x12\x0c\n\x04name\x18\x02 \x01(\t\"\xc1\x01\n\nCipherInfo\x12\n\n\x02id\x18\x64 \x01(\t\x12-\n\x11keyExchangeScheme\x18\x01 \x01(\x0e\x32\x12.KeyExchangeScheme\x12+\n\x10\x65ncryptionScheme\x18\x02 \x01(\x0e\x32\x11.EncryptionScheme\x12\x14\n\x0cinitialValue\x18\x03 \x01(\x0c\x12\x12\n\npublicCert\x18\x04 \x01(\x0c\x12\x0e\n\x06sha256\x18\x06 \x01(\t\x12\x11\n\tsignature\x18\x05 \x01(\x0c*/\n\x11KeyExchangeScheme\x12\x0c\n\x08KEA_NONE\x10\x00\x12\x0c\n\x08KEA_ECDH\x10\x01*3\n\x10\x45ncryptionScheme\x12\x0b\n\x07SA_NONE\x10\x00\x12\x12\n\x0eSA_AES_256_CFB\x10\x01\x42G\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
   ,
   dependencies=[devmodel__pb2.DESCRIPTOR,])
 
+_KEYEXCHANGESCHEME = _descriptor.EnumDescriptor(
+  name='KeyExchangeScheme',
+  full_name='KeyExchangeScheme',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='KEA_NONE', index=0, number=0,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='KEA_ECDH', index=1, number=1,
+      serialized_options=None,
+      type=None),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=445,
+  serialized_end=492,
+)
+_sym_db.RegisterEnumDescriptor(_KEYEXCHANGESCHEME)
+
+KeyExchangeScheme = enum_type_wrapper.EnumTypeWrapper(_KEYEXCHANGESCHEME)
+_ENCRYPTIONSCHEME = _descriptor.EnumDescriptor(
+  name='EncryptionScheme',
+  full_name='EncryptionScheme',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='SA_NONE', index=0, number=0,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='SA_AES_256_CFB', index=1, number=1,
+      serialized_options=None,
+      type=None),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=494,
+  serialized_end=545,
+)
+_sym_db.RegisterEnumDescriptor(_ENCRYPTIONSCHEME)
+
+EncryptionScheme = enum_type_wrapper.EnumTypeWrapper(_ENCRYPTIONSCHEME)
+KEA_NONE = 0
+KEA_ECDH = 1
+SA_NONE = 0
+SA_AES_256_CFB = 1
 
 
 
@@ -185,11 +236,89 @@ _ADAPTER = _descriptor.Descriptor(
   serialized_end=247,
 )
 
+
+_CIPHERINFO = _descriptor.Descriptor(
+  name='CipherInfo',
+  full_name='CipherInfo',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='id', full_name='CipherInfo.id', index=0,
+      number=100, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='keyExchangeScheme', full_name='CipherInfo.keyExchangeScheme', index=1,
+      number=1, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='encryptionScheme', full_name='CipherInfo.encryptionScheme', index=2,
+      number=2, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='initialValue', full_name='CipherInfo.initialValue', index=3,
+      number=3, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b(""),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='publicCert', full_name='CipherInfo.publicCert', index=4,
+      number=4, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b(""),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='sha256', full_name='CipherInfo.sha256', index=5,
+      number=6, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='signature', full_name='CipherInfo.signature', index=6,
+      number=5, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b(""),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=250,
+  serialized_end=443,
+)
+
 _ADAPTER.fields_by_name['type'].enum_type = devmodel__pb2._PHYIOTYPE
+_CIPHERINFO.fields_by_name['keyExchangeScheme'].enum_type = _KEYEXCHANGESCHEME
+_CIPHERINFO.fields_by_name['encryptionScheme'].enum_type = _ENCRYPTIONSCHEME
 DESCRIPTOR.message_types_by_name['UUIDandVersion'] = _UUIDANDVERSION
 DESCRIPTOR.message_types_by_name['DeviceOpsCmd'] = _DEVICEOPSCMD
 DESCRIPTOR.message_types_by_name['ConfigItem'] = _CONFIGITEM
 DESCRIPTOR.message_types_by_name['Adapter'] = _ADAPTER
+DESCRIPTOR.message_types_by_name['CipherInfo'] = _CIPHERINFO
+DESCRIPTOR.enum_types_by_name['KeyExchangeScheme'] = _KEYEXCHANGESCHEME
+DESCRIPTOR.enum_types_by_name['EncryptionScheme'] = _ENCRYPTIONSCHEME
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 UUIDandVersion = _reflection.GeneratedProtocolMessageType('UUIDandVersion', (_message.Message,), dict(
@@ -219,6 +348,13 @@ Adapter = _reflection.GeneratedProtocolMessageType('Adapter', (_message.Message,
   # @@protoc_insertion_point(class_scope:Adapter)
   ))
 _sym_db.RegisterMessage(Adapter)
+
+CipherInfo = _reflection.GeneratedProtocolMessageType('CipherInfo', (_message.Message,), dict(
+  DESCRIPTOR = _CIPHERINFO,
+  __module__ = 'devcommon_pb2'
+  # @@protoc_insertion_point(class_scope:CipherInfo)
+  ))
+_sym_db.RegisterMessage(CipherInfo)
 
 
 DESCRIPTOR._options = None
--- api/python/config/storage_pb2.py
+++ api/python/config/storage_pb2.py
@@ -14,7 +14,6 @@ _sym_db = _symbol_database.Default()
 
 
 import devcommon_pb2 as devcommon__pb2
-import cipherinfo_pb2 as cipherinfo__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
@@ -22,9 +21,9 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=_b('\n\037com.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/config'),
-  serialized_pb=_b('\n\rstorage.proto\x1a\x0f\x64\x65vcommon.proto\x1a\x10\x63ipherinfo.proto\"P\n\rSignatureInfo\x12\x15\n\rintercertsurl\x18\x01 \x01(\t\x12\x15\n\rsignercerturl\x18\x02 \x01(\t\x12\x11\n\tsignature\x18\x03 \x01(\x0c\"\xbc\x01\n\x0f\x44\x61tastoreConfig\x12\n\n\x02id\x18\x64 \x01(\t\x12\x16\n\x05\x64Type\x18\x01 \x01(\x0e\x32\x07.DsType\x12\x0c\n\x04\x66qdn\x18\x02 \x01(\t\x12\x0e\n\x06\x61piKey\x18\x03 \x01(\t\x12\x10\n\x08password\x18\x04 \x01(\t\x12\r\n\x05\x64path\x18\x05 \x01(\t\x12\x0e\n\x06region\x18\x06 \x01(\t\x12\x1a\n\x12\x63ipherTextPassword\x18\x07 \x01(\x0c\x12\x1a\n\x05\x63Info\x18\x08 \x01(\x0b\x32\x0b.CipherInfo\"\xaa\x01\n\x05Image\x12\'\n\x0euuidandversion\x18\x01 \x01(\x0b\x32\x0f.UUIDandVersion\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0e\n\x06sha256\x18\x03 \x01(\t\x12\x18\n\x07iformat\x18\x04 \x01(\x0e\x32\x07.Format\x12\x1f\n\x07siginfo\x18\x05 \x01(\x0b\x32\x0e.SignatureInfo\x12\x0c\n\x04\x64sId\x18\x06 \x01(\t\x12\x11\n\tsizeBytes\x18\x08 \x01(\x03\"\x8e\x01\n\x05\x44rive\x12\x15\n\x05image\x18\x01 \x01(\x0b\x32\x06.Image\x12\x10\n\x08readonly\x18\x05 \x01(\x08\x12\x10\n\x08preserve\x18\x06 \x01(\x08\x12\x1b\n\x07\x64rvtype\x18\x08 \x01(\x0e\x32\n.DriveType\x12\x17\n\x06target\x18\t \x01(\x0e\x32\x07.Target\x12\x14\n\x0cmaxsizebytes\x18\n \x01(\x03*p\n\x06\x44sType\x12\r\n\tDsUnknown\x10\x00\x12\n\n\x06\x44sHttp\x10\x01\x12\x0b\n\x07\x44sHttps\x10\x02\x12\x08\n\x04\x44sS3\x10\x03\x12\n\n\x06\x44sSFTP\x10\x04\x12\x17\n\x13\x44sContainerRegistry\x10\x05\x12\x0f\n\x0b\x44sAzureBlob\x10\x06*k\n\x06\x46ormat\x12\x0e\n\nFmtUnknown\x10\x00\x12\x07\n\x03RAW\x10\x01\x12\x08\n\x04QCOW\x10\x02\x12\t\n\x05QCOW2\x10\x03\x12\x07\n\x03VHD\x10\x04\x12\x08\n\x04VMDK\x10\x05\x12\x07\n\x03OVA\x10\x06\x12\x08\n\x04VHDX\x10\x07\x12\r\n\tCONTAINER\x10\x08*G\n\x06Target\x12\x0e\n\nTgtUnknown\x10\x00\x12\x08\n\x04\x44isk\x10\x01\x12\n\n\x06Kernel\x10\x02\x12\n\n\x06Initrd\x10\x03\x12\x0b\n\x07RamDisk\x10\x04*I\n\tDriveType\x12\x10\n\x0cUnclassified\x10\x00\x12\t\n\x05\x43\x44ROM\x10\x01\x12\x07\n\x03HDD\x10\x02\x12\x07\n\x03NET\x10\x03\x12\r\n\tHDD_EMPTY\x10\x04\x42G\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
+  serialized_pb=_b('\n\rstorage.proto\x1a\x0f\x64\x65vcommon.proto\"P\n\rSignatureInfo\x12\x15\n\rintercertsurl\x18\x01 \x01(\t\x12\x15\n\rsignercerturl\x18\x02 \x01(\t\x12\x11\n\tsignature\x18\x03 \x01(\x0c\"\xa0\x01\n\x0f\x44\x61tastoreConfig\x12\n\n\x02id\x18\x64 \x01(\t\x12\x16\n\x05\x64Type\x18\x01 \x01(\x0e\x32\x07.DsType\x12\x0c\n\x04\x66qdn\x18\x02 \x01(\t\x12\x0e\n\x06\x61piKey\x18\x03 \x01(\t\x12\x10\n\x08password\x18\x04 \x01(\t\x12\r\n\x05\x64path\x18\x05 \x01(\t\x12\x0e\n\x06region\x18\x06 \x01(\t\x12\x1a\n\x12\x63ipherTextPassword\x18\x07 \x01(\x0c\"\xaa\x01\n\x05Image\x12\'\n\x0euuidandversion\x18\x01 \x01(\x0b\x32\x0f.UUIDandVersion\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0e\n\x06sha256\x18\x03 \x01(\t\x12\x18\n\x07iformat\x18\x04 \x01(\x0e\x32\x07.Format\x12\x1f\n\x07siginfo\x18\x05 \x01(\x0b\x32\x0e.SignatureInfo\x12\x0c\n\x04\x64sId\x18\x06 \x01(\t\x12\x11\n\tsizeBytes\x18\x08 \x01(\x03\"\x8e\x01\n\x05\x44rive\x12\x15\n\x05image\x18\x01 \x01(\x0b\x32\x06.Image\x12\x10\n\x08readonly\x18\x05 \x01(\x08\x12\x10\n\x08preserve\x18\x06 \x01(\x08\x12\x1b\n\x07\x64rvtype\x18\x08 \x01(\x0e\x32\n.DriveType\x12\x17\n\x06target\x18\t \x01(\x0e\x32\x07.Target\x12\x14\n\x0cmaxsizebytes\x18\n \x01(\x03*p\n\x06\x44sType\x12\r\n\tDsUnknown\x10\x00\x12\n\n\x06\x44sHttp\x10\x01\x12\x0b\n\x07\x44sHttps\x10\x02\x12\x08\n\x04\x44sS3\x10\x03\x12\n\n\x06\x44sSFTP\x10\x04\x12\x17\n\x13\x44sContainerRegistry\x10\x05\x12\x0f\n\x0b\x44sAzureBlob\x10\x06*k\n\x06\x46ormat\x12\x0e\n\nFmtUnknown\x10\x00\x12\x07\n\x03RAW\x10\x01\x12\x08\n\x04QCOW\x10\x02\x12\t\n\x05QCOW2\x10\x03\x12\x07\n\x03VHD\x10\x04\x12\x08\n\x04VMDK\x10\x05\x12\x07\n\x03OVA\x10\x06\x12\x08\n\x04VHDX\x10\x07\x12\r\n\tCONTAINER\x10\x08*G\n\x06Target\x12\x0e\n\nTgtUnknown\x10\x00\x12\x08\n\x04\x44isk\x10\x01\x12\n\n\x06Kernel\x10\x02\x12\n\n\x06Initrd\x10\x03\x12\x0b\n\x07RamDisk\x10\x04*I\n\tDriveType\x12\x10\n\x0cUnclassified\x10\x00\x12\t\n\x05\x43\x44ROM\x10\x01\x12\x07\n\x03HDD\x10\x02\x12\x07\n\x03NET\x10\x03\x12\r\n\tHDD_EMPTY\x10\x04\x42G\n\x1f\x63om.zededa.cloud.uservice.protoZ$github.com/lf-edge/eve/api/go/configb\x06proto3')
   ,
-  dependencies=[devcommon__pb2.DESCRIPTOR,cipherinfo__pb2.DESCRIPTOR,])
+  dependencies=[devcommon__pb2.DESCRIPTOR,])
 
 _DSTYPE = _descriptor.EnumDescriptor(
   name='DsType',
@@ -63,8 +62,8 @@ _DSTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=643,
-  serialized_end=755,
+  serialized_start=597,
+  serialized_end=709,
 )
 _sym_db.RegisterEnumDescriptor(_DSTYPE)
 
@@ -114,8 +113,8 @@ _FORMAT = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=757,
-  serialized_end=864,
+  serialized_start=711,
+  serialized_end=818,
 )
 _sym_db.RegisterEnumDescriptor(_FORMAT)
 
@@ -149,8 +148,8 @@ _TARGET = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=866,
-  serialized_end=937,
+  serialized_start=820,
+  serialized_end=891,
 )
 _sym_db.RegisterEnumDescriptor(_TARGET)
 
@@ -184,8 +183,8 @@ _DRIVETYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=939,
-  serialized_end=1012,
+  serialized_start=893,
+  serialized_end=966,
 )
 _sym_db.RegisterEnumDescriptor(_DRIVETYPE)
 
@@ -259,8 +258,8 @@ _SIGNATUREINFO = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=52,
-  serialized_end=132,
+  serialized_start=34,
+  serialized_end=114,
 )
 
 
@@ -327,13 +326,6 @@ _DATASTORECONFIG = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
-    _descriptor.FieldDescriptor(
-      name='cInfo', full_name='DatastoreConfig.cInfo', index=8,
-      number=8, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -346,8 +338,8 @@ _DATASTORECONFIG = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=135,
-  serialized_end=323,
+  serialized_start=117,
+  serialized_end=277,
 )
 
 
@@ -419,8 +411,8 @@ _IMAGE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=326,
-  serialized_end=496,
+  serialized_start=280,
+  serialized_end=450,
 )
 
 
@@ -485,12 +477,11 @@ _DRIVE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=499,
-  serialized_end=641,
+  serialized_start=453,
+  serialized_end=595,
 )
 
 _DATASTORECONFIG.fields_by_name['dType'].enum_type = _DSTYPE
-_DATASTORECONFIG.fields_by_name['cInfo'].message_type = cipherinfo__pb2._CIPHERINFO
 _IMAGE.fields_by_name['uuidandversion'].message_type = devcommon__pb2._UUIDANDVERSION
 _IMAGE.fields_by_name['iformat'].enum_type = _FORMAT
 _IMAGE.fields_by_name['siginfo'].message_type = _SIGNATUREINFO
