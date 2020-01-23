6e50733120154b1d5c8280dbbc7e0e7789705ae9
eriknordmark <erik@zededa.com>
2020-01-22 16:49:53 -0800

    move incompatible/old rsyslog files out of the way
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/rsyslog/init.sh
+++ pkg/rsyslog/init.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 RSYSLOG_WORK_DIR=/persist/rsyslog
+OLD_DIR=/persist/syslog
+# We need to clean up old state it seems. Use $OLD_DIR as the hint
+if [ -d "$OLD_DIR" ]; then
+    echo "Moving old $OLD_DIR and $RSYSLOG_WORK_DIR out of the way"
+    mv "$OLD_DIR" "$OLD_DIR".old
+    mv "$RSYSLOG_WORK_DIR" "$RSYSLOG_WORK_DIR".old
+fi
 if [ ! -d "$RSYSLOG_WORK_DIR" ]; then
   mkdir -p $RSYSLOG_WORK_DIR
   chmod 644 $RSYSLOG_WORK_DIR
 fi
+# XXX IMGB?
 IMGP=IMGA /usr/sbin/rsyslogd
