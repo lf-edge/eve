6f062b5e1cefe3e0f9aa1ad0fa6f40f5fbf25307
adarsh-zededa <adarsh@zededa.com>
2020-01-27 11:29:02 +0530

    Dummy commit to re-trigger yetus
    Signed-off-by: adarsh-zededa <adarsh@zededa.com>

--- pkg/pillar/cmd/domainmgr/domainmgr.go
+++ pkg/pillar/cmd/domainmgr/domainmgr.go
@@ -86,7 +86,7 @@ type domainContext struct {
 	GCInitialized          bool
 }
 
-//Holds mount information which will be used when bringing up container.
+//Holds mount information which will be used when bringing up container
 type Mounts struct {
 	sourcePath string //Source path to be mounted
 	targetPath string //Target path inside VM or container
