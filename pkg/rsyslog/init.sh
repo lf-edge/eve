1018b34de75b5a25380503d130ac5a57b63277b1
GopiKrishna Kodali <gkodali@zededa.com>
2020-01-29 18:53:18 +0530

    1) Move rsyslog container from onboot to service section. 2) Launch a monitor script for monitoring rsyslogd process
    Pending: make pillar services send logs to stdout.
    
    Signed-off-by: GopiKrishna Kodali <gkodali@zededa.com>
    
    Resolve yetus complaints
    
    Signed-off-by: GopiKrishna Kodali <gkodali@zededa.com>

--- pkg/rsyslog/init.sh
+++ pkg/rsyslog/init.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 
-RSYSLOG_WORK_DIR=/persist/rsyslog
-OLD_DIR=/persist/syslog
-# We need to clean up old state it seems. Use $OLD_DIR as the hint
-if [ -d "$OLD_DIR" ]; then
-    echo "Moving old $OLD_DIR and $RSYSLOG_WORK_DIR out of the way"
-    mv "$OLD_DIR" "$OLD_DIR".old
-    mv "$RSYSLOG_WORK_DIR" "$RSYSLOG_WORK_DIR".old
-fi
-if [ ! -d "$RSYSLOG_WORK_DIR" ]; then
-  mkdir -p $RSYSLOG_WORK_DIR
-  chmod 644 $RSYSLOG_WORK_DIR
-fi
+./start-rsyslogd.sh
 
-IMGP=$(cat /run/eve.id 2>/dev/null)
-IMGP=${IMGP:-IMGX} /usr/sbin/rsyslogd
+./monitor-rsyslog.sh
 
 while true; do /usr/bin/logread -F -socket /run/memlogdq.sock | logger ; done
