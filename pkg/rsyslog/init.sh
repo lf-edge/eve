effc02f759d00dac3ac6893e14245bcfafa1f401
GopiKrishna Kodali <gkodali@zededa.com>
2020-01-31 17:01:39 +0530

    1) Clean up device-steps.sh and move logread PID writing logic to rsyslog container 2) Do not send logs with lower than configured priority to zedcloud
    Signed-off-by: GopiKrishna Kodali <gkodali@zededa.com>

--- pkg/rsyslog/init.sh
+++ pkg/rsyslog/init.sh
@@ -2,4 +2,30 @@
 
 ./monitor-rsyslog.sh &
 
-while true; do /usr/bin/logread -F -socket /run/memlogdq.sock | logger ; done
+LOGREAD_PID_WAIT=3600
+while true;
+do
+    if [ -n "$(pgrep logread)" ]; then
+        sleep 10
+        continue
+    fi
+    /usr/bin/logread -F -socket /run/memlogdq.sock | logger &
+
+    LOOP_COUNT=0
+    while [ -z "$(pgrep logread)" ];
+    do
+        sleep 1
+        LOOP_COUNT=$((LOOP_COUNT + 1))
+        if [ "$LOOP_COUNT" -ge "$LOGREAD_PID_WAIT" ]; then
+            echo "$(date -Ins -u) Error: Could not find logread process"
+            break
+        fi
+    done
+
+    LOGREAD_PID=$(pgrep logread)
+    if [ -n "$LOGREAD_PID" ]; then
+        echo "$LOGREAD_PID" > /run/logread.pid
+    else
+        echo "$(date -Ins -u) Error: logread has not started"
+    fi
+done
