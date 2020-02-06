053705c3e079c180d1fed7904af4cc0631e8e92c
Roman Shaposhnik <rvs@zededa.com>
2020-02-06 12:12:40 -0800

    A few tweaks
    Signed-off-by: Roman Shaposhnik <rvs@zededa.com>

--- pkg/xen-tools/init.sh
+++ pkg/xen-tools/init.sh
@@ -3,6 +3,7 @@
 
 # Finally, we need to start Xen
 # In case it hangs and we have no hardware watchdog we run it in the background
+mkdir -p /var/run/xen/ /var/run/xenstored
 XENCONSOLED_ARGS='--log=all --log-dir=/var/log/xen' /etc/init.d/xencommons start
 
 # We have the following filesystem logs from Xen to care about under /var/log/xen
