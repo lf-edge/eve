ad8c7cbbf952b0bbd662243d28ebad1e5a208816
eriknordmark <erik@zededa.com>
2020-02-13 17:09:29 -0800

    go mod update
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/lisp/vendor/github.com/lf-edge/eve/pkg/pillar/pubsub/legacy/legacy.go
+++ pkg/lisp/vendor/github.com/lf-edge/eve/pkg/pillar/pubsub/legacy/legacy.go
@@ -13,21 +13,32 @@ var (
 // Publish create a `Publication` for the given agent name and topic type.
 func Publish(agentName string, topicType interface{}) (pubsub.Publication, error) {
 	log.Infof("legacy.Publish agentName(%s)", agentName)
-	return defaultPubsub.Publish(agentName, topicType)
+	return defaultPubsub.NewPublication(pubsub.PublicationOptions{
+		AgentName: agentName,
+		TopicType: topicType,
+	})
 }
 
 // PublishPersistent create a `Publication` for the given agent name and topic
 // type, but with persistence of the messages across reboots.
 func PublishPersistent(agentName string, topicType interface{}) (pubsub.Publication, error) {
 	log.Infof("legacy.PublishPersistent agentName(%s)", agentName)
-	return defaultPubsub.PublishPersistent(agentName, topicType)
+	return defaultPubsub.NewPublication(pubsub.PublicationOptions{
+		AgentName:  agentName,
+		TopicType:  topicType,
+		Persistent: true,
+	})
 }
 
 // PublishScope create a `Publication` for the given agent name and topic,
 // restricted to a given scope.
 func PublishScope(agentName string, agentScope string, topicType interface{}) (pubsub.Publication, error) {
 	log.Infof("legacy.PublishScope agentName(%s), agentScope (%s)", agentName, agentScope)
-	return defaultPubsub.PublishScope(agentName, agentScope, topicType)
+	return defaultPubsub.NewPublication(pubsub.PublicationOptions{
+		AgentName:  agentName,
+		TopicType:  topicType,
+		AgentScope: agentScope,
+	})
 }
 
 // Subscribe create a subscription for the given agent name and topic
@@ -36,7 +47,24 @@ func PublishScope(agentName string, agentScope string, topicType interface{}) (p
 // on the channel or process them until `Subscription.Start()` is called.
 func Subscribe(agentName string, topicType interface{}, activate bool,
 	ctx interface{}, options *pubsub.SubscriptionOptions) (pubsub.Subscription, error) {
-	return defaultPubsub.Subscribe(agentName, topicType, activate, ctx, options)
+
+	if options == nil {
+		options = &pubsub.SubscriptionOptions{}
+	}
+	return defaultPubsub.NewSubscription(pubsub.SubscriptionOptions{
+		CreateHandler:  options.CreateHandler,
+		ModifyHandler:  options.ModifyHandler,
+		DeleteHandler:  options.DeleteHandler,
+		RestartHandler: options.RestartHandler,
+		SyncHandler:    options.SyncHandler,
+		WarningTime:    options.WarningTime,
+		ErrorTime:      options.ErrorTime,
+		AgentName:      agentName,
+		TopicImpl:      topicType,
+		Activate:       activate,
+		Ctx:            ctx,
+		Persistent:     false,
+	})
 }
 
 // SubscribeScope create a subscription for the given agent name and topic,
@@ -46,7 +74,24 @@ func Subscribe(agentName string, topicType interface{}, activate bool,
 // on the channel or process them until `Subscription.Start()` is called.
 func SubscribeScope(agentName string, agentScope string, topicType interface{},
 	activate bool, ctx interface{}, options *pubsub.SubscriptionOptions) (pubsub.Subscription, error) {
-	return defaultPubsub.SubscribeScope(agentName, agentScope, topicType, activate, ctx, options)
+	if options == nil {
+		options = &pubsub.SubscriptionOptions{}
+	}
+	return defaultPubsub.NewSubscription(pubsub.SubscriptionOptions{
+		CreateHandler:  options.CreateHandler,
+		ModifyHandler:  options.ModifyHandler,
+		DeleteHandler:  options.DeleteHandler,
+		RestartHandler: options.RestartHandler,
+		SyncHandler:    options.SyncHandler,
+		WarningTime:    options.WarningTime,
+		ErrorTime:      options.ErrorTime,
+		AgentName:      agentName,
+		AgentScope:     agentScope,
+		TopicImpl:      topicType,
+		Activate:       activate,
+		Ctx:            ctx,
+		Persistent:     false,
+	})
 }
 
 // SubscribePersistent create a subscription for the given agent name and topic,
@@ -56,5 +101,21 @@ func SubscribeScope(agentName string, agentScope string, topicType interface{},
 // on the channel or process them until `Subscription.Start()` is called.
 func SubscribePersistent(agentName string, topicType interface{}, activate bool,
 	ctx interface{}, options *pubsub.SubscriptionOptions) (pubsub.Subscription, error) {
-	return defaultPubsub.SubscribePersistent(agentName, topicType, activate, ctx, options)
+	if options == nil {
+		options = &pubsub.SubscriptionOptions{}
+	}
+	return defaultPubsub.NewSubscription(pubsub.SubscriptionOptions{
+		CreateHandler:  options.CreateHandler,
+		ModifyHandler:  options.ModifyHandler,
+		DeleteHandler:  options.DeleteHandler,
+		RestartHandler: options.RestartHandler,
+		SyncHandler:    options.SyncHandler,
+		WarningTime:    options.WarningTime,
+		ErrorTime:      options.ErrorTime,
+		AgentName:      agentName,
+		TopicImpl:      topicType,
+		Activate:       activate,
+		Ctx:            ctx,
+		Persistent:     true,
+	})
 }
