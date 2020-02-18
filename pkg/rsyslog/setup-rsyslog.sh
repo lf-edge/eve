7babeede3466d9f08fcc7375933471de895c3a71
GopiKrishna Kodali <gkodali@zededa.com>
2020-02-18 17:18:47 +0530

    Instead of copying complete Makefiles and imemelogd.c file, create a patch that can be applied before building
    Signed-off-by: GopiKrishna Kodali <gkodali@zededa.com>

--- pkg/rsyslog/setup-rsyslog.sh
+++ pkg/rsyslog/setup-rsyslog.sh
@@ -3,12 +3,8 @@
 git clone -b v8-stable https://github.com/rsyslog/rsyslog.git
 cd rsyslog || exit
 mkdir -p contrib/imemlogd
-cp ../imemlogd.c contrib/imemlogd/imemlogd.c
-cp ../Makefile.am.imemlogd contrib/imemlogd/Makefile.am
-cp ../Makefile.am.tests tests/Makefile.am
-cp ../Makefile.am Makefile.am
-cp ../configure.ac configure.ac
-git pull
+cp ../imemlogd.diff imemlogd.diff
+git apply imemlogd.diff
 mkdir utils
 echo "./configure --prefix=/usr/local $RSYSLOG_CONFIGURE_OPTIONS" --enable-compile-warnings=yes > utils/conf
 chmod +x utils/conf
