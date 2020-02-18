06d7437583972f02b3782d180f0dfa46f65ba44f
GopiKrishna Kodali <gkodali@zededa.com>
2020-02-18 17:21:13 +0530

    Removed unnecessary man/man-pages packages from alpine build container
    Signed-off-by: GopiKrishna Kodali <gkodali@zededa.com>

--- pkg/rsyslog/build-rsyslog.sh
+++ pkg/rsyslog/build-rsyslog.sh
@@ -3,8 +3,7 @@
 git clone -b v8-stable https://github.com/rsyslog/rsyslog.git
 cd rsyslog || exit
 mkdir -p contrib/imemlogd
-cp ../imemlogd.diff imemlogd.diff
-git apply imemlogd.diff
+git apply ../imemlogd.diff
 mkdir utils
 echo "./configure --prefix=/usr/local $RSYSLOG_CONFIGURE_OPTIONS" --enable-compile-warnings=yes > utils/conf
 chmod +x utils/conf
