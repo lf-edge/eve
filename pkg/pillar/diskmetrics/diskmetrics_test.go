b30e87ed8d6b9605b395ad31461f423f10bc0cbb
zed-rishabh <rgupta@zededa.com>
2020-02-24 13:27:11 +0530

    Modified disk metrics unit tests
    Signed-off-by: zed-rishabh <rgupta@zededa.com>

--- pkg/pillar/diskmetrics/diskmetrics_test.go
+++ pkg/pillar/diskmetrics/diskmetrics_test.go
@@ -10,70 +10,66 @@ import (
 
 type diskImgInfo struct {
 	diskFile    string
-	VirtualSize uint64
-	Filename    string
-	ClusterSize uint64
-	Format      string
-	ActualSize  uint64
-	DirtyFlag   bool
+	virtualSize uint64
+	format      string
 }
 
 func TestCreateScratchDisk(t *testing.T) {
 	testMatrix := map[string]diskImgInfo{
 		"qcow disk create": {
 			diskFile:    "/tmp/sample1.qcow",
-			VirtualSize: 1073741824,
-			Format:      "qcow",
+			virtualSize: 1073741824,
+			format:      "qcow",
 		},
 		"qcow2 disk create": {
 			diskFile:    "/tmp/sample2.qcow2",
-			VirtualSize: 1073741824,
-			Format:      "qcow2",
+			virtualSize: 1073741824,
+			format:      "qcow2",
 		},
 		"raw disk create": {
 			diskFile:    "/tmp/sample3.raw",
-			VirtualSize: 1073741824,
-			Format:      "raw",
+			virtualSize: 1073741824,
+			format:      "raw",
 		},
 		"vmdk disk create": {
 			diskFile:    "/tmp/sample4.vmdk",
-			VirtualSize: 1073741824,
-			Format:      "vmdk",
+			virtualSize: 1073741824,
+			format:      "vmdk",
 		},
 		"vhdx disk create": {
 			diskFile:    "/tmp/sample5.vhdx",
-			VirtualSize: 1073741824,
-			Format:      "vhdx",
+			virtualSize: 1073741824,
+			format:      "vhdx",
 		},
 		"qcow (in uppercase) disk create": {
 			diskFile:    "/tmp/sample6.qcow",
-			VirtualSize: 1073741824,
-			Format:      "QCOW",
+			virtualSize: 1073741824,
+			format:      "QCOW",
 		},
 		"qcow2 (in uppercase) disk create": {
 			diskFile:    "/tmp/sample7.qcow2",
-			VirtualSize: 1073741824,
-			Format:      "QCOW2",
+			virtualSize: 1073741824,
+			format:      "QCOW2",
 		},
 		"raw (in uppercase) disk create": {
 			diskFile:    "/tmp/sample8.raw",
-			VirtualSize: 1073741824,
-			Format:      "RAW",
+			virtualSize: 1073741824,
+			format:      "RAW",
 		},
 		"vmdk (in uppercase) disk create": {
 			diskFile:    "/tmp/sample9.vmdk",
-			VirtualSize: 1073741824,
-			Format:      "VMDK",
+			virtualSize: 1073741824,
+			format:      "VMDK",
 		},
 		"vhdx (in uppercase) disk create": {
 			diskFile:    "/tmp/sample10.vhdx",
-			VirtualSize: 1073741824,
-			Format:      "VHDX",
+			virtualSize: 1073741824,
+			format:      "VHDX",
 		},
 	}
 	for testname, test := range testMatrix {
 		t.Logf("Running test case %s", testname)
-		err := CreateScratchDisk(test.diskFile, test.Format, test.VirtualSize)
+		err := CreateScratchDisk(test.diskFile, test.format, test.virtualSize)
 		if err != nil {
 			t.Errorf("CreateScratchDisk failed: %v", err)
 		} else {
