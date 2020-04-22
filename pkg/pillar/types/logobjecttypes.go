1606247c34d1c09115b6300c23cba3bb19f9aec3
GopiKrishna Kodali <gkodali@zededa.com>
2020-04-22 20:13:59 +0530

    1. Moved LogObject to types package 2. Added an exmple usage with AppNetworkStatus 3. Addressed some review comments
    Signed-off-by: GopiKrishna Kodali <gkodali@zededa.com>

--- pkg/pillar/types/logobjecttypes.go
+++ pkg/pillar/types/logobjecttypes.go
@@ -1,34 +1,58 @@
 // Copyright (c) 2020 Zededa, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-package agentlog
+package types
 
 import (
 	"encoding/json"
+	"github.com/satori/go.uuid"
 	log "github.com/sirupsen/logrus"
 )
 
-// LogObject : Holds all key values pairs to be logged later
+// LogObjectType : Predefined log object types
+type LogObjectType string
+
+// The following is NOT an extensive list.
+// Intent is to create a reference list for engineers to get an idea of its usage.
+const (
+	LogType                LogObjectType = "log"
+	RelationLogType        LogObjectType = "relation"
+	ImageLogType           LogObjectType = "image"
+	AppInstanceLogType     LogObjectType = "app_instance"
+	NetworkInstanceLogType LogObjectType = "network_instance"
+)
+
+// LogObject : Holds all key value pairs to be logged later.
 type LogObject struct {
-	fields map[string]interface{}
+	Fields map[string]interface{}
 }
 
 // NewLogObject :
-// objType -> log, action, relation etc
+// objType -> log, action, relation, app_instance, image etc
 // objName -> App instance name, name of file being downloaded etc
 // objUUID -> UUID of the object if present (App instance UUID) or UUID of parent object
-func NewLogObject(objType string, objName string, objUUID string) *LogObject {
+func NewLogObject(objType LogObjectType, objName string, objUUID uuid.UUID) *LogObject {
 	fields := make(map[string]interface{})
 	fields["obj_type"] = objType
 	fields["obj_name"] = objName
-	fields["obj_uuid"] = objUUID
+	fields["obj_uuid"] = objUUID.String()
 
-	return &LogObject{fields: fields}
+	return &LogObject{Fields: fields}
+}
+
+// InitLogObject : Initialize an already allocated LogObject
+func InitLogObject(object *LogObject, objType LogObjectType,
+	objName string, objUUID uuid.UUID) {
+	fields := make(map[string]interface{})
+	fields["obj_type"] = objType
+	fields["obj_name"] = objName
+	fields["obj_uuid"] = objUUID.String()
+	object.Fields = fields
 }
 
 // AddField : Add a key value pair to be logged
 func (object *LogObject) AddField(key string, value interface{}) {
-	object.fields[key] = value
+	object.Fields[key] = value
 }
 
 // AddCompositeField : Add a structure/map to be logged
@@ -49,7 +73,7 @@ func (object *LogObject) AddCompositeField(key string, value interface{}) error 
 // AddFields : Values of exiting keys in the object will be overwritten with new values passed
 func (object *LogObject) AddFields(fields map[string]interface{}) {
 	for key, value := range fields {
-		object.fields[key] = value
+		object.Fields[key] = value
 	}
 }
 
@@ -57,8 +81,8 @@ func (object *LogObject) AddFields(fields map[string]interface{}) {
 // Values of existing fields in destination object will be overwritten with values
 // from source object.
 func (object *LogObject) Merge(source *LogObject) *LogObject {
-	for key, value := range source.fields {
-		object.fields[key] = value
+	for key, value := range source.Fields {
+		object.Fields[key] = value
 	}
 	return object
 }
@@ -66,10 +90,10 @@ func (object *LogObject) Merge(source *LogObject) *LogObject {
 // Clone : Create a clone from an existing Log object
 func (object *LogObject) Clone() *LogObject {
 	newLogObject := &LogObject{
-		fields: make(map[string]interface{}),
+		Fields: make(map[string]interface{}),
 	}
-	for key, value := range object.fields {
-		newLogObject.fields[key] = value
+	for key, value := range object.Fields {
+		newLogObject.Fields[key] = value
 	}
 	return newLogObject
 }
@@ -104,115 +128,110 @@ func (object *LogObject) CloneAndMerge(source *LogObject) *LogObject {
 
 // Debug :
 func (object *LogObject) Debug(args ...interface{}) {
-	log.WithFields(object.fields).Debug(args...)
+	log.WithFields(object.Fields).Debug(args...)
 }
 
 // Print :
 func (object *LogObject) Print(args ...interface{}) {
-	log.WithFields(object.fields).Print(args...)
+	log.WithFields(object.Fields).Print(args...)
 }
 
 // Info :
 func (object *LogObject) Info(args ...interface{}) {
-	log.WithFields(object.fields).Info(args...)
+	log.WithFields(object.Fields).Info(args...)
 }
 
 // Warn :
 func (object *LogObject) Warn(args ...interface{}) {
-	log.WithFields(object.fields).Warn(args...)
+	log.WithFields(object.Fields).Warn(args...)
 }
 
 // Warning :
 func (object *LogObject) Warning(args ...interface{}) {
-	log.WithFields(object.fields).Warning(args...)
-}
-
-// Error :
-func (object *LogObject) Error(args ...interface{}) {
-	log.WithFields(object.fields).Error(args...)
+	log.WithFields(object.Fields).Warning(args...)
 }
 
 // Panic :
 func (object *LogObject) Panic(args ...interface{}) {
-	log.WithFields(object.fields).Panic(args...)
+	log.WithFields(object.Fields).Panic(args...)
 }
 
 // Fatal :
 func (object *LogObject) Fatal(args ...interface{}) {
-	log.WithFields(object.fields).Fatal(args...)
+	log.WithFields(object.Fields).Fatal(args...)
 }
 
 // Debugf :
 func (object *LogObject) Debugf(format string, args ...interface{}) {
-	log.WithFields(object.fields).Debugf(format, args...)
+	log.WithFields(object.Fields).Debugf(format, args...)
 }
 
 // Infof :
 func (object *LogObject) Infof(format string, args ...interface{}) {
-	log.WithFields(object.fields).Infof(format, args...)
+	log.WithFields(object.Fields).Infof(format, args...)
 }
 
 // Warnf :
 func (object *LogObject) Warnf(format string, args ...interface{}) {
-	log.WithFields(object.fields).Warnf(format, args...)
+	log.WithFields(object.Fields).Warnf(format, args...)
 }
 
 // Warningf :
 func (object *LogObject) Warningf(format string, args ...interface{}) {
-	log.WithFields(object.fields).Warningf(format, args...)
+	log.WithFields(object.Fields).Warningf(format, args...)
 }
 
 // Panicf :
 func (object *LogObject) Panicf(format string, args ...interface{}) {
-	log.WithFields(object.fields).Panicf(format, args...)
+	log.WithFields(object.Fields).Panicf(format, args...)
 }
 
 // Fatalf :
 func (object *LogObject) Fatalf(format string, args ...interface{}) {
-	log.WithFields(object.fields).Fatalf(format, args...)
+	log.WithFields(object.Fields).Fatalf(format, args...)
 }
 
 // Errorf :
 func (object *LogObject) Errorf(format string, args ...interface{}) {
-	log.WithFields(object.fields).Errorf(format, args...)
+	log.WithFields(object.Fields).Errorf(format, args...)
 }
 
 // Debugln :
 func (object *LogObject) Debugln(args ...interface{}) {
-	log.WithFields(object.fields).Debugln(args...)
+	log.WithFields(object.Fields).Debugln(args...)
 }
 
 // Println :
 func (object *LogObject) Println(args ...interface{}) {
-	log.WithFields(object.fields).Println(args...)
+	log.WithFields(object.Fields).Println(args...)
 }
 
 // Infoln :
 func (object *LogObject) Infoln(args ...interface{}) {
-	log.WithFields(object.fields).Infoln(args...)
+	log.WithFields(object.Fields).Infoln(args...)
 }
 
 // Warnln :
 func (object *LogObject) Warnln(args ...interface{}) {
-	log.WithFields(object.fields).Warnln(args...)
+	log.WithFields(object.Fields).Warnln(args...)
 }
 
 // Warningln :
 func (object *LogObject) Warningln(args ...interface{}) {
-	log.WithFields(object.fields).Warningln(args...)
+	log.WithFields(object.Fields).Warningln(args...)
 }
 
 // Errorln :
 func (object *LogObject) Errorln(args ...interface{}) {
-	log.WithFields(object.fields).Errorln(args...)
+	log.WithFields(object.Fields).Errorln(args...)
 }
 
 // Panicln :
 func (object *LogObject) Panicln(args ...interface{}) {
-	log.WithFields(object.fields).Panicln(args...)
+	log.WithFields(object.Fields).Panicln(args...)
 }
 
 // Fatalln :
 func (object *LogObject) Fatalln(args ...interface{}) {
-	log.WithFields(object.fields).Fatalln(args...)
+	log.WithFields(object.Fields).Fatalln(args...)
 }
