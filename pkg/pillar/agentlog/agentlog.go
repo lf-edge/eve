f7cba14c64539c0411d1b64d6d37d5aab04b93ca
Gopi krishna Kodali <gkodali@zededa.com>
2020-05-29 01:53:16 +0530

    send EVE service debug fatal traces, USR1 traces, USR2 traces and stdout to service specific files on persistent storage
    Signed-off-by: Gopi krishna Kodali <gkodali@zededa.com>
    
    Redirect along with sending to cloud - fatal stack traces, USR1, USR2 output to files in /persist/agentdebug/<agentname>/ directory. Also touch service specific file to indicate service start time
    
    Signed-off-by: Gopi krishna Kodali <gkodali@zededa.com>
    
    Send sigusr, sigusr2, fata-reason and fatal-stack to different files in /persist/agentdebug/<agentname>/
    
    Signed-off-by: Gopi krishna Kodali <gkodali@zededa.com>
    
    minor changes in comments
    
    Signed-off-by: Gopi krishna Kodali <gkodali@zededa.com>
    
    os.O_TRUNC should be passed while opening files to make sure complete overwrite of file.
    
    Signed-off-by: Gopi krishna Kodali <gkodali@zededa.com>

--- pkg/pillar/agentlog/agentlog.go
+++ pkg/pillar/agentlog/agentlog.go
@@ -68,6 +68,10 @@ func (hook *SourceHook) Levels() []log.Level {
 
 // Wait on channel then handle the signals
 func handleSignals(sigs chan os.Signal) {
+	agentDebugDir := fmt.Sprintf("%s/%s/", types.PersistDebugDir, savedAgentName)
+	sigUsr1FileName := agentDebugDir + "/sigusr1"
+	sigUsr2FileName := agentDebugDir + "/sigusr2"
+
 	for {
 		select {
 		case sig := <-sigs:
@@ -76,6 +80,19 @@ func handleSignals(sigs chan os.Signal) {
 			case syscall.SIGUSR1:
 				stacks := getStacks(true)
 				stackArray := strings.Split(stacks, "\n\n")
+
+				sigUsr1File, err := os.OpenFile(sigUsr1FileName,
+					os.O_WRONLY|os.O_CREATE|os.O_SYNC|os.O_TRUNC, 0755)
+				if err == nil {
+					for _, stack := range stackArray {
+						// This goes to /persist/agentdebug/<agentname>/sigusr1 file
+						sigUsr1File.WriteString(stack)
+					}
+					sigUsr1File.Close()
+				} else {
+					log.Errorf("handleSignals: Error opening file %s with: %s", sigUsr1FileName, err)
+				}
+
 				log.Warnf("SIGUSR1 triggered with %d stacks", len(stackArray))
 				for _, stack := range stackArray {
 					log.Warnf("%v", stack)
@@ -86,8 +103,20 @@ func handleSignals(sigs chan os.Signal) {
 				RebootStack(stacks)
 			case syscall.SIGUSR2:
 				log.Warnf("SIGUSR2 triggered memory info:\n")
-				logMemUsage()
-				logMemAllocationSites()
+				sigUsr2File, err := os.OpenFile(sigUsr2FileName,
+					os.O_WRONLY|os.O_CREATE|os.O_SYNC|os.O_TRUNC, 0755)
+				if err != nil {
+					log.Errorf("handleSignals: Error opening file %s with: %s", sigUsr2FileName, err)
+				} else {
+					// This goes to /persist/agentdebug/<agentname>/sigusr2 file
+					sigUsr2File.WriteString("SIGUSR2 triggered memory info:\n")
+				}
+
+				logMemUsage(sigUsr2File)
+				logMemAllocationSites(sigUsr2File)
+				if sigUsr2File != nil {
+					sigUsr2File.Close()
+				}
 			}
 		}
 	}
@@ -130,6 +159,16 @@ func RebootReason(reason string, normal bool) {
 		// Note: can not use log here since we are called from a log hook!
 		fmt.Printf("printToFile failed %s\n", err)
 	}
+
+	agentDebugDir := fmt.Sprintf("%s/%s/", types.PersistDebugDir, savedAgentName)
+
+	agentFatalReasonFilename := agentDebugDir + "/fatal-reason"
+	err = overWriteFile(agentFatalReasonFilename, reason)
+	if err != nil {
+		// Note: can not use log here since we are called from a log hook!
+		fmt.Printf("printToFile failed %s\n", err)
+	}
+
 	filename = "/persist/" + rebootImage
 	curPart := EveCurrentPartition()
 	err = printToFile(filename, curPart)
@@ -151,6 +190,12 @@ func RebootStack(stacks string) {
 	}
 	filename = "/persist/log/" + stackFile + ".log"
 	err = printToFile(filename, fmt.Sprintf("%v\n", stacks))
+	if err != nil {
+		log.Errorf("printToFile failed %s\n", err)
+	}
+	agentDebugDir := fmt.Sprintf("%s/%s/", types.PersistDebugDir, savedAgentName)
+	agentStackTraceFile := agentDebugDir + "/fatal-stack"
+	err = overWriteFile(agentStackTraceFile, fmt.Sprintf("%v\n", stacks))
 	if err != nil {
 		log.Errorf("printToFile failed %s\n", err)
 	}
@@ -204,6 +249,20 @@ func statAndRead(filename string) (string, time.Time) {
 func printToFile(filename string, str string) error {
 	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE,
 		os.ModeAppend)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.WriteString(str)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Overwrite if file exists.
+func overWriteFile(filename string, str string) error {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
 	if err != nil {
 		return err
 	}
@@ -278,7 +337,7 @@ func logGCStats() {
 }
 
 // Print in sorted order based on top bytes
-func logMemAllocationSites() {
+func logMemAllocationSites(file *os.File) {
 	reportZeroInUse := false
 	numSites, sites := GetMemAllocationSites(reportZeroInUse)
 	log.Warnf("alloc %d sites len %d", numSites, len(sites))
@@ -292,22 +351,32 @@ func logMemAllocationSites() {
 		log.Warnf("alloc %d bytes %d objects total %d/%d at:\n%s",
 			site.InUseBytes, site.InUseObjects, site.AllocBytes,
 			site.AllocObjects, site.PrintedStack)
+
+		if file != nil {
+			// This goes to /persist/agentdebug/<agentname>/sigusr2 file
+			// And there in not much difference from the above log except the CRNL at the end.
+			statString := fmt.Sprintf("alloc %d bytes %d objects total %d/%d at:\n%s\n",
+				site.InUseBytes, site.InUseObjects, site.AllocBytes,
+				site.AllocObjects, site.PrintedStack)
+			file.WriteString(statString)
+		}
 	}
 }
 
-// LogMemoryUsage provides for user-triggered memory reports
-func LogMemoryUsage() {
-	log.Info("User-triggered memory report")
-	logMemUsage()
-	logMemAllocationSites()
-}
-
-func logMemUsage() {
+func logMemUsage(file *os.File) {
 	var m runtime.MemStats
 
 	runtime.ReadMemStats(&m)
 	log.Infof("Alloc %d Mb, TotalAlloc %d Mb, Sys %d Mb, NumGC %d",
 		roundToMb(m.Alloc), roundToMb(m.TotalAlloc), roundToMb(m.Sys), m.NumGC)
+
+	if file != nil {
+		// This goes to /persist/agentdebug/<agentname>/sigusr2 file
+		// And there in not much difference from the above log except the CRNL at the end.
+		statString := fmt.Sprintf("Alloc %d Mb, TotalAlloc %d Mb, Sys %d Mb, NumGC %d\n",
+			roundToMb(m.Alloc), roundToMb(m.TotalAlloc), roundToMb(m.Sys), m.NumGC)
+		file.WriteString(statString)
+	}
 }
 
 func roundToMb(b uint64) uint64 {
@@ -317,10 +386,65 @@ func roundToMb(b uint64) uint64 {
 	return mb
 }
 
+func touchFile(filename string) {
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0755)
+		if err != nil {
+			log.Fatalf("touchFile: Failed touching file %s with err: %s", filename, err)
+		}
+		defer file.Close()
+	} else {
+		currentTime := time.Now()
+		err = os.Chtimes(filename, currentTime, currentTime)
+		if err != nil {
+			log.Fatalf("touchFile: Failed touching file %s with err: %s", filename, err)
+		}
+	}
+}
+
+func spoofStdFDs(agentName string) *os.File {
+	agentDebugDir := fmt.Sprintf("%s/%s/", types.PersistDebugDir, agentName)
+	if _, err := os.Stat(agentDebugDir); os.IsNotExist(err) {
+		// Create the agent specific debug directory
+		if err := os.MkdirAll(agentDebugDir, 0755); err != nil {
+			log.Fatalf("spoofStdFDs: Agent specific debug directory (%s) does not exist: %s",
+				agentDebugDir, err)
+		}
+	}
+	startTimeFile := agentDebugDir + "/starttime"
+	touchFile(startTimeFile)
+
+	stdOutFile := agentDebugDir + "/stdout"
+	stdOut, err := os.OpenFile(stdOutFile, os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0755)
+	if err != nil {
+		log.Fatalf("spoofStdFDs: Failed opening stdout file %s with err: %s", stdOutFile, err)
+	}
+
+	fd2, err := syscall.Dup(int(os.Stdout.Fd()))
+	if err != nil {
+		log.Fatalf("spoofStdFDs: Error duplicating Stdout: %s", err)
+	}
+	originalStdout, err := os.OpenFile(fmt.Sprintf("/dev/fd/%d", fd2),
+		os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0755)
+	if err != nil {
+		log.Fatalf("spoofStdFDs: Error opening duplicate stdout with fd: %v", fd2)
+	}
+	// replace stdout
+	err = syscall.Dup2(int(stdOut.Fd()), 1)
+	if err != nil {
+		log.Fatalf("spoofStdFDs: Error replacing stdout with panic file %s: %s",
+			stdOutFile, err)
+	}
+	return originalStdout
+}
+
 func Init(agentName string) {
 	savedAgentName = agentName
 	savedPid = os.Getpid()
 	log.SetOutput(os.Stdout)
+	originalStdout := spoofStdFDs(agentName)
+	log.SetOutput(originalStdout)
 	hook := new(FatalHook)
 	log.AddHook(hook)
 	hook2 := new(SourceHook)
--- pkg/pillar/types/locationconsts.go
+++ pkg/pillar/types/locationconsts.go
@@ -21,6 +21,8 @@ const (
 	RWImgDirname = PersistDir + "/img"
 	// ROContImgDirname - Location of read only images used by containerd
 	ROContImgDirname = PersistDir + "/runx/pods/prepared"
+	// PersistDebugDir - Location for service specific debug/traces
+	PersistDebugDir = PersistDir + "/agentdebug"
 	// AppImgDirname - location of downloaded app images. Read-only images
 	// named based on sha256 hash each in its own directory
 	AppImgDirname = DownloadDirname + "/" + AppImgObj
