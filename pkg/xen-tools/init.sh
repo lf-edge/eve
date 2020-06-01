17a4e590f4ee1bc1bb77f3a2656642e251a718a6
Roman Shaposhnik <rvs@zededa.com>
2020-06-01 22:45:18 +0200

    Getting qemu-system-i386 back on ARM64
    Signed-off-by: Roman Shaposhnik <rvs@zededa.com>

--- pkg/xen-tools/init.sh
+++ pkg/xen-tools/init.sh
@@ -18,12 +18,7 @@ if [ -d /proc/xen/ ]; then
    # Finally, we need to start Xen
    # In case it hangs and we have no hardware watchdog we run it in the background
    mkdir -p /var/run/xen/ /var/run/xenstored
-   # FIXME: this is a workaround for Xen on ARM still requiring qemu-system-i386
-   #   https://wiki.xenproject.org/wiki/Xen_ARM_with_Virtualization_Extensions#Use_of_qemu-system-i386_on_ARM
-   if [ "$(uname -m)" = aarch64 ]; then
-      export QEMU_XEN=/bin/true
-      echo 1 > /var/run/xen/qemu-dom0.pid
-   fi
+
    XENCONSOLED_ARGS='--log=all --log-dir=/var/log/xen' /etc/init.d/xencommons start
 
    # Now start the watchdog
