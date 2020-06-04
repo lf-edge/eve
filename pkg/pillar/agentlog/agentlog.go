b2cdf10655559e582526b8889d6a368b914b7e8e
Gopi krishna Kodali <gkodali@zededa.com>
2020-06-04 12:01:35 +0530

    Precent normal reboot reason from being printed to fatal-reason and stdout files
    Signed-off-by: Gopi krishna Kodali <gkodali@zededa.com>

--- pkg/pillar/agentlog/agentlog.go
+++ pkg/pillar/agentlog/agentlog.go
@@ -140,8 +140,6 @@ func printStack() {
 // It also appends to /persist/log/reboot-reason.log
 // NOTE: can not use log here since we are called from a log hook!
 func RebootReason(reason string, normal bool) {
-	// NOTE: can not use log here since we are called from a log hook!
-	fmt.Printf("RebootReason(%s)", reason)
 	filename := fmt.Sprintf("%s/%s", types.PersistDir, reasonFile)
 	dateStr := time.Now().Format(time.RFC3339Nano)
 	if !normal {
@@ -160,13 +158,15 @@ func RebootReason(reason string, normal bool) {
 		fmt.Printf("printToFile failed %s\n", err)
 	}
 
-	agentDebugDir := fmt.Sprintf("%s/%s/", types.PersistDebugDir, savedAgentName)
+	if !normal {
+		agentDebugDir := fmt.Sprintf("%s/%s/", types.PersistDebugDir, savedAgentName)
 
-	agentFatalReasonFilename := agentDebugDir + "/fatal-reason"
-	err = overWriteFile(agentFatalReasonFilename, reason)
-	if err != nil {
-		// Note: can not use log here since we are called from a log hook!
-		fmt.Printf("printToFile failed %s\n", err)
+		agentFatalReasonFilename := agentDebugDir + "/fatal-reason"
+		err = overWriteFile(agentFatalReasonFilename, reason)
+		if err != nil {
+			// Note: can not use log here since we are called from a log hook!
+			fmt.Printf("printToFile failed %s\n", err)
+		}
 	}
 
 	filename = "/persist/" + rebootImage
