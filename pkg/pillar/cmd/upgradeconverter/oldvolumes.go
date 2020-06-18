33876c8d3a932a81a450cb827f44d3f3051e199a
eriknordmark <erik@zededa.com>
2020-06-18 13:36:18 +0200

    upgradeconverter /persist layout migration for VM and OCI-based volumes assume generationCounter is different than purgeCounter and most likely zero parse/print format at end of pathname have to copy instead of rename due to vault
    Signed-off-by: eriknordmark <erik@zededa.com>

---
+++ pkg/pillar/cmd/upgradeconverter/oldvolumes.go
@@ -0,0 +1,75 @@
+// Copyright (c) 2020 Zededa, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package upgradeconverter
+
+// Look for old VM and OCI volumes in /persist
+
+import (
+	"io/ioutil"
+	"os"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	log "github.com/sirupsen/logrus"
+)
+
+// We fill in this based on the files/dirs we find on disk
+type oldVolume struct {
+	pathname     string
+	sha256       string
+	appInstID    uuid.UUID
+	purgeCounter uint32
+	format       string
+	modTime      time.Time // If we have multiple we pick latest
+}
+
+// recursive scanning for volumes
+func scanDir(dirName string, isContainer bool) []oldVolume {
+
+	log.Debugf("scanDir(%s)", dirName)
+	var old []oldVolume
+
+	locations, err := ioutil.ReadDir(dirName)
+	if err != nil {
+		log.Errorf("scanDir: read directory '%s' failed: %v",
+			dirName, err)
+		return old
+	}
+
+	for _, location := range locations {
+		filelocation := dirName + "/" + location.Name()
+		if location.IsDir() && !isContainer {
+			log.Debugf("scanDir: directory %s ignored", filelocation)
+			continue
+		}
+		info, err := os.Stat(filelocation)
+		if err != nil {
+			log.Errorf("Error in getting file information. Err: %s. "+
+				"Ignoring file %s", err, filelocation)
+			continue
+		}
+		_, sha256, appUUIDStr, purgeCounter, format := parseAppRwVolumeName(filelocation, isContainer)
+		log.Infof("scanDir: Processing sha256: %s, AppUuid: %s, "+
+			"fileLocation:%s, format:%s",
+			sha256, appUUIDStr, filelocation, format)
+
+		appUUID, err := uuid.FromString(appUUIDStr)
+		if err != nil {
+			log.Errorf("scanDir: Invalid UUIDStr(%s) in "+
+				"filename (%s). err: %s. Ignored",
+				appUUIDStr, filelocation, err)
+			continue
+		}
+		item := oldVolume{
+			sha256:       sha256,
+			appInstID:    appUUID,
+			purgeCounter: purgeCounter,
+			format:       format,
+			pathname:     filelocation,
+			modTime:      info.ModTime(),
+		}
+		old = append(old, item)
+	}
+	return old
+}
