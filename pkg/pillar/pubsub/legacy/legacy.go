bd5fe7348e45e2e2723ebdf7cbb593ba130ee97a
eriknordmark <erik@zededa.com>
2020-09-01 15:06:02 +0200

    remove use of legacy-unknown-agent for pubsub legacy logs
    Signed-off-by: eriknordmark <erik@zededa.com>
    (cherry picked from commit 9721e8f3d6fb5e1f23825ac49d37e2ad67429112)

--- pkg/pillar/pubsub/legacy/legacy.go
+++ pkg/pillar/pubsub/legacy/legacy.go
@@ -1,20 +1,42 @@
 package legacy
 
 import (
+	"os"
+	"sync"
+
 	"github.com/lf-edge/eve/pkg/pillar/base"
 	"github.com/lf-edge/eve/pkg/pillar/pubsub"
 	"github.com/lf-edge/eve/pkg/pillar/pubsub/socketdriver"
 )
 
+var onceLock = &sync.Mutex{}
+var onceVal bool
+
+// Protected by once()
 var (
-	basename      = "legacy-unknown-agent"
-	log           = base.NewSourceLogObject(basename, 0)
-	defaultPubsub = pubsub.New(&socketdriver.SocketDriver{Log: log}, log)
+	log           *base.LogObject
+	defaultPubsub *pubsub.PubSub
 )
+
+// once returns true the first time and then false
+func once() bool {
+	onceLock.Lock()
+	defer onceLock.Unlock()
+	if onceVal {
+		return false
+	} else {
+		onceVal = true
+		return true
+	}
+}
 
 // Publish create a `Publication` for the given agent name and topic type.
 // XXX remove? Used by ledmanagerutils.go
 func Publish(agentName string, topicType interface{}) (pubsub.Publication, error) {
+	if once() {
+		log = base.NewSourceLogObject(agentName, os.Getpid())
+		defaultPubsub = pubsub.New(&socketdriver.SocketDriver{Log: log}, log)
+	}
 	log.Debugf("legacy.Publish agentName(%s)", agentName)
 	return defaultPubsub.NewPublication(pubsub.PublicationOptions{
 		AgentName: agentName,
@@ -26,6 +48,10 @@ func Publish(agentName string, topicType interface{}) (pubsub.Publication, error
 // type, but with persistence of the messages across reboots.
 // XXX remove? Used by globalutils.go
 func PublishPersistent(agentName string, topicType interface{}) (pubsub.Publication, error) {
+	if once() {
+		log = base.NewSourceLogObject(agentName, os.Getpid())
+		defaultPubsub = pubsub.New(&socketdriver.SocketDriver{Log: log}, log)
+	}
 	log.Infof("legacy.PublishPersistent agentName(%s)", agentName)
 	return defaultPubsub.NewPublication(pubsub.PublicationOptions{
 		AgentName:  agentName,
