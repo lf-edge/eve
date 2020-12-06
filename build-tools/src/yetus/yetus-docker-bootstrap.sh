56a7ce28a9ea07ab317e2d1ca9b1090fcc911cde
Allen Wittenauer <aw@effectivemachines.com>
2020-12-05 23:31:57 -0800

    Upgrade to Apache Yetus 0.13.0
    Signed-off-by: Allen Wittenauer <aw@effectivemachines.com>

--- build-tools/src/yetus/yetus-docker-bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-export CGO_ENABLED=${CGO_ENABLED:-1}
-export GO111MODULE=${GO111MODULE:-on}
-export CGO_LDFLAGS=${CGO_LDFLAGS:--lm}
-export YETUS_OUT=${YETUS_OUT:-/tmp/yetus-out}
-apt-get -q update && apt-get -q install --no-install-recommends -y libpcap-dev
