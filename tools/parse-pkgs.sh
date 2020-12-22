9c857818b4dc3871612159c0fabf7e7f103aad86
Sergey Temerkhanov <s.temerkhanov@gmail.com>
2020-12-22 10:07:16 +0300

    eve: Support SPDK package
    Signed-off-by: Sergey Temerkhanov <s.temerkhanov@gmail.com>

--- tools/parse-pkgs.sh
+++ tools/parse-pkgs.sh
@@ -86,6 +86,7 @@ sed -e '/-.*linuxkit\/.*:/s# *$#'${ARCH}# \
     -e "s#UEFI_TAG#${UEFI_TAG}#" \
     -e "s#EVE_TAG#${EVE_TAG}#" \
     -e "s#KVMTOOLS_TAG#${KVMTOOLS_TAG}#" \
+    -e "s#SPDK_TAG#${SPDK_TAG}#" \
     ${1:-}
 }
 
@@ -132,6 +133,7 @@ DEBUG_TAG=$(linuxkit_tag pkg/debug)
 VTPM_TAG=$(linuxkit_tag pkg/vtpm)
 UEFI_TAG=$(linuxkit_tag pkg/uefi)
 KVMTOOLS_TAG=$(linuxkit_tag pkg/kvm-tools)
+SPDK_TAG=$(linuxkit_tag pkg/spdk)
 
 # Synthetic tags: the following tags are based on hashing
 # the contents of all the Dockerfile.in that we can find.
