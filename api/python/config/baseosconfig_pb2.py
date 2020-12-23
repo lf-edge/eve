b20d5a8e25a920a148c509dcb170cf94f7122abe
Roman Shaposhnik <rvs@zededa.com>
2020-12-22 20:12:07 -0800

    Building proto and vendoring it
    Signed-off-by: Roman Shaposhnik <rvs@zededa.com>

--- api/python/config/baseosconfig_pb2.py
+++ api/python/config/baseosconfig_pb2.py
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'\n\025org.lfedge.eve.configZ$github.com/lf-edge/eve/api/go/config',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x19\x63onfig/baseosconfig.proto\x12\x15org.lfedge.eve.config\x1a\x16\x63onfig/devcommon.proto\x1a\x14\x63onfig/storage.proto\"\x0b\n\tOSKeyTags\"\x0e\n\x0cOSVerDetails\"\xa4\x01\n\x0c\x42\x61seOSConfig\x12=\n\x0euuidandversion\x18\x01 \x01(\x0b\x32%.org.lfedge.eve.config.UUIDandVersion\x12,\n\x06\x64rives\x18\x03 \x03(\x0b\x32\x1c.org.lfedge.eve.config.Drive\x12\x10\n\x08\x61\x63tivate\x18\x04 \x01(\x08\x12\x15\n\rbaseOSVersion\x18\n \x01(\tB=\n\x15org.lfedge.eve.configZ$github.com/lf-edge/eve/api/go/configb\x06proto3'
+  serialized_pb=b'\n\x19\x63onfig/baseosconfig.proto\x12\x15org.lfedge.eve.config\x1a\x16\x63onfig/devcommon.proto\x1a\x14\x63onfig/storage.proto\"\x0b\n\tOSKeyTags\"\x0e\n\x0cOSVerDetails\"\xb6\x01\n\x0c\x42\x61seOSConfig\x12=\n\x0euuidandversion\x18\x01 \x01(\x0b\x32%.org.lfedge.eve.config.UUIDandVersion\x12,\n\x06\x64rives\x18\x03 \x03(\x0b\x32\x1c.org.lfedge.eve.config.Drive\x12\x10\n\x08\x61\x63tivate\x18\x04 \x01(\x08\x12\x15\n\rbaseOSVersion\x18\n \x01(\t\x12\x10\n\x08volumeID\x18\x0c \x01(\tB=\n\x15org.lfedge.eve.configZ$github.com/lf-edge/eve/api/go/configb\x06proto3'
   ,
   dependencies=[config_dot_devcommon__pb2.DESCRIPTOR,config_dot_storage__pb2.DESCRIPTOR,])
 
@@ -114,6 +114,13 @@ _BASEOSCONFIG = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='volumeID', full_name='org.lfedge.eve.config.BaseOSConfig.volumeID', index=4,
+      number=12, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -127,7 +134,7 @@ _BASEOSCONFIG = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=128,
-  serialized_end=292,
+  serialized_end=310,
 )
 
 _BASEOSCONFIG.fields_by_name['uuidandversion'].message_type = config_dot_devcommon__pb2._UUIDANDVERSION
