410804ecafa4c69babd0ee1b8f6dec293d76f652
gkodali-zededa <gkodali@zededa.com>
2021-04-15 18:53:57 +0000

    Add object logging to memory/disk notifications
    Signed-off-by: gkodali-zededa <gkodali@zededa.com>

--- pkg/pillar/types/watcher.go
+++ pkg/pillar/types/watcher.go
@@ -5,6 +5,7 @@ package types
 
 import (
 	"github.com/lf-edge/eve/pkg/pillar/base"
+	uuid "github.com/satori/go.uuid"
 )
 
 type UsageZone int
@@ -28,12 +29,59 @@ type MemoryNotification struct {
 	PrevSlab  uint64
 }
 
+func zonetoString(zone UsageZone) string {
+	switch zone {
+	case GREEN_ZONE:
+		return "GREEN"
+	case YELLOW_ZONE:
+		return "YELLOW"
+	case ORANGE_ZONE:
+		return "ORANGE"
+	case RED_ZONE:
+		return "RED"
+	}
+	return "UNKNOWN"
+}
+
 // LogCreate :
 func (mem MemoryNotification) LogCreate(logBase *base.LogObject) {
+	logObject := base.NewLogObject(logBase, base.MemoryNotificationType, "MemoryNotification",
+		uuid.UUID{}, mem.LogKey())
+	if logObject == nil {
+		return
+	}
+	logObject.CloneAndAddField("Total", mem.Total).
+		CloneAndAddField("Used", mem.Used).
+		CloneAndAddField("Zone", zonetoString(mem.Zone)).
+		CloneAndAddField("PrevUsage", mem.PrevUsage).
+		CloneAndAddField("LastFive", mem.LastFive).
+		CloneAndAddField("UsageSlab", mem.UsageSlab).
+		CloneAndAddField("PrevSlab", mem.PrevSlab).
+		Noticef("MemoryNotification create")
 }
 
 // LogModify :
 func (mem MemoryNotification) LogModify(logBase *base.LogObject, old interface{}) {
+	logObject := base.NewLogObject(logBase, base.MemoryNotificationType, "MemoryNotification",
+		uuid.UUID{}, mem.LogKey())
+	if logObject == nil {
+		return
+	}
+	oldMem, ok := old.(MemoryNotification)
+	if !ok {
+		return
+	}
+	if mem.UsageSlab == oldMem.UsageSlab && mem.Zone == oldMem.Zone {
+		return
+	}
+	logObject.CloneAndAddField("Total", mem.Total).
+		CloneAndAddField("Used", mem.Used).
+		CloneAndAddField("Zone", zonetoString(mem.Zone)).
+		CloneAndAddField("PrevUsage", mem.PrevUsage).
+		CloneAndAddField("LastFive", mem.LastFive).
+		CloneAndAddField("UsageSlab", mem.UsageSlab).
+		CloneAndAddField("PrevSlab", mem.PrevSlab).
+		Noticef("MemoryNotification modify")
 }
 
 // LogDelete :
@@ -42,7 +90,7 @@ func (mem MemoryNotification) LogDelete(logBase *base.LogObject) {
 
 // LogKey :
 func (mem MemoryNotification) LogKey() string {
-	return ""
+	return "MemoryNotification"
 }
 
 type DiskNotification struct {
@@ -59,10 +107,43 @@ type DiskNotification struct {
 
 // LogCreate :
 func (disk DiskNotification) LogCreate(logBase *base.LogObject) {
+	logObject := base.NewLogObject(logBase, base.DiskNotificationType, "DiskNotification",
+		uuid.UUID{}, disk.LogKey())
+	if logObject == nil {
+		return
+	}
+	logObject.CloneAndAddField("Total", disk.Total).
+		CloneAndAddField("Used", disk.Used).
+		CloneAndAddField("Zone", zonetoString(disk.Zone)).
+		CloneAndAddField("PrevUsage", disk.PrevUsage).
+		CloneAndAddField("LastFive", disk.LastFive).
+		CloneAndAddField("UsageSlab", disk.UsageSlab).
+		CloneAndAddField("PrevSlab", disk.PrevSlab).
+		Noticef("DiskNotification create")
 }
 
 // LogModify :
 func (disk DiskNotification) LogModify(logBase *base.LogObject, old interface{}) {
+	logObject := base.NewLogObject(logBase, base.DiskNotificationType, "DiskNotification",
+		uuid.UUID{}, disk.LogKey())
+	if logObject == nil {
+		return
+	}
+	oldDisk, ok := old.(DiskNotification)
+	if !ok {
+		return
+	}
+	if disk.UsageSlab == oldDisk.UsageSlab && disk.Zone == oldDisk.Zone {
+		return
+	}
+	logObject.CloneAndAddField("Total", disk.Total).
+		CloneAndAddField("Used", disk.Used).
+		CloneAndAddField("Zone", zonetoString(disk.Zone)).
+		CloneAndAddField("PrevUsage", disk.PrevUsage).
+		CloneAndAddField("LastFive", disk.LastFive).
+		CloneAndAddField("UsageSlab", disk.UsageSlab).
+		CloneAndAddField("PrevSlab", disk.PrevSlab).
+		Noticef("DiskNotification modify")
 }
 
 // LogDelete :
@@ -71,5 +152,5 @@ func (disk DiskNotification) LogDelete(logBase *base.LogObject) {
 
 // LogKey :
 func (disk DiskNotification) LogKey() string {
-	return ""
+	return "DiskNotification"
 }
