5cdc6c28316f39a9b9772f28bc53af5b67efb191
Milan Lenco <milan@zededa.com>
2021-08-31 19:47:44 +0200

    Refactor wwan service, add radio-silence support
    wwan service now supports multiple LTE modems.
    It waits for and reads configuration from /run/wwan/config.json and publishes
    state information into status.json and metrics into metrics.json under
    the same directory.
    
    wwan service was further improved to:
     - allow to reference modem(s) using configurable physical addresses
       (pci / usb port / interface name)
     - support configuration change
     - react to configuration change immediately (by watching for
       config.json file changes instead of polling with a long time interval)
     - support radio-silence mode (i.e. allow to switch the radio on/off)
     - allow to configure IP address for probing
     - publish configuration/probing error up to the zedbox
     - publish packet stats, modem & SIM card information, operating mode and more
     - unify format of published state/metrics data between qmi and mbim protocols
       (i.e. zedbox does not need to know which protocol is being used)
     - acknowledge applied configuration with a SHA256 checksum
     - prepare support for multiple modems (not tested)
    
    For each network/modem it is still limited to only a single APN.
    
    Signed-off-by: Milan Lenco <milan@zededa.com>
    (cherry picked from commit b6f14aa27bc881efce50283e88ed70ea2224be55)

--- pkg/wwan/usr/bin/wwan-init.sh
+++ pkg/wwan/usr/bin/wwan-init.sh
@@ -13,36 +13,46 @@
 # Some inspiration (but not the code!) taken from:
 #    https://github.com/openwrt-mirror/openwrt/blob/master/package/network/utils/uqmi/files/lib/netifd/proto/qmi.sh
 BBS=/run/wwan
+CONFIG_PATH="${BBS}/config.json"
+STATUS_PATH="${BBS}/status.json"
+METRICS_PATH="${BBS}/metrics.json"
 
-# FIXME: we really need to pick the following from some config
-IFACE=wwan0
-CDC_DEV=cdc-wdm0
-MODE=qmi
-
-WATCHDOG_TIMEOUT=300
 LTESTAT_TIMEOUT=120
+PROBE_INTERVAL=300  # how often to probe the connectivity status (in seconds)
+METRICS_INTERVAL=60 # how often to obtain and publish metrics (in seconds)
+UNAVAIL_SIGNAL_METRIC=$(printf "%d" 0x7FFFFFFF) # max int32
 
-get_apn() {
-  APN="$(cat /run/accesspoint/"$IFACE" 2>/dev/null)"
-  echo "${APN:-internetd.gdsp}"
+DEFAULT_APN="internet"
+DEFAULT_PROBE_ADDR="8.8.8.8"
+
+IPV4_REGEXP='[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+'
+
+SRC=$(cd $(dirname "$0"); pwd)
+. "${SRC}/wwan-qmi.sh"
+. "${SRC}/wwan-mbim.sh"
+
+json_attr() {
+  printf '"%s":%s' "$1" "$2"
 }
 
-mbus_publish() {
-  [ -d "$BBS" ] || mkdir -p $BBS || exit 1
-  cat > "$BBS/${1}.json"
+json_str_attr() {
+  printf '"%s":"%s"' "$1" "$2"
 }
 
-mbim() {
-  timeout -s KILL "$LTESTAT_TIMEOUT" mbimcli -p -d "/dev/$CDC_DEV" "$@"
+json_struct() {
+  local ITEMS="$(for ARG in "${@}"; do printf ",%s" "$ARG"; done | cut -c2-)"
+  printf "{%s}" "$ITEMS"
 }
 
-qmi() {
-  JSON=`timeout -s KILL "$LTESTAT_TIMEOUT" uqmi -d "/dev/$CDC_DEV" "$@" 2>&1`
-  if [ $? -eq 0 ] && (echo "$JSON" | jq -ea . > /dev/null 2>&1) ; then
-    echo "$JSON"
-    return 0
-  fi
-  return 1
+json_array() {
+  local ITEMS="$(while read LINE; do [ ! -z "${LINE}" ] && printf ",%s" "${LINE}"; done | cut -c2-)"
+  printf "[%s]" "$ITEMS"
+}
+
+parse_json_attr() {
+  local JSON="$1"
+  local JSON_PATH="$2"
+  echo $JSON | jq -rc ".$JSON_PATH | select (.!=null)"
 }
 
 mod_reload() {
@@ -57,29 +67,8 @@ mod_reload() {
   done
 }
 
-start_network() {
-  echo "Starting network for APN $(get_apn)"
-  if [ "$MODE" = mbim ]; then
-     # NOTE that after --attach-packet-service we may end in a state
-     # where packet service is attached but WDS hasn't come up online
-     # just yet. We're blocking on WDS in wait_for_wds(). However, it
-     # may be useful to check --query-packet-service-state just in case.
-     mbim --attach-packet-service
-     sleep 10
-     mbim --connect="apn='$(get_apn)'"
-  else
-     ip link set $IFACE down
-     echo Y > /sys/class/net/$IFACE/qmi/raw_ip
-     ip link set $IFACE up
-
-     qmi --sync
-     qmi --start-network --apn "$(get_apn)" --keep-client-id wds |\
-         mbus_publish pdh_$IFACE
-  fi
-}
-
 wait_for() {
-  EXPECT="$1"
+  local EXPECT="$1"
   shift
   for i in `seq 1 10`; do
      eval RES='"$('"$*"')"'
@@ -89,56 +78,115 @@ wait_for() {
   return 1
 }
 
-wait_for_sim() {
-  # FIXME XXX this is only for MBIM for now
-  if [ "$MODE" = mbim ]; then
-     CMD="mbim --query-subscriber-ready-status | grep -q 'Ready state: .initialized.' && echo initialized"
-  else
-     CMD="echo initialized"
-  fi
-
-  wait_for initialized "$CMD"
+mbus_publish() {
+  [ -d "$BBS" ] || mkdir -p $BBS || exit 1
+  cat > "$BBS/${1}.json"
 }
 
-wait_for_wds() {
-  echo "Waiting for DATA services to connect"
-  if [ "$MODE" = mbim ]; then
-     # FIXME XXX there seems to be cases where this looks like connected
-     CMD="mbim --query-connection-state | grep -q 'Activation state: .activated.' && echo connected"
-  else
-     CMD="qmi --get-data-status | jq -r ."
+# parse value of an attribute returned by mbimcli or qmicli
+parse_modem_attr() {
+  local STDOUT="$1"
+  local ATTR="$2"
+  local VAL="$(echo "$STDOUT" | sed -n "s/\s*$ATTR: \(.*\)/\1/p" | tr -d "'")"
+  if [ "$VAL" = "unknown" ]; then
+    # "unknown" is used by VALIDATE_UNKNOWN macro in libqmi and libmbim
+    VAL=""
   fi
-
-  wait_for connected "$CMD"
+  printf "%s" "$VAL"
 }
 
-wait_for_register() {
-  echo "Waiting for the device to register on the network"
-  if [ "$MODE" = mbim ]; then
-     CMD="mbim --query-registration-state | grep -qE 'Register state:.*(home|roaming|partner)' && echo registered"
-  else
-     CMD="qmi --get-serving-system | jq -r .registration"
-  fi
-
-  wait_for registered "$CMD"
+config_checksum() {
+  local CONFIG="$1"
+  printf "%s" "$CONFIG" | sha256sum | cut -d " " -f1
 }
 
-wait_for_settings() {
-  echo "Waiting for IP configuration for the $IFACE interface"
-  if [ "$MODE" = mbim ]; then
-     CMD="mbim --query-ip-configuration"
-  else
-     CMD="qmi --get-current-settings"
-  fi
+sys_get_modem_protocol() {
+  local SYS_DEV="$1"
+  local MODULE="$(basename "$(readlink "${SYS_DEV}/device/driver/module")")"
+  case "$MODULE" in
+    "cdc_mbim") echo "mbim"
+    ;;
+    "qmi_wwan") echo "qmi"
+    ;;
+    *) return 1
+    ;;
+  esac
+}
 
-  wait_for connected "$CMD | jq -r .ipv4.ip | grep -q '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*' && echo connected"
+sys_get_modem_interface() {
+  local SYS_DEV="$1"
+  ls "${SYS_DEV}/device/net"
+}
+
+sys_get_modem_usbaddr() {
+  local SYS_DEV="$1"
+  local DEV_PATH="$(readlink -f "${SYS_DEV}/device")"
+  while [ -e "$DEV_PATH/subsystem" ]; do
+    if [ "$(basename "$(readlink "$DEV_PATH/subsystem")")" != "usb" ]; then
+      DEV_PATH="$(dirname "$DEV_PATH")"
+      continue
+    fi
+    echo "$(basename $DEV_PATH | cut -d ":" -f 1 | tr '-' ':')"
+    return
+  done
+}
+
+sys_get_modem_pciaddr() {
+  local SYS_DEV="$1"
+  local DEV_PATH="$(readlink -f "${DEV}/device")"
+  while [ -e "$DEV_PATH/subsystem" ]; do
+    if [ "$(basename "$(readlink "$DEV_PATH/subsystem")")" != "pci" ]; then
+      DEV_PATH="$(dirname "$DEV_PATH")"
+    continue
+    fi
+    echo "$(basename $DEV_PATH)"
+    return
+  done
+}
+
+# If successful, sets CDC_DEV, PROTOCOL, IFACE, USB_ADDR and PCI_ADDR variables.
+lookup_modem() {
+  local ARG_IF="$1"
+  local ARG_USB="$2"
+  local ARG_PCI="$3"
+
+  for DEV in /sys/class/usbmisc/*; do
+    DEV_PROT=$(sys_get_modem_protocol "$DEV") || continue
+
+    # check interface name
+    DEV_IF="$(sys_get_modem_interface "$DEV")"
+    [ ! -z "$ARG_IF" ] && [ "$ARG_IF" != "$DEV_IF" ] && continue
+
+    # check USB address
+    DEV_USB="$(sys_get_modem_usbaddr "$DEV")"
+    [ ! -z "$ARG_USB" ] && [ "$ARG_USB" != "$DEV_USB" ] && continue
+
+    # check PCI address
+    DEV_PCI="$(sys_get_modem_pciaddr "$DEV")"
+    [ ! -z "$ARG_PCI" ] && [ "$ARG_PCI" != "$DEV_PCI" ] && continue
+
+    PROTOCOL="$DEV_PROT"
+    IFACE="$DEV_IF"
+    USB_ADDR="$DEV_USB"
+    PCI_ADDR="$DEV_PCI"
+    CDC_DEV="$(basename "${DEV}")"
+    return 0
+  done
+
+  echo "Failed to find modem for "\
+    "interface=${ARG_IF:-<ANY>}, USB=${ARG_USB:-<ANY>}, PCI=${ARG_PCI:-<ANY>}" >&2
+  return 1
 }
 
 bringup_iface() {
-  if [ "$MODE" = mbim ]; then
-     JSON=$(mbim --query-ip-configuration)
+  if [ "$PROTOCOL" = mbim ]; then
+     local JSON=$(mbim --query-ip-configuration)
+     local DNS0="dns0"
+     local DNS1="dns1"
   else
-     JSON=$(qmi --get-current-settings)
+     local JSON=$(uqmi --get-current-settings)
+     local DNS0="dns1"
+     local DNS1="dns2"
   fi
   ifconfig $IFACE `echo "$JSON" | jq -r .ipv4.ip` \
                    netmask `echo "$JSON" | jq -r .ipv4.subnet` \
@@ -148,85 +196,256 @@ bringup_iface() {
   ip route add default via `echo "$JSON" | jq -r .ipv4.gateway` dev $IFACE metric 65000
   mkdir $BBS/resolv.conf || :
   cat > $BBS/resolv.conf/${IFACE}.dhcp <<__EOT__
-nameserver `echo "$JSON" | jq -r .ipv4.dns1`
-nameserver `echo "$JSON" | jq -r .ipv4.dns2`
+nameserver `echo "$JSON" | jq -r .ipv4.$DNS0`
+nameserver `echo "$JSON" | jq -r .ipv4.$DNS1`
 __EOT__
 }
 
-scan_providers() {
-  if [ "$MODE" = mbim ]; then
-     # FIXME XXX this seems to return Busy more often than not
-     # detach-packet-service may be required
-     mbim --query-visible-providers |\
-          mbus_publish networks-info
-     return $?
+probe() {
+  if [ "$PROBE_DISABLED" = "true" ]; then
+    # probing disabled, skip it
+    unset PROBE_ERROR
+    return 0
   fi
-
-  qmi --network-scan |\
-      mbus_publish networks-info
-}
-
-reset_modem() {
-  if [ "$MODE" = mbim ]; then
-     mbim --disconnect
-     mbim --detach-packet-service
-     return $?
-  fi
-
-  # last ditch attempt to reset our modem -- not sure how effective :-(
-  # mod_reload qcserial usb_wwan qmi_wwan cdc_wdm
-  local PDH=`cat $BBS/pdh_$IFACE.json 2>/dev/null`
-
-  for i in $PDH 0xFFFFFFFF ; do
-    qmi --stop-network $i --autoconnect
-  done
-
-  qmi --reset-dms
-
-  for i in $PDH 0xFFFFFFFF ; do
-    qmi --stop-network $i --autoconnect
-  done
-}
-
-detect_mode() {
-  modprobe -a qcserial usb_wwan qmi_wwan cdc_wdm cdc_mbim cdc_acm
-  if [ "$(basename "$(readlink "/sys/class/usbmisc/$CDC_DEV/device/driver/module")")" = cdc_mbim ]; then
-     MODE=mbim
-  fi
-}
-
-# lets start with detecting what we're dealing with
-detect_mode
-echo "Starting wwan manager in $MODE mode"
-
-# poor man's watchdog
-while true ; do
   # ping is supposed to return 0 even if just a single packet out of 3 gets through
-  if ! ping -W 20 -w 20 -c 3 -I $IFACE 8.8.8.8 > /dev/null 2>&1 ; then
-    reset_modem
-
-    # lets see what networks are available still
-    scan_providers
-
-    # hopefully we can recover now
-    wait_for_sim || continue
-    wait_for_register || continue
-    start_network
-    wait_for_wds || continue
-    wait_for_settings || continue
-    bringup_iface
-  fi
-
-  # collect current stats
-  if [ "$MODE" = mbim ]; then
-     for i in home-provider signal-state ip-configuration ; do
-         mbim --query-$i | mbus_publish $i
-     done
+  local PROBE_OUTPUT
+  PROBE_OUTPUT="$(ping -W 20 -w 20 -c 3 -I $IFACE $PROBE_ADDR 2>&1)"
+  if [ $? -eq 0 ]; then
+    unset PROBE_ERROR
+    return 0
   else
-     for i in serving-system signal-info current-settings ; do
-         qmi --get-$i | mbus_publish $i
-     done
+    PROBE_ERROR="$(printf "%s" "$PROBE_OUTPUT" | grep "packet loss")"
+    if [ -z "$PROBE_ERROR" ]; then
+      PROBE_ERROR="$PROBE_OUTPUT"
+    fi
+    PROBE_ERROR="Failed to ping $PROBE_ADDR via $IFACE: $PROBE_ERROR"
+    return 1
+  fi
+}
+
+collect_network_status() {
+  local QUICK="$1"
+  local PROVIDERS="[]"
+  if [ "$QUICK" != "y" ]; then
+    # The process of scanning for available providers takes up to 1 minute.
+    # It is done only during PROBING events and skipped when config is changed
+    # (e.g. radio-silence mode is switched ON/OFF) so that the updated status is promptly
+    # published for better user experience.
+    PROVIDERS="$(${PROTOCOL}_get_providers)"
+  fi
+  local MODULE="$(json_struct \
+    "$(json_str_attr imei     "$(${PROTOCOL}_get_imei)")" \
+    "$(json_str_attr model    "$(${PROTOCOL}_get_modem_model)")" \
+    "$(json_str_attr revision "$(${PROTOCOL}_get_modem_revision)")" \
+    "$(json_str_attr control-protocol "$PROTOCOL")" \
+    "$(json_str_attr operating-mode   "$(${PROTOCOL}_get_op_mode)")")"
+  local NETWORK_STATUS="$(json_struct \
+    "$(json_str_attr logical-label    "$LOGICAL_LABEL")" \
+    "$(json_attr     physical-addrs   "$ADDRS")" \
+    "$(json_attr     cellular-module  "$MODULE")" \
+    "$(json_attr     sim-cards        "$(${PROTOCOL}_get_sim_cards)")" \
+    "$(json_str_attr config-error     "$CONFIG_ERROR")" \
+    "$(json_str_attr probe-error      "$PROBE_ERROR")" \
+    "$(json_attr     providers        "$PROVIDERS")")"
+  STATUS="${STATUS}${NETWORK_STATUS}\n"
+}
+
+collect_network_metrics() {
+  local NETWORK_METRICS="$(json_struct \
+    "$(json_str_attr logical-label  "$LOGICAL_LABEL")" \
+    "$(json_attr     physical-addrs "$ADDRS")" \
+    "$(json_attr     packet-stats   "$(${PROTOCOL}_get_packet_stats)")" \
+    "$(json_attr     signal-info    "$(${PROTOCOL}_get_signal_info)")")"
+  METRICS="${METRICS}${NETWORK_METRICS}\n"
+}
+
+event_stream() {
+  inotifywait -qm ${BBS} -e create -e modify -e delete &
+  while true; do
+    echo "PROBE"
+    sleep $PROBE_INTERVAL
+  done &
+  while true; do
+    echo "METRICS"
+    sleep $METRICS_INTERVAL
+  done
+}
+
+echo "Starting wwan manager"
+mkdir -p ${BBS}
+modprobe -a qcserial usb_wwan qmi_wwan cdc_wdm cdc_mbim cdc_acm
+
+# For cellular modems we do not rely on rfkill to enable/disable radio transmission.
+# This is because rfkill driver for wwan is often not available.
+# Instead, the operational state and RF of cellular modems is managed via QMI or MBIM.
+# But should rfkill driver for wwan be provided and because by default RF is blocked
+# for all wireless devices (rfkill.default_state=0), we need to preventively unblock rfkill
+# for wwan to ensure that it doesn't override wwan RF state as configured from here.
+rfkill unblock wwan
+
+# Main event loop
+event_stream | while read -r EVENT; do
+  if ! echo "$EVENT" | grep -q "PROBE\|METRICS\|config.json"; then
+    continue
   fi
 
-  sleep $WATCHDOG_TIMEOUT
+  CONFIG_CHANGE=n
+  if [ "$EVENT" != "PROBE" ] && [ "$EVENT" != "METRICS" ]; then
+    CONFIG_CHANGE=y
+  fi
+
+  CONFIG="$(cat "${CONFIG_PATH}" 2>/dev/null)"
+  if [ "$CONFIG_CHANGE" = "y" ]; then
+    if [ "$LAST_CONFIG" = "$CONFIG" ]; then
+      # spurious notification, ignore
+      continue
+    else
+      LAST_CONFIG="$CONFIG"
+    fi
+  fi
+  CHECKSUM="$(config_checksum "$CONFIG")"
+
+  unset MODEMS
+  unset STATUS
+  unset METRICS
+  RADIO_SILENCE="$(parse_json_attr "$CONFIG" "\"radio-silence\"")"
+
+  # iterate over each configured cellular network
+  while read NETWORK; do
+    [ -z "$NETWORK" ] && continue
+    unset CONFIG_ERROR
+    unset PROBE_ERROR
+
+    # parse network configuration
+    LOGICAL_LABEL="$(parse_json_attr "$NETWORK" "\"logical-label\"")"
+    ADDRS="$(parse_json_attr "$NETWORK" "\"physical-addrs\"")"
+    IFACE="$(parse_json_attr "$ADDRS" "interface")"
+    USB_ADDR="$(parse_json_attr "$ADDRS" "usb")"
+    PCI_ADDR="$(parse_json_attr "$ADDRS" "pci")"
+    PROBE="$(parse_json_attr "$NETWORK" "probe")"
+    PROBE_DISABLED="$(parse_json_attr "$PROBE" "disable")"
+    PROBE_ADDR="$(parse_json_attr "$PROBE" "address")"
+    PROBE_ADDR="${PROBE_ADDR:-$DEFAULT_PROBE_ADDR}"
+    APN="$(parse_json_attr "$NETWORK" "apns[0]")" # FIXME XXX limited to a single APN for now
+    APN="${APN:-$DEFAULT_APN}"
+
+    lookup_modem "${IFACE}" "${USB_ADDR}" "${PCI_ADDR}" 2>/tmp/wwan.stderr
+    if [ $? -ne 0 ]; then
+      CONFIG_ERROR="$(cat /tmp/wwan.stderr)"
+      NETWORK_STATUS="$(json_struct \
+        "$(json_str_attr logical-label  "$LOGICAL_LABEL")" \
+        "$(json_attr     physical-addrs "$ADDRS")" \
+        "$(json_str_attr config-error   "$CONFIG_ERROR")")"
+      STATUS="${STATUS}${NETWORK_STATUS}\n"
+      continue
+    fi
+    MODEMS="${MODEMS}${CDC_DEV}\n"
+    echo "Processing managed modem (event: $EVENT): $CDC_DEV"
+
+    # in status.json and metrics.json print all modem addresses (as found by lookup_modem),
+    # not just the ones used in config.json
+    ADDRS="$(json_struct \
+      "$(json_str_attr interface "$IFACE")" \
+      "$(json_str_attr usb       "$USB_ADDR")" \
+      "$(json_str_attr pci       "$PCI_ADDR")")"
+
+    if [ "$EVENT" = "METRICS" ]; then
+      collect_network_metrics 2>/dev/null
+      continue
+    fi
+
+    # reflect updated config or just probe the current status
+    if [ "$RADIO_SILENCE" != "true" ]; then
+      if [ "$CONFIG_CHANGE" = "y" ] || ! probe; then
+        echo "[$CDC_DEV] Restarting connection (APN=${APN}, interface=${IFACE})"
+        {
+          ${PROTOCOL}_reset_modem       &&\
+          ${PROTOCOL}_toggle_rf on      &&\
+          ${PROTOCOL}_wait_for_sim      &&\
+          ${PROTOCOL}_wait_for_register &&\
+          ${PROTOCOL}_start_network     &&\
+          ${PROTOCOL}_wait_for_wds      &&\
+          ${PROTOCOL}_wait_for_settings &&\
+          bringup_iface                 &&\
+          echo "[$CDC_DEV] Connection successfully restarted"
+        } 2>/tmp/wwan.stderr
+        RV=$?
+        if [ $RV -ne 0 ]; then
+          CONFIG_ERROR="$(cat /tmp/wwan.stderr | sort -u)"
+          CONFIG_ERROR="${CONFIG_ERROR:-(Re)Connection attempt failed with rv=$RV}"
+        fi
+        # retry probe to update PROBE_ERROR
+        sleep 3
+        probe
+      fi
+    else # Radio-silence is ON
+      if [ "$(${PROTOCOL}_get_op_mode)" != "radio-off" ]; then
+        echo "[$CDC_DEV] Trying to disable radio (APN=${APN}, interface=${IFACE})"
+        ${PROTOCOL}_toggle_rf off 2>/tmp/wwan.stderr
+        if [ $? -ne 0 ]; then
+          CONFIG_ERROR="$(cat /tmp/wwan.stderr)"
+        else
+          if ! wait_for radio-off "${PROTOCOL}_get_op_mode"; then
+            CONFIG_ERROR="Timeout waiting for radio to turn off"
+          fi
+        fi
+      fi
+    fi
+
+    collect_network_status "$CONFIG_CHANGE"
+  done <<__EOT__
+  $(echo "$CONFIG" | jq -c '.networks[]' 2>/dev/null)
+__EOT__
+
+  # manage RF state also for modems not configured by the controller
+  for DEV in /sys/class/usbmisc/*; do
+    unset CONFIG_ERROR
+    unset PROBE_ERROR
+    unset LOGICAL_LABEL # unmanaged modems do not have logical name
+
+    PROTOCOL=$(sys_get_modem_protocol "$DEV") || continue
+    CDC_DEV="$(basename "${DEV}")"
+    if printf "%b" "$MODEMS" | grep -q "^$CDC_DEV$"; then
+      # this modem has configuration and was already processed
+      continue
+    fi
+    echo "Processing unmanaged modem (event: $EVENT): $CDC_DEV"
+    IFACE=$(sys_get_modem_interface "$DEV")
+    USB_ADDR=$(sys_get_modem_usbaddr "$DEV")
+    PCI_ADDR=$(sys_get_modem_pciaddr "$DEV")
+    ADDRS="$(json_struct \
+        "$(json_str_attr interface "$IFACE")" \
+        "$(json_str_attr usb       "$USB_ADDR")" \
+        "$(json_str_attr pci       "$PCI_ADDR")")"
+
+    if [ "$EVENT" = "METRICS" ]; then
+      collect_network_metrics 2>/dev/null
+      continue
+    fi
+
+    if [ "$(${PROTOCOL}_get_op_mode)" != "radio-off" ]; then
+      echo "[$CDC_DEV] Trying to disable radio (interface=${IFACE})"
+      ${PROTOCOL}_toggle_rf off 2>/tmp/wwan.stderr
+      if [ $? -ne 0 ]; then
+        CONFIG_ERROR="$(cat /tmp/wwan.stderr)"
+      else
+        if ! wait_for radio-off "${PROTOCOL}_get_op_mode"; then
+          CONFIG_ERROR="Timeout waiting for radio to turn off"
+        fi
+      fi
+    fi
+
+    collect_network_status "$CONFIG_CHANGE"
+  done
+
+  if [ "$EVENT" = "METRICS" ]; then
+    json_struct \
+      "$(json_attr networks "$(printf "%b" "$METRICS" | json_array)")" \
+        | jq > "$METRICS_PATH"
+  else
+    json_struct \
+      "$(json_attr     networks        "$(printf "%b" "$STATUS" | json_array)")" \
+      "$(json_str_attr config-checksum "$CHECKSUM")" \
+        | jq > "$STATUS_PATH"
+  fi
 done
---
+++ pkg/wwan/usr/bin/wwan-qmi.sh
@@ -0,0 +1,220 @@
+uqmi() {
+  local JSON
+  JSON=`timeout -s KILL "$LTESTAT_TIMEOUT" uqmi -d "/dev/$CDC_DEV" "$@"`
+  if [ $? -eq 0 ] && (echo "$JSON" | jq -ea . > /dev/null 2>&1) ; then
+    echo "$JSON"
+    return 0
+  fi
+  return 1
+}
+
+# We prefer to use uqmi over qmicli as a CLI agent for QMI devices.
+# However, some fields are available for retrieval only with qmicli.
+qmicli() {
+  timeout -s KILL "$LTESTAT_TIMEOUT" qmicli -d "/dev/$CDC_DEV" "$@"
+}
+
+qmi_get_packet_stats() {
+  local STATS="$(qmicli --wds-get-packet-statistics)"
+  local TXP=$(parse_modem_attr "$STATS" "TX packets OK")
+  local TXB=$(parse_modem_attr "$STATS" "TX bytes OK")
+  local TXD=$(parse_modem_attr "$STATS" "TX packets dropped")
+  local RXP=$(parse_modem_attr "$STATS" "RX packets OK")
+  local RXB=$(parse_modem_attr "$STATS" "RX bytes OK")
+  local RXD=$(parse_modem_attr "$STATS" "RX packets dropped")
+  json_struct \
+    "$(json_attr tx-bytes ${TXB:-0})" "$(json_attr tx-packets ${TXP:-0})" "$(json_attr tx-drops ${TXD:-0})" \
+    "$(json_attr rx-bytes ${RXB:-0})" "$(json_attr rx-packets ${RXP:-0})" "$(json_attr rx-drops ${RXD:-0})"
+}
+
+qmi_get_signal_info() {
+  local INFO
+  INFO="$(uqmi --get-signal-info)" || INFO="{}"
+  FILTER="{rssi: (if .rssi == null then $UNAVAIL_SIGNAL_METRIC else .rssi end),
+           rsrq: (if .rsrq == null then $UNAVAIL_SIGNAL_METRIC else .rsrq end),
+           rsrp: (if .rsrp == null then $UNAVAIL_SIGNAL_METRIC else .rsrp end),
+           snr:  (if .snr  == null then $UNAVAIL_SIGNAL_METRIC else .snr end)}"
+  echo "$INFO" | jq -c "$FILTER"
+}
+
+# qmi_get_op_mode returns one of: "" (aka unspecified), "online", "online-and-connected", "radio-off", "offline", "unrecognized"
+qmi_get_op_mode() {
+  local OP_MODE="$(qmicli --dms-get-operating-mode | sed -n "s/\s*Mode: '\(.*\)'/\1/p")"
+  case "$OP_MODE" in
+    "online")
+      if [ "$(uqmi --get-data-status)" = '"connected"' ]; then
+        echo "online-and-connected"
+      else
+        echo "online"
+      fi
+    ;;
+    "offline") echo "$OP_MODE"
+    ;;
+    "low-power" | "persistent-low-power" | "mode-only-low-power") echo "radio-off"
+    ;;
+    *) echo "unrecognized"
+    ;;
+  esac
+}
+
+qmi_get_imei() {
+  uqmi --get-imei | tr -d '"'
+}
+
+qmi_get_modem_model() {
+  qmicli --dms-get-model | sed -n "s/\s*Model: '\(.*\)'/\1/p"
+}
+
+qmi_get_modem_revision() {
+  qmicli --dms-get-revision | sed -n "s/\s*Revision: '\(.*\)'/\1/p"
+}
+
+qmi_get_providers() {
+  local PROVIDERS
+  PROVIDERS="$(uqmi --network-scan)"
+  if [ $? -ne 0 ]; then
+    echo "[]"
+    return 1
+  fi
+  FILTER='[.network_info[] | { "plmn": [if .mcc == null then "000" else .mcc end, if .mnc == null then "000" else .mnc end] | join("-"),
+                               "description": .description,
+                               "current-serving": .status | contains(["current_serving"]),
+                               "roaming":  .status | contains(["roaming"])}
+          ] | unique'
+  echo "$PROVIDERS" | jq -c "$FILTER"
+}
+
+get_get_sim_iccid() {
+  local OUTPUT
+  # Get ICCID from User Identity Module (UIM).
+  # Please refer to ETSI/3GPP "TS 102 221" section 13.2 for the coding of this EF.
+  OUTPUT="$(qmicli --uim-read-transparent=0x3F00,0x2FE2)"
+  if [ $? -ne 0 ]; then
+    return 1
+  fi
+  printf "%s" "$OUTPUT" | awk '
+    BEGIN{FS=":"; ORS=""}
+    /Read result:/ {target=NR+1}
+    (NR==target) {
+      for(i=1; i<=NF; i++) {
+        gsub(/[ \tF]*/,"",$i);
+        # Each byte contains 2 digits.
+        # First digit of each pair is encoded by the less significant half of the byte.
+        # For digits to be read from left to right, they need to be swapped.
+        print substr($i, 2, 1);
+        print substr($i, 1, 1);
+      }
+    }'
+}
+
+get_get_sim_imsi() {
+  local OUTPUT
+  # Get IMSI from User Identity Module (UIM).
+  # Please refer to ETSI/3GPP "TS 31.102" section 4.2.2 for the coding of this EF.
+  OUTPUT="$(qmicli --uim-read-transparent=0x3F00,0x7FFF,0x6F07)"
+  if [ $? -ne 0 ]; then
+    return 1
+  fi
+  printf "%s" "$OUTPUT" | awk '
+    BEGIN{FS=":"; ORS=""}
+    /Read result:/ {target=NR+1}
+    (NR==target) {
+      # We skip the first byte (starting with i=2) containing the IMSI length.
+      for(i=2; i<=NF; i++) {
+        gsub(/[ \tF]*/,"",$i);
+        # Each byte contains 2 digits.
+        # First digit of each pair is encoded by the less significant half of the byte.
+        # For digits to be read from left to right, they need to be swapped.
+        # Also, we skip the third digit (first substr for i=2) with parity check.
+        if (i>2) print substr($i, 2, 1);
+        print substr($i, 1, 1);
+      }
+    }'
+}
+
+qmi_get_sim_cards() {
+  # FIXME XXX Limited to a single SIM card
+  ICCID="$(get_get_sim_iccid)"
+  if [ $? -ne 0 ]; then
+    echo "[]"
+    return 1
+  fi
+  IMSI="$(get_get_sim_imsi)"
+  if [ $? -ne 0 ]; then
+    echo "[]"
+    return 1
+  fi
+  SIM="$(json_struct "$(json_str_attr "iccid" "$ICCID")" "$(json_str_attr "imsi" "$IMSI")")\n"
+  printf "%b" $SIM | json_array
+}
+
+qmi_start_network() {
+  echo "[$CDC_DEV] Starting network for APN ${APN}"
+  ip link set $IFACE down
+  echo Y > /sys/class/net/$IFACE/qmi/raw_ip
+  ip link set $IFACE up
+
+  uqmi --sync
+  uqmi --start-network --apn "${APN}" --keep-client-id wds |\
+      mbus_publish pdh_$IFACE
+}
+
+qmi_wait_for_sim() {
+  # FIXME XXX this is only for MBIM for now
+  :
+}
+
+qmi_wait_for_wds() {
+  echo "[$CDC_DEV] Waiting for DATA services to connect"
+  local CMD="uqmi --get-data-status | jq -r ."
+
+  if ! wait_for connected "$CMD"; then
+    echo "Timeout waiting for DATA services to connect" >&2
+    return 1
+  fi
+}
+
+qmi_wait_for_register() {
+  echo "[$CDC_DEV] Waiting for the device to register on the network"
+  local CMD="uqmi --get-serving-system | jq -r .registration"
+
+  if ! wait_for registered "$CMD"; then
+    echo "Timeout waiting for the device to register on the network" >&2
+    return 1
+  fi
+}
+
+qmi_wait_for_settings() {
+  echo "[$CDC_DEV] Waiting for IP configuration for the $IFACE interface"
+  local CMD="uqmi --get-current-settings"
+
+  if ! wait_for connected "$CMD | jq -r .ipv4.ip | grep -q \"$IPV4_REGEXP\" && echo connected"; then
+    echo "Timeout waiting for IP configuration for the $IFACE interface" >&2
+    return 1
+  fi
+}
+
+qmi_reset_modem() {
+  # last ditch attempt to reset our modem -- not sure how effective :-(
+  local PDH=`cat ${BBS}/pdh_${IFACE}.json 2>/dev/null`
+
+  for i in $PDH 0xFFFFFFFF ; do
+    uqmi --stop-network $i --autoconnect || continue
+  done
+
+  qmicli --dms-reset
+
+  for i in $PDH 0xFFFFFFFF ; do
+    uqmi --stop-network $i --autoconnect || continue
+  done
+}
+
+qmi_toggle_rf() {
+  if [ "$1" = "off" ]; then
+    echo "[$CDC_DEV] Disabling RF"
+    uqmi --set-device-operating-mode "persistent_low_power"
+  else
+    echo "[$CDC_DEV] Enabling RF"
+    uqmi --set-device-operating-mode "online"
+  fi
+}
