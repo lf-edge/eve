e79e997559affe66cdff3eece1bc702e924bda85
Petr Fedchenkov <giggsoff@gmail.com>
2021-11-12 15:31:06 +0300

    add error on nil CloudInitUserData
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/pillar/cmd/domainmgr/domainmgr.go
+++ pkg/pillar/cmd/domainmgr/domainmgr.go
@@ -2211,6 +2211,12 @@ func getCloudInitUserData(ctx *domainContext,
 		if err != nil {
 			log.Errorf("%s, domain config cipherblock decryption unsuccessful, falling back to cleartext: %v",
 				dc.Key(), err)
+			if dc.CloudInitUserData == nil {
+				cipher.RecordFailure(log, agentName,
+					types.MissingFallback)
+				return decBlock, fmt.Errorf("domain config cipherblock decryption unsuccessful (%s); "+
+					"no fallback data", err)
+			}
 			decBlock.ProtectedUserData = *dc.CloudInitUserData
 			// We assume IsCipher is only set when there was some
 			// data. Hence this is a fallback if there is
@@ -2229,6 +2235,10 @@ func getCloudInitUserData(ctx *domainContext,
 	}
 	log.Functionf("%s, domain config cipherblock not present", dc.Key())
 	decBlock := types.EncryptionBlock{}
+	if dc.CloudInitUserData == nil {
+		cipher.RecordFailure(log, agentName, types.NoCipher)
+		return decBlock, nil
+	}
 	decBlock.ProtectedUserData = *dc.CloudInitUserData
 	if decBlock.ProtectedUserData != "" {
 		cipher.RecordFailure(log, agentName, types.NoCipher)
