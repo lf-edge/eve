7c0e006910c5844d5ea67d104ff403e2c62a3cb9
Petr Fedchenkov <giggsoff@gmail.com>
2021-12-09 17:07:55 +0100

    use pid namespace to run entrypoint under pid 1
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/xen-tools/initrd/chroot2.c
+++ pkg/xen-tools/initrd/chroot2.c
@@ -1,9 +1,38 @@
+#define _GNU_SOURCE
+#include <sched.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/mount.h>
+#include <signal.h>
+
+typedef struct clone_args clone_args;
+
+struct clone_args {
+    char **args;
+    char *command;
+    uid_t uid, gid;
+};
+
+#define STACK_SIZE (8 * 1024 * 1024)
+static char child_stack[STACK_SIZE];    /* Space for child's stack */
+
+static int childFunc(void *args)
+{
+    clone_args *parsed_args = (clone_args *)args;
+    mount("proc", "/proc", "proc", 0, NULL);
+
+    setgid(parsed_args->gid);
+    setuid(parsed_args->uid);
+
+    execvp(parsed_args->command, parsed_args->args);
+}
 
 int main(int argc, char **argv) {
     uid_t uid, gid;
     char *endptr;
+    pid_t child_pid;
+    struct clone_args args;
 
     setsid();
     ioctl(0, TIOCSCTTY, 1);
@@ -14,8 +43,12 @@ int main(int argc, char **argv) {
     uid = strtol(argv[3], &endptr, 10);
     gid = strtol(argv[4], &endptr, 10);
 
-    setgid(gid);
-    setuid(uid);
+    args.uid = uid;
+    args.gid = gid;
+    args.command = argv[5];
+    args.args = argv + 5;
 
-    return execvp(argv[5], argv + 5);
+    child_pid = clone(childFunc, child_stack + STACK_SIZE, CLONE_NEWPID | SIGCHLD, (void *)(&args));
+
+    waitpid(child_pid, NULL, 0);
 }
