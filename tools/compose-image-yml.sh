947a61b41089b7d05220f7c48cdac12a5a902c53
Yuri Volchkov <yuri@volch.org>
2022-06-13 17:00:40 +0000

    Makefile: introduce DEV flag
    If enabled (e.g `make DEV=y live`), the "-dev" suffics will be added
    to resuling version string. Which will be later picked up by
    `compose-image-yml.sh`.
    
    For every package, where the build-dev.yml exists, it will be used
    instead of default build.yml.
    
    This patch only introduces the machinery. No build-dev.yml are
    introduced yet, and `compose-img-yml.sh` doesn't do anything.  At this
    stage DEV=y changes only the rootfs version, but the actual context
    stays the same.
    
    Signed-off-by: Yuri Volchkov <yuri@volch.org>

--- tools/compose-image-yml.sh
+++ tools/compose-image-yml.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+process-image-template() {
+    local out_templ_path="$1"
+    local eve_version="$2"
+
+    local flags
+    local -a bits
+
+    # Drop everything before the git hashcode (including the hash)
+    flags="$(sed -r 's/.*[0-9a-fA-F]{8}(.*)/\1/p' <<< "${eve_version}")"
+    # Drop dirty flag
+    flags="$(sed -r 's/-dirty[0-9.\-]{18}//g' <<< "${flags}")"
+    IFS='-' read -r -a bits <<< "${flags}"
+
+    for bit in "${bits[@]}"; do
+        case "${bit}" in
+            dev)
+                # TODO: process DEV flag
+                ;;
+        esac
+    done
+}
+
 main() {
     local base_templ_path="$1"
     local out_templ_path="$2"
@@ -12,6 +34,8 @@ main() {
     fi
 
     sed "s/EVE_VERSION/${eve_version}/g" < "${out_templ_path}".sed > "${out_templ_path}"
+
+    process-image-template "${out_templ_path}" "${eve_version}"
 }
 
 main "$@"
