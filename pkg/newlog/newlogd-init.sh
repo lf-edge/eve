b0fe424d422288def30c6bd0bdbbf06266560a71
Petr Fedchenkov <giggsoff@gmail.com>
2022-07-18 11:48:23 +0300

    Use pid from start of newlogd and wait
    We start newlogd as a long-running application, and do not expect that
    it will change its pid. Let's check for touch file as we do it for
    another agents and increase wait time.
    
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/newlog/newlogd-init.sh
+++ pkg/newlog/newlogd-init.sh
@@ -1,29 +1,44 @@
 #!/bin/sh
 
+echo "$(date --iso-8601=ns --utc) starting newlogd"
 /usr/bin/newlogd &
-mkdir -p /run/watchdog/pid
-touch /run/watchdog/pid/newlogd.pid
+NEWLOGD_PID=$!
+echo "$(date --iso-8601=ns --utc) newlogd is starting..., pid $NEWLOGD_PID"
 
-NEWLOGD_PID=$(cat /run/newlogd.pid)
-LOOP_COUNT=0
-while [ -z "$NEWLOGD_PID" ] && [ "$LOOP_COUNT" -le 30 ];
+NEWLOGD_TOUCH_FILE="/run/newlogd.touch"
+SECONDS_PASSED=0
+while [ ! -f "$NEWLOGD_TOUCH_FILE" ] && [ "$SECONDS_PASSED" -le 60 ];
 do
-    sleep 1
-    LOOP_COUNT=$((LOOP_COUNT + 1))
-    NEWLOGD_PID=$(cat /run/newlogd.pid)
+    echo "$(date --iso-8601=ns --utc) waiting for $NEWLOGD_TOUCH_FILE"
+    sleep 3
+    SECONDS_PASSED=$((SECONDS_PASSED + 3))
 done
+
+if [ ! -f "$NEWLOGD_TOUCH_FILE" ]; then
+    echo "$(date --iso-8601=ns --utc) gave up waiting for $NEWLOGD_TOUCH_FILE"
+else
+    echo "$(date --iso-8601=ns --utc) waited $SECONDS_PASSED seconds for $NEWLOGD_TOUCH_FILE"
+fi
+
+# subject for watchdog
+mkdir -p /run/watchdog/pid /run/watchdog/file
+touch /run/watchdog/pid/newlogd.pid /run/watchdog/file/newlogd.touch
 
 NEWLOGD_RESTART_COUNT=0
 
-echo "newlogd init.sh starting..., newlogd pid $NEWLOGD_PID"
 while true;
 do
     sleep 10
     PID=$(pgrep /usr/bin/newlogd)
-    if [ "$NEWLOGD_RESTART_COUNT" -eq "0" ] && { [ "$PID" != "$NEWLOGD_PID" ] || [ -z "$NEWLOGD_PID" ]; }; then
+    if [ "$NEWLOGD_RESTART_COUNT" -eq "0" ] && [ "$PID" != "$NEWLOGD_PID" ]; then
+        if [ -n "$PID" ]; then
+          # kill old process
+          echo "$(date --iso-8601=ns --utc) kill old newlogd with pid $PID"
+          kill -9 "$PID"
+        fi
         ## restart it once to pickup the stack trace of newlogd
         /usr/bin/newlogd -r &
         NEWLOGD_RESTART_COUNT=$((NEWLOGD_RESTART_COUNT + 1))
-        echo "newlogd init.sh restarted"
+        echo "$(date --iso-8601=ns --utc) newlogd restarted"
     fi
 done
