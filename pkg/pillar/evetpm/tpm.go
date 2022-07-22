8fc8144d8603acfde09bed372dfe08737b552e66
eriknordmark <erik@zededa.com>
2022-07-22 17:03:47 +0200

    Move per-device files back to /config
    With these changes config will be read-only once the device has been
    installed and the device certificate has been generated (the device cert
    is generated either by the installer if the device has a real-time clock
    or otherwise on first boot of EVE-OS). This assumes that the way will
    will measure /config into a PCR will be able to skip the per-device
    unique files to get the same PCR values across a fleet.
    
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/pillar/evetpm/tpm.go
+++ pkg/pillar/evetpm/tpm.go
@@ -51,8 +51,7 @@ const (
 	TpmDeviceKeyHdl tpmutil.Handle = 0x817FFFFF
 
 	//TpmCredentialsFileName is the file that holds the dynamically created TPM credentials
-	// Read from TPM NVRAM into /run/
-	TpmCredentialsFileName = types.CertificateRunDirname + "/tpm_credential"
+	TpmCredentialsFileName = types.IdentityDirname + "/tpm_credential"
 
 	//MaxPasswdLength is the max length allowed for a TPM password
 	MaxPasswdLength = 7 //limit TPM password to this length
