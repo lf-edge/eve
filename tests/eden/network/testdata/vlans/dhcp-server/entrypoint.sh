1e8ad96a4f434e77ced303f94d30036052819177
Petr Fedchenkov <giggsoff@gmail.com>
2022-07-29 10:47:16 +0300

    Update of Eden tests
    1. Sync version of eclient across tests
    2. Add json format for info and metrics
    3. Reduce metrics and config intervals to reduce load
    4. Reduce load in switch_net_vlans by and reduce apps and dnsmasq
    5. Add root certificate to v2tlsbaseroot
    6. Expand volumes test to check no space and recovery from no space
    7. Update ROL to support logs
    8. Update EVE-OS versions
    
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- tests/eden/network/testdata/vlans/dhcp-server/entrypoint.sh
+++ tests/eden/network/testdata/vlans/dhcp-server/entrypoint.sh
@@ -14,31 +14,14 @@ ip link add link "${eth1}" name "${eth1}.200" type vlan id 200
 ip link set dev "${eth1}.200" up
 ip addr add 10.2.200.1/24 dev "${eth1}.200"
 
-# DHCP server for apps without VLANs
+# DHCP server for apps
 cat <<EOF > /etc/dnsmasq.conf
 bind-interfaces
 except-interface=lo
 dhcp-leasefile=/run/dnsmasq.leases
-interface=${eth1}
-dhcp-range=10.2.0.2,10.2.0.254,60m
-EOF
-
-# DHCP server for VLAN 100
-cat <<EOF > /etc/dnsmasq.100.conf
-bind-interfaces
-except-interface=lo
-dhcp-leasefile=/run/dnsmasq.100.leases
-interface=${eth1}.100
-dhcp-range=10.2.100.2,10.2.100.254,60m
-EOF
-
-# DHCP server for VLAN 200
-cat <<EOF > /etc/dnsmasq.200.conf
-bind-interfaces
-except-interface=lo
-dhcp-leasefile=/run/dnsmasq.200.leases
-interface=${eth1}.200
-dhcp-range=10.2.200.2,10.2.200.254,60m
+dhcp-range=${eth1},10.2.0.2,10.2.0.254,60m
+dhcp-range=${eth1}.100,10.2.100.2,10.2.100.254,60m
+dhcp-range=${eth1}.200,10.2.200.2,10.2.200.254,60m
 EOF
 
 cat <<EOF > /etc/supervisord.conf
@@ -47,24 +30,27 @@ nodaemon=true
 
 [program:dnsmasq]
 command=dnsmasq -d -b -C /etc/dnsmasq.conf
-
-[program:dnsmasq.100]
-command=dnsmasq -d -b -C /etc/dnsmasq.100.conf
-
-[program:dnsmasq.200]
-command=dnsmasq -d -b -C /etc/dnsmasq.200.conf
+stdout_logfile=/dev/fd/1
+stdout_logfile_maxbytes=0
+redirect_stderr=true
 
 [program:nginx]
 command=nginx -g "daemon off;"
 autorestart=true
 stopsignal=KILL
 stopasgroup=true
+stdout_logfile=/dev/fd/1
+stdout_logfile_maxbytes=0
+redirect_stderr=true
 
 [program:ip]
 command=/bin/sh -c "while true; do sleep 5; ifconfig>/var/www/html/ifconfig.html; done"
 autorestart=true
 stopsignal=KILL
 stopasgroup=true
+stdout_logfile=/dev/fd/1
+stdout_logfile_maxbytes=0
+redirect_stderr=true
 EOF
 
 exec /usr/bin/supervisord -c /etc/supervisord.conf
