c6ee5bcb4ea5122708de9d3039847bef7cb277fc
Petr Fedchenkov <giggsoff@gmail.com>
2022-09-17 22:03:28 +0300

    Add activate field to BaseOS message proto
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- api/python/config/baseosconfig_pb2.py
+++ api/python/config/baseosconfig_pb2.py
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'\n\025org.lfedge.eve.configZ$github.com/lf-edge/eve/api/go/config',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x19\x63onfig/baseosconfig.proto\x12\x15org.lfedge.eve.config\x1a\x16\x63onfig/devcommon.proto\x1a\x14\x63onfig/storage.proto\"\x0b\n\tOSKeyTags\"\x0e\n\x0cOSVerDetails\"\xb6\x01\n\x0c\x42\x61seOSConfig\x12=\n\x0euuidandversion\x18\x01 \x01(\x0b\x32%.org.lfedge.eve.config.UUIDandVersion\x12,\n\x06\x64rives\x18\x03 \x03(\x0b\x32\x1c.org.lfedge.eve.config.Drive\x12\x10\n\x08\x61\x63tivate\x18\x04 \x01(\x08\x12\x15\n\rbaseOSVersion\x18\n \x01(\t\x12\x10\n\x08volumeID\x18\x0c \x01(\t\"^\n\x06\x42\x61seOS\x12\x19\n\x11\x63ontent_tree_uuid\x18\x01 \x01(\t\x12\x39\n\x0cretry_update\x18\x02 \x01(\x0b\x32#.org.lfedge.eve.config.DeviceOpsCmdB=\n\x15org.lfedge.eve.configZ$github.com/lf-edge/eve/api/go/configb\x06proto3'
+  serialized_pb=b'\n\x19\x63onfig/baseosconfig.proto\x12\x15org.lfedge.eve.config\x1a\x16\x63onfig/devcommon.proto\x1a\x14\x63onfig/storage.proto\"\x0b\n\tOSKeyTags\"\x0e\n\x0cOSVerDetails\"\xb6\x01\n\x0c\x42\x61seOSConfig\x12=\n\x0euuidandversion\x18\x01 \x01(\x0b\x32%.org.lfedge.eve.config.UUIDandVersion\x12,\n\x06\x64rives\x18\x03 \x03(\x0b\x32\x1c.org.lfedge.eve.config.Drive\x12\x10\n\x08\x61\x63tivate\x18\x04 \x01(\x08\x12\x15\n\rbaseOSVersion\x18\n \x01(\t\x12\x10\n\x08volumeID\x18\x0c \x01(\t\"p\n\x06\x42\x61seOS\x12\x19\n\x11\x63ontent_tree_uuid\x18\x01 \x01(\t\x12\x39\n\x0cretry_update\x18\x02 \x01(\x0b\x32#.org.lfedge.eve.config.DeviceOpsCmd\x12\x10\n\x08\x61\x63tivate\x18\x03 \x01(\x08\x42=\n\x15org.lfedge.eve.configZ$github.com/lf-edge/eve/api/go/configb\x06proto3'
   ,
   dependencies=[config_dot_devcommon__pb2.DESCRIPTOR,config_dot_storage__pb2.DESCRIPTOR,])
 
@@ -160,6 +160,13 @@ _BASEOS = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='activate', full_name='org.lfedge.eve.config.BaseOS.activate', index=2,
+      number=3, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -173,7 +180,7 @@ _BASEOS = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=312,
-  serialized_end=406,
+  serialized_end=424,
 )
 
 _BASEOSCONFIG.fields_by_name['uuidandversion'].message_type = config_dot_devcommon__pb2._UUIDANDVERSION
