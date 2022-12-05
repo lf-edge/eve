lf-edge/eve

commit 7e6274c2edb86f0a7b8e218cf4697cf9e6049fa0

kdump: collect dmesg along with a kdump
Colect plain-text dmesg log along with a kernel dump in order
to simplify kernel debugging: sometimes it can be enough to
take a look into a dmesg once in order to understand a root
cause rather than to download kernel debug symbols, use the
crash tool and dive deeper into the analysis.

And yes, I renamed the 'kcrash-*' to 'kdump-*' in order to
have both files aligned, thus the log looks perfect now:

   >>>>>>>>>>>> Crashed kernel dmesg END <<<<<<<<<<
   kdump collected: /persist/kcrashes/kdump-2022-11-30-13-58-40.dump
   dmesg collected: /persist/kcrashes/dmesg-2022-11-30-13-58-40.log

Promise not to change filenames anymore!

Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

pkg/kdump/kdump.sh
@@ -25,10 +25,17 @@ if test -f /proc/vmcore; then
     while read -r line; do echo "$line" > /dev/kmsg; done < /tmp/backtrace
     echo ">>>>>>>>>> Crashed kernel dmesg END <<<<<<<<<<" > /dev/kmsg
 
+    TS=$(date +%Y-%m-%d-%H-%M-%S)
+
     # Collect a minimal kernel dump for security reasons
-    KDUMP_PATH="$DIR/kcrash-$(date +%Y-%m-%d-%H-%M-%S).dump"
+    KDUMP_PATH="$DIR/kdump-$TS.dump"
     makedumpfile -d 31 /proc/vmcore "$KDUMP_PATH" > /dev/null 2>&1
     echo "kdump collected: $KDUMP_PATH" > /dev/kmsg
+
+    # Collect dmesg
+    DMESG_PATH="$DIR/dmesg-$TS.log"
+    cp /tmp/dmesg "$DMESG_PATH"
+    echo "dmesg collected: $DMESG_PATH" > /dev/kmsg
 
     # Prepare reboot-reason, reboot-stack and boot-reason
     echo "kernel panic, kdump collected: $KDUMP_PATH" > /persist/reboot-reason

commit bd377ad3f64ac41a0c0feef1dbf772632cdf9d63

kdump: double number of kept files after a kernel crash
Commit 7e6274c2edb8 ("kdump: collect dmesg along with a kdump")
introduced generation of the dmesg file of a crashed kernel,
so double the maximum value in order to keep files for 5 crashes
(i.e. both dump and dmesg).

Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

pkg/kdump/kdump.sh
@@ -6,7 +6,12 @@ if test -f /proc/vmcore; then
     #
     # We are in dump capture kernel, nice.
     #
-    MAX=5
+
+    # Maximum number of kernel dumps and dmesgs we keep in the folder.
+    # After each kernel crash 2 files are generated: dump and dmesg,
+    # so the divide MAX on number of files to get total number of
+    # kernel crashes which we keep persist.
+    MAX=10
     DIR=/persist/kcrashes
 
     # Create a folder