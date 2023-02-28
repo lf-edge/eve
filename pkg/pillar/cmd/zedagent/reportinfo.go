6b08cc8c90f4e2d35f3884eafeecd89424d98073
Roman Penyaev <r.peniaev@gmail.com>
2023-02-28 16:15:02 +0100

    zedagent,zedcloud: introduce 'ignoreErr' flag for a deferred item creation
    All errors will be ignored and an item will be removed from the queue
    regardless of an error while send, if 'ignoreErr' is passed as true.
    The default behavior (ignoreErr == false) to retry sending from
    the message which failed. Ignoring errors will be needed for the
    LOC /info updates.
    
    Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>
    (cherry picked from commit f450a55b764f3ef8d62168345f37a7ed76d6947a)
    Signed-off-by: eriknordmark <erik@zededa.com>
    (cherry picked from commit 3faaead030aa222eedd97e6bca256c452b8d02e1)
    Signed-off-by: eriknordmark <erik@zededa.com>
    (cherry picked from commit 6fdaff406603ee7ebacb179955628325ffb800d0)
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/pillar/cmd/zedagent/reportinfo.go
+++ pkg/pillar/cmd/zedagent/reportinfo.go
@@ -639,7 +639,7 @@ func PublishDeviceInfoToZedCloud(ctx *zedagentContext) {
 	//If there are no failures and defers we'll send this message,
 	//but if there is a queue we'll retry sending the highest priority message.
 	zedcloud.SetDeferred(zedcloudCtx, deviceUUID, buf, size,
-		statusUrl, true, info.ZInfoTypes_ZiDevice)
+		statusUrl, true, false, info.ZInfoTypes_ZiDevice)
 	zedcloud.HandleDeferred(zedcloudCtx, time.Now(), 0, true)
 }
 
@@ -691,7 +691,8 @@ func PublishAppInstMetaDataToZedCloud(ctx *zedagentContext, appInstMetadata *typ
 	//We queue the message and then get the highest priority message to send.
 	//If there are no failures and defers we'll send this message,
 	//but if there is a queue we'll retry sending the highest priority message.
-	zedcloud.SetDeferred(zedcloudCtx, deferKey, buf, size, statusURL, true, info.ZInfoTypes_ZiAppInstMetaData)
+	zedcloud.SetDeferred(zedcloudCtx, deferKey, buf, size, statusURL, true,
+		false, info.ZInfoTypes_ZiAppInstMetaData)
 	zedcloud.HandleDeferred(zedcloudCtx, time.Now(), 0, true)
 }
 
