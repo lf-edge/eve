3ae2133f9759eb9cb0c23286ee0e1b28b9915c4e
eriknordmark <erik@zededa.com>
2023-03-22 14:48:51 +0530

    Have SenderStatusCertMiss trigger fetching certs in attest case
    In some cases the /config API endpoint might be accessed less frequently
    than the /attest API endpoint so it makes sense to check for
    SenderStatusCertMiss and SenderStatusCertInvalid.
    
    Signed-off-by: eriknordmark <erik@zededa.com>
    (cherry picked from commit 8eeec89cbce28f95405e1ed1d566b141d86e22a2)
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/pillar/cmd/zedagent/attesttask.go
+++ pkg/pillar/cmd/zedagent/attesttask.go
@@ -70,7 +70,7 @@ const (
 )
 
 //One shot send, if fails, return an error to the state machine to retry later
-func trySendToController(attestReq *attest.ZAttestReq, iteration int) (*http.Response, []byte, types.SenderResult, error) {
+func trySendToController(attestReq *attest.ZAttestReq, attestCtx *attestContext) (*http.Response, []byte, types.SenderResult, error) {
 	log.Noticef("trySendToController type %d", attestReq.ReqType)
 	data, err := proto.Marshal(attestReq)
 	if err != nil {
@@ -84,12 +84,19 @@ func trySendToController(attestReq *attest.ZAttestReq, iteration int) (*http.Res
 	ctxWork, cancel := zedcloud.GetContextForAllIntfFunctions(zedcloudCtx)
 	defer cancel()
 	resp, contents, senderStatus, err := zedcloud.SendOnAllIntf(ctxWork,
-		zedcloudCtx, attestURL, size, buf, iteration, true)
+		zedcloudCtx, attestURL, size, buf, attestCtx.Iteration, true)
 	if err != nil || len(contents) == 0 {
-		return resp, contents, senderStatus, err
+		// Error case handled below
+	} else {
+		contents, senderStatus, err = zedcloud.RemoveAndVerifyAuthContainer(zedcloudCtx,
+			attestURL, contents, false, senderStatus)
 	}
-	contents, senderStatus, err = zedcloud.RemoveAndVerifyAuthContainer(zedcloudCtx,
-		attestURL, contents, false, senderStatus)
+	switch senderStatus {
+	case types.SenderStatusCertMiss, types.SenderStatusCertInvalid:
+		// trigger to acquire new controller certs from cloud
+		log.Noticef("%s trigger", senderStatus.String())
+		triggerControllerCertEvent(attestCtx.zedagentCtx)
+	}
 	return resp, contents, senderStatus, err
 }
 
@@ -133,7 +140,7 @@ func (server *VerifierImpl) SendNonceRequest(ctx *zattest.Context) error {
 	attestCtx.Iteration++
 	log.Tracef("Sending Nonce request %v", attestReq)
 
-	_, contents, senderStatus, err := trySendToController(attestReq, attestCtx.Iteration)
+	_, contents, senderStatus, err := trySendToController(attestReq, attestCtx)
 	if err != nil || senderStatus != types.SenderStatusNone {
 		errorDescription := types.ErrorDescription{
 			Error: fmt.Sprintf("[ATTEST] Error %v, senderStatus %v",
@@ -315,7 +322,7 @@ func (server *VerifierImpl) SendAttestQuote(ctx *zattest.Context) error {
 	log.Tracef("Sending Quote request")
 	recordAttestationTry(attestCtx.zedagentCtx)
 
-	_, contents, senderStatus, err := trySendToController(attestReq, attestCtx.Iteration)
+	_, contents, senderStatus, err := trySendToController(attestReq, attestCtx)
 	if err != nil || senderStatus != types.SenderStatusNone {
 		errorDescription := types.ErrorDescription{
 			Error: fmt.Sprintf("[ATTEST] Error %v, senderStatus %v",
@@ -470,7 +477,7 @@ func (server *VerifierImpl) SendAttestEscrow(ctx *zattest.Context) error {
 	attestCtx.Iteration++
 	log.Noticef("[ATTEST] Sending Escrow data len %d", len(key.Key))
 
-	_, contents, senderStatus, err := trySendToController(attestReq, attestCtx.Iteration)
+	_, contents, senderStatus, err := trySendToController(attestReq, attestCtx)
 	if err != nil || senderStatus != types.SenderStatusNone {
 		errorDescription := types.ErrorDescription{
 			Error: fmt.Sprintf("[ATTEST] Error %v, senderStatus %v", err, senderStatus),
