d30506ec9fd5e5c7c63ee22b3916278c2cfde484
Christoph Ostarek <christoph@zededa.com>
2023-04-03 14:18:44 +0200

    controllerdns: move creating hosts file content
    into writeHostsFile method
    
    Signed-off-by: Christoph Ostarek <christoph@zededa.com>

--- pkg/pillar/cmd/nim/controllerdns_test.go
+++ pkg/pillar/cmd/nim/controllerdns_test.go
@@ -1,14 +1,17 @@
 package nim
 
 import (
+	"io"
+	"os"
+	"testing"
+
 	"github.com/lf-edge/eve/pkg/pillar/base"
+	"github.com/lf-edge/eve/pkg/pillar/devicenetwork"
 	"github.com/lf-edge/eve/pkg/pillar/dpcmanager"
 	"github.com/sirupsen/logrus"
-	"testing"
 )
 
-func TestControllerDNSCacheIndexOutOfRange(t *testing.T) {
-	// Regression test for bug introduced by switching to miekg/dns
+func createTestNim() *nim {
 	var n nim
 
 	dpcManager := dpcmanager.DpcManager{}
@@ -18,5 +21,49 @@ func TestControllerDNSCacheIndexOutOfRange(t *testing.T) {
 	n.Logger = logger
 	n.Log = log
 
-	n.controllerDNSCache([]byte("/etc/hosts"), []byte("1.1"), "")
+	return &n
+}
+
+func TestControllerDNSCacheIndexOutOfRange(t *testing.T) {
+	// Regression test for bug introduced by switching to miekg/dns
+	n := createTestNim()
+
+	n.controllerDNSCache([]byte(""), []byte("1.1"), "")
+}
+
+func TestWriteHostsFile(t *testing.T) {
+	n := createTestNim()
+
+	dnsResponses := []devicenetwork.DNSResponse{
+		{
+			IP: []byte{1, 1, 1, 1},
+		},
+		{
+			IP: []byte{1, 0, 0, 1},
+		},
+	}
+
+	f, err := os.CreateTemp("", "writeHostsFile.*.etchosts")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(f.Name())
+	f.Close()
+
+	n.writeHostsFileToDestination(dnsResponses, []byte{}, []byte("one.one.one.one"), f.Name())
+
+	// reopen the file to be able to read what has been written by writeHostsFileToDestination; f.Seek(0, 0) unfortunately is not enough
+	f, err = os.Open(f.Name())
+	if err != nil {
+		panic(err)
+	}
+	content, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+
+	expectedContent := "1.1.1.1 one.one.one.one\n"
+	if string(content) != expectedContent {
+		t.Fatalf("writing to hosts file failed, expected: '%s', got: '%s'", expectedContent, content)
+	}
 }
