50d6c205d4a70a6228e6ec14716244b794fd1df5
Roman Penyaev <r.peniaev@gmail.com>
2023-04-26 14:40:54 +0200

    zedmanager: don't require restart field set in case of FixedResources.DisableLogs field change
    The default behavior of the quantifyChanges() is to return 'restart == true'
    in case of any change in the `FixedResources` structure (type VmConfig).
    
    The field `DisableLogs` is handled without Application restart requirement,
    thus pull the field out of the `VmConfig` structure (which is used as
    'fixed resources') and move it in the parent structure.
    
    This fixes the bug, when 'Disable logs' field is changed on the controller
    side but EVE returns an error: "Need restart due to FixedResources changed".
    
    Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

--- pkg/pillar/cmd/zedagent/parseconfig.go
+++ pkg/pillar/cmd/zedagent/parseconfig.go
@@ -535,7 +535,7 @@ func parseAppInstanceConfig(getconfigCtx *getconfigContext,
 		appInstance.FixedResources.EnableVnc = cfgApp.Fixedresources.EnableVnc
 		appInstance.FixedResources.VncDisplay = cfgApp.Fixedresources.VncDisplay
 		appInstance.FixedResources.VncPasswd = cfgApp.Fixedresources.VncPasswd
-		appInstance.FixedResources.DisableLogs = cfgApp.Fixedresources.DisableLogs
+		appInstance.DisableLogs = cfgApp.Fixedresources.DisableLogs
 		appInstance.MetaDataType = types.MetaDataType(cfgApp.MetaDataType)
 		appInstance.Delay = time.Duration(cfgApp.StartDelayInSeconds) * time.Second
 		appInstance.Service = cfgApp.Service
