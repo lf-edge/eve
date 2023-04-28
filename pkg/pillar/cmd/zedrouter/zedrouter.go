1f426fd7177cbba60c07fe52e848c7ca6d63df47
eriknordmark <erik@zededa.com>
2023-04-28 14:39:09 -0700

    Add missing free of NetworkInstanceStatus
    From comments in https://github.com/lf-edge/eve/pull/2285
    
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/pillar/cmd/zedrouter/zedrouter.go
+++ pkg/pillar/cmd/zedrouter/zedrouter.go
@@ -1930,6 +1930,14 @@ func doAppNetworkModifyUNetAppNum(
 		log.Error(err)
 		return err
 	}
+	// Did the freeAppIntfNum release any last reference?
+	netstatus := lookupNetworkInstanceStatus(ctx, ulStatus.Network.String())
+	if netstatus != nil {
+		if maybeNetworkInstanceDelete(ctx, netstatus) {
+			log.Functionf("post freeAppIntfNum(%v) for %s deleted %s",
+				oldNetworkID, appID, netstatus.Key())
+		}
+	}
 	return nil
 }
 
@@ -1982,7 +1990,7 @@ func handleDelete(ctx *zedrouterContext, key string,
 		netstatus := lookupNetworkInstanceStatus(ctx, ulStatus.Network.String())
 		if netstatus != nil {
 			if maybeNetworkInstanceDelete(ctx, netstatus) {
-				log.Functionf("post appNumsOnUNetFree(%v) for %s deleted %s",
+				log.Functionf("post freeAppIntfNums(%v) for %s deleted %s",
 					status.UUIDandVersion, status.DisplayName,
 					netstatus.Key())
 			}
