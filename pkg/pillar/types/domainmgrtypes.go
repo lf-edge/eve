c8a5cac2c56e17848b14c3d2cfd936f7a84daaab
Roman Penyaev <r.peniaev@gmail.com>
2023-05-04 12:23:08 +0200

    domainmgrtypes: introduce 'VMMMaxMem' for the 'VmConfig' struct'
    Set the 'VMMMaxMem' field for the 'VmConfig' run-time structure
    from the 'VmConfig' protocol structure.
    
    Also add minor comment tweaks.
    
    Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>
    (cherry picked from commit abdfbdc91d1cc144cf8baa9dad8b1486443ef2f3)

--- pkg/pillar/types/domainmgrtypes.go
+++ pkg/pillar/types/domainmgrtypes.go
@@ -202,7 +202,7 @@ type VmConfig struct {
 	Kernel     string // default ""
 	Ramdisk    string // default ""
 	Memory     int    // in kbytes; Rounded up to Mbytes for xen
-	MaxMem     int    // Default not set i.e. no ballooning
+	MaxMem     int    // in kbytes; Default equal to 'Memory', so no ballooning for xen
 	VCpus      int    // default 1
 	MaxCpus    int    // default VCpus
 	RootDev    string // default "/dev/xvda1"
@@ -225,6 +225,7 @@ type VmConfig struct {
 	VncPasswd          string
 	DisableLogs        bool
 	CPUsPinned         bool
+	VMMMaxMem          int // in kbytes
 }
 
 type VmMode uint8
