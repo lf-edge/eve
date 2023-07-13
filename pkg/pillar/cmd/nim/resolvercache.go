8cc28a2f4abcd8a03c4fd44b78e388163d3bfca7
Milan Lenco <milan@zededa.com>
2023-07-13 15:54:13 +0200

    Avoid using cached controller IP for connectivity verification
    This commit completely refactors the mechanism of caching the controller
    IP address. Previously, the routine (run by NIM) that periodically looks
    up controller IP would store it to /etc/hosts to basically cache it
    and apply it for all processes running on EVE.
    However, this for example then means that even connectivity verification
    (as performed by DPCManager and UplinkProber) uses cached IP address and
    doesn't always check the availability of the DNS service. This can be
    an issue in various scenarious. For example, user may change device
    config and set a wrong DNS IP address (inside static network IP config).
    EVE will try this new config and because controller IP is cached from
    a previous lookup, it will not check if DNS is working and may therefore
    evaluate the new config as OK and forget the previous one. Once the TTL
    of the cached IP runs out, EVE will finds itself in a situation where
    DNS with the new IP is not responsive, controller hostname cannot be
    resolved into IP and at the same time there is no network config to
    fallback to. Thus device looses connectivity permanently.
    
    Having cache applied to all processes also hinders troubleshooting
    activities. Collected netdumps are likely not going to contain traces of
    DNS requests because cached IP was used instead. Similarly, users that
    manually will try to perform lookup to check how their DNS server behaves
    may get tricked thinking that everything is working, when in fact IP
    address stored in /etc/hosts was only returned. Output they collect
    using tools like nslookup or dig is not going to be very useful if there
    was no actual DNS query performed.
    
    The purpose of the cache is to limit the number of DNS queries executed
    over time. This is just an optimization and it should not hinder
    essential functionality, such as network config fallback.
    This commit changes controller IP caching to use pubsub for storage
    and apply the cached IP only within pillar and only in situations where
    we do no care if DNS server was actually queried. This is for example
    in cmd/client, loguploader and zedagent. For NIM, diag and UplinkProber
    we always want to run full DNS queries. They do not run them that often
    anyway.
    
    Signed-off-by: Milan Lenco <milan@zededa.com>
    (cherry picked from commit 0af4043f715baa76fe2374786d01269b9d010006)

---
+++ pkg/pillar/cmd/nim/resolvercache.go
@@ -0,0 +1,122 @@
+// Copyright (c) 2023 Zededa, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package nim
+
+import (
+	"net"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/lf-edge/eve/pkg/pillar/devicenetwork"
+	"github.com/lf-edge/eve/pkg/pillar/types"
+)
+
+const (
+	// TTL in seconds assumed when DNS response is missing (has zero) TTL.
+	defaultTTL = 30
+
+	// How often to rerun lookup and store fresh entries into the cache.
+	defaultRefetchPeriod = 30 * time.Second
+	maxRefetchPeriod     = 1 * time.Hour
+	refetchDelay         = 3 * time.Second
+)
+
+// Go routine that periodically resolves and caches controller IP address.
+// The cached IP address can be used with SendOnIntf function to speed up
+// controller API calls by avoiding repeated hostname resolutions.
+func (n *nim) runResolverCacheForController() {
+	content, err := os.ReadFile(types.ServerFileName)
+	if err != nil {
+		n.Log.Errorf("Failed to read %s: %v; "+
+			"will not run resolver cache for the controller hostname",
+			types.ServerFileName, err)
+		return
+	}
+	controllerHostname := string(content)
+	controllerHostname = strings.TrimSpace(controllerHostname)
+	if host, _, err := net.SplitHostPort(controllerHostname); err == nil {
+		controllerHostname = host
+	}
+	if net.ParseIP(controllerHostname) != nil {
+		// Controller hostname is already defined as an IP address.
+		return
+	}
+
+	dnsQueryTimer := time.NewTimer(defaultRefetchPeriod)
+
+	wdName := agentName + "-resolverCache"
+	stillRunning := time.NewTicker(stillRunTime)
+	n.PubSub.StillRunning(wdName, warningTime, errorTime)
+	n.PubSub.RegisterFileWatchdog(wdName)
+
+	for {
+		select {
+		case <-dnsQueryTimer.C:
+			// Use smallest returned TTL as the update frequency.
+			// Even if the DNS server implementation returns the remaining value
+			// of the TTL it caches, it will still work.
+			minTTL := n.resolveAndCacheIP(controllerHostname)
+			var retryAfter time.Duration
+			if minTTL == 0 {
+				// No response or a failure; make sure we redo the query after 30 seconds.
+				retryAfter = defaultRefetchPeriod
+			} else {
+				retryAfter = time.Duration(minTTL) * time.Second
+				// DNS server may return TTL as the remaining time of its own cached TTL.
+				// In order to avoid re-fetching controller IP when TTL is close to zero
+				// (and thus caching it is practically pointless), we wait few extra
+				// seconds before running DNS query again.
+				retryAfter += refetchDelay
+			}
+			// Make sure we do not stop re-fetching for a long time if the returned
+			// TTL is some crazy high value.
+			if retryAfter > maxRefetchPeriod {
+				retryAfter = maxRefetchPeriod
+			}
+			dnsQueryTimer = time.NewTimer(retryAfter)
+
+		case <-stillRunning.C:
+		}
+		n.PubSub.StillRunning(wdName, warningTime, errorTime)
+	}
+}
+
+func (n *nim) doDNSQuery(hostname string) []devicenetwork.DNSResponse {
+	dnsResponse, errs := devicenetwork.ResolveWithPortsLambda(
+		hostname,
+		n.dpcManager.GetDNS(),
+		devicenetwork.ResolveWithSrcIP,
+	)
+	if len(errs) > 0 {
+		n.Log.Warnf("doDNSQuery failed: %+v", errs)
+	}
+	return dnsResponse
+}
+
+// Try to resolve the IP address for the given hostname and cache it using pubsub.
+// Currently used only for the controller hostname.
+func (n *nim) resolveAndCacheIP(hostname string) (minTTL uint32) {
+	queryTime := time.Now()
+	dnsResponses := n.doDNSQuery(hostname)
+	cachedData := types.CachedResolvedIPs{Hostname: hostname}
+	for _, dnsResp := range dnsResponses {
+		if dnsResp.TTL == 0 {
+			dnsResp.TTL = defaultTTL
+		}
+		cachedData.CachedIPs = append(cachedData.CachedIPs, types.CachedIP{
+			IPAddress:  dnsResp.IP,
+			ValidUntil: queryTime.Add(time.Duration(dnsResp.TTL) * time.Second),
+		})
+		if minTTL == 0 || dnsResp.TTL < minTTL {
+			minTTL = dnsResp.TTL
+		}
+	}
+	err := n.pubCachedResolvedIPs.Publish(hostname, cachedData)
+	if err != nil {
+		n.Log.Errorf("Failed to cache resolved IPs for hostname %s: %v",
+			hostname, err)
+	}
+	return minTTL
+}
