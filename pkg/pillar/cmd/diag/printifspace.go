bf35fc2ef93ff6fa938849dccaaf2c970c51a339
eriknordmark <erik@zededa.com>
2023-09-04 18:32:36 -0700

    diag: rename handle variable
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/pillar/cmd/diag/printifspace.go
+++ pkg/pillar/cmd/diag/printifspace.go
@@ -29,7 +29,7 @@ type PrintHandle struct {
 // PrintIfSpaceInit sets up the limits and initializes current lines to zero
 func PrintIfSpaceInit(outfile *os.File, stateFilename string,
 	maxRows int, maxColumns int) *PrintHandle {
-	ctx := PrintHandle{
+	h := PrintHandle{
 		curLines:      0,
 		maxRows:       maxRows - 1, // Save one line for an "exceeded" message
 		maxColumns:    maxColumns,
@@ -38,29 +38,29 @@ func PrintIfSpaceInit(outfile *os.File, stateFilename string,
 		stateFilename: stateFilename,
 		exceededCount: 0,
 	}
-	return &ctx
+	return &h
 }
 
 // Flush writes all the output to statefilename and resets to zero.
 // It prints a line if exceeded
-func (ctx *PrintHandle) Flush() {
-	if ctx.exceededCount > 0 {
-		ctx.Print("WARNING: screen exceeded size by %d\n",
-			ctx.exceededCount)
+func (h *PrintHandle) Flush() {
+	if h.exceededCount > 0 {
+		h.Print("WARNING: screen exceeded size by %d\n",
+			h.exceededCount)
 	}
-	if ctx.stateFilename != "" {
-		fileutils.WriteRename(ctx.stateFilename,
-			[]byte(ctx.allBytes.String()))
+	if h.stateFilename != "" {
+		fileutils.WriteRename(h.stateFilename,
+			[]byte(h.allBytes.String()))
 	}
-	ctx.Reset()
+	h.Reset()
 }
 
 // Reset forgets about what has been printed
-func (ctx *PrintHandle) Reset() {
-	ctx.curLines = 0
-	ctx.exceeded = false
-	ctx.exceededCount = 0
-	ctx.allBytes.Reset()
+func (h *PrintHandle) Reset() {
+	h.curLines = 0
+	h.exceeded = false
+	h.exceededCount = 0
+	h.allBytes.Reset()
 }
 
 // Print checks the number of lines since last PrintIfSpaceInit taking
@@ -68,36 +68,36 @@ func (ctx *PrintHandle) Reset() {
 // A terminating non-empty line counts as a line.
 // Returns true if it was printed.
 // In addition it always saves to allBytes
-func (ctx *PrintHandle) Print(format string, a ...any) (bool, error) {
+func (h *PrintHandle) Print(format string, a ...any) (bool, error) {
 	// Determine how many lines this will take based on \n plus the
 	// line wrap past maxColumns
 	out := fmt.Sprintf(format, a...)
-	if ctx.exceeded {
-		ctx.exceededCount++
-		ctx.allBytes.WriteString(out)
+	if h.exceeded {
+		h.exceededCount++
+		h.allBytes.WriteString(out)
 		return false, fmt.Errorf("already exceeded. count %d",
-			ctx.exceededCount-1)
+			h.exceededCount-1)
 	}
 	lineCnt := 0
 	lines := strings.Split(out, "\n")
 	for _, line := range lines {
 		// Round up to determine wrap
-		cnt := 1 + (len(line)-1)/ctx.maxColumns
+		cnt := 1 + (len(line)-1)/h.maxColumns
 		lineCnt += cnt
 	}
 	// lines has at least one element; check length of last line
 	if len(lines[len(lines)-1]) == 0 {
 		lineCnt--
 	}
-	if ctx.curLines+lineCnt > ctx.maxRows {
-		ctx.exceeded = true
-		ctx.exceededCount++
-		ctx.allBytes.WriteString(out)
+	if h.curLines+lineCnt > h.maxRows {
+		h.exceeded = true
+		h.exceededCount++
+		h.allBytes.WriteString(out)
 		return false, fmt.Errorf("had %d lines, added %d, exceeded %d",
-			ctx.curLines, lineCnt, ctx.maxRows)
+			h.curLines, lineCnt, h.maxRows)
 	}
-	ctx.curLines += lineCnt
-	fmt.Fprint(ctx.outfile, out)
-	ctx.allBytes.WriteString(out)
+	h.curLines += lineCnt
+	fmt.Fprint(h.outfile, out)
+	h.allBytes.WriteString(out)
 	return true, nil
 }
