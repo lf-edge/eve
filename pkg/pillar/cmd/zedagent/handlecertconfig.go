lf-edge/eve

commit 92c4e9d680630b8c76b25c704c9f2271f332e330

Stale signing cert should not block sending device info
If the controller signing cerificate has expired then the attestion
messages will appear to fail, since the responses will not authenticate.
In such a case we still want to send device and app info messages.

Signed-off-by: eriknordmark <erik@zededa.com>

pkg/pillar/cmd/zedagent/handlecertconfig.go
@@ -444,8 +444,10 @@ func sendAttestReqProtobuf(attestReq *attest.ZAttestReq, iteration int) {
 	//We queue the message and then get the highest priority message to send.
 	//If there are no failures and defers we'll send this message,
 	//but if there is a queue we'll retry sending the highest priority message.
+	// Since attest messages can fail if there is a certificate mismatch
+	// we set ignoreErr to allow other messages to be sent as well.
 	zedcloudCtx.DeferredEventCtx.SetDeferred(deferKey, buf, size, attestURL,
-		false, false, false, attestReq.ReqType)
+		false, false, true, attestReq.ReqType)
 	zedcloudCtx.DeferredEventCtx.HandleDeferred(time.Now(), 0, true)
 }
 

pkg/pillar/cmd/zedagent/zedagent.go
@@ -754,8 +754,10 @@ func handleDeferredPeriodicTask(zedagentCtx *zedagentContext) {
 		select {
 		case change := <-zedcloudCtx.DeferredPeriodicCtx.Ticker.C:
 			start := time.Now()
-			zedcloudCtx.DeferredPeriodicCtx.HandleDeferred(
-				change, 100*time.Millisecond, false)
+			if !zedcloudCtx.DeferredPeriodicCtx.HandleDeferred(
+				change, 100*time.Millisecond, false) {
+				log.Noticef("handleDeferredPeriodicTask: some deferred items remain to be sent")
+			}
 			zedagentCtx.ps.CheckMaxTimeTopic(agentName, "deferredPeriodicCtx",
 				start, warningTime, errorTime)
 		case <-stillRunning.C:

commit e071204395a91428f685dc4a88bff4b280d9376a

pillar/zedagent: repeat an attestation in case of a send failure
92c4e9d68063 ("Stale signing cert should not block sending device info")
introduced a problem of missing EVE certs on the controller side if very
first attestation request fails: specified commit ignores a send error of
the attestation request and does not repeat attestation.

The bug can be reproduced by applying iptables rules rejecting 443 port on
early boot, so that first send requests fail and attestation never happens
again.

The fix is simple: on a completion callback repeat attestation in case of
failure in 10 seconds delay to avoid tight fail-repeat loops.

Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

pkg/pillar/cmd/zedagent/handlecertconfig.go
@@ -534,6 +534,13 @@ func triggerEdgeNodeCertEvent(ctxPtr *zedagentContext) {
 	}
 }
 
+func triggerEdgeNodeCertDelayedEvent(ctxPtr *zedagentContext, d time.Duration) {
+	go func() {
+		time.Sleep(d)
+		triggerEdgeNodeCertEvent(ctxPtr)
+	}()
+}
+
 func convertLocalToApiHashAlgo(algo types.CertHashType) evecommon.HashAlgorithm {
 	switch algo {
 	case types.CertHashTypeSha256First16:

pkg/pillar/cmd/zedagent/zedagent.go
@@ -2503,6 +2503,13 @@ func getDeferredSentHandlerFunction(ctx *zedagentContext) *zedcloud.SentHandlerF
 					log.Functionf("sendAttestReqProtobuf: Controller SenderStatusNotFound")
 					potentialUUIDUpdate(ctx.getconfigCtx)
 				}
+				if !ctx.publishedEdgeNodeCerts {
+					// Attestation request does not clog the send queue (issued
+					// with the `ignoreErr` set to true), but once fails has to
+					// be repeated in reasonable time to avoid tight fail-repeat
+					// loop.
+					triggerEdgeNodeCertDelayedEvent(ctx, 10*time.Second)
+				}
 			}
 		}
 	}