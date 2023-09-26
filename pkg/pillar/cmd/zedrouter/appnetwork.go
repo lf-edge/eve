2fd3c2f70abdf68f7e554ba874b4f26b7c0ef3ca
Milan Lenco <milan@zededa.com>
2023-09-26 12:28:49 +0200

    Use 'defer' to simplify handling of AppNetworkStatus pending flags
    Currently, we have to remember to set PendingAdd/PendingModify inside
    AppNetworkStatus to false just before the corresponding handler returns.
    However, there are few error branches where we forgot to clear
    the pending flag. This causes zedmanager to just wait and not report
    the error published inside the AppNetworkStatus.
    Instead of fixing this case-by-case, let's take advantage of 'defer'
    to make sure that we will never forget to clear the Pending flag,
    even if a new branch with return statement is added in the future.
    
    Signed-off-by: Milan Lenco <milan@zededa.com>
    (cherry picked from commit fbacc17bd315b668174cc2c065f2ece24c5168a9)

--- pkg/pillar/cmd/zedrouter/appnetwork.go
+++ pkg/pillar/cmd/zedrouter/appnetwork.go
@@ -140,8 +140,6 @@ func (z *zedrouter) doActivateAppNetwork(config types.AppNetworkConfig,
 
 	// Update AppNetwork and NetworkInstance status.
 	status.Activated = true
-	status.PendingAdd = false
-	status.PendingModify = false
 	z.publishAppNetworkStatus(status)
 	z.updateNIStatusAfterAppNetworkActivate(status)
 
@@ -307,7 +305,6 @@ func (z *zedrouter) doUpdateActivatedAppNetwork(oldConfig, newConfig types.AppNe
 	// Update app network status as well as status of connected network instances.
 	z.processAppConnReconcileStatus(appConnRecStatus, status)
 	z.reloadStatusOfAssignedIPs(status)
-	status.PendingModify = false
 	z.publishAppNetworkStatus(status)
 	z.updateNIStatusAfterAppNetworkActivate(status)
 }
@@ -335,8 +332,6 @@ func (z *zedrouter) doInactivateAppNetwork(config types.AppNetworkConfig,
 
 	// Update AppNetwork and NetworkInstance status.
 	status.Activated = false
-	status.PendingModify = false
-	status.PendingDelete = false
 	z.updateNIStatusAfterAppNetworkInactivate(status)
 	z.removeAssignedIPsFromAppNetStatus(status)
 	z.publishAppNetworkStatus(status)
