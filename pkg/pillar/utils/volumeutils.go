74d280200cf354d17608865dd9ed9e79fd918116
Naiming Shen <naiming@zededa.com>
2023-09-29 13:59:10 -0700

    - add check for pod runningstatus after start it  - fix a golang format of previous merge
    Signed-off-by: Naiming Shen <naiming@zededa.com>

--- pkg/pillar/utils/volumeutils.go
+++ pkg/pillar/utils/volumeutils.go
@@ -18,9 +18,9 @@ func GetVolumeFormat(log *base.LogObject, fileLocation string) (config.Format, e
 
 	// If kubevirt type, format is always PVC.
 	if base.IsHVTypeKube() {
-        	// Might be cleaner to call GetImgInfo(), i'll cleanup in a later commit
+		// Might be cleaner to call GetImgInfo(), i'll cleanup in a later commit
 		if strings.HasSuffix(fileLocation, ".cidata") {
-                	return config.Format_RAW, nil
+			return config.Format_RAW, nil
 		}
 		return config.Format_PVC, nil
 	}
