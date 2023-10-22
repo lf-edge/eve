a3029bf591a61672514ddc51f9a0b2518dd86312
Roman Penyaev <r.peniaev@gmail.com>
2023-10-22 13:06:11 +0200

    xen-tools/chroot2: move chroot() and chdir() to a child process
    There is no need to do chroot for the parent process before actual
    clone is called, all the environment preparation should be as close
    as possible to the actual execvp().
    
    The other motivation for this change is the next patch, where PID
    file of the child process will be created on the host, but not
    in the chrooted container environment. So postpone chroot and move
    it to the child process.
    
    Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

--- pkg/xen-tools/initrd/chroot2.c
+++ pkg/xen-tools/initrd/chroot2.c
@@ -9,6 +9,8 @@
 typedef struct clone_args clone_args;
 
 struct clone_args {
+    const char *chroot;
+    const char *workdir;
     char **args;
     char *command;
     uid_t uid, gid;
@@ -20,6 +22,10 @@ static char child_stack[STACK_SIZE];    /* Space for child's stack */
 static int childFunc(void *args)
 {
     clone_args *parsed_args = (clone_args *)args;
+
+    chroot(parsed_args->chroot);
+    chdir(parsed_args->workdir);
+
     mount("proc", "/proc", "proc", 0, NULL);
 
     setgid(parsed_args->gid);
@@ -37,16 +43,17 @@ int main(int argc, char **argv) {
     setsid();
     ioctl(0, TIOCSCTTY, 1);
 
-    chroot(argv[1]);
-    chdir(argv[2]);
-
     uid = strtol(argv[3], &endptr, 10);
     gid = strtol(argv[4], &endptr, 10);
 
-    args.uid = uid;
-    args.gid = gid;
-    args.command = argv[5];
-    args.args = argv + 5;
+    args = (struct clone_args) {
+        .chroot = argv[1],
+        .workdir = argv[2],
+        .uid = uid,
+        .gid = gid,
+        .command = argv[5],
+        .args = argv + 5,
+    };
 
     child_pid = clone(childFunc, child_stack + STACK_SIZE, CLONE_NEWPID | SIGCHLD, (void *)(&args));
 
