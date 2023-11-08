4f57021e208ed9cfa69dff539dd13337d5fd70f0
Mikhail Malyshev <mike.malyshev@gmail.com>
2023-11-08 17:37:16 +0000

    Kernel update - [amd64-generic, amd64-rt, arm64-nvidia, riscv64-generic]
    eve-kernel-amd64-v5.10.186-generic
        27965edfa82a: Incorporate BUILD_USER into DIRTY for CI/CD Compatibility
        7f74b9f662de: to support PR build and branch build
        49625a208edb: modified the conditonal trigger, removed an extra docker login command
        1d579fc6e549: Add SBOM generation
    
    eve-kernel-amd64-v6.1.38-generic
        d2da815271a2: Incorporate BUILD_USER into DIRTY for CI/CD Compatibility
        cb66f6534f03: to support PR build and branch build
        a00c3fbfbad9: modified the conditonal trigger, removed an extra docker login command
        c37a7dc3f0d6: Add SBOM generation
        81f0b6d46693: Set LOCALVERSION variable to display commit hash in kernel version
        e0f492255a5a: Set a number of commit hash digits for kernel version
        6846321a29c9: Override ACS capability even if it is supported by device
    
    eve-kernel-amd64-v6.1.38-rt
        332ec0cf9b0e: Incorporate BUILD_USER into DIRTY for CI/CD Compatibility
        a7f93757e665: to support PR build and branch build
        69474badb32d: modified the conditonal trigger, removed an extra docker login command
        61eafbd71fca: Add SBOM generation
        c9b57e13ae32: Set LOCALVERSION variable to display commit hash in kernel version
        1eaa5c3edfe7: Set a number of commit hash digits for kernel version
    
    eve-kernel-arm64-v5.10.104-nvidia
        e8383fe4cf0b: Incorporate BUILD_USER into DIRTY for CI/CD Compatibility
        c098fe9c4456: to support PR build and branch build
        dc1ee42780f4: modified the conditonal trigger, removed an extra docker login command
        d1b31bfde809: Add SBOM generation
        0b225ac39563: bluetooth: Fix Intel bluetooth driver for newer cards
        0a64a037a39a: Set LOCALVERSION variable to display commit hash in kernel version
        e40be81b505e: Set a number of commit hash digits for kernel version
    
    eve-kernel-arm64-v5.10.186-generic
        e1f7e4bb26e4: Incorporate BUILD_USER into DIRTY for CI/CD Compatibility
        783041b09b28: to support PR build and branch build
        552d9310cfc6: modified the conditonal trigger, removed an extra docker login command
        6116ff9b6508: Add SBOM generation
    
    eve-kernel-arm64-v6.1.38-generic
        261c4f91e39c: Incorporate BUILD_USER into DIRTY for CI/CD Compatibility
        d629c8d69348: to support PR build and branch build
        02cc374a5ce5: modified the conditonal trigger, removed an extra docker login command
        6c74b0bf37ce: Add SBOM generation
        e895255dea64: Override ACS capability even if it is supported by device
        a88b1e8934dd: Set LOCALVERSION variable to display commit hash in kernel version
        5dbb7e11289f: Set a number of commit hash digits for kernel version
        0c0ea2fc1587: configs: Fix local version for EVE's kernel
    
    eve-kernel-riscv64-v6.1.38-generic
        62076de26044: Incorporate BUILD_USER into DIRTY for CI/CD Compatibility
        69624df5e158: to support PR build and branch build
        07d3adc7c957: modified the conditonal trigger, removed an extra docker login command
        23419806d716: Add SBOM generation
    
    Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

--- kernel-commits.mk
+++ kernel-commits.mk
@@ -1,7 +1,7 @@
-KERNEL_COMMIT_amd64_v5.10.186_generic = c23a03114a05
-KERNEL_COMMIT_amd64_v6.1.38_generic = 0c3194d84
-KERNEL_COMMIT_amd64_v6.1.38_rt = 585c1cf1e
-KERNEL_COMMIT_arm64_v5.10.104_nvidia = e6cd9a55f
-KERNEL_COMMIT_arm64_v5.10.186_generic = d728c693c149
-KERNEL_COMMIT_arm64_v6.1.38_generic = b18cb9e3e
-KERNEL_COMMIT_riscv64_v6.1.38_generic = 105a2b9bd8e7
+KERNEL_COMMIT_amd64_v5.10.186_generic = 27965edfa82a
+KERNEL_COMMIT_amd64_v6.1.38_generic = d2da815271a2
+KERNEL_COMMIT_amd64_v6.1.38_rt = 332ec0cf9b0e
+KERNEL_COMMIT_arm64_v5.10.104_nvidia = e8383fe4cf0b
+KERNEL_COMMIT_arm64_v5.10.186_generic = e1f7e4bb26e4
+KERNEL_COMMIT_arm64_v6.1.38_generic = 261c4f91e39c
+KERNEL_COMMIT_riscv64_v6.1.38_generic = 62076de26044
