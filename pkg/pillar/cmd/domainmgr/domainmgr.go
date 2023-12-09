5b93d5d852353db922495cd0f19a31051781efb7
eriknordmark <erik@zededa.com>
2023-12-08 16:40:53 -0800

    XXX change DiskStatus to use /mnt
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/pillar/cmd/domainmgr/domainmgr.go
+++ pkg/pillar/cmd/domainmgr/domainmgr.go
@@ -1933,9 +1933,16 @@ func configToStatus(ctx *domainContext, config types.DomainConfig,
 				status.ContainerList = append(status.ContainerList, dcs)
 				// XXX Why did we need to add this?
 				// Note that Format can not be set to CONTAINER
+				// XXX qemu tries to access FileLocation
+				// based on path in fsdev config! Keep at /mnt
+				// {"source":"f67b22d4-66ed-4275-98fb-a0855b6fd224.2.1","content":"qemu-system-x86_
+				// 64:/run/domainmgr/xen/xen1.cfg:157: cannot initialize fsdev 'fsdev1': failed to
+				// open '/mnt1': No such file or directory","msgid":1,"timestamp":{"seconds":170208
+				// 0202,"nanos":438342704}}
+
 				addDiskStatus = append(addDiskStatus,
 					types.DiskStatus{
-						FileLocation: fileLocation,
+						FileLocation: "/mnt",
 						Devtype:      "9P",
 						ReadOnly:     false,
 					})
