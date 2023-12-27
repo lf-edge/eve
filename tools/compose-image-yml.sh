0177dbbbec326d9d458b3697c9d3687ab6015983
Avi Deitcher <avi@deitcher.net>
2023-12-27 15:00:08 +0200

    switch compose-image-file.sh to use flags, update yq version
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- tools/compose-image-yml.sh
+++ tools/compose-image-yml.sh
@@ -3,7 +3,7 @@
 set -e
 
 yq() {
-  docker run -i --rm -v "${PWD}/":/workdir intoiter/yq:3.1.0 -i -y "$@"
+  docker run -i --rm -v "${PWD}/":/workdir -w /workdir mikefarah/yq:4.40.5 "$@"
 }
 
 process-image-template() {
@@ -22,31 +22,83 @@ process-image-template() {
     for bit in "${bits[@]}"; do
         case "${bit}" in
             dev)
-                yq '(.onboot[] | select(.image == "PILLAR_TAG").image) |= "PILLAR_DEV_TAG"' "${out_templ_path}"
-                yq '(.services[] | select(.image == "PILLAR_TAG").image) |= "PILLAR_DEV_TAG"' "${out_templ_path}"
+                yq '(.onboot[] | select(.image == "PILLAR_TAG").image) |= "PILLAR_DEV_TAG"' < "${out_templ_path}" | spongefile "${out_templ_path}"
+                yq '(.services[] | select(.image == "PILLAR_TAG").image) |= "PILLAR_DEV_TAG"' < "${out_templ_path}" | spongefile "${out_templ_path}"
                 ;;
         esac
     done
 }
 
 patch_hv() {
+    # note that we have to do that careful shell substitution, because yq 4
+    # doesn't support passing in a variable as the value to --arg, requiring
+    # setting it as an env var; which is more difficult to do when passed to docker
     # shellcheck disable=SC2016
-    yq --arg hv "$1" '(.files[] | select(.contents == "EVE_HV")).contents |= $hv' "$2"
+    yq '(.files[] | select(.contents == "EVE_HV")).contents |= "'"$1"'"' < "$2" | spongefile "$2"
 }
+
+# because sponge doesn't exist everywhere, and this one uses a tmpfile
+spongefile() {
+    local tmp=""
+    tmp=$(mktemp)
+    cat > "$tmp"
+    cat "$tmp" > "$1"
+    rm "$tmp"
+}
+
 main() {
-    local base_templ_path="$1"
-    local out_templ_path="$2"
-    local eve_version="$3"
-    local eve_hv="$4"
+    local base_templ_path=""
+    local out_templ_path=""
+    local eve_version=""
+    local eve_hv=""
+
+    while getopts "b:o:v:h:" opt; do
+        case ${opt} in
+            b )
+                base_templ_path=$OPTARG
+                ;;
+            o )
+                out_templ_path=$OPTARG
+                ;;
+            v )
+                eve_version=$OPTARG
+                ;;
+            h )
+                eve_hv=$OPTARG
+                ;;
+            \? )
+                echo "Invalid option: -$OPTARG" 1>&2
+                exit 1
+                ;;
+            : )
+                echo "Invalid option: -$OPTARG requires an argument" 1>&2
+                exit 1
+                ;;
+        esac
+    done
+    shift $((OPTIND -1))
+    if [ -z "$out_templ_path" ] || [ -z "$eve_version" ] || [ -z "$eve_hv" ] || [ -z "$base_templ_path" ]; then
+        usage
+    fi
+    local modifiers="$*"
 
     cp "${base_templ_path}" "${out_templ_path}"
-    if [ -e "${out_templ_path}".yq ]; then
-        yq -f "${out_templ_path}".yq "${out_templ_path}" || exit 1
-    fi
+
+    for modifier in ${modifiers}; do
+        if [ ! -f "${modifier}" ]; then
+            continue
+        fi
+        yq --from-file "${modifier}" < "${out_templ_path}" | spongefile "${out_templ_path}"|| exit 1
+    done
 
     patch_hv "${eve_hv}" "${out_templ_path}"
 
     process-image-template "${out_templ_path}" "${eve_version}"
+}
+
+usage() {
+    echo "Usage: $0 -b <base template> -o <output template> -v <eve version> -h <eve hv> <modifiers>"
+    exit 1
 }
 
 main "$@"
