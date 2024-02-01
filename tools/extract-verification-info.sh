050abddaabf5a7d59db950ccec122d5a8fa4554f
Ioannis Sfakianakis <jsfakas@gmail.com>
2024-02-01 15:55:23 +0200

    Fixes in the verification scripts and documentation - Added renderer in the curl command of publish-verification-info.sh.   This is necessary because we changed the URL of the web application   that stores the published verification results from http to https - Added the necessary commands in the extract-verification-info.sh   script so that it works in MAC as well. - Updated the documentation that explains how to extract the logs   of the verification process to match the current status. - Added a section in the documentation that explains how to publish   the verification process in the certification web application.
    Signed-off-by: Ioannis Sfakianakis <jsfakas@gmail.com>

--- tools/extract-verification-info.sh
+++ tools/extract-verification-info.sh
@@ -2,15 +2,42 @@
 
 img=$1
 
-if [ "$img" == "" ]; then
-  echo "Usage ./extract_verification_info.sh <verification.img>"
+if [ "$img" == "" ] || [ "$EUID" -ne 0 ]; then
+  echo "Usage sudo ./extract-verification-info.sh <USB_device_name|verification_img>"
+  echo "E.g., sudo ./extract-verification-info.sh /dev/disk4"
+  echo "Or, sudo ./extract-verification-info.sh dist/amd64/current/verification.raw"
   exit
 fi
 
-size=$(fdisk -l "$img" | grep raw5 | awk '{print $2}')
-mkdir /tmp/verification_mnt
-sudo mount -o offset=$((size*512)) "$img" /tmp/verification_mnt
-cp /tmp/verification_mnt/*/summary.log ./
-cat /tmp/verification_mnt/*/summary.log
-sudo umount /tmp/verification_mnt
-rmdir /tmp/verification_mnt
+checkOScmd=$(echo "${OSTYPE}" | grep darwin) # Running on MacOS
+mountDir="/tmp/verification_mnt"
+if [ -n "${checkOScmd}" ]; then # MacOS
+  devicename="${img}"
+  if [ -f "${img}" ]; then # file
+    tmp=$(/usr/bin/hdiutil attach -imagekey diskimage-class=CRawDiskImage -nomount "${img}")
+    devicename=$(echo "${tmp}" | grep "GUID_partition_scheme" | awk '{print $1}')
+  fi
+  /usr/sbin/diskutil mount "${devicename}"s5
+  mountDir="/Volumes/INVENTORY"
+else # Linux
+  mkdir "${mountDir}"
+  if [ -f "${img}" ]; then # file
+    size=$(fdisk -l "$img" | grep raw5 | awk '{print $2}')
+    mount -o offset=$((size*512)) "$img" "${mountDir}"
+  else # block device
+    mount "${img}"5 "${mountDir}"
+  fi
+fi
+
+cp -r ${mountDir}/* ./
+cat ${mountDir}/*/summary.log
+
+if [ -n "${checkOScmd}" ]; then # MacOS
+  /usr/sbin/diskutil umount "${devicename}"s5
+  if [ -f "${img}" ]; then # file
+    /usr/bin/hdiutil detach "${devicename}"
+  fi
+else # Linux
+  umount "${mountDir}"
+  rmdir "${mountDir}"
+fi
