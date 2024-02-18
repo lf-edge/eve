2b36f9c5d9f441a689db1d2e108cdd4f2813d8ae
eriknordmark <erik@zededa.com>
2024-02-18 22:41:22 +0100

    Do not delete ControllerCert if in use
    Populate cipherContexts on boot and check against it before deleting a
    ControllerCert.
    
    Signed-off-by: eriknordmark <erik@zededa.com>
    (cherry picked from commit c1a0e10c482a12e15cc4c854a81c6bc97506e3d6)

--- pkg/pillar/cmd/zedagent/zedagent.go
+++ pkg/pillar/cmd/zedagent/zedagent.go
@@ -1133,6 +1133,7 @@ func initPublications(zedagentCtx *zedagentContext) {
 		log.Fatal(err)
 	}
 	getconfigCtx.pubCipherContext.ClearRestarted()
+	populateCipherContexts(getconfigCtx)
 
 	// for ContentTree config Publisher
 	getconfigCtx.pubContentTreeConfig, err = ps.NewPublication(
