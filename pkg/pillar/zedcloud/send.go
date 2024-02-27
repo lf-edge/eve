5e8f1822852401911b22dcbf8312a5242eaca7d3
eriknordmark <erik@zededa.com>
2024-02-27 20:33:30 +0100

    Remove spurious error: OCSP expired
    Signed-off-by: eriknordmark <erik@zededa.com>
    (cherry picked from commit 000b3561baa41600d038b718bea8c1bf8d013467)

--- pkg/pillar/zedcloud/send.go
+++ pkg/pillar/zedcloud/send.go
@@ -1053,19 +1053,11 @@ func SendOnIntf(workContext context.Context, ctx *ZedCloudContext, destURL strin
 				continue
 			}
 
-			if connState.OCSPResponse == nil ||
-				!stapledCheck(log, connState) {
-
-				if connState.OCSPResponse == nil {
-					// XXX remove debug check
-					log.Tracef("no OCSP response for %s\n",
-						reqURL)
-				}
-				errStr := fmt.Sprintf("OCSP stapled check failed for %s",
-					reqURL)
-
-				//XXX OSCP is not implemented in cloud side so
-				// commenting out it for now.
+			if ok, err := stapledCheck(log, connState); !ok {
+				errStr := fmt.Sprintf("OCSP stapled check failed for %s: %s",
+					reqURL, err)
+				// XXX OSCP is not implemented in controller
+				// so commenting out it for now.
 				if false {
 					log.Errorln(errStr)
 					// Inform ledmanager about broken cloud connectivity
