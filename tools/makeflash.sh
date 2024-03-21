b05dbc53ade95e402b752567f842e93f313f7052
Roman Penyaev <r.peniaev@gmail.com>
2024-03-21 15:33:21 +0100

    Makefile,tools/makeflash.sh: switch to the dynamic image size calculation
    This patch removes creation of the image in the 'makeflash.sh' by passing
    '-C' option to the 'mkimage-raw-efi' and 'mkverification-raw-efi' in order
    to use dynamic image calculation.
    
    Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

--- tools/makeflash.sh
+++ tools/makeflash.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 # Usage:
 #
-#      ./makeflash.sh <raw.img> [-C size] <input dir> <output.img> [partitions]
+#      ./makeflash.sh <raw.img> [-C] <input dir> <output.img> [partitions]
 #
 EVE="$(cd "$(dirname "$0")" && pwd)/../"
 PATH="$EVE/build-tools/bin:$PATH"
 MKFLASH_TAG="$(linuxkit pkg show-tag "$EVE/pkg/$1")"
 shift 1
 
+# Recreate image file
+CREATE_IMG_ARG=""
 if [ "$1" = "-C" ]; then
-    SIZE="$2"
-    dd if=/dev/zero of="$4" seek=$(( SIZE * 1024 * 1024 - 1)) bs=1 count=1
-    # If we're a non-root user, the bind mount gets permissions sensitive.
-    # So we go docker^Wcowboy style
-    chmod ugo+w "$4"
-    shift 2
+    rm -f "$3"
+    touch "$3"
+    chmod ugo+w "$3"
+    # Pass '-C' further
+    CREATE_IMG_ARG="-C"
+    shift
 fi
 
 SOURCE="$(cd "$1" && pwd)"
 IMAGE="$(cd "$(dirname "$2")" && pwd)/$(basename "$2")"
 shift 2
 
-docker run --rm -v "$SOURCE:/parts" -v "$IMAGE:/output.img" "$MKFLASH_TAG" /output.img "$@"
+docker run --rm -v "$SOURCE:/parts" -v "$IMAGE:/output.img" "$MKFLASH_TAG" "$CREATE_IMG_ARG" /output.img "$@"
