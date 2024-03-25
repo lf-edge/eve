d268e1ffe927f343c6a2a96f1428174a7afb0c3e
Pramodh Pallapothu <pramodh@zededa.com>
2024-03-25 16:47:55 -0700

    Pass the KubeImageName to domainmgr
    If in the app manifest HV is set to NOHYPER and the eve flavor is kubevirt
    we set the KubeImageName in the domainconfig to the volumereference name
    
    domainmgr will use that to start pods (native containers)
    
    Signed-off-by: Pramodh Pallapothu <pramodh@zededa.com>

--- pkg/pillar/cmd/zedmanager/handledomainmgr.go
+++ pkg/pillar/cmd/zedmanager/handledomainmgr.go
@@ -61,7 +61,9 @@ func MaybeAddDomainConfig(ctx *zedmanagerContext,
 			continue
 		}
 		location := vrs.ActiveFileLocation
-		if location == "" {
+		// Volumes in kubevirt eve are of PVC type and managed by kubernetes.
+		// There is no specific filelocation
+		if location == "" && !ctx.hvTypeKube {
 			errStr := fmt.Sprintf("No ActiveFileLocation for %s", vrs.DisplayName)
 			log.Error(errStr)
 			return nil, errors.New(errStr)
@@ -77,6 +79,12 @@ func MaybeAddDomainConfig(ctx *zedmanagerContext,
 		disk.Target = vrs.Target
 		disk.CustomMeta = vrs.CustomMeta
 		dc.DiskConfigList = append(dc.DiskConfigList, disk)
+		// For kubevirt eve native containers (NOHYPER) we need set the KubeImageName in domainconfig
+		// pods will be launched using that KubeImageName
+		if aiConfig.FixedResources.VirtualizationMode == types.NOHYPER && ctx.hvTypeKube {
+			dc.VirtualizationMode = types.NOHYPER
+			dc.KubeImageName = vrs.ReferenceName
+		}
 	}
 	// let's fill some of the default values (arguably we may want controller
 	// to do this for us and give us complete config, but it is easier to
@@ -150,12 +158,12 @@ func lookupDomainStatus(ctx *zedmanagerContext, key string) *types.DomainStatus 
 }
 
 func publishDomainConfig(ctx *zedmanagerContext,
-	status *types.DomainConfig) {
+	config *types.DomainConfig) {
 
-	key := status.Key()
+	key := config.Key()
 	log.Tracef("publishDomainConfig(%s)", key)
 	pub := ctx.pubDomainConfig
-	pub.Publish(key, *status)
+	pub.Publish(key, *config)
 }
 
 func unpublishDomainConfig(ctx *zedmanagerContext, uuidStr string) {
