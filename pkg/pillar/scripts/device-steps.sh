b9b2289aeb68e1db51c1306bbbb98c2a50f1975f
eriknordmark <erik@zededa.com>
2024-04-16 22:04:20 -0700

    Conditionally invoke fdo script
    Signed-off-by: eriknordmark <erik@zededa.com>

--- pkg/pillar/scripts/device-steps.sh
+++ pkg/pillar/scripts/device-steps.sh
@@ -352,11 +352,12 @@ else
     echo "$(date -Ins -u) Using existing device key pair"
 fi
 while [ ! -s $CONFIGDIR/server ] || [ ! -s $CONFIGDIR/root-certificate.pem ]; do
-    echo "$(date -Ins -u) No server or root-certificate to connect to. Wait for FDO" | tee /dev/console
-    # XXX insert FDO client call here
-    sleep 10
-    # XXX need mount -o remount,rw /config to copy files
-    # XXX and eve config mount for the persistent copy.
+    if ! [ -f /opt/zededa/bin/run-fdo.sh ]; then
+        echo "$(date -Ins -u) No server or root-certificate to connect to. Done" | tee /dev/console
+        exit 0
+    fi
+    echo "$(date -Ins -u) No server or root-certificate to connect to. Run FDO" | tee /dev/console
+    /opt/zededa/bin/run-fdo.sh | tee /dev/console
 done
 
 if [ -c $TPM_DEVICE_PATH ] && ! [ -f $DEVICE_KEY_NAME ]; then
