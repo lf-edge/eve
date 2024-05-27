lf-edge/eve

commit 449b6f268ae9b6d366985985bbff5fe595a206c6

extend Run() signature to include baseDir; add WithBaseDir option to agent.Init; remove pidfile creation from all pillar services, replacing with call to agent.Init(WithPidfile()); passed inline or not explicitly to Run()
Signed-off-by: Avi Deitcher <avi@deitcher.net>

pkg/pillar/cmd/vaultmgr/vaultmgr.go
@@ -36,7 +36,6 @@ import (
 	"github.com/lf-edge/eve/pkg/pillar/base"
 	uc "github.com/lf-edge/eve/pkg/pillar/cmd/upgradeconverter"
 	etpm "github.com/lf-edge/eve/pkg/pillar/evetpm"
-	"github.com/lf-edge/eve/pkg/pillar/pidfile"
 	"github.com/lf-edge/eve/pkg/pillar/pubsub"
 	"github.com/lf-edge/eve/pkg/pillar/types"
 	"github.com/lf-edge/eve/pkg/pillar/utils/persist"
@@ -173,7 +172,7 @@ func checkAndPublishVaultConfig(ctx *vaultMgrContext) bool {
 }
 
 // Run is the entrypoint for running vaultmgr as a standalone program
-func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string) int {
+func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string, baseDir string) int {
 	logger = loggerArg
 	log = logArg
 
@@ -182,21 +181,33 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 		ps:     ps,
 		ucChan: make(chan struct{}),
 	}
-	agentbase.Init(&ctx, logger, log, agentName,
-		agentbase.WithArguments(arguments))
+
+	// do we run a single command, or long-running service?
+	// if any args defined, will run that single command and exit.
+	// otherwise, will run the agent
+	var (
+		command string
+		args    []string
+	)
+	if len(arguments) > 0 {
+		command = arguments[0]
+	}
+	if len(arguments) > 1 {
+		args = arguments[1:]
+	}
+
+	// if an explicit command was given, run that command and return, else run the agent
+	if command != "" {
+		return runCommand(ps, command, args)
+	}
+
+	agentArgs := []agentbase.AgentOpt{agentbase.WithBaseDir(baseDir), agentbase.WithArguments(arguments), agentbase.WithPidFile()}
+	agentbase.Init(&ctx, logger, log, agentName, agentArgs...)
 
 	handler = vault.GetHandler(log)
 
-	// if any args defined, will run command inline and return
-	if len(ctx.args) > 0 {
-		return runInline(ps, ctx.args[0], ctx.args[1:])
-	}
-
 	log.Functionf("Starting %s\n", agentName)
 
-	if err := pidfile.CheckAndCreatePidfile(log, agentName); err != nil {
-		log.Fatal(err)
-	}
 	// Run a periodic timer so we always update StillRunning
 	stillRunning := time.NewTicker(15 * time.Second)
 	ps.StillRunning(agentName, warningTime, errorTime)
@@ -310,7 +321,7 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 	}
 }
 
-func runInline(ps *pubsub.PubSub, command string, _ []string) int {
+func runCommand(ps *pubsub.PubSub, command string, _ []string) int {
 	switch command {
 	case "setupDeprecatedVaults":
 		if err := handler.SetupDeprecatedVaults(); err != nil {

pkg/pillar/agentbase/agent.go
@@ -2,10 +2,11 @@ package agentbase
 
 import (
 	"flag"
+	"time"
+
 	"github.com/lf-edge/eve/pkg/pillar/base"
 	"github.com/lf-edge/eve/pkg/pillar/pidfile"
 	"github.com/lf-edge/eve/pkg/pillar/pubsub"
-	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -36,6 +37,7 @@ type AgentBase struct {
 	needWatchdog bool
 	needPidFile  bool
 	arguments    []string
+	baseDir      string
 }
 
 // AgentOpt function may be used to modify options
@@ -62,6 +64,13 @@ func WithWatchdog(pubSub *pubsub.PubSub, warningTime, errorTime time.Duration) A
 		a.warningTime = warningTime
 		a.errorTime = errorTime
 		a.needWatchdog = true
+	}
+}
+
+// WithBaseDir defines base directory for file-related activities, such as pidfile
+func WithBaseDir(baseDir string) AgentOpt {
+	return func(a *AgentBase) {
+		a.baseDir = baseDir
 	}
 }
 
@@ -119,7 +128,7 @@ func Init(agent Agent, logger *logrus.Logger, log *base.LogObject, agentName str
 		logger.SetLevel(logrus.InfoLevel)
 	}
 	if agentBase.needPidFile {
-		if err := pidfile.CheckAndCreatePidfile(log, agentBase.agentName); err != nil {
+		if err := pidfile.CheckAndCreatePidfile(log, agentBase.agentName, pidfile.WithBaseDir(agentBase.baseDir)); err != nil {
 			log.Fatal(err)
 		}
 	}

pkg/pillar/cmd/baseosmgr/baseosmgr.go
@@ -15,7 +15,6 @@ import (
 	"github.com/lf-edge/eve/pkg/pillar/agentbase"
 	"github.com/lf-edge/eve/pkg/pillar/agentlog"
 	"github.com/lf-edge/eve/pkg/pillar/base"
-	"github.com/lf-edge/eve/pkg/pillar/pidfile"
 	"github.com/lf-edge/eve/pkg/pillar/pubsub"
 	"github.com/lf-edge/eve/pkg/pillar/types"
 	"github.com/lf-edge/eve/pkg/pillar/utils/wait"
@@ -72,7 +71,7 @@ func (ctxPtr *baseOsMgrContext) AddAgentSpecificCLIFlags(flagSet *flag.FlagSet) 
 var logger *logrus.Logger
 var log *base.LogObject
 
-func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string) int {
+func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string, baseDir string) int {
 	logger = loggerArg
 	log = logArg
 
@@ -81,14 +80,13 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 		globalConfig: types.DefaultConfigItemValueMap(),
 	}
 	agentbase.Init(&ctx, logger, log, agentName,
+		agentbase.WithPidFile(),
+		agentbase.WithBaseDir(baseDir),
 		agentbase.WithArguments(arguments))
 
 	if *ctx.versionPtr {
 		fmt.Printf("%s: %s\n", agentName, Version)
 		return 0
-	}
-	if err := pidfile.CheckAndCreatePidfile(log, agentName); err != nil {
-		log.Fatal(err)
 	}
 
 	// Run a periodic timer so we always update StillRunning

pkg/pillar/cmd/client/client.go
@@ -22,7 +22,6 @@ import (
 	"github.com/lf-edge/eve/pkg/pillar/base"
 	"github.com/lf-edge/eve/pkg/pillar/flextimer"
 	"github.com/lf-edge/eve/pkg/pillar/hardware"
-	"github.com/lf-edge/eve/pkg/pillar/pidfile"
 	"github.com/lf-edge/eve/pkg/pillar/pubsub"
 	"github.com/lf-edge/eve/pkg/pillar/types"
 	"github.com/lf-edge/eve/pkg/pillar/utils"
@@ -116,7 +115,7 @@ var (
 	log               *base.LogObject
 )
 
-func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string) int { //nolint:gocyclo
+func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string, baseDir string) int { //nolint:gocyclo
 	logger = loggerArg
 	log = logArg
 
@@ -130,18 +129,16 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 		},
 	}
 
-	agentbase.Init(&clientCtx, logger, log, agentName,
-		agentbase.WithArguments(arguments))
+	args := []agentbase.AgentOpt{agentbase.WithArguments(arguments), agentbase.WithBaseDir(baseDir)}
+	if !*clientCtx.noPidPtr {
+		args = append(args, agentbase.WithPidFile())
+	}
+	agentbase.Init(&clientCtx, logger, log, agentName, args...)
 
 	maxRetries := *clientCtx.maxRetriesPtr
 	if *clientCtx.versionPtr {
 		fmt.Printf("%s: %s\n", agentName, Version)
 		return 0
-	}
-	if !*clientCtx.noPidPtr {
-		if err := pidfile.CheckAndCreatePidfile(log, agentName); err != nil {
-			log.Fatal(err)
-		}
 	}
 
 	pub, err := ps.NewPublication(pubsub.PublicationOptions{

pkg/pillar/cmd/command/command.go
@@ -82,7 +82,7 @@ func (ctx *commandAgentState) AddAgentSpecificCLIFlags(flagSet *flag.FlagSet) {
 }
 
 // Run is the main aka only entrypoint
-func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string) int {
+func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string, baseDir string) int { //nolint:gocyclo
 	logger = loggerArg
 	log = logArg
 	// Report nano timestamps
@@ -94,7 +94,7 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 	// Context to pass around
 	ctx := commandAgentState{}
 	agentbase.Init(&ctx, logger, log, agentName,
-		agentbase.WithArguments(arguments))
+		agentbase.WithArguments(arguments), agentbase.WithBaseDir(baseDir))
 
 	timeLimit = *ctx.timeLimitPtr
 	combinedOutput = *ctx.combinedPtr

pkg/pillar/cmd/conntrack/conntrack.go
@@ -45,12 +45,13 @@ func (state *connTrackAgentState) AddAgentSpecificCLIFlags(flagSet *flag.FlagSet
 	state.markMask = flagSet.Int("mask", 0, "Delete flow with Mark mask")
 }
 
-func Run(_ *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string) int {
+func Run(_ *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string, baseDir string) int {
 	logger = loggerArg
 	log = logArg
 
 	ctx := connTrackAgentState{}
 	agentbase.Init(&ctx, logger, log, agentName,
+		agentbase.WithBaseDir(baseDir),
 		agentbase.WithArguments(arguments))
 
 	// conntrack [-D <-s address> [-p proto][-P port][-m Mark]]

pkg/pillar/cmd/diag/diag.go
@@ -116,7 +116,7 @@ var nilUUID uuid.UUID
 var logger *logrus.Logger
 var log *base.LogObject
 
-func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string) int {
+func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string, baseDir string) int { //nolint:gocyclo
 	logger = loggerArg
 	log = logArg
 	triggerPrintChan := make(chan string, 1)
@@ -126,6 +126,7 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 		triggerPrintChan: triggerPrintChan,
 	}
 	agentbase.Init(&ctx, logger, log, agentName,
+		agentbase.WithBaseDir(baseDir),
 		agentbase.WithArguments(arguments))
 
 	ctx.forever = *ctx.foreverPtr

pkg/pillar/cmd/domainmgr/domainmgr.go
@@ -37,7 +37,6 @@ import (
 	"github.com/lf-edge/eve/pkg/pillar/flextimer"
 	"github.com/lf-edge/eve/pkg/pillar/hypervisor"
 	"github.com/lf-edge/eve/pkg/pillar/kubeapi"
-	"github.com/lf-edge/eve/pkg/pillar/pidfile"
 	"github.com/lf-edge/eve/pkg/pillar/pubsub"
 	"github.com/lf-edge/eve/pkg/pillar/sema"
 	"github.com/lf-edge/eve/pkg/pillar/sriov"
@@ -148,7 +147,7 @@ var hyper hypervisor.Hypervisor // Current hypervisor
 var logger *logrus.Logger
 var log *base.LogObject
 
-func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string) int {
+func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, arguments []string, baseDir string) int { //nolint:gocyclo
 	logger = loggerArg
 	log = logArg
 
@@ -169,6 +168,8 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 		hvTypeKube:          base.IsHVTypeKube(),
 	}
 	agentbase.Init(&domainCtx, logger, log, agentName,
+		agentbase.WithBaseDir(baseDir),
+		agentbase.WithPidFile(),
 		agentbase.WithArguments(arguments))
 
 	var err error
@@ -183,9 +184,6 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 		log.Fatal(err)
 	}
 
-	if err := pidfile.CheckAndCreatePidfile(log, agentName); err != nil {
-		log.Fatal(err)
-	}
 	log.Functionf("Starting %s with %s hypervisor backend", agentName, hyper.Name())
 
 	// Run a periodic timer so we always update StillRunning

commit 7e49b1fe3aba0767c7f23cb61b0e72a28698f47b

pillar/vaultmgr.go: fix segfault on attempt to init fscrypt
This fixes the following crash:

  (ns: pillar) 617351bd-6ed6-4cb5-a8e1-04ad4a53e63f:/# /opt/zededa/bin/vaultmgr setupDeprecatedVaults
  INFO[0000] Containerd Init
  panic: runtime error: invalid memory address or nil pointer dereference
  [signal SIGSEGV: segmentation violation code=0x1 addr=0x40 pc=0x2382518]

  goroutine 1 [running]:
  github.com/lf-edge/eve/pkg/pillar/cmd/vaultmgr.runCommand(0xc0007ffa70?, {0x7ffdfb082f21, 0x15}, {0x40b58a0?, 0x1?, 0xc0007ffa68?})
	        /pillar/cmd/vaultmgr/vaultmgr.go:327 +0x158
		github.com/lf-edge/eve/pkg/pillar/cmd/vaultmgr.Run(0xc00088d2f0, 0x2b75c4c?, 0x23?, {0xc000052050?, 0x1, 0x1}, {0x0, 0x0})
	        /pillar/cmd/vaultmgr/vaultmgr.go:201 +0x776
		main.runService({0x7ffdfb082f18?, 0xc000052050?}, {0x2c0f7e8?, 0x8?}, 0xa0?)
	        /pillar/zedbox/zedbox.go:157 +0x217
		main.main()
	        /pillar/zedbox/zedbox.go:130 +0x145

This crash leads to a missing fscrypt first initialization, so node onboards,
but vault is not ready and no applications are started. The following error
message can be observed:

  Encryption failed: exit status 1, ,
  [ERROR] fscrypt encrypt: filesystem /hostfs/persist is not setup for use with fscrypt
          Run "sudo fscrypt setup /hostfs/persist" to use fscrypt on this filesystem.

Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>
Reported-by: Milan Lenco <milan@zededa.com>
Fixes: 449b6f268ae9 ("extend Run() signature to include baseDir; add
	              WithBaseDir option to agent.Init; remove pidfile
		      creation from all pillar services, replacing with
		      call to agent.Init(WithPidfile()); passed inline
		      or not explicitly to Run()")

pkg/pillar/cmd/vaultmgr/vaultmgr.go
@@ -196,6 +196,8 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 		args = arguments[1:]
 	}
 
+	handler = vault.GetHandler(log)
+
 	// if an explicit command was given, run that command and return, else run the agent
 	if command != "" {
 		return runCommand(ps, command, args)
@@ -203,8 +205,6 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 
 	agentArgs := []agentbase.AgentOpt{agentbase.WithBaseDir(baseDir), agentbase.WithArguments(arguments), agentbase.WithPidFile()}
 	agentbase.Init(&ctx, logger, log, agentName, agentArgs...)
-
-	handler = vault.GetHandler(log)
 
 	log.Functionf("Starting %s\n", agentName)
 