ef9abcfaca7880b062ae9d9ce90a55bd1a95000f
Renê de Souza Pinto <rene@renesp.com.br>
2024-05-28 17:59:06 +0200

    tools: Introduces get-deps tool
    This commit introduces the tool get-deps (under tools/get-deps/), which
    scans the pkg directory and process each Dockerfile (or Dockerfile.in) file
    in order to find all build dependencies (containers) of each package. It
    can also check package dependencies for rootfs images. The final package
    dependency tree can be generated as a PNG image (using dot tool) or as a
    Makefile that can be included to the main Makefile.
    
    The usage of the tool the following:
    
        ./tools/get-deps/get-deps [-r] <-i|-m> <output_file>
    
      -i string
        	Generate a PNG image file
      -m string
        	Generate a Makefile auxiliary file
      -r	Also generates dependencies for rootfs image
    
    Examples:
    
        ./tools/get-deps/get-deps -i deptree.png
        ./tools/get-deps/get-deps -r -i deptree.png
        ./tools/get-deps/get-deps -m pkg-deps.mk
    
    Co-authored-by: Christoph Ostarek <christoph@zededa.com>
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

---
+++ tools/get-deps/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestParseDockerfile(t *testing.T) {
+	f := strings.NewReader(`
+			FROM abcd:12345 AS f
+			RUN echo \
+			FROM thisshouldnotbeincluded:666666
+
+			FROM f
+			RUN echo
+
+			FROM f-${TARGETARCH}
+		`)
+
+	targets := parseDockerfile(f)
+
+	foundMap := map[string]bool{
+		"abcd:12345": false,
+		"f":          false,
+		"f-amd64":    false,
+	}
+
+	for _, target := range targets {
+		foundMap[target] = true
+	}
+
+	for target, found := range foundMap {
+		if !found {
+			t.Fatalf("did not find target %s", target)
+		}
+	}
+}
