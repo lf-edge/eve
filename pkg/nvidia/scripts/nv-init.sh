077cbc8b27a0f21cd3a93af30af5680ce7da8d28
Renê de Souza Pinto <rene@renesp.com.br>
2024-06-05 17:42:37 +0200

    nvidia: Introduce NVIDIA package
    This commit introduces the pkg/nvidia:
    
    This package fetches the Jetson Linux (currently 35.5.0) and extracts all
    .deb packages containing all relevant files (libraries, firmwares, etc)
    needed to run specific NVIDIA GPU applications, such as CUDA applications.
    
    Then, all provided CDI files are parsed by the process-cdi.sh script, which
    will copy all files pointed in the CDI from the extracted .deb packages.
    
    The output container will provide a directory /opt/vendor/nvidia, where:
    
    - /opt/vendor/nvidia/dist: Contains all files pointed in all CDI files
    - /opt/vendor/nvidia/bin/nvidia-ctk and
    - /opt/vendor/nvidia/bin/nvidia-cdi-hook: applications needed to process
      libraries files from the CDI spec.
    - /opt/vendor/nvidia/bin/ldconfig-glibc: ldconfig tool for GNU libc
    - /opt/vendor/nvidia/init.d/nv-init.sh: Script to perform platform setup
      actions, to be executed during pillar's initialization
    
    All supported CDI files will be available at /etc/cdi.
    
    The following CDI files are provided:
    
    - jetson-xavier-nx.yaml: For devices based on Jetson Xavier NX
    - jetson-orin-nano.yaml: For devices based on Jetson Orin Nano
    
    The CDI files were originally generated on a bare metal Jetson Linux
    using the nvidia-ctk tool.
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

---
+++ pkg/nvidia/scripts/nv-init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Copyright (c) 2024 Zededa, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+VENDOR="/opt/vendor/nvidia"
+
+# This script is executed from pillar, so we need to export the variables
+# below to execute udevadm from hostfs
+export PATH="$PATH:/hostfs/bin"
+export LD_LIBRARY_PATH="/hostfs/lib"
+
+# Setup udev rules
+mkdir -p /run/udev/rules.d/
+cp "${VENDOR}"/etc/udev/rules.d/* /run/udev/rules.d/
+# Reload rules and trigger udev events
+udevadm control --reload
+udevadm info -a -p /devices/gpu.0
+
+# Load modules
+modprobe nvidia
+modprobe nvidia_modeset
+
+# Enforces add for framebuffer and nvidia modules, so we have /dev/fb0 and
+# /dev/nvidiactrl even when there is no monitor connected to the display
+# port. These devices must be present because they are on the CDI spec.
+echo "add" > /sys/module/fb/uevent 2> /dev/null
+echo "add" > /sys/module/nvidia/uevent 2> /dev/null
