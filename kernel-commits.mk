daed2c576acb16335a190f625216bc3a3def57a5
Mikhail Malyshev <mike.malyshev@gmail.com>
2024-06-13 10:23:30 +0000

    Kernel update - [amd64-generic, amd64-generic, amd64-rt, amd64-generic, arm64-nvidia, arm64-generic, arm64-generic, riscv64-generic]
    eve-kernel-amd64-v5.10.186-generic
        d61682724485: Dockerfile.clang: fix spelling errors
    
    eve-kernel-amd64-v6.1.38-generic
        e0f7423a397f: Update Hailo driver to 4.16 version
        c00fe6ae5149: Add Hailo 8 GPU driver
    
    eve-kernel-amd64-v6.1.38-rt
        6e14ff1f47dd: Dockerfile.clang: fix spelling errors
    
    eve-kernel-amd64-v6.1.68-generic
        c825cbffe24f: Dockerfile.clang: fix spelling errors
    
    eve-kernel-arm64-v5.10.104-nvidia
        2473fcc9c1bb: Dockerfile.clang: fix spelling errors
    
    eve-kernel-arm64-v5.10.186-generic
        804663a82829: Dockerfile.clang: fix spelling errors
    
    eve-kernel-arm64-v6.1.38-generic
        39d6c83d31ea: Dockerfile.clang: fix spelling errors
    
    eve-kernel-riscv64-v6.1.38-generic
        a90bcdfa08f8: Dockerfile.clang: fix spelling errors
    
    Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

--- kernel-commits.mk
+++ kernel-commits.mk
@@ -1,8 +1,8 @@
-KERNEL_COMMIT_amd64_v5.10.186_generic = 37256d6aff33
-KERNEL_COMMIT_amd64_v6.1.38_generic = 5807270eacf2
-KERNEL_COMMIT_amd64_v6.1.38_rt = ef571b34f5ad
-KERNEL_COMMIT_amd64_v6.1.68_generic = 662de9abec4d
-KERNEL_COMMIT_arm64_v5.10.104_nvidia = 02c64aba1d9f
-KERNEL_COMMIT_arm64_v5.10.186_generic = 594f2361a83f
-KERNEL_COMMIT_arm64_v6.1.38_generic = aa4b24ade858
-KERNEL_COMMIT_riscv64_v6.1.38_generic = 72192e3cbc74
+KERNEL_COMMIT_amd64_v5.10.186_generic = d61682724485
+KERNEL_COMMIT_amd64_v6.1.38_generic = e0f7423a397f
+KERNEL_COMMIT_amd64_v6.1.38_rt = 6e14ff1f47dd
+KERNEL_COMMIT_amd64_v6.1.68_generic = c825cbffe24f
+KERNEL_COMMIT_arm64_v5.10.104_nvidia = 2473fcc9c1bb
+KERNEL_COMMIT_arm64_v5.10.186_generic = 804663a82829
+KERNEL_COMMIT_arm64_v6.1.38_generic = 39d6c83d31ea
+KERNEL_COMMIT_riscv64_v6.1.38_generic = a90bcdfa08f8
