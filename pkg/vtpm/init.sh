dceb61dd30e6f3df5ed74e3221ad1c877542e001
Shahriyar Jalayeri <shahriyar@zededa.com>
2024-07-08 17:26:13 +0300

    get swtpm to build in alpine 3.16
    Signed-off-by: Shahriyar Jalayeri <shahriyar@zededa.com>

--- pkg/vtpm/init.sh
+++ pkg/vtpm/init.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-cd jail || exit;
-#Too much stdout noise from tpm2_tools and vtpm_server,
-#so redirecting stdout to /dev/null. But stderr will be
-#picked up by logging infra as usual
-/usr/bin/vtpm_server > /dev/null
+sleep INF
