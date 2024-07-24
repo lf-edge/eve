9c39b4980f439554a58ec9496f722575b80a35d2
Renê de Souza Pinto <rene@renesp.com.br>
2024-07-24 17:50:30 +0200

    Introduces udev init container
    This commit introduces a new init container called udev that aims to run
    the udev daemon. The daemon is initialized before onboot and service
    containers.
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

--- tools/parse-pkgs.sh
+++ tools/parse-pkgs.sh
@@ -125,6 +125,7 @@ BSP_IMX_TAG=${BSP_IMX_TAG}
 APPARMOR_TAG=${APPARMOR_TAG}
 KUBE_TAG=${KUBE_TAG}
 RECOVERTPM_TAG=${RECOVERTPM_TAG}
+UDEV_TAG=${UDEV_TAG}
 EOF
 }
 
@@ -178,6 +179,7 @@ BSP_IMX_TAG=$(linuxkit_tag pkg/bsp-imx)
 APPARMOR_TAG=$(linuxkit_tag pkg/apparmor)
 KUBE_TAG=$(linuxkit_tag pkg/kube)
 RECOVERTPM_TAG=$(linuxkit_tag pkg/recovertpm)
+UDEV_TAG=$(linuxkit_tag pkg/udev)
 
 # Synthetic tags: the following tags are based on hashing
 # the contents of all the Dockerfile.in that we can find.
