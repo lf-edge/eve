2556a0186d8f0dc1236c9396072551d7826bf27e
Christoph Ostarek <christoph@zededa.com>
2024-08-06 14:10:29 +0200

    domainmgr: config.enable.usb for new usb type
    instead of IoUSB, now IoUSBController is used for the PCI controller
    
    this means that if the model is created with a newer spec.sh,
    config.enable.usb would not work
    
    Signed-off-by: Christoph Ostarek <christoph@zededa.com>
    (cherry picked from commit 43adcc24a0db9129d36c5b7fad7e7b97f7b4fe3e)

--- pkg/pillar/cmd/domainmgr/domainmgr_test.go
+++ pkg/pillar/cmd/domainmgr/domainmgr_test.go
@@ -17,11 +17,28 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/lf-edge/eve/pkg/pillar/agentlog"
 	"github.com/lf-edge/eve/pkg/pillar/base"
+	"github.com/lf-edge/eve/pkg/pillar/hypervisor"
+	"github.com/lf-edge/eve/pkg/pillar/types"
 	"github.com/lf-edge/eve/pkg/pillar/utils/cloudconfig"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
+
+func init() {
+	if logger == nil || log == nil {
+		logger, log = agentlog.Init(agentName)
+	}
+
+	if hyper == nil {
+		var err error
+		hyper, err = hypervisor.GetHypervisor("null")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
 func TestFetchEnvVariablesFromCloudInit(t *testing.T) {
 	log = base.NewSourceLogObject(logrus.StandardLogger(), "domainmgr", 0)
@@ -464,4 +481,67 @@ func TestHandleMimeMultipart(t *testing.T) {
 		}
 	}
 	os.RemoveAll(dir)
+}
+
+func TestConfigEnableUsbUpdatePortAndPciBackIoBundle(t *testing.T) {
+	assignableAdapters := types.AssignableAdapters{
+		IoBundleList: []types.IoBundle{
+			{
+				Phylabel:        "IoUSB",
+				Type:            types.IoUSB,
+				KeepInHost:      false,
+				AssignmentGroup: "1",
+				PciLong:         "00:01",
+			},
+			{
+				Phylabel:        "IoUSBController",
+				Type:            types.IoUSBController,
+				KeepInHost:      false,
+				AssignmentGroup: "1",
+				PciLong:         "00:01",
+			},
+			{
+				Phylabel:              "IoUSBDevice",
+				Type:                  types.IoUSBDevice,
+				KeepInHost:            false,
+				AssignmentGroup:       "2",
+				ParentAssignmentGroup: "3",
+			},
+		},
+	}
+	ctx := &domainContext{
+		assignableAdapters: &assignableAdapters,
+		usbAccess:          true,
+	}
+	ib := &types.IoBundle{
+		AssignmentGroup: "1",
+	}
+
+	updatePortAndPciBackIoBundle(ctx, ib)
+
+	for _, ib := range ctx.assignableAdapters.IoBundleList {
+		if (ib.Phylabel == "IoUSB" || ib.Phylabel == "IoUSBController") && !ib.KeepInHost {
+			t.Fatalf("IoBundle %+v should be kept in host", ib)
+		}
+		if ib.Phylabel == "IoUSBDevice" && ib.KeepInHost {
+			t.Fatalf("IoBundle %+v should not be kept in host", ib)
+		}
+	}
+
+	for i := range ctx.assignableAdapters.IoBundleList {
+		ctx.assignableAdapters.IoBundleList[i].KeepInHost = false
+	}
+	ib.KeepInHost = false
+
+	ctx.usbAccess = false
+	updatePortAndPciBackIoBundle(ctx, ib)
+
+	for _, ib := range ctx.assignableAdapters.IoBundleList {
+		if (ib.Phylabel == "IoUSB" || ib.Phylabel == "IoUSBController") && ib.KeepInHost {
+			t.Fatalf("IoBundle %+v should be not kept in host", ib)
+		}
+		if ib.Phylabel == "IoUSBDevice" && ib.KeepInHost {
+			t.Fatalf("IoBundle %+v should not be kept in host", ib)
+		}
+	}
 }
