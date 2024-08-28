be3ffcd9e5c3d06f190bd72b3d6aa3c389705852
Mikhail Malyshev <mike.malyshev@gmail.com>
2024-08-28 14:55:16 +0200

    Print messages on /dev/tty instead of /dev/console
    /dev/console ins the current console for kernel so even when we switch
    to /dev/tty3 messages still appear and corrupt monitor application. Fo
    now print them on /dev/tty which is the default kernel tty.
    
    TODO:
    1. diag output must appear on /dev/ttyS* if defined in the list of
       kernel consoles
    2. The same applies to other subsystems
    
    In order to do so the kernel command line must be parsed and the last
    console= must be detected. Unfortunately there is no easier way to do
    this
    
    Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

--- pkg/pillar/scripts/device-steps.sh
+++ pkg/pillar/scripts/device-steps.sh
@@ -319,7 +319,7 @@ for AGENT in $AGENTS; do
       # NOTE: it is safe to do either kill -STOP or an outright
       # kill -9 on the following cat process if you want to stop
       # receiving those messages on the console.
-      size="$(stty -F /dev/console size)"
+      size="$(stty -F /dev/tty1 size)"
       rows=$(echo "$size" | awk '{print $1}')
       columns=$(echo "$size" | awk '{print $2}')
       [ "$rows" != 0 ] || rows=""
@@ -327,7 +327,7 @@ for AGENT in $AGENTS; do
       [ "$columns" != 0 ] || columns=""
       [ -z "$columns" ] || columns="-c $columns"
       mkfifo /run/diag.pipe
-      (while true; do cat; done) < /run/diag.pipe >/dev/console 2>&1 &
+      (while true; do cat; done) < /run/diag.pipe >/dev/tty1 2>&1 &
       # shellcheck disable=SC2086
       $BINDIR/diag -f -o /run/diag.pipe -s /run/diag.out $rows $columns &
     else
@@ -441,7 +441,7 @@ if [ ! -s "$DEVICE_CERT_NAME" ]; then
     umount $CONFIGDIR_PERSIST
     # Did we fail to generate a certificate?
     if [ ! -s "$DEVICE_CERT_NAME" ]; then
-        echo "$(date -Ins -u) Failed to generate a device certificate. Done" | tee /dev/console
+        echo "$(date -Ins -u) Failed to generate a device certificate. Done" | tee /dev/tty
         exit 0
     fi
 else
