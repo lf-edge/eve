924aa349fe736715bffec814a02b44e43bf39963
Nikolay Martyanov <nikolay@zededa.com>
2024-08-30 16:21:19 +0200

    tools: Refactor SPDX check script for easier future modifications.
    Extracted file type check logic into a separate function
    `file_to_be_checked`.
    
    Extracted SPDX check logic into a separate function `check_spdx`. Make
    the check case insesitive.
    
    Simplified the main loop to use the new functions for better readability
    and maintainability.
    
    Signed-off-by: Nikolay Martyanov <nikolay@zededa.com>

--- tools/spdx-check.sh
+++ tools/spdx-check.sh
@@ -12,39 +12,61 @@ files=$(git diff --name-only --diff-filter=A "${BASE_COMMIT}"..HEAD | grep -v "v
 license_identifier="SPDX-License-Identifier: Apache-2.0"
 
 # Array of file extensions to check for SPDX header
-file_extensions=("sh" "c" "h" "go" "py" "rs" "yaml" "yml" "proto")
-file_names=("Dockerfile" "Dockerfile.in" "Makefile")
+file_extensions=(
+  "sh"
+  "c" "h"
+  "go"
+  "py"
+  "rs"
+  "yaml" "yml"
+  "proto"
+)
+
+# Array of file names to check for SPDX header
+file_names=(
+  "Dockerfile" "Dockerfile.in"
+  "Makefile"
+)
 
 # Flag to check if all files contain the SPDX header
 all_files_contain_spdx=true
 
-# Loop through the files and check for the SPDX header
-for file in $files; do
-  echo "Checking $file"
-  # Get the file extension
-  file_extension="${file##*.}"
+check_spdx() {
+  local file="$1"
+  # Check if the file contains the SPDX identifier
+  if ! grep -q -i "$license_identifier" "$file"; then
+    return 1
+  fi
+  return 0
+}
 
+file_to_be_checked() {
+  local file="$1"
   # Check if the file is a source file that should have a license header
   for ext in "${file_extensions[@]}"; do
-    if [[ "$file_extension" == "$ext" ]]; then
-      # Check if the file contains the SPDX identifier
-      if ! grep -q "$license_identifier" "$file"; then
-        all_files_contain_spdx=false
-        echo "Missing SPDX-License-Identifier in $file"
-      fi
-      break
+    if [[ "$file" == *.$ext ]]; then
+      return 0
     fi
   done
   for name in "${file_names[@]}"; do
-    if [[ "$file" == "$name" ]]; then
-      # Check if the file contains the SPDX identifier
-      if ! grep -q "$license_identifier" "$file"; then
-        all_files_contain_spdx=false
-        echo "Missing SPDX-License-Identifier in $file"
-      fi
-      break
+    if [[ "$1" == "$name" ]]; then
+      return 0
     fi
   done
+  return 1
+}
+
+# Loop through the files and check for the SPDX header
+for file in $files; do
+  if file_to_be_checked "$file"; then
+    echo -n "Checking $file ..."
+    if ! check_spdx "$file"; then
+      echo " missing SPDX-License-Identifier."
+      all_files_contain_spdx=false
+    else
+      echo " OK"
+    fi
+  fi
 done
 
 if [ "$all_files_contain_spdx" = true ]; then
