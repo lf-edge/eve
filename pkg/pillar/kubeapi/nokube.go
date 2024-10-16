ca2b21626eb14d0c5a9fad8b23fbeeac52e9c922
Andrew Durbin <andrewd@zededa.com>
2024-10-16 15:21:06 -0600

    Node Drain Request/Status API
    The base of an upcoming PR to enable deferring node
    reboot and shutdown operations.  In an Edge-Node-Cluster
    each node can hold a replica copy of a cluster volume.
    If the other nodes 1 and 2 in a cluster have recently recovered
    from a failure they could be rebuilding their replicas
    (1.x, 1.y, 2.x, 2.y) off of the only remaining readable copies
    (3.x, 3.y) on node 3.
    This API is to enable zedagent,nodeagent,baseosmgr to
    request a drain of replicas off of an edge-node (rebuild completion) before allowing
    a node outage.
    
    Signed-off-by: Andrew Durbin <andrewd@zededa.com>

--- pkg/pillar/kubeapi/nokube.go
+++ pkg/pillar/kubeapi/nokube.go
@@ -6,6 +6,7 @@
 package kubeapi
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/lf-edge/eve/pkg/pillar/base"
@@ -26,4 +27,16 @@ func CleanupStaleVMI() (int, error) {
 // GetPVCList in this file is just stub for non-kubevirt hypervisors.
 func GetPVCList(*base.LogObject) ([]string, error) {
 	panic("GetPVCList is not built")
+}
+
+// RequestNodeDrain is a stub for non-kubevirt builds
+func RequestNodeDrain(pubsub.Publication, DrainRequester, string) error {
+	// Nothing to do here, just noop
+	return fmt.Errorf("nokube requested drain, should not get here")
+}
+
+// GetNodeDrainStatus is a stub for non-kubevirt builds
+func GetNodeDrainStatus(pubsub.Subscription) *NodeDrainStatus {
+	// No need to query for inprogress operations, just a noop
+	return &NodeDrainStatus{Status: NOTSUPPORTED}
 }
