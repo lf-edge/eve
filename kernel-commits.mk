a1972fc2510da77522320ebe2760d425e9619b13
Dimitrios Poulios <dpoulios85@gmail.com>
2024-10-18 16:26:00 +0300

    kernel: Update kernel version for arm64
    This commit updates the kernel version for arm64 6.1.106 to enable
    support for OnLogic FR201 device.
    
    eve-kernel-arm64-v6.1.106-generic
        5639bec6ce45: Dockerfile.gcc: enable ccache
        2b779234bf68: arm64: dts: Create seperate device-tree for OnLogic FR201 device
        caa473bc60fa: configs: Enable raspberrypi-power driver
        772cb20d4c13: arm64: dts: Add peripheral nodes for OnLogic FR201 device
        d93268e9d0e1: configs: Enable SPI GPIO device driver
        c72bacf30581: configs: Enable RTC PCF85063 driver
        a523fa8f26cc: arm64: dts: Port device tree files for Raspberry Pi CM4
    
    Signed-off-by: Dimitrios Poulios <dpoulios85@gmail.com>

--- kernel-commits.mk
+++ kernel-commits.mk
@@ -8,6 +8,6 @@ KERNEL_COMMIT_arm64_v5.10.104_nvidia = 27012163c6a0
 KERNEL_COMMIT_arm64_v5.10.186_generic = 804663a82829
 KERNEL_COMMIT_arm64_v5.10.192_nvidia = bbf7ee8da79f
 KERNEL_COMMIT_arm64_v6.1.38_generic = 7ebde29217fc
-KERNEL_COMMIT_arm64_v6.1.106_generic = 22dd0af549fc
+KERNEL_COMMIT_arm64_v6.1.106_generic = 5639bec6ce45
 KERNEL_COMMIT_riscv64_v6.1.38_generic = 2dbca8568caf
 KERNEL_COMMIT_riscv64_v6.1.106_generic = f8f3bd1d26f6
