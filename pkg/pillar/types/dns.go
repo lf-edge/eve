4e177eadba73e9a7237fb384ed11cf5e744f241d
Christoph Ostarek <christoph@zededa.com>
2024-11-25 18:00:09 +0100

    pillar/nireconciler: resolve ntp servers
    continuously in the background so that updated
    IP addresses can be used if the edge application is
    restarted (from controller)
    
    Signed-off-by: Christoph Ostarek <christoph@zededa.com>

--- pkg/pillar/types/dns.go
+++ pkg/pillar/types/dns.go
@@ -562,8 +562,9 @@ func GetDNSServers(dns DeviceNetworkStatus, ifname string) []net.IP {
 }
 
 // GetNTPServers returns all, or the ones on one interface if ifname is set
-func GetNTPServers(dns DeviceNetworkStatus, ifname string) []string {
-	var servers []string
+func GetNTPServers(dns DeviceNetworkStatus, ifname string) ([]net.IP, []string) {
+	var serverDomainsOrIPs []string
+	var serverIPs []net.IP
 	for _, us := range dns.Ports {
 		if ifname != "" && ifname != us.IfName {
 			continue
@@ -581,12 +582,12 @@ func GetNTPServers(dns DeviceNetworkStatus, ifname string) []string {
 		}
 		// Add statically configured NTP server as well.
 		if us.ConfiguredNtpServers != nil {
-			servers = append(servers, us.ConfiguredNtpServers...)
+			serverDomainsOrIPs = append(serverDomainsOrIPs, us.ConfiguredNtpServers...)
 		}
 	}
 	// Avoid duplicates.
-	servers = generics.FilterDuplicates(servers)
-	return servers
+	serverDomainsOrIPs = generics.FilterDuplicates(serverDomainsOrIPs)
+	return serverIPs, serverDomainsOrIPs
 }
 
 // CountLocalIPv4AddrAnyNoLinkLocalIf is like CountLocalAddrAnyNoLinkLocalIf but
