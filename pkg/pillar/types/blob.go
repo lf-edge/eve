2c099976ec532a38f9d0226bb194e9dd21d2553f
Paul Gaiduk <paulg@zededa.com>
2025-01-07 17:56:21 +0100

    set MediaType in BlobStatus from VerifyImageStatus
    The MediaType field should not be empty because this can affect correct
    function of IsManifest() and IsIndex().
    
    This commit additionally logs MediaType in all pubsub structs to
    facilitate future debugging.
    
    Signed-off-by: Paul Gaiduk <paulg@zededa.com>
    (cherry picked from commit fe9af657fe019c6b5097881e81c75af804ed6b1c)

--- pkg/pillar/types/blob.go
+++ pkg/pillar/types/blob.go
@@ -87,7 +87,7 @@ func (status BlobStatus) LogCreate(logBase *base.LogObject) {
 	logObject.CloneAndAddField("state", status.State.String()).
 		AddField("datastoreid-uuids", uuids).
 		AddField("size-int64", status.Size).
-		AddField("blobtype-string", status.MediaType).
+		AddField("mediatype", status.MediaType).
 		AddField("refcount-int64", status.RefCount).
 		AddField("has-verifier-ref-bool", status.HasVerifierRef).
 		AddField("has-downloader-ref-bool", status.HasDownloaderRef).
@@ -108,6 +108,7 @@ func (status BlobStatus) LogModify(logBase *base.LogObject, old interface{}) {
 		oldStatus.Size != status.Size {
 
 		logObject.CloneAndAddField("state", status.State.String()).
+			AddField("mediatype", status.MediaType).
 			AddField("refcount-int64", status.RefCount).
 			AddField("size-int64", status.Size).
 			AddField("has-verifier-ref-bool", status.HasVerifierRef).
@@ -137,6 +138,7 @@ func (status BlobStatus) LogDelete(logBase *base.LogObject) {
 	logObject := base.EnsureLogObject(logBase, base.BlobStatusLogType, status.RelativeURL,
 		nilUUID, status.LogKey())
 	logObject.CloneAndAddField("state", status.State.String()).
+		AddField("mediatype", status.MediaType).
 		AddField("refcount-int64", status.RefCount).
 		AddField("size-int64", status.Size).
 		AddField("has-verifier-ref-bool", status.HasVerifierRef).
