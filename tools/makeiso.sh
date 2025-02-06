ee15f4d2d1438b605b58febb7c002d34c1f04072
Renê de Souza Pinto <rene@renesp.com.br>
2025-02-06 13:58:40 +0100

    tools/makeiso.sh: Run pkg/mkimage-iso-efi for target architecture
    The architecture must be taking into account for package mkimage-iso-efi
    because the boot.img can be generated directly from the make-efi script,
    which will copy some binary files from the container, so they must match
    the target architecture.
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

--- tools/makeiso.sh
+++ tools/makeiso.sh
@@ -2,6 +2,19 @@
 
 [ -n "$DEBUG" ] && set -x
 
+if [ -z "$DOCKER_ARCH_TAG" ] ; then
+  case $(uname -m) in
+    x86_64) ARCH=amd64
+      ;;
+    aarch64) ARCH=arm64
+      ;;
+    *) echo "Unsupported architecture $(uname -m). Exiting" && exit 1
+      ;;
+  esac
+else
+  ARCH="${DOCKER_ARCH_TAG}"
+fi
+
 EVE="$(cd "$(dirname "$0")" && pwd)/../"
 PATH="$EVE/build-tools/bin:$PATH"
 INSTALLER_TAR="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
@@ -15,4 +28,4 @@ fi
 
 : > "$ISO"
 # shellcheck disable=SC2086
-cat $INSTALLER_TAR | docker run -i --rm -e DEBUG="$DEBUG" -e VOLUME_LABEL=EVEISO -v "$ISO:/output.iso" "$MKIMAGE_TAG" $3
+cat $INSTALLER_TAR | docker run -i --platform "linux/${ARCH}" --rm -e DEBUG="$DEBUG" -e VOLUME_LABEL=EVEISO -v "$ISO:/output.iso" "$MKIMAGE_TAG" $3
