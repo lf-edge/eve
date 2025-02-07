80d57e3a307257271d4cae00fd97c5d17be00e0d
Pavel Abramov <uncle.decart@gmail.com>
2025-02-07 15:34:25 +0100

    Introduce nkv package
    This is step one of restructuring PubSub. This commit introduces
    Notify Key Value (nkv) to EVE ecosystem. Next step would be to
    use nkv as PubSub backend by introducing an nkv PubSub driver,
    third and last step would be to switch PubSub to using nkv driver.
    That way current systems won't change, but there will be opportunity
    to switch to native nkv with easier API
    
    Signed-off-by: Pavel Abramov <uncle.decart@gmail.com>

--- tools/parse-pkgs.sh
+++ tools/parse-pkgs.sh
@@ -155,6 +155,7 @@ RECOVERTPM_TAG=${RECOVERTPM_TAG}
 UDEV_TAG=${UDEV_TAG}
 INSTALLER_TAG=${INSTALLER_TAG}
 MONITOR_TAG=${MONITOR_TAG}
+NKV_TAG=${NKV_TAG}
 EOF
 }
 
@@ -204,6 +205,7 @@ RECOVERTPM_TAG=$(linuxkit_tag pkg/recovertpm)
 UDEV_TAG=$(linuxkit_tag pkg/udev)
 INSTALLER_TAG=$(linuxkit_tag pkg/installer)
 MONITOR_TAG=$(linuxkit_tag pkg/monitor)
+NKV_TAG=$(linuxkit_tag pkg/nkv)
 
 # Synthetic tags: the following tags are based on hashing
 # the contents of all the Dockerfile.in that we can find.
