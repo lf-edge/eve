ca2b49aac78003eb4d995d2568bf99ae8c92709d
Christoph Ostarek <christoph@zededa.com>
2025-02-25 11:25:31 +0100

    pillar/xen: use better indicator for usb controlle
    this one checks for IoUSBController and IoUSB and works
    better with deprecated models that still use the old type
    
    Signed-off-by: Christoph Ostarek <christoph@zededa.com>
    (cherry picked from commit 65676fdb329047719bcc29233257f2ab9c6acf04)

--- pkg/pillar/hypervisor/xen.go
+++ pkg/pillar/hypervisor/xen.go
@@ -397,7 +397,7 @@ func (ctx xenContext) CreateDomConfig(domainName string, config types.DomainConf
 			short := types.PCILongToShort(pa.ioBundle.PciLong)
 			// USB controller are subject to legacy USB support from
 			// some BIOS. Use relaxed to get past that.
-			if pa.ioBundle.Type == types.IoUSBController {
+			if pa.ioBundle.IsUSBController() {
 				cfg = cfg + fmt.Sprintf("'%s,rdm_policy=relaxed'",
 					short)
 			} else {
