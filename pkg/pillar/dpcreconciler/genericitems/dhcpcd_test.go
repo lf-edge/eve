lf-edge/eve

commit bcb650f9346c62b4e991c660eb514b7e81e1881c

pillar: enhance NTP server handling
* allow setting more NTP servers through controller
* allow setting NTP server by domain instead of just IP address
  (problem: we only resolve the domains once at the beginning)

Signed-off-by: Christoph Ostarek <christoph@zededa.com>

pkg/pillar/dpcreconciler/genericitems/dhcpcd.go
@@ -324,13 +324,14 @@ func (c *DhcpcdConfigurator) dhcpcdArgs(config types.DhcpConfig) (op string, arg
 				fmt.Sprintf("domain_name_servers=%s",
 					strings.Join(dnsServers, " ")))
 		}
-		if config.NTPServer != nil && !config.NTPServer.IsUnspecified() {
-			args = append(args, "--static",
-				fmt.Sprintf("ntp_servers=%s",
-					config.NTPServer.String()))
+		if config.NTPServers != nil {
+			for _, ntpServer := range config.NTPServers {
+				args = append(args, "--static", fmt.Sprintf("ntp_servers=%s", ntpServer))
+				args = append(args, extras...)
+			}
 		}
-		args = append(args, extras...)
 	}
+
 	return op, args
 }
 

pkg/pillar/dpcreconciler/genericitems/dhcpcd_test.go
@@ -25,7 +25,7 @@ func TestDhcpcdEqual(t *testing.T) {
 					AddrSubnet: "192.168.1.44/24",          // irrelevant
 					Gateway:    net.ParseIP("192.168.1.1"), // irrelevant
 					DomainName: "mydomain",                 // irrelevant
-					NTPServer:  net.ParseIP("192.168.1.1"), // irrelevant
+					NTPServers: []string{"192.168.1.1"},    // irrelevant
 					Type:       types.NetworkTypeIpv4Only,  // must match
 				},
 			},
@@ -93,7 +93,7 @@ func TestDhcpcdEqual(t *testing.T) {
 					Dhcp:       types.DhcpTypeStatic,
 					AddrSubnet: "192.168.1.44/24",
 					DomainName: "mydomain",
-					NTPServer:  net.ParseIP("192.168.1.1"),
+					NTPServers: []string{"192.168.1.1"},
 					DNSServers: []net.IP{net.ParseIP("8.8.8.8")},
 					Type:       types.NetworkTypeIpv4Only, // irrelevant
 				},
@@ -103,7 +103,7 @@ func TestDhcpcdEqual(t *testing.T) {
 					Dhcp:       types.DhcpTypeStatic,
 					AddrSubnet: "192.168.1.44/24",
 					DomainName: "mydomain",
-					NTPServer:  net.ParseIP("192.168.1.1"),
+					NTPServers: []string{"192.168.1.1"},
 					DNSServers: []net.IP{net.ParseIP("8.8.8.8")},
 					Type:       types.NetworkTypeIPv4, // irrelevant
 				},
@@ -117,7 +117,7 @@ func TestDhcpcdEqual(t *testing.T) {
 					Dhcp:       types.DhcpTypeStatic,
 					AddrSubnet: "192.168.1.44/24",
 					DomainName: "mydomain",
-					NTPServer:  net.ParseIP("192.168.1.1"),
+					NTPServers: []string{"192.168.1.1"},
 					DNSServers: []net.IP{net.ParseIP("8.8.8.8")}, // does not match
 				},
 			},
@@ -126,7 +126,7 @@ func TestDhcpcdEqual(t *testing.T) {
 					Dhcp:       types.DhcpTypeStatic,
 					AddrSubnet: "192.168.1.44/24",
 					DomainName: "mydomain",
-					NTPServer:  net.ParseIP("192.168.1.1"),
+					NTPServers: []string{"192.168.1.1"},
 					DNSServers: []net.IP{net.ParseIP("1.1.1.1")}, // does not match
 				},
 			},

pkg/pillar/cmd/diag/diag.go
@@ -961,8 +961,8 @@ func printOutput(ctx *diagContext, caller string) {
 			}
 			ctx.ph.Print("INFO: %s: Static Domain Name: %s\n",
 				ifname, port.DomainName)
-			ctx.ph.Print("INFO: %s: Static NTP server: %s\n",
-				ifname, port.NtpServer.String())
+			ctx.ph.Print("INFO: %s: Static NTP server: %+v\n",
+				ifname, port.ConfiguredNtpServers)
 		}
 		printProxy(ctx, port, ifname)
 		if port.HasError() {

pkg/pillar/cmd/zedagent/handlemetrics.go
@@ -1106,10 +1106,7 @@ func PublishAppInfoToZedCloud(ctx *zedagentContext, uuid string,
 			networkInfo.DevName = *proto.String(name)
 			niStatus := appIfnameToNetworkInstance(ctx, aiStatus, ifname)
 			if niStatus != nil {
-				for _, ntpServer := range niStatus.NTPServers {
-					networkInfo.NtpServers = append(networkInfo.NtpServers,
-						ntpServer.String())
-				}
+				networkInfo.NtpServers = append(networkInfo.NtpServers, niStatus.NTPServers...)
 				networkInfo.DefaultRouters = []string{niStatus.Gateway.String()}
 				networkInfo.Dns = &info.ZInfoDNS{
 					DNSservers: []string{},

pkg/pillar/cmd/zedagent/parseconfig.go
@@ -1380,7 +1380,7 @@ func parseOneSystemAdapterConfig(getconfigCtx *getconfigContext,
 			port.WirelessCfg = network.WirelessCfg
 			port.Gateway = network.Gateway
 			port.DomainName = network.DomainName
-			port.NTPServer = network.NTPServer
+			port.NTPServers = network.NTPServers
 			port.DNSServers = network.DNSServers
 			// Need to be careful since zedcloud can feed us bad Dhcp type
 			port.Dhcp = network.Dhcp
@@ -2260,11 +2260,10 @@ func parseIpspecNetworkXObject(ipspec *zconfig.Ipspec, config *types.NetworkXObj
 			return fmt.Errorf("invalid gateway IP (%s)", g)
 		}
 	}
-	if n := ipspec.GetNtp(); n != "" {
-		config.NTPServer = net.ParseIP(n)
-		if config.NTPServer == nil {
-			return fmt.Errorf("invalid NTP IP (%s)", n)
-		}
+
+	ntpServers := append([]string{ipspec.GetNtp()}, ipspec.GetMoreNtp()...)
+	if len(ntpServers) > 0 && ntpServers[0] != "" {
+		config.NTPServers = ntpServers
 	}
 	for _, dsStr := range ipspec.GetDns() {
 		ds := net.ParseIP(dsStr)
@@ -2293,11 +2292,11 @@ func parseIpspec(ipspec *zconfig.Ipspec,
 
 	config.DomainName = ipspec.GetDomain()
 	// Parse NTP Server
+	if config.NtpServers == nil {
+		config.NtpServers = make([]string, 0)
+	}
 	if n := ipspec.GetNtp(); n != "" {
-		config.NtpServer = net.ParseIP(n)
-		if config.NtpServer == nil {
-			return fmt.Errorf("invalid NTP IP (%s)", n)
-		}
+		config.NtpServers = append(config.NtpServers, n)
 	}
 	// Parse Dns Servers
 	for _, dsStr := range ipspec.GetDns() {

pkg/pillar/cmd/zedagent/parseconfig_test.go
@@ -147,7 +147,7 @@ func TestParsePhysicalNetworkAdapters(t *testing.T) {
 	g.Expect(port.DhcpConfig.AddrSubnet).To(BeEmpty())
 	g.Expect(port.DhcpConfig.DNSServers).To(BeEmpty())
 	g.Expect(port.DhcpConfig.Gateway).To(BeNil())
-	g.Expect(port.DhcpConfig.NTPServer).To(BeNil())
+	g.Expect(port.DhcpConfig.NTPServers).To(BeNil())
 	g.Expect(port.ProxyConfig.Proxies).To(BeEmpty())
 	g.Expect(port.L2LinkConfig.L2Type).To(Equal(types.L2LinkTypeNone))
 	g.Expect(port.WirelessCfg.WType).To(Equal(types.WirelessTypeNone))

pkg/pillar/cmd/zedagent/reportinfo.go
@@ -885,8 +885,10 @@ func encodeNetInfo(port types.NetworkPortStatus) *info.ZInfoNetwork {
 
 	networkInfo.Proxy = encodeProxyStatus(&port.ProxyConfig)
 	networkInfo.NtpServers = []string{}
-	for _, server := range port.NtpServers {
-		networkInfo.NtpServers = append(networkInfo.NtpServers, server.String())
+	if !port.IgnoreDhcpNtpServers {
+		for _, server := range port.DhcpNtpServers {
+			networkInfo.NtpServers = append(networkInfo.NtpServers, server.String())
+		}
 	}
 	return networkInfo
 }
@@ -1035,12 +1037,25 @@ func encodeNetworkPortStatus(ctx *zedagentContext,
 	devicePort.Up = port.Up
 	devicePort.Mtu = uint32(port.MTU)
 	devicePort.Domainname = port.DomainName
-	// TODO: modify EVE APIs and allow to publish full list of NTP servers
-	if port.NtpServer != nil {
-		devicePort.NtpServer = port.NtpServer.String()
-	} else if len(port.NtpServers) > 0 {
-		devicePort.NtpServer = port.NtpServers[0].String()
+	ntpServers := make([]string, 0)
+	if port.ConfiguredNtpServers != nil {
+		ntpServers = append(ntpServers, port.ConfiguredNtpServers...)
 	}
+	if len(port.DhcpNtpServers) > 0 && !port.IgnoreDhcpNtpServers {
+		ntpServers = append(ntpServers, port.DhcpNtpServers[0].String())
+		if len(port.DhcpNtpServers) > 1 {
+			for _, ntpServer := range port.DhcpNtpServers[1:] {
+				ntpServers = append(ntpServers, ntpServer.String())
+			}
+		}
+	}
+	if len(ntpServers) > 0 {
+		devicePort.NtpServer = ntpServers[0]
+	}
+	if len(ntpServers) > 1 {
+		devicePort.MoreNtpServers = ntpServers[1:]
+	}
+
 	devicePort.Proxy = encodeProxyStatus(&port.ProxyConfig)
 	devicePort.MacAddr = port.MacAddr.String()
 	for _, ipAddr := range port.AddrInfoList {
@@ -1143,7 +1158,13 @@ func encodeNetworkPortConfig(ctx *zedagentContext,
 	dp.DefaultRouters = make([]string, 0)
 	dp.DefaultRouters = append(dp.DefaultRouters, npc.Gateway.String())
 
-	dp.NtpServer = npc.NTPServer.String()
+	if npc.NTPServers != nil && len(npc.NTPServers) > 0 {
+		dp.NtpServer = npc.NTPServers[0]
+
+		if len(npc.NTPServers) > 1 {
+			dp.MoreNtpServers = append(dp.MoreNtpServers, npc.NTPServers[1:]...)
+		}
+	}
 
 	dp.Dns = new(info.ZInfoDNS)
 	dp.Dns.DNSdomain = npc.DomainName

pkg/pillar/cmd/zedrouter/networkinstance.go
@@ -124,12 +124,12 @@ func (z *zedrouter) updateNIPorts(niConfig types.NetworkInstanceConfig,
 	var (
 		newPorts         []*types.NetworkPortStatus
 		validatedPortLLs []string
-		newNTPServers    []net.IP
+		newNTPServers    []string
 		errorMsgs        []string
 	)
-	if niStatus.NtpServer != nil {
+	if niStatus.NtpServers != nil {
 		// The NTP server explicitly configured for the NI.
-		newNTPServers = append(newNTPServers, niStatus.NtpServer)
+		newNTPServers = append(newNTPServers, niStatus.NtpServers...)
 	}
 	if niStatus.PortLabel != "" {
 		newPorts = z.deviceNetworkStatus.LookupPortsByLabel(niStatus.PortLabel)
@@ -205,23 +205,26 @@ func (z *zedrouter) updateNIPorts(niConfig types.NetworkInstanceConfig,
 		}
 		// Port is valid for this network instance.
 		validatedPortLLs = append(validatedPortLLs, port.Logicallabel)
-		if port.NtpServer != nil {
+		if port.ConfiguredNtpServers != nil {
 			// The NTP server explicitly configured for the port.
-			newNTPServers = append(newNTPServers, port.NtpServer)
+			newNTPServers = append(newNTPServers, port.ConfiguredNtpServers...)
 		}
 		// NTP servers received via DHCP.
-		newNTPServers = append(newNTPServers, port.NtpServers...)
+		if !port.IgnoreDhcpNtpServers {
+			for _, dhcpNtpserver := range port.DhcpNtpServers {
+				newNTPServers = append(newNTPServers, dhcpNtpserver.String())
+			}
+		}
 	}
 	if niStatus.PortLabel != "" && len(newPorts) == 0 {
 		// This is potentially a transient state, wait for DNS update.
 		errorMsgs = append(errorMsgs,
 			fmt.Sprintf("no port is matching label '%s'", niStatus.PortLabel))
 	}
-	newNTPServers = generics.FilterDuplicatesFn(newNTPServers, netutils.EqualIPs)
+	newNTPServers = generics.FilterDuplicates(newNTPServers)
 	changed = changed || !generics.EqualSets(niStatus.Ports, validatedPortLLs)
 	niStatus.Ports = validatedPortLLs
-	changed = changed || !generics.EqualSetsFn(niStatus.NTPServers, newNTPServers,
-		netutils.EqualIPs)
+	changed = changed || !generics.EqualSets(niStatus.NTPServers, newNTPServers)
 	niStatus.NTPServers = newNTPServers
 	// Update BridgeMac for Switch NI bridge created by NIM.
 	if z.niBridgeIsCreatedByNIM(niConfig) {

commit e13ed56a6c6c6c16aec87525bccffbf933a05ba0

Fix dhcpcd arguments for static IP config
Commit bcb650f93 (pillar: enhance NTP server handling) introduced
a bug in DhcpcdConfigurator (EVE version 14.0.0), where certain
arguments for dhcpcd may be unintentionally omitted in static IP
configurations.

Specifically, the configuration file option (-f), background
execution (-b), and infinite timeout (-t 0) may be missing.
As a result, if dhcpcd starts while the network cable is unplugged,
it will time out after 30 seconds and exit. Consequently, even
after the cable is reconnected, dhcpcd will no longer be running
to apply the static IP.

Signed-off-by: Milan Lenco <milan@zededa.com>

pkg/pillar/dpcreconciler/genericitems/dhcpcd.go
@@ -59,8 +59,8 @@ func (c Dhcpcd) Equal(other depgraph.Item) bool {
 	// Consider two DHCP configs as equal if they result in the same set of arguments for dhcpcd.
 	// This avoids unnecessary restarts of dhcpcd (when e.g. going from override to zedagent DPC).
 	configurator := &DhcpcdConfigurator{}
-	op1, args1 := configurator.dhcpcdArgs(c.DhcpConfig)
-	op2, args2 := configurator.dhcpcdArgs(c2.DhcpConfig)
+	op1, args1 := configurator.DhcpcdArgs(c.DhcpConfig)
+	op2, args2 := configurator.DhcpcdArgs(c2.DhcpConfig)
 	if op1 != op2 || len(args1) != len(args2) {
 		return false
 	}
@@ -151,7 +151,7 @@ func (c *DhcpcdConfigurator) Create(ctx context.Context, item depgraph.Item) err
 		}
 
 		// Prepare input arguments for dhcpcd.
-		op, args := c.dhcpcdArgs(config)
+		op, args := c.DhcpcdArgs(config)
 
 		// Start DHCP client.
 		if c.dhcpcdExists(client.AdapterIfName) {
@@ -275,7 +275,9 @@ func (c *DhcpcdConfigurator) NeedsRecreate(oldItem, newItem depgraph.Item) (recr
 	return true
 }
 
-func (c *DhcpcdConfigurator) dhcpcdArgs(config types.DhcpConfig) (op string, args []string) {
+// DhcpcdArgs returns command line arguments for dhcpcd corresponding to the given
+// DHCP config. The method is exported only for the purpose of unit testing.
+func (c *DhcpcdConfigurator) DhcpcdArgs(config types.DhcpConfig) (op string, args []string) {
 	switch config.Dhcp {
 	case types.DhcpTypeClient:
 		op = "--request"
@@ -327,9 +329,9 @@ func (c *DhcpcdConfigurator) dhcpcdArgs(config types.DhcpConfig) (op string, arg
 		if config.NTPServers != nil {
 			for _, ntpServer := range config.NTPServers {
 				args = append(args, "--static", fmt.Sprintf("ntp_servers=%s", ntpServer))
-				args = append(args, extras...)
 			}
 		}
+		args = append(args, extras...)
 	}
 
 	return op, args

pkg/pillar/dpcreconciler/genericitems/dhcpcd_test.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2022-2025 Zededa, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 package genericitems_test
 
 import (
@@ -6,6 +9,7 @@ import (
 
 	configitems "github.com/lf-edge/eve/pkg/pillar/dpcreconciler/genericitems"
 	"github.com/lf-edge/eve/pkg/pillar/types"
+	"github.com/lf-edge/eve/pkg/pillar/utils/generics"
 )
 
 func TestDhcpcdEqual(t *testing.T) {
@@ -137,6 +141,96 @@ func TestDhcpcdEqual(t *testing.T) {
 		if test.item1.Equal(test.item2) != test.expEqual {
 			t.Errorf("TEST CASE \"%s\" FAILED - Equal() returned: %t, expected: %t",
 				test.name, test.item1.Equal(test.item2), test.expEqual)
+		}
+	}
+}
+
+func TestDhcpcdArgs(t *testing.T) {
+	t.Parallel()
+	type test struct {
+		name    string
+		config  types.DhcpConfig
+		expOp   string
+		expArgs []string
+	}
+	var tests = []test{
+		{
+			name: "DHCP client for IPv4 only",
+			config: types.DhcpConfig{
+				Dhcp: types.DhcpTypeClient,
+				Type: types.NetworkTypeIpv4Only,
+			},
+			expOp:   "--request",
+			expArgs: []string{"-f", "/dhcpcd.conf", "--noipv4ll", "--ipv4only", "-b", "-t", "0"},
+		},
+		{
+			name: "DHCP client for IPv4 only with zero gateway",
+			config: types.DhcpConfig{
+				Dhcp:    types.DhcpTypeClient,
+				Type:    types.NetworkTypeIpv4Only,
+				Gateway: net.IP{0, 0, 0, 0},
+			},
+			expOp:   "--request",
+			expArgs: []string{"-f", "/dhcpcd.conf", "--noipv4ll", "--ipv4only", "-b", "-t", "0", "--nogateway"},
+		},
+		{
+			name: "DHCP client for IPv6 only",
+			config: types.DhcpConfig{
+				Dhcp: types.DhcpTypeClient,
+				Type: types.NetworkTypeIpv6Only,
+			},
+			expOp:   "--request",
+			expArgs: []string{"-f", "/dhcpcd.conf", "--ipv6only", "-b", "-t", "0"},
+		},
+		{
+			name: "DHCP client for dual stack",
+			config: types.DhcpConfig{
+				Dhcp: types.DhcpTypeClient,
+				Type: types.NetworkTypeDualStack,
+			},
+			expOp:   "--request",
+			expArgs: []string{"-f", "/dhcpcd.conf", "--noipv4ll", "-b", "-t", "0"},
+		},
+		{
+			name: "Static IPv4 config",
+			config: types.DhcpConfig{
+				Dhcp:       types.DhcpTypeStatic,
+				AddrSubnet: "192.168.1.44/24",
+				Gateway:    net.IP{192, 168, 1, 1},
+				DomainName: "mydomain",
+				NTPServers: []string{"192.168.1.1", "10.10.12.13"},
+				DNSServers: []net.IP{net.ParseIP("8.8.8.8")},
+				Type:       types.NetworkTypeIpv4Only, // irrelevant
+			},
+			expOp: "--static",
+			expArgs: []string{"ip_address=192.168.1.44/24", "--static", "routers=192.168.1.1",
+				"--static", "domain_name=mydomain", "--static", "domain_name_servers=8.8.8.8",
+				"--static", "ntp_servers=192.168.1.1", "--static", "ntp_servers=10.10.12.13",
+				"-f", "/dhcpcd.conf", "-b", "-t", "0"},
+		},
+		{
+			name: "Static IPv4 config with unspecified gateway",
+			config: types.DhcpConfig{
+				Dhcp:       types.DhcpTypeStatic,
+				AddrSubnet: "192.168.1.44/24",
+				DomainName: "mydomain",
+				NTPServers: []string{"192.168.1.1", "10.10.12.13"},
+				DNSServers: []net.IP{net.ParseIP("8.8.8.8")},
+				Type:       types.NetworkTypeIpv4Only, // irrelevant
+			},
+			expOp: "--static",
+			expArgs: []string{"ip_address=192.168.1.44/24",
+				"--static", "domain_name=mydomain", "--static", "domain_name_servers=8.8.8.8",
+				"--static", "ntp_servers=192.168.1.1", "--static", "ntp_servers=10.10.12.13",
+				"-f", "/dhcpcd.conf", "-b", "-t", "0", "--nogateway"},
+		},
+	}
+	configurator := configitems.DhcpcdConfigurator{}
+	for _, test := range tests {
+		op, args := configurator.DhcpcdArgs(test.config)
+		if op != test.expOp || !generics.EqualLists(args, test.expArgs) {
+			t.Errorf("TEST CASE \"%s\" FAILED - DhcpcdArgs() returned: %s %v, "+
+				"expected: %s %v", test.name, op, args, test.expOp, test.expArgs)
 		}
 	}
 }