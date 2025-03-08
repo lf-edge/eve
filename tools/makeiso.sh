ad5856dd5d5e377c4ade88d5d6fc0d331d24e87a
Avi Deitcher <avi@deitcher.net>
2025-03-08 20:49:38 +0200

    Use new cross-build capabilities of mkimage-iso-efi
    mkimage-iso-efi recently gained the capability to build for another architecture
    other than the one it is running on. This takes advantage of it.
    
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- tools/makeiso.sh
+++ tools/makeiso.sh
@@ -18,7 +18,7 @@ fi
 EVE="$(cd "$(dirname "$0")" && pwd)/../"
 PATH="$EVE/build-tools/bin:$PATH"
 INSTALLER_TAR="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
-MKIMAGE_TAG="$(linuxkit pkg show-tag "$EVE/pkg/mkimage-iso-efi")-${ARCH}"
+MKIMAGE_TAG="$(linuxkit pkg show-tag "$EVE/pkg/mkimage-iso-efi")"
 ISO="$(cd "$(dirname "$2")" && pwd)/$(basename "$2")"
 
 if [ ! -f "$INSTALLER_TAR" ] || [ $# -lt 2 ]; then
@@ -28,4 +28,4 @@ fi
 
 : > "$ISO"
 # shellcheck disable=SC2086
-cat $INSTALLER_TAR | docker run -i --platform "linux/${ARCH}" --rm -e DEBUG="$DEBUG" -e VOLUME_LABEL=EVEISO -v "$ISO:/output.iso" "$MKIMAGE_TAG" $3
+cat $INSTALLER_TAR | docker run -i --rm -e DEBUG="$DEBUG" -e VOLUME_LABEL=EVEISO -e TARGETARCH=${ARCH} -v "$ISO:/output.iso" "$MKIMAGE_TAG" $3
