3460e8302962cf658871f177c22720f0d3bc86b4
Renê de Souza Pinto <rene@renesp.com.br>
2025-03-10 10:53:16 +0100

    docs: Fix security warnings pointed by dependabot
    - Bump up jinja2 version
    - Improve regex for URLs
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

--- docs/mkdocs/prepare_mkdocs.py
+++ docs/mkdocs/prepare_mkdocs.py
@@ -18,7 +18,8 @@ else:
 
 # Regex
 reLinks  = re.compile(r'\[([^\]]+)\]\(([^)]+)\)')
-reURL    = re.compile(r'[A-Za-z0-9]+://[A-Za-z0-9%-_]+(/[A-Za-z0-9%-_])*(#|\\?)[A-Za-z0-9%-_&=]*')
+# pylint: disable-next=line-too-long
+reURL    = re.compile(r'^(https?|ftp):\/\/([A-Za-z0-9-]+\.)+[A-Za-z]{2,6}(:\d+)?(\/[A-Za-z0-9-._~:?#[\]@!$&\'()*+,;=\/]*)?$')
 reMAIL   = re.compile(r'mailto:.*')
 reANCHOR = re.compile(r'^#.*')
 
