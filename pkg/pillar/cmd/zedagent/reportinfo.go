b6037983e290b4f228a3a0ec5f6f44b5ad5faaef
Milan Lenco <milan@zededa.com>
2025-04-04 10:54:26 +0200

    Implement user-configurable LTE Attach configuration
    Users can now configure LTE attach parameters and IP type directly,
    eliminating the need for speculative methods previously used to
    "guess" this configuration. This should resolve many issues related
    to modem registration failures we have been seeing.
    
    Signed-off-by: Milan Lenco <milan@zededa.com>

--- pkg/pillar/cmd/zedagent/reportinfo.go
+++ pkg/pillar/cmd/zedagent/reportinfo.go
@@ -650,7 +650,7 @@ func PublishDeviceInfoToZedCloud(ctx *zedagentContext, dest destinationBitset) {
 	// device returns a runtime error. Similarly, we only support enforced application network
 	// interface order for the KVM hypervisor. If enabled for application deployed under Xen
 	// or Kubevirt hypervisor, EVE returns error and the application will not be started.
-	ReportDeviceInfo.ApiCapability = info.APICapability_API_CAPABILITY_SINGLE_STACK_IP_NETWORK
+	ReportDeviceInfo.ApiCapability = info.APICapability_API_CAPABILITY_CELLULAR_ATTACH_CONFIG
 
 	// Report if there is a local override of profile
 	if ctx.getconfigCtx.sideController.currentProfile !=
