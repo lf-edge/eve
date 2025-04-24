23c54eb1ee930d0bf0e592071b3ea87fe8b07e67
Nikolay Martyanov <nikolay@zededa.com>
2025-04-24 11:01:23 +0200

    node_exporter: Add the service for system metrics collection.
    Introduce node_exporter as a new system service to enable metrics
    collection via Prometheus. The service is added to the rootfs and cgroup
    configuration to ensure it starts with other system services and is
    properly resource-managed.
    
    A new Dockerfile defines the node_exporter container based on the
    official image with necessary command-line options. The corresponding
    build configuration sets up required filesystem bindings and
    capabilities for accessing host metrics. Integration also includes
    handling the new image tag in the packaging script.
    
    Signed-off-by: Nikolay Martyanov <nikolay@zededa.com>

--- tools/parse-pkgs.sh
+++ tools/parse-pkgs.sh
@@ -93,6 +93,7 @@ ACRN_KERNEL_TAG=${ACRN_KERNEL_TAG}
 KERNEL_TAG=${KERNEL_TAG}
 FW_TAG=${FW_TAG}
 XENTOOLS_TAG=${XENTOOLS_TAG}
+NODE_EXPORTER_TAG=${NODE_EXPORTER_TAG}
 DOM0ZTOOLS_TAG=${DOM0ZTOOLS_TAG}
 RNGD_TAG=${RNGD_TAG}
 XEN_TAG=${XEN_TAG}
@@ -140,6 +141,7 @@ EOF
 
 ACRN_KERNEL_TAG=$(linuxkit_tag pkg/acrn-kernel)
 FW_TAG=$(linuxkit_tag pkg/fw)
+NODE_EXPORTER_TAG=$(linuxkit_tag pkg/node-exporter)
 XENTOOLS_TAG=$(linuxkit_tag pkg/xen-tools)
 XEN_TAG=$(linuxkit_tag pkg/xen)
 ACRN_TAG=$(linuxkit_tag pkg/acrn)
