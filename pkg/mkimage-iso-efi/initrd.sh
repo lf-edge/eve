60ea5f1c2fcf01072a9f391b6b85134a34257337
Avi Deitcher <avi@deitcher.net>
2025-05-27 19:40:06 +0300

    put config.img into installer root and avoid resquashfs
    we already were putting config.img into rootfs_installer.img but unsquashfs that, add it, and then mksquashfs again.
    That is very expensive, and slows the process down.
    This places it in the ISO root instead, where grub.cfg can read it, and removes unsquashfs/mksquashfs when option passed.
    
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- pkg/mkimage-iso-efi/initrd.sh
+++ pkg/mkimage-iso-efi/initrd.sh
@@ -8,9 +8,9 @@ mount -t proc none /proc
 mount -t sysfs none /sys
 mount -t devtmpfs none /dev
 
-# debug enabled?
+# debug enabled? Could be via `debug` or `eve_install_debug=<something>` parameter in cmdline
 # shellcheck disable=SC2002
-debug_param=$(cat /proc/cmdline | tr ' ' '\n' | grep '^debug$' | head -n 1)
+debug_param=$(tr ' ' '\n' < /proc/cmdline | grep -E '^debug$|^eve_install_debug(=|$)' | head -n 1)
 if [ -n "$debug_param" ]; then
     set -x
 fi
@@ -27,6 +27,11 @@ root_value=${root_param#root=}
 rootimg_param=$(cat /proc/cmdline | tr ' ' '\n' | grep '^rootimg=' | head -n 1)
 # remove the leading "root="  to get the actual value
 root_img=${rootimg_param#rootimg=}
+
+# Search for the rootaddmount= cmdline property
+# shellcheck disable=SC2002
+rootaddmount_param=$(cat /proc/cmdline | tr ' ' '\n' | grep '^rootaddmount=')
+# remove the leading "rootaddmount="  to get the actual value
 
 # Check if root_value is set
 if [ -z "$root_value" ]; then
@@ -94,6 +99,28 @@ if [ -n "$rootdev" ]; then
             # Mount the image and call switch_root
             mkdir -p /installer_root
             mount "$rootfsimg" /installer_root
+            # check if the rootaddmount parameter is set and add those mounts
+            if [ -n "$rootaddmount_param" ]; then
+                # remove the leading "rootaddmount=" to get the actual value
+                for mountpair in $rootaddmount_param; do
+                    mount=${mountpair#rootaddmount=}
+                    if [ -z "$mount" ]; then
+                        continue
+                    fi
+                    mount_source=$(echo "$mount" | cut -d':' -f1)
+                    mount_target=$(echo "$mount" | cut -d':' -f2)
+                    # make sure the mount target exists, after stripping leading slashes
+                    mount_target="${mount_target#/}"
+                    targetpath="/installer_root/$mount_target"
+                    mount_source="${mount_source#/}"
+                    sourcepath="/newroot/$mount_source"
+                    if [ ! -e "$sourcepath" ]; then
+                        echo "Source path $mount_source does not exist, skipping mount"
+                        continue
+                    fi
+                    mount --bind "${sourcepath}" "${targetpath}"
+                done
+            fi
             exec switch_root /installer_root /sbin/init
         else
             echo "$root_img image not found!"
