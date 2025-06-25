98927a6a2270b638050a5d4e349fd7e8be4231d7
Paul Gaiduk <paulg@zededa.com>
2025-06-25 15:44:32 +0200

    add config to vector through global options
    This change handles the new vector config uploaded by user in base64
    format. Newlogd will decode the config and put it on persist, where
    vector can validate it and apply if valid.
    
    Signed-off-by: Paul Gaiduk <paulg@zededa.com>

--- pkg/newlog/cmd/vector.go
+++ pkg/newlog/cmd/vector.go
@@ -5,12 +5,16 @@ package main
 
 import (
 	"bufio"
+	"encoding/base64"
 	"fmt"
 	"net"
 	"os"
+	"path/filepath"
 	"slices"
 	"strings"
 	"time"
+
+	fileutils "github.com/lf-edge/eve/pkg/pillar/utils/file"
 )
 
 var (
@@ -18,6 +22,9 @@ var (
 	keepSockVectorSource   = "/run/devKeep_source.sock"
 	uploadSockVectorSink   = "/run/devUpload_sink.sock"
 	keepSockVectorSink     = "/run/devKeep_sink.sock"
+
+	defaultConfigPath   = "/persist/vector/config/vector.yaml.default"
+	candidateConfigPath = "/persist/vector/config/vector.yaml.new"
 )
 
 // BufferedSockWriter is a writer that buffers messages and writes them to a unix socket.
@@ -123,4 +130,46 @@ func handleIncomingConnection(conn net.Conn, sendToChan chan<- string) {
 	if err := scanner.Err(); err != nil {
 		log.Errorf("handleIncomingConnection: scanner error for socket: %v", err)
 	}
+}
+
+func writeVectorConfig(text []byte) error {
+	// Create the directory if it doesn't exist
+	if err := os.MkdirAll(filepath.Dir(candidateConfigPath), 0755); err != nil {
+		return fmt.Errorf("failed to create config directory: %w", err)
+	}
+
+	if err := fileutils.WriteRename(candidateConfigPath, text); err != nil {
+		return fmt.Errorf("failed to write config file: %w", err)
+	}
+
+	log.Noticef("Vector configuration written successfully")
+	return nil
+}
+
+func handleVectorConfig(config string) error {
+	if config == "" {
+		log.Noticef("No vector config provided, setting up default configuration")
+		// Copy the default vector config from default location
+		defaultConfig, err := os.ReadFile(defaultConfigPath)
+		if err != nil {
+			return fmt.Errorf("failed to read default vector config: %w", err)
+		}
+		if err := writeVectorConfig(defaultConfig); err != nil {
+			return fmt.Errorf("failed to write default vector config: %w", err)
+		}
+		log.Functionf("wrote default vector config to %s", candidateConfigPath)
+	} else {
+		// vector.config parameter is in base64 encoded format
+		decodedConfig, err := base64.StdEncoding.DecodeString(config)
+		if err != nil {
+			return fmt.Errorf("failed to decode vector config: %w", err)
+		}
+		// write the decoded config to vector config file
+		if err := writeVectorConfig(decodedConfig); err != nil {
+			return fmt.Errorf("failed to write vector config: %w", err)
+		}
+		log.Functionf("wrote vector config to %s", candidateConfigPath)
+	}
+
+	return nil
 }
