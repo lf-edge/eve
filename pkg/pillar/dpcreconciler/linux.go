3bc2ba38984992fa5116ebc3db70dc6bd377003c
Milan Lenco <milan@zededa.com>
2025-07-11 11:41:38 +0200

    Add IPv6 rule for the local table
    Just like we have IPv4 rule for the local table, with priority below
    the NI rules, we should replicate the same for IPv6.
    
    Signed-off-by: Milan Lenco <milan@zededa.com>

--- pkg/pillar/dpcreconciler/linux.go
+++ pkg/pillar/dpcreconciler/linux.go
@@ -894,11 +894,17 @@ func (r *LinuxDpcReconciler) getIntendedGlobalCfg(dpc types.DevicePortConfig,
 		Priority: types.PbrLocalDestPrio,
 		Table:    unix.RT_TABLE_LOCAL,
 	}, nil)
+	intendedCfg.PutItem(linux.IPRule{
+		Priority: types.PbrLocalDestPrio,
+		Table:    unix.RT_TABLE_LOCAL,
+		IPv6:     true,
+	}, nil)
 	if r.HVTypeKube {
 		intendedCfg.PutItem(linux.IPRule{
 			Dst:      kubePodCIDR,
 			Priority: types.PbrKubeNetworkPrio,
 			Table:    unix.RT_TABLE_MAIN,
+			IPv6:     false,
 		}, nil)
 		tableForKubeSvc := unix.RT_TABLE_MAIN
 		if clusterStatus.ClusterInterface != "" {
@@ -908,6 +914,7 @@ func (r *LinuxDpcReconciler) getIntendedGlobalCfg(dpc types.DevicePortConfig,
 			Dst:      kubeSvcCIDR,
 			Priority: types.PbrKubeNetworkPrio,
 			Table:    tableForKubeSvc,
+			IPv6:     false,
 		}, nil)
 	}
 	if len(dpc.Ports) == 0 {
@@ -1203,6 +1210,7 @@ func (r *LinuxDpcReconciler) getIntendedSrcIPRules(dpc types.DevicePortConfig) d
 				Src:      netutils.HostSubnet(ipAddr.IP),
 				Priority: types.PbrLocalOrigPrio,
 				Table:    types.DPCBaseRTIndex + ifIndex,
+				IPv6:     ipAddr.IP.To4() == nil,
 			}, nil)
 		}
 	}
