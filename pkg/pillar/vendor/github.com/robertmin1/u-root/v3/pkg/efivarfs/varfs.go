88fa10cd747ee584c9ed16b0ce3228d086194ebb
Mikhail Malyshev <mike.malyshev@gmail.com>
2025-09-10 16:42:54 +0000

    pillar: vendor github.com/robertmin1/u-root/v3/pkg/efivarfs
    This library is needed for EFI variable access from pillar
    
    Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

---
+++ pkg/pillar/vendor/github.com/robertmin1/u-root/v3/pkg/efivarfs/varfs.go
@@ -0,0 +1,247 @@
+// Copyright 2022 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package efivarfs
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
+
+	guid "github.com/google/uuid"
+	"golang.org/x/sys/unix"
+)
+
+// DefaultVarFS is the path to the efivarfs mount point
+const DefaultVarFS = "/sys/firmware/efi/efivars/"
+
+var (
+	// ErrVarsUnavailable is caused by not having a valid backend
+	ErrVarsUnavailable = fmt.Errorf("no variable backend is available:%w", os.ErrNotExist)
+
+	// ErrVarNotExist is caused by accessing a non-existing variable
+	ErrVarNotExist = os.ErrNotExist
+
+	// ErrVarPermission is caused by not haven the right permissions either
+	// because of not being root or xattrs not allowing changes
+	ErrVarPermission = os.ErrPermission
+
+	// ErrNoFS is returned when the file system is not available for some
+	// reason.
+	ErrNoFS = errors.New("varfs not available")
+)
+
+// EFIVar is the interface for EFI variables. Note that it need not use a file system,
+// but typically does.
+type EFIVar interface {
+	Get(desc VariableDescriptor) (VariableAttributes, []byte, error)
+	List() ([]VariableDescriptor, error)
+	Remove(desc VariableDescriptor) error
+	Set(desc VariableDescriptor, attrs VariableAttributes, data []byte) error
+}
+
+// EFIVarFS implements EFIVar
+type EFIVarFS struct {
+	path string
+}
+
+var _ EFIVar = &EFIVarFS{}
+
+// NewPath returns an EFIVarFS given a path.
+func NewPath(p string) (*EFIVarFS, error) {
+	e := &EFIVarFS{path: p}
+	if err := e.probe(); err != nil {
+		return nil, err
+	}
+	return e, nil
+}
+
+// New returns an EFIVarFS using the default path.
+func New() (*EFIVarFS, error) {
+	return NewPath(DefaultVarFS)
+}
+
+// probe will probe for the EFIVarFS filesystem
+// magic value on the expected mountpoint inside the sysfs.
+// If the correct magic value was found it will return
+// the a pointer to an EFIVarFS struct on which regular
+// operations can be done. Otherwise it will return an
+// error of type ErrFsNotMounted.
+func (v *EFIVarFS) probe() error {
+	var stat unix.Statfs_t
+	if err := unix.Statfs(v.path, &stat); err != nil {
+		return fmt.Errorf("%w: not mounted", ErrNoFS)
+	}
+	if uint(stat.Type) != uint(unix.EFIVARFS_MAGIC) {
+		return fmt.Errorf("%w: wrong magic", ErrNoFS)
+	}
+	return nil
+}
+
+// Get reads the contents of an efivar if it exists and has the necessary permission
+func (v *EFIVarFS) Get(desc VariableDescriptor) (VariableAttributes, []byte, error) {
+	path := filepath.Join(v.path, fmt.Sprintf("%s-%s", desc.Name, desc.GUID.String()))
+	f, err := os.OpenFile(path, os.O_RDONLY, 0)
+	switch {
+	case os.IsNotExist(err):
+		return 0, nil, ErrVarNotExist
+	case os.IsPermission(err):
+		return 0, nil, ErrVarPermission
+	case err != nil:
+		return 0, nil, err
+	}
+	defer f.Close()
+
+	var attrs VariableAttributes
+	if err := binary.Read(f, binary.LittleEndian, &attrs); err != nil {
+		if err == io.EOF {
+			return 0, nil, ErrVarNotExist
+		}
+		return 0, nil, err
+	}
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return 0, nil, err
+	}
+	return attrs, data, nil
+}
+
+// Set modifies a given efivar with the provided contents
+func (v *EFIVarFS) Set(desc VariableDescriptor, attrs VariableAttributes, data []byte) error {
+	path := filepath.Join(v.path, fmt.Sprintf("%s-%s", desc.Name, desc.GUID.String()))
+	flags := os.O_WRONLY | os.O_CREATE
+	if attrs&AttributeAppendWrite != 0 {
+		flags |= os.O_APPEND
+	}
+
+	read, err := os.OpenFile(path, os.O_RDONLY, 0)
+	switch {
+	case os.IsNotExist(err):
+	case os.IsPermission(err):
+		return ErrVarPermission
+	case err != nil:
+		return err
+	default:
+		defer read.Close()
+
+		restoreImmutable, err := makeMutable(read)
+		switch {
+		case os.IsPermission(err):
+			return ErrVarPermission
+		case err != nil:
+			return err
+		}
+		defer restoreImmutable()
+	}
+
+	write, err := os.OpenFile(path, flags, 0644)
+	switch {
+	case os.IsNotExist(err):
+		return ErrVarNotExist
+	case os.IsPermission(err):
+		return ErrVarPermission
+	case err != nil:
+		return err
+	}
+	defer write.Close()
+
+	var buf bytes.Buffer
+	if err := binary.Write(&buf, binary.LittleEndian, attrs); err != nil {
+		return err
+	}
+	if _, err := buf.Write(data); err != nil {
+		return err
+	}
+	if _, err := buf.WriteTo(write); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Remove makes the specified EFI var mutable and then deletes it
+func (v *EFIVarFS) Remove(desc VariableDescriptor) error {
+	path := filepath.Join(v.path, fmt.Sprintf("%s-%s", desc.Name, desc.GUID.String()))
+	f, err := os.OpenFile(path, os.O_WRONLY, 0)
+	switch {
+	case os.IsNotExist(err):
+		return ErrVarNotExist
+	case os.IsPermission(err):
+		return ErrVarPermission
+	case err != nil:
+		return err
+	default:
+		_, err := makeMutable(f)
+		switch {
+		case os.IsPermission(err):
+			return ErrVarPermission
+		case err != nil:
+			return err
+		default:
+			f.Close()
+		}
+	}
+	return os.Remove(path)
+}
+
+// List returns the VariableDescriptor for each efivar in the system
+// TODO: why can't list implement
+func (v *EFIVarFS) List() ([]VariableDescriptor, error) {
+	const guidLength = 36
+	f, err := os.OpenFile(v.path, os.O_RDONLY, 0)
+	switch {
+	case os.IsNotExist(err):
+		return nil, ErrVarNotExist
+	case os.IsPermission(err):
+		return nil, ErrVarPermission
+	case err != nil:
+		return nil, err
+	}
+	defer f.Close()
+
+	dirents, err := f.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+	var entries []VariableDescriptor
+	for _, dirent := range dirents {
+		if !dirent.Mode().IsRegular() {
+			// Skip non-regular files
+			continue
+		}
+		if len(dirent.Name()) < guidLength+1 {
+			// Skip files with a basename that isn't long enough
+			// to contain a GUID and a hyphen
+			continue
+		}
+		if dirent.Name()[len(dirent.Name())-guidLength-1] != '-' {
+			// Skip files where the basename doesn't contain a
+			// hyphen between the name and GUID
+			continue
+		}
+		if dirent.Size() == 0 {
+			// Skip files with zero size. These are variables that
+			// have been deleted by writing an empty payload
+			continue
+		}
+
+		name := dirent.Name()[:len(dirent.Name())-guidLength-1]
+		guid, err := guid.Parse(dirent.Name()[len(name)+1:])
+		if err != nil {
+			continue
+		}
+
+		entries = append(entries, VariableDescriptor{Name: name, GUID: guid})
+	}
+
+	sort.Slice(entries, func(i, j int) bool {
+		return fmt.Sprintf("%s-%v", entries[i].Name, entries[i].GUID) < fmt.Sprintf("%s-%v", entries[j].Name, entries[j].GUID)
+	})
+	return entries, nil
+}
---
+++ pkg/pillar/vendor/github.com/robertmin1/u-root/v3/pkg/efivarfs/vars.go
@@ -0,0 +1,140 @@
+// Copyright 2022 the u-root Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package efivarfs allows interaction with efivarfs of the
+// linux kernel.
+package efivarfs
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"strings"
+
+	guid "github.com/google/uuid"
+)
+
+// VariableAttributes is an uint32 identifying the variables attributes.
+type VariableAttributes uint32
+
+const (
+	// AttributeNonVolatile indicates a variable is non volatile.
+	AttributeNonVolatile VariableAttributes = 0x00000001
+	// AttributeBootserviceAccess indicates a variable is accessible during boot service.
+	AttributeBootserviceAccess VariableAttributes = 0x00000002
+	// AttributeRuntimeAccess indicates a variable is accessible during runtime.
+	AttributeRuntimeAccess VariableAttributes = 0x00000004
+	// AttributeHardwareErrorRecord indicates a variable holds hardware error records.
+	AttributeHardwareErrorRecord VariableAttributes = 0x00000008
+	// AttributeAuthenticatedWriteAccess indicates a variable needs authentication before write access.
+	AttributeAuthenticatedWriteAccess VariableAttributes = 0x00000010
+	// AttributeTimeBasedAuthenticatedWriteAccess indicates a variable needs time based authentication before write access.
+	AttributeTimeBasedAuthenticatedWriteAccess VariableAttributes = 0x00000020
+	// AttributeAppendWrite indicates data written to this variable is appended.
+	AttributeAppendWrite VariableAttributes = 0x00000040
+	// AttributeEnhancedAuthenticatedAccess indicate a variable uses the new authentication format.
+	AttributeEnhancedAuthenticatedAccess VariableAttributes = 0x00000080
+)
+
+// VariableDescriptor contains the name and GUID identifying a variable
+type VariableDescriptor struct {
+	Name string
+	GUID guid.UUID
+}
+
+var (
+	// ErrBadGUID is for any errors parsing GUIDs.
+	ErrBadGUID = errors.New("Bad GUID")
+)
+
+func guidParse(v string) ([]string, *guid.UUID, error) {
+	vs := strings.SplitN(v, "-", 2)
+	if len(vs) < 2 {
+		return nil, nil, fmt.Errorf("GUID must have name-GUID format: %w", ErrBadGUID)
+	}
+	g, err := guid.Parse(vs[1])
+	if err != nil {
+		return nil, nil, fmt.Errorf("%w:%v", ErrBadGUID, err)
+	}
+	return vs, &g, nil
+}
+
+// ReadVariable calls get() on the current efivarfs backend.
+func ReadVariable(e EFIVar, desc VariableDescriptor) (VariableAttributes, []byte, error) {
+	return e.Get(desc)
+}
+
+// SimpleReadVariable is like ReadVariables but takes the combined name and guid string
+// of the form name-guid and returns a bytes.Reader instead of a []byte.
+func SimpleReadVariable(e EFIVar, v string) (VariableAttributes, *bytes.Reader, error) {
+	vs, g, err := guidParse(v)
+	if err != nil {
+		return 0, nil, err
+	}
+	attrs, data, err := ReadVariable(e,
+		VariableDescriptor{
+			Name: vs[0],
+			GUID: *g,
+		},
+	)
+	return attrs, bytes.NewReader(data), err
+}
+
+// WriteVariable calls set() on the current efivarfs backend.
+func WriteVariable(e EFIVar, desc VariableDescriptor, attrs VariableAttributes, data []byte) error {
+	return e.Set(desc, attrs, data)
+}
+
+// SimpleWriteVariable is like WriteVariables but takes the combined name and guid string
+// of the form name-guid and returns a bytes.Buffer instead of a []byte.
+func SimpleWriteVariable(e EFIVar, v string, attrs VariableAttributes, data *bytes.Buffer) error {
+	vs, g, err := guidParse(v)
+	if err != nil {
+		return err
+	}
+	return WriteVariable(e,
+		VariableDescriptor{
+			Name: vs[0],
+			GUID: *g,
+		}, attrs, data.Bytes(),
+	)
+}
+
+// RemoveVariable calls remove() on the current efivarfs backend.
+func RemoveVariable(e EFIVar, desc VariableDescriptor) error {
+	return e.Remove(desc)
+}
+
+// SimpleRemoveVariable is like RemoveVariable but takes the combined name and guid string
+// of the form name-guid.
+func SimpleRemoveVariable(e EFIVar, v string) error {
+	vs, g, err := guidParse(v)
+	if err != nil {
+		return err
+	}
+	return RemoveVariable(e,
+		VariableDescriptor{
+			Name: vs[0],
+			GUID: *g,
+		},
+	)
+}
+
+// ListVariables calls list() on the current efivarfs backend.
+func ListVariables(e EFIVar) ([]VariableDescriptor, error) {
+	return e.List()
+}
+
+// SimpleListVariables is like ListVariables but returns a []string instead of a []VariableDescriptor.
+func SimpleListVariables(e EFIVar) ([]string, error) {
+	list, err := ListVariables(e)
+	if err != nil {
+		return nil, err
+	}
+	var out []string
+	for _, v := range list {
+		out = append(out, v.Name+"-"+v.GUID.String())
+	}
+	return out, nil
+}
