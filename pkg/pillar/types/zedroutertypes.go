1bf52d388520bc5972f62f91d51afcb40253b804
Pavel Abramov <uncle.decart@gmail.com>
2025-09-15 13:58:58 +0200

    important changes

--- pkg/pillar/types/zedroutertypes.go
+++ pkg/pillar/types/zedroutertypes.go
@@ -26,7 +26,7 @@ type AppNetworkConfig struct {
 	Activate          bool
 	GetStatsIPAddr    net.IP
 	AppNetAdapterList []AppNetAdapterConfig
-	CloudInitUserData *string `json:"pubsub-large-CloudInitUserData"`
+	CloudInitUserData *string
 	CipherBlockStatus CipherBlockStatus
 	MetaDataType      MetaDataType
 	DeploymentType    AppRuntimeType
