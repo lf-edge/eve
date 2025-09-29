1f12045b52bca1a4cb3e88ad012f5e8520778792
Christoph Ostarek <christoph@zededa.com>
2025-09-29 14:17:07 +0200

    collect-info: do not use form-upload
    as the upload is incomplete, because `-d` means `--data-ascii`,
    but we upload a tarball which is binary data, so curl
    messes it up
    
    Instead use `--data-binary` parameter which uses
    `application/octet-stream`
    
    Signed-off-by: Christoph Ostarek <christoph@zededa.com>

--- pkg/debug/scripts/collect-info.sh
+++ pkg/debug/scripts/collect-info.sh
@@ -589,7 +589,7 @@ echo "EVE info is collected into '$TARBALL_FILE'"
 if [ -n "$UPLOAD" ];
 then
     echo "Uploading tarball to $UPLOAD"
-    curl --retry-all-errors --retry 10 --retry-delay 3 -s -d @"$TARBALL_FILE" -H "Authorization: $AUTHORIZATION" "$UPLOAD/$INFO_DIR_SUFFIX.tar.gz" && \
+    curl --retry-all-errors --retry 10 --retry-delay 3 -s --data-binary @"$TARBALL_FILE" -H "Authorization: $AUTHORIZATION" "$UPLOAD/$INFO_DIR_SUFFIX.tar.gz" && \
         rm -f "$TARBALL_FILE"
     echo "Uploading tarball to $UPLOAD done"
 fi
