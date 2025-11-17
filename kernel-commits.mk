fa5da3cda515f2bc0c498e96a41622be8961b0fb
Renê de Souza Pinto <rene@renesp.com.br>
2025-11-17 12:30:07 +0100

    Kernel update - [amd64-generic]
    This commit changes:
    eve-kernel-amd64-v6.12.49-generic
        ef7ccc4d151c: configs: Disable INTEL_PCH_THERMAL driver
        693b0a177344: [rt] Adjust rt.fragment according to Intel recommendations
        0f763de04df5: Add RT patch download/apply to rt-config and rt-clean target
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

--- kernel-commits.mk
+++ kernel-commits.mk
@@ -1,5 +1,5 @@
 KERNEL_COMMIT_amd64_next_generic = 3152b8fe36ea
-KERNEL_COMMIT_amd64_v6.12.49_generic = 770b90183ee7
+KERNEL_COMMIT_amd64_v6.12.49_generic = ef7ccc4d151c
 KERNEL_COMMIT_arm64_v5.10.192_nvidia-jp5 = b4464b03583e
 KERNEL_COMMIT_arm64_v5.15.136_nvidia-jp6 = 2154a157c3f4
 KERNEL_COMMIT_arm64_v6.1.155_generic = ece043d8b403
