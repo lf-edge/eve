a2d50ab2bf12e59a77eaa462674b65cc84e70f35
Andrew Durbin <andrewd@zededa.com>
2026-01-02 18:21:25 -0700

    Support ClusterType EVE-API for modes
    Use ClusterType enum to determine cluster mode.
    Now Base Mode and Replicated Storage can define a registration yaml.
    
    Signed-off-by: Andrew Durbin <andrewd@zededa.com>

--- pkg/kube/lib/config.sh
+++ pkg/kube/lib/config.sh
@@ -3,6 +3,14 @@
 # Copyright (c) 2026 Zededa, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+#
+# Library for all kube config received from the controller from:
+#   EVE-API, pubsub, zedkube
+#
+
+# shellcheck source=pkg/kube/pubsub.sh
+. /usr/bin/pubsub.sh
+
 # Base Static k3s Config is in /etc/rancher/k3s/config.yaml, following config in each file
 K3S_CONFIG_DIR="/etc/rancher/k3s/config.yaml.d"
 # shellcheck disable=SC2034
@@ -40,4 +48,37 @@ Config_k3s_override_apply() {
     fi
     # Config exists, unchanged
     return 0
+}
+
+Config_cluster_exists() {
+    if [ -f "$ENCC_FILE_PATH" ]; then
+        return 0
+    fi
+    return 1
+}
+
+# CLUSTER_TYPE_ values must match eve-api ClusterType
+# shellcheck disable=SC2034
+CLUSTER_TYPE_UNSPECIFIED=0
+# shellcheck disable=SC2034
+CLUSTER_TYPE_K3S_BASE=1
+# shellcheck disable=SC2034
+CLUSTER_TYPE_REPLICATED_STORAGE=2
+
+# Config_cluster_type_get will return eve-api ClusterType
+Config_cluster_type_get() {
+    if ! Config_cluster_exists; then
+        # Currently default behavior before ENCC arrives
+        return $CLUSTER_TYPE_REPLICATED_STORAGE
+    fi
+    api_has_type=$(jq -r 'has("ClusterType")' < "$ENCC_FILE_PATH")
+    if [ "$api_has_type" != "true" ]; then
+        # Older controller, handle backwards compatibility
+        if Registration_ConfigExists; then
+            return $CLUSTER_TYPE_K3S_BASE
+        fi
+        return $CLUSTER_TYPE_REPLICATED_STORAGE
+    fi
+    cluster_type=$(jq -r .ClusterType < "$ENCC_FILE_PATH")
+    return "$cluster_type"
 }
