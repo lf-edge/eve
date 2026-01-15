6979a0c83c021f6fdc815f40ecf5fbdca071def0
Andrew Durbin <andrewd@zededa.com>
2026-01-15 16:31:50 -0700

    eve-k: allow overriding k3s version via config properties.
    Addition of a new config property `k3s.version` which accepts a
    semantic version formatted string matching a k3s release.
    eg. 'v1.34.3+k3s1'.
    
    Config property validator makeSemverValidator is implemented to:
    - ensure the value matches a semver string, with metadata matching
            a k3s release ("+k3s*")
    - Accept constraint strings, currently restricting to only k3s
            versions with a major version '1'.
    
    Zedkube handles publishing this version value string to a new pubsub
    object 'KubeConfig' used to pass config from the pillar service
    container to the kube container. Future config additions should attempt
    to re-use this new object.
    
    Kube service container cluster-update.sh Update_CheckNodeComponents()
    handles checking this pubsub object on the persist fs, terminating k3s,
    downloading new k3s, and installing.  This path is checked by the main kube
    service loop even if k3s is not running, to assist in cases where the
    current k3s version cannot start.
    
    Signed-off-by: Andrew Durbin <andrewd@zededa.com>

--- pkg/pillar/types/global_test.go
+++ pkg/pillar/types/global_test.go
@@ -249,6 +249,7 @@ func TestNewConfigItemSpecMap(t *testing.T) {
 		EnableTCPMSSClamping,
 		K3sConfigOverride,
 		AppBootOrder,
+		K3sVersionOverride,
 	}
 	if len(specMap.GlobalSettings) != len(gsKeys) {
 		t.Errorf("GlobalSettings has more (%d) than expected keys (%d)",
