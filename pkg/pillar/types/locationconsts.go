f18a1a131044041ae77e1233b2ade3b01af2e844
Shahriyar Jalayeri <shahriyar@posteo.de>
2026-01-15 13:46:55 +0100

    Load TLS root CA directly from /config instead of /persist/certs
    This change fixes the security issue where TLS root certificates were
    copied from the integrity-protected /config partition to the mutable
    /persist/certs directory and referenced via SHA256 indirection.
    
    Changes:
    - Remove certificate copy logic from InitializeCertDir() - now only
      creates the certs directory without copying files
    - Update GetTLSConfig() to read certificates directly from
      types.V2TLSBaseFile (/config/v2tlsbaseroot-certificates.pem)
    - Update UpdateTLSProxyCerts() to read from V2TLSBaseFile instead of
      V2TLSCertShaFilename
    - Remove V2TLSCertShaFilename constant from locationconsts.go as it's
      no longer needed
    - Update getSecurityInfo() in handlemetrics.go to compute SHA256 hash
      directly from the V2TLSBaseFile content instead of reading a
      pre-computed SHA from persist
    
    Signed-off-by: Shahriyar Jalayeri <shahriyar@posteo.de>
    (cherry picked from commit aefb47f7f047f7f4e73545a918bb37640a6c8a62)
    Signed-off-by: Shahriyar Jalayeri <shahriyar@posteo.de>

--- pkg/pillar/types/locationconsts.go
+++ pkg/pillar/types/locationconsts.go
@@ -56,8 +56,6 @@ const (
 	OnboardKeyName = IdentityDirname + "/onboard.key.pem"
 	// RootCertFileName - what we trust for signatures and object encryption
 	RootCertFileName = IdentityDirname + "/root-certificate.pem"
-	// V2TLSCertShaFilename - find TLS root cert for API V2 based on this sha
-	V2TLSCertShaFilename = CertificateDirname + "/v2tlsbaseroot-certificates.sha256"
 	// V2TLSBaseFile is where the initial file
 	V2TLSBaseFile = IdentityDirname + "/v2tlsbaseroot-certificates.pem"
 	// APIV1FileName - user can statically allow for API v1
