6735042a015e3dddafccb238f484d75bd24f5918
Renê de Souza Pinto <rene@renesp.com.br>
2026-03-02 17:41:33 +0000

    pkg/nvidia: Introduce support to Jetpack 7.1
    This commit introduces the support for Jetpack 7.1. This version is
    supported by the Jetson AXG Thor device. This commit includes:
    
    - CDI files
    - Init scripts
    - udev rules
    - Firmware package
    
    The nvidia package is prepared to support a new platform variant,
    nvidia-jp7, that is going to be introduced in the next commits.
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

---
+++ pkg/nvidia/scripts/jp7/nv-init.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Copyright (c) 2026 Zededa, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+VENDOR="/opt/vendor/nvidia"
+FANCTRL="${VENDOR}/bin/nvfanctrl"
+
+# This script is executed from pillar, so we need to export the variables
+# below to execute udevadm from hostfs
+export PATH="$PATH:/hostfs/bin"
+export LD_LIBRARY_PATH="/hostfs/lib"
+
+# Setup modules and devices
+modprobe nvidia
+modprobe nvidia_modeset
+modprobe nvidia_uvm
+modprobe governor_pod_scaling
+modprobe rtcpu-debug
+modprobe camchar
+modprobe camera-diagnostics
+
+mkdir -p /dev/dri/by-path
+
+# Setup udev rules
+mkdir -p /run/udev/rules.d/
+cp "${VENDOR}"/etc/udev/rules.d/* /run/udev/rules.d/
+# Reload rules and trigger udev events
+udevadm control --reload
+
+# Enforces add for framebuffer and nvidia modules, so we have /dev/fb0 and
+# /dev/nvidiactrl even when there is no monitor connected to the display
+# port. These devices must be present because they are on the CDI spec.
+echo "add" > /sys/module/fb/uevent 2> /dev/null
+echo "add" > /sys/module/nvidia/uevent 2> /dev/null
+echo "add" > /sys/module/nvidia_modeset/uevent 2> /dev/null
+echo "add" > /sys/module/nvidia_uvm/uevent 2> /dev/null
+
+# Start FAN controller detached from terminal
+if [ -f "$FANCTRL" ]; then
+    "$FANCTRL" -m quiet > /dev/kmsg 2>&1 &
+fi
