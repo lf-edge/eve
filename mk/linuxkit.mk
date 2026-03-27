59e07b4868417c99acb019593026652fb0a561a8
Paul Gaiduk <paulg@zededa.com>
2026-03-27 11:32:48 +0100

    point linuxkit to europaul fork
    This is a temporary measure to move faster on the linuxkit front.
    It will be removed once the changes are merged upstream.
    
    Signed-off-by: Paul Gaiduk <paulg@zededa.com>

--- mk/linuxkit.mk
+++ mk/linuxkit.mk
@@ -20,8 +20,8 @@ LINUXKIT_SOURCE  ?= https://github.com/linuxkit/linuxkit
 
 # LINUXKIT_GIT_REF must be a commit hash (reproducible, no ls-remote needed).
 # Update by running: git ls-remote https://github.com/linuxkit/linuxkit master
-LINUXKIT_GIT_URL ?= https://github.com/linuxkit/linuxkit
-LINUXKIT_GIT_REF ?= 4cfb70d3cc9256024bb0d4de760c631df0ad06f6
+LINUXKIT_GIT_URL ?= https://github.com/europaul/linuxkit
+LINUXKIT_GIT_REF ?= 420d550a9e0df323ce01e0324dfb6053468c4814
 # Optional local source tree — takes priority over LINUXKIT_GIT_URL.
 #   make LINUXKIT_SRC=/path/to/linuxkit <target>
 LINUXKIT_SRC ?=
