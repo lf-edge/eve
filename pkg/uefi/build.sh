2111f1d8037874e203cb37c099266147de2a0b2c
Mikhail Malyshev <mike.malyshev@gmail.com>
2026-04-01 01:56:40 +0000

    pkg/uefi: upgrade to edk2-stable202508.01 and build VfioIgdPkg Option ROM
    Upgrade EDK2 from edk2-stable202408.01 to edk2-stable202508.01
    (commit 3d244c3b364bd4e21261380662186d064659161c). This version added
    MemDebugLogLib which VfioIgdPkg >= commit c7b537ef requires.
    
    Build VfioIgdPkg (commit 348860da) as a standalone EFI Option ROM
    (igd.rom) containing IgdAssignmentDxe. The DXE driver reads
    etc/igd-opregion and etc/igd-bdsm-size from QEMU fw_cfg and sets
    ASLS (0xFC) and BDSM (0x5C or 0xC0 for Gen11+) in the iGPU PCI
    config space, enabling i915 to initialize the device on q35/UEFI.
    
    Regenerate edk2 patches for the new EDK2 base. All four existing
    patches still apply:
      0000: Xen hypervisor spoofing in XenPlatformPei
      0001: EveBootOrderLib addition
      0002: EVE OS logo replacement
      0003: EveBootOrderLib fw_cfg boot order support
    
    Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

--- pkg/uefi/build.sh
+++ pkg/uefi/build.sh
@@ -2,7 +2,7 @@
 
 TARGET=RELEASE
 
-make -C BaseTools
+make -C BaseTools -j "$(nproc)"
 OVMF_COMMON_FLAGS="-DNETWORK_TLS_ENABLE"
 OVMF_COMMON_FLAGS+=" -DSECURE_BOOT_ENABLE=TRUE"
 OVMF_COMMON_FLAGS+=" -DTPM2_CONFIG_ENABLE=TRUE"
@@ -21,18 +21,24 @@ case $(uname -m) in
              cp /opensbi/build/platform/generic/firmware/fw_payload.bin OVMF_VARS.fd
              cp /opensbi/build/platform/generic/firmware/fw_jump.bin OVMF.fd
              ;;
-    aarch64) build -b ${TARGET} -t GCC5 -a AARCH64 -p ArmVirtPkg/ArmVirtQemu.dsc -D TPM2_ENABLE=TRUE -D TPM2_CONFIG_ENABLE=TRUE
+    aarch64) build -b ${TARGET} -t GCC5 -a AARCH64 -n "$(nproc)" -p ArmVirtPkg/ArmVirtQemu.dsc -D TPM2_ENABLE=TRUE -D TPM2_CONFIG_ENABLE=TRUE
              cp Build/ArmVirtQemu-AARCH64/${TARGET}_GCC5/FV/QEMU_EFI.fd OVMF.fd
              cp Build/ArmVirtQemu-AARCH64/${TARGET}_GCC5/FV/QEMU_VARS.fd OVMF_VARS.fd
              # now let's build PVH UEFI kernel
-             make -C BaseTools/Source/C
-             build -b ${TARGET} -t GCC5 -a AARCH64  -p ArmVirtPkg/ArmVirtXen.dsc
+             make -C BaseTools/Source/C -j "$(nproc)"
+             build -b ${TARGET} -t GCC5 -a AARCH64 -n "$(nproc)" -p ArmVirtPkg/ArmVirtXen.dsc
              cp Build/ArmVirtXen-AARCH64/${TARGET}_*/FV/XEN_EFI.fd OVMF_PVH.fd
              ;;
-     x86_64) build -b ${TARGET} -t GCC5 -a X64 -p OvmfPkg/OvmfPkgX64.dsc ${OVMF_COMMON_FLAGS}
+     x86_64) build -b ${TARGET} -t GCC5 -a X64 -n "$(nproc)" -p OvmfPkg/OvmfPkgX64.dsc ${OVMF_COMMON_FLAGS}
              cp Build/OvmfX64/${TARGET}_*/FV/OVMF*.fd .
-             build -b ${TARGET} -t GCC5 -a X64 -p OvmfPkg/OvmfXen.dsc
+             build -b ${TARGET} -t GCC5 -a X64 -n "$(nproc)" -p OvmfPkg/OvmfXen.dsc
              cp Build/OvmfXen/${TARGET}_*/FV/OVMF.fd OVMF_PVH.fd
+             # Build VfioIgdPkg open-source IGD Option ROM (IgdAssignmentDxe only).
+             # Handles Gen11+ 64-bit BDSM at PCI config 0xC0, unlike classic IgdAssignmentDxe.
+             build -b ${TARGET} -t GCC5 -a X64 -n "$(nproc)" -p VfioIgdPkg/VfioIgdPkg.dsc
+             EfiRom -f 0x8086 -i 0xffff \
+                 -e Build/VfioIgdPkg/${TARGET}_GCC5/X64/IgdAssignmentDxe.efi \
+                 -o igd.rom
              ;;
           *) echo "Unsupported architecture $(uname). Bailing."
              exit 1
