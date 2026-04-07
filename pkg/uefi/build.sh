966362693cb387be1db617659f3f9598af4bde48
Mikhail Malyshev <mike.malyshev@gmail.com>
2026-04-07 14:14:14 +0000

    pkg/uefi: enable parallel builds with -j/-n $(nproc)
    Add -j "$(nproc)" to make invocations and -n "$(nproc)" to EDK2 build
    commands so UEFI firmware builds use all available CPU cores. This
    significantly reduces build time on multi-core machines.
    
    Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

--- pkg/uefi/build.sh
+++ pkg/uefi/build.sh
@@ -2,7 +2,7 @@
 
 TARGET=RELEASE
 
-make -C BaseTools
+make -C BaseTools -j "$(nproc)"
 OVMF_COMMON_FLAGS="-DNETWORK_TLS_ENABLE"
 OVMF_COMMON_FLAGS+=" -DSECURE_BOOT_ENABLE=TRUE"
 OVMF_COMMON_FLAGS+=" -DTPM2_CONFIG_ENABLE=TRUE"
@@ -21,17 +21,17 @@ case $(uname -m) in
              cp /opensbi/build/platform/generic/firmware/fw_payload.bin OVMF_VARS.fd
              cp /opensbi/build/platform/generic/firmware/fw_jump.bin OVMF.fd
              ;;
-    aarch64) build -b ${TARGET} -t GCC5 -a AARCH64 -p ArmVirtPkg/ArmVirtQemu.dsc -D TPM2_ENABLE=TRUE -D TPM2_CONFIG_ENABLE=TRUE
+    aarch64) build -b ${TARGET} -t GCC5 -a AARCH64 -n "$(nproc)" -p ArmVirtPkg/ArmVirtQemu.dsc -D TPM2_ENABLE=TRUE -D TPM2_CONFIG_ENABLE=TRUE
              cp Build/ArmVirtQemu-AARCH64/${TARGET}_GCC5/FV/QEMU_EFI.fd OVMF.fd
              cp Build/ArmVirtQemu-AARCH64/${TARGET}_GCC5/FV/QEMU_VARS.fd OVMF_VARS.fd
              # now let's build PVH UEFI kernel
-             make -C BaseTools/Source/C
-             build -b ${TARGET} -t GCC5 -a AARCH64  -p ArmVirtPkg/ArmVirtXen.dsc
+             make -C BaseTools/Source/C -j "$(nproc)"
+             build -b ${TARGET} -t GCC5 -a AARCH64 -n "$(nproc)" -p ArmVirtPkg/ArmVirtXen.dsc
              cp Build/ArmVirtXen-AARCH64/${TARGET}_*/FV/XEN_EFI.fd OVMF_PVH.fd
              ;;
-     x86_64) build -b ${TARGET} -t GCC5 -a X64 -p OvmfPkg/OvmfPkgX64.dsc ${OVMF_COMMON_FLAGS}
+     x86_64) build -b ${TARGET} -t GCC5 -a X64 -n "$(nproc)" -p OvmfPkg/OvmfPkgX64.dsc ${OVMF_COMMON_FLAGS}
              cp Build/OvmfX64/${TARGET}_*/FV/OVMF*.fd .
-             build -b ${TARGET} -t GCC5 -a X64 -p OvmfPkg/OvmfXen.dsc
+             build -b ${TARGET} -t GCC5 -a X64 -n "$(nproc)" -p OvmfPkg/OvmfXen.dsc
              cp Build/OvmfXen/${TARGET}_*/FV/OVMF.fd OVMF_PVH.fd
              ;;
           *) echo "Unsupported architecture $(uname). Bailing."
