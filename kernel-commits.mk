41fa438c43bc2faa5b920d6f17662df7eaaac8e6
Shahriyar Jalayeri <shahriyar@posteo.de>
2026-05-08 12:00:39 +0200

    kernel: update kernel commits to patch CVE-2026-31431
    Update kernel-commits.mk with the latest commits across all branches to
    include the fix for CVE-2026-31431.
    
    Signed-off-by: Shahriyar Jalayeri <shahriyar@posteo.de>

--- kernel-commits.mk
+++ kernel-commits.mk
@@ -1,6 +1,6 @@
-KERNEL_COMMIT_amd64_next_generic = 3152b8fe36ea
-KERNEL_COMMIT_amd64_v6.12.49_generic = ef7ccc4d151c
-KERNEL_COMMIT_arm64_v5.10.192_nvidia-jp5 = b4464b03583e
-KERNEL_COMMIT_arm64_v5.15.136_nvidia-jp6 = 2154a157c3f4
-KERNEL_COMMIT_arm64_v6.1.155_generic = ece043d8b403
-KERNEL_COMMIT_riscv64_v6.1.112_generic = 18e1d313b90b
+KERNEL_COMMIT_amd64_next_generic = a65e45508b45
+KERNEL_COMMIT_amd64_v6.12.49_generic = 465d242734dc
+KERNEL_COMMIT_arm64_v5.10.192_nvidia-jp5 = 2e0dcfd3260d
+KERNEL_COMMIT_arm64_v5.15.136_nvidia-jp6 = 4929f15eda41
+KERNEL_COMMIT_arm64_v6.1.155_generic = da7d6950689c
+KERNEL_COMMIT_riscv64_v6.1.112_generic = 30aa75d58cdd
