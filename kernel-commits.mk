eae15b00d9aca55f3ce52ec967819600e93e8446
Renê de Souza Pinto <rene@renesp.com.br>
2026-06-25 12:34:33 +0200

    Kernel update - [arm64-generic]
    This commit changes:
    eve-kernel-arm64-v6.1.155-generic
        88e6efd1067a: drivers: pwm: Add RP1 PWM driver
        f68bd17c4668: arm64: configs: enable SPI support for Raspberry Pi 5
        8ed723972af2: irqchip/bcm2712-mip, PCI: brcmstb: support upstream brcm,bcm2712-mip binding
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

--- kernel-commits.mk
+++ kernel-commits.mk
@@ -3,5 +3,5 @@ KERNEL_COMMIT_amd64_v6.12.49_generic = dcdba3ddf871
 KERNEL_COMMIT_arm64_v5.10.192_nvidia-jp5 = 2e0dcfd3260d
 KERNEL_COMMIT_arm64_v5.15.136_nvidia-jp6 = 4929f15eda41
 KERNEL_COMMIT_arm64_v6.8.12_nvidia-jp7 = 452eaffef5ed
-KERNEL_COMMIT_arm64_v6.1.155_generic = da7d6950689c
+KERNEL_COMMIT_arm64_v6.1.155_generic = 88e6efd1067a
 KERNEL_COMMIT_riscv64_v6.1.112_generic = 30aa75d58cdd
