08693f9c6519016f02730364418122f26dbe51ac
Renê de Souza Pinto <rene@renesp.com.br>
2026-07-24 12:16:35 +0200

    pkg/dom0-ztools: Enable to kill rungetty.sh
    rungetty.sh runs on an infinite loop so it can re-spawn a console shell
    when exited from getty. However, this makes impossible to disable a console
    during runtime if device property debug.enable.console is set to false.
    
    This script changes to rungetty.sh to trap signal USR1 and kill the current
    getty process, which will allow stop the console from pillar during
    runtime.
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>
    (cherry picked from commit 81424ce36459787addbf2c38e0fe7a7796ca08bc)

--- pkg/dom0-ztools/rootfs/usr/bin/rungetty.sh
+++ pkg/dom0-ztools/rootfs/usr/bin/rungetty.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 infinite_loop() {
-	while true; do
-		$@
+	stop=0
+	child=""
+	trap 'stop=1; [ -n "$child" ] && kill $child 2> /dev/null' USR1
+	while [ "$stop" -eq 0 ]; do
+		$@ &
+		child=$!
+		wait "$child"
 	done
 }
 
