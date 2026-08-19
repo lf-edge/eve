lf-edge/eve

commit 786a54885542b46e9a8e2dff9e982f66466fa3ae

Handle the Patch-Envelope Encrypted Artifacts
- the application is for the nested-app secure configuration
- modified the cipher GetCipherCredentials() function, it does not need
  to assume the encrypted data is in 'EncryptionBlock' struct, this is
  easier to have an entire struct to be encrypted
- handle on the zedagent side of setup of publication for encrypted data
  inside the 'PatchEnvelopeInfo' with slice of 'BinaryCipherBlob'
- handle the cipher data in MSRV, decrypt the data to populate inline or
  volumeref blobs
- handle the meta-data service for description query, the inline
  decrypted file downloading and zipfile downloading
- add some test cases using sample cipher blobs

Signed-off-by: Naiming Shen <naiming@zededa.com>

pkg/pillar/cipher/cipher.go
@@ -34,13 +34,42 @@ func getEncryptionBlock(
 // GetCipherCredentials : decrypt encryption block
 func GetCipherCredentials(ctx *DecryptCipherContext,
 	status types.CipherBlockStatus) (types.CipherBlockStatus, types.EncryptionBlock, error) {
+	var decBlock types.EncryptionBlock
+	cipherBlock, clearBytes, err := GetCipherMarshalledData(ctx, status)
+	if err != nil {
+		return handleCipherBlockCredError(ctx, &cipherBlock,
+			decBlock, err, types.DecryptFailed)
+	}
+
+	var zconfigDecBlock zcommon.EncryptionBlock
+	err = UnmarshalCipherData(ctx, clearBytes, &zconfigDecBlock)
+	if err != nil {
+		ctx.Log.Errorf("%s, encryption block unmarshall failed, %v\n",
+			cipherBlock.Key(), err)
+		return handleCipherBlockCredError(ctx, &cipherBlock,
+			decBlock, err, types.UnmarshalFailed)
+	}
+	ctx.Log.Functionf("%s, cipherblock decryption successful", cipherBlock.Key())
+	decBlock = getEncryptionBlock(&zconfigDecBlock)
+	ctx.AgentMetrics.RecordSuccess(ctx.Log)
+	return cipherBlock, decBlock, err
+}
+
+// GetCipherMarshalledData : decrypt encryption block, without assuming the proto
+// encoding data is zcommon.EncryptionBlock, since the data before encryption may have
+// its own data structure, it is up to the caller to unmarshal the data using
+// 'UnmarshalCipherData' function. Future use cases may not need to add a new
+// item in the EncryptionBLock.
+func GetCipherMarshalledData(ctx *DecryptCipherContext,
+	status types.CipherBlockStatus) (types.CipherBlockStatus, []byte, error) {
 	cipherBlock := new(types.CipherBlockStatus)
 	*cipherBlock = status
 	var decBlock types.EncryptionBlock
 	if !cipherBlock.IsCipher {
 		// Should not be called if IsCipher is not set
-		return handleCipherBlockCredError(ctx, cipherBlock,
+		cblock, _, err := handleCipherBlockCredError(ctx, cipherBlock,
 			decBlock, nil, types.Invalid)
+		return cblock, nil, err
 	}
 	ctx.Log.Functionf("%s, cipherblock decryption, using cipher-context: %s\n",
 		cipherBlock.Key(), cipherBlock.CipherContextID)
@@ -49,29 +78,31 @@ func GetCipherCredentials(ctx *DecryptCipherContext,
 			cipherBlock.Key(), cipherBlock.Error)
 		ctx.Log.Errorln(errStr)
 		err := errors.New(errStr)
-		return handleCipherBlockCredError(ctx, cipherBlock,
+		cblock, _, err := handleCipherBlockCredError(ctx, cipherBlock,
 			decBlock, err, types.NotReady)
+		return cblock, nil, err
 	}
 	clearBytes, err := DecryptCipherBlock(ctx, *cipherBlock)
 	if err != nil {
 		ctx.Log.Errorf("%s, cipherblock decryption failed, %v\n",
 			cipherBlock.Key(), err)
-		return handleCipherBlockCredError(ctx, cipherBlock,
-			decBlock, err, types.DecryptFailed)
+		cblock, _, err := handleCipherBlockCredError(ctx, cipherBlock,
+			decBlock, nil, types.Invalid)
+		return cblock, nil, err
 	}
 
-	var zconfigDecBlock zcommon.EncryptionBlock
-	err = proto.Unmarshal(clearBytes, &zconfigDecBlock)
+	return *cipherBlock, clearBytes, err
+}
+
+// UnmarshalCipherData generalizes the unmarshalling of cipher data into different Go data structures.
+func UnmarshalCipherData(ctx *DecryptCipherContext, clearBytes []byte, out proto.Message) error {
+	err := proto.Unmarshal(clearBytes, out)
 	if err != nil {
-		ctx.Log.Errorf("%s, encryption block unmarshall failed, %v\n",
-			cipherBlock.Key(), err)
-		return handleCipherBlockCredError(ctx, cipherBlock,
-			decBlock, err, types.UnmarshalFailed)
+		ctx.Log.Errorf("encryption block unmarshall failed, %v\n", err)
+		return fmt.Errorf("failed to unmarshal cipher block: %w", err)
 	}
-	ctx.Log.Functionf("%s, cipherblock decryption successful", cipherBlock.Key())
-	decBlock = getEncryptionBlock(&zconfigDecBlock)
-	ctx.AgentMetrics.RecordSuccess(ctx.Log)
-	return *cipherBlock, decBlock, err
+	ctx.Log.Functionf("cipherblock decryption successful")
+	return nil
 }
 
 // GetCipherData : decrypt plain text

pkg/pillar/cmd/msrv/handlers.go
@@ -602,7 +602,26 @@ func (msrv *Msrv) handlePatchDownload() func(http.ResponseWriter, *http.Request)
 					fmt.Sprintf("failed to create temp dir %v", err))
 				return
 			}
+
+			// temp URL data populated in the cipher blobs for zip operation
+			for _, cipher := range e.CipherBlobs {
+				if cipher.EncType == types.BlobEncrytedTypeInline && cipher.Inline != nil {
+					cipher.Inline.URL, err = msrv.PopulateBinaryBlobFromCipher(&cipher, false)
+					if err != nil {
+						sendError(w, http.StatusInternalServerError,
+							fmt.Sprintf("failed to populate cipher blob %v", err))
+						return
+					}
+				}
+			}
 			zipFilename, err := utils.GetZipArchive(path, *e)
+
+			// clear the temp URL data populated in the cipher blobs
+			for _, cipher := range e.CipherBlobs {
+				if cipher.EncType == types.BlobEncrytedTypeInline && cipher.Inline != nil {
+					cipher.Inline.URL = ""
+				}
+			}
 
 			if err != nil {
 				sendError(w, http.StatusInternalServerError,
@@ -651,6 +670,15 @@ func (msrv *Msrv) handlePatchFileDownload() func(http.ResponseWriter, *http.Requ
 				http.ServeFile(w, r, e.BinaryBlobs[idx].URL)
 				msrv.increasePatchEnvelopeDownloadCounter(appUUID, *e)
 				return
+			} else if idx := types.CompletedCipherBlobIdxByName(e.CipherBlobs, fileName); idx != -1 {
+				base64Data, err := msrv.PopulateBinaryBlobFromCipher(&e.CipherBlobs[idx], false)
+				if err != nil {
+					sendError(w, http.StatusInternalServerError, fmt.Sprintf("failed to populate cipher blob %v", err))
+					return
+				}
+				serveBase64Data(w, r, base64Data)
+				msrv.increasePatchEnvelopeDownloadCounter(appUUID, *e)
+				return
 			} else {
 				sendError(w, http.StatusNotFound, "file is not found")
 				return
@@ -659,6 +687,18 @@ func (msrv *Msrv) handlePatchFileDownload() func(http.ResponseWriter, *http.Requ
 
 		sendError(w, http.StatusNotFound, "patch is not found")
 	}
+}
+
+// Handler to serve the base64 data
+func serveBase64Data(w http.ResponseWriter, r *http.Request, base64Data string) {
+	// Set the appropriate headers
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Disposition", "attachment; filename=\"data.txt\"")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(base64Data)))
+
+	// Write the base64 data to the response
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(base64Data))
 }
 
 // handleAppInstanceDiscovery returns all IP addresses of each port

pkg/pillar/cmd/msrv/msrv.go
@@ -111,6 +111,8 @@ type Msrv struct {
 
 	subDeviceNetworkStatus pubsub.Subscription
 
+	pubCipherMetrics pubsub.Publication
+
 	// Subscriptions to gather information about
 	// patch envelopes from volumemgr and zedagent
 	// external envelopes have to be downloaded via
@@ -169,7 +171,7 @@ func (msrv *Msrv) Init(cachePath string, persist bool) (err error) {
 	msrv.decryptCipherContext.PubSubControllerCert = msrv.subControllerCert
 	msrv.decryptCipherContext.PubSubEdgeNodeCert = msrv.subEdgeNodeCert
 
-	msrv.PatchEnvelopes = NewPatchEnvelopes(msrv.Log, msrv.PubSub)
+	msrv.PatchEnvelopes = NewPatchEnvelopes(msrv)
 	msrv.patchEnvelopesUsage = generics.NewLockedMap[string, types.PatchEnvelopeUsage]()
 
 	msrv.peUsagePersist, err = persistcache.New(cachePath)
@@ -214,6 +216,17 @@ func (msrv *Msrv) initPublications() (err error) {
 		pubsub.PublicationOptions{
 			AgentName: agentName,
 			TopicType: types.PatchEnvelopeUsage{},
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	msrv.pubCipherBlockStatus, err = msrv.PubSub.NewPublication(
+		pubsub.PublicationOptions{
+			AgentName:  agentName,
+			Persistent: true,
+			TopicType:  types.CipherBlockStatus{},
 		},
 	)
 	if err != nil {
@@ -291,6 +304,17 @@ func (msrv *Msrv) initSubscriptions(persist bool) (err error) {
 	if err != nil {
 		return err
 	}
+
+	msrv.cipherMetrics = cipher.NewAgentMetrics(agentName)
+	pubCipherMetrics, err := msrv.PubSub.NewPublication(
+		pubsub.PublicationOptions{
+			AgentName: agentName,
+			TopicType: types.CipherMetrics{},
+		})
+	if err != nil {
+		return err
+	}
+	msrv.pubCipherMetrics = pubCipherMetrics
 
 	// Subscribe to AppNetworkConfig from zedmanager
 	msrv.subAppNetworkConfig, err = msrv.PubSub.NewSubscription(pubsub.SubscriptionOptions{
@@ -547,6 +571,33 @@ func (msrv *Msrv) Run(ctx context.Context) (err error) {
 
 	if err = msrv.Activate(); err != nil {
 		return err
+	}
+
+	// Wait for the EdgeNodeInfo, and EdgeNodeCert and ControllerCert publications
+	var edgenodeCertInitialized, controllerCertInitialized, edgenodeInfoInitialized bool
+	for !edgenodeCertInitialized || !controllerCertInitialized || !edgenodeInfoInitialized {
+		select {
+		case change := <-msrv.subEdgeNodeCert.MsgChan():
+			msrv.subEdgeNodeCert.ProcessChange(change)
+			msrv.Log.Noticef("msrv: EdgeNodeCert, len %d", len(msrv.subEdgeNodeCert.GetAll()))
+			if len(msrv.subEdgeNodeCert.GetAll()) > 0 {
+				edgenodeCertInitialized = true
+			}
+		case change := <-msrv.subControllerCert.MsgChan():
+			msrv.subControllerCert.ProcessChange(change)
+			msrv.Log.Noticef("msrv: ControllerCert, len %d", len(msrv.subEdgeNodeCert.GetAll()))
+			if len(msrv.subControllerCert.GetAll()) > 0 {
+				controllerCertInitialized = true
+			}
+		case change := <-msrv.subEdgeNodeInfo.MsgChan():
+			msrv.subEdgeNodeInfo.ProcessChange(change)
+			msrv.Log.Noticef("msrv: EdgeNodeInfo, len %d", len(msrv.subEdgeNodeInfo.GetAll()))
+			if len(msrv.subEdgeNodeInfo.GetAll()) > 0 {
+				edgenodeInfoInitialized = true
+			}
+		case <-stillRunning.C:
+		}
+		msrv.PubSub.StillRunning(agentName, warningTime, errorTime)
 	}
 
 	for {

pkg/pillar/cmd/msrv/msrv_test.go
@@ -205,6 +205,26 @@ func TestRequestPatchEnvelopes(t *testing.T) {
 						URL:      "a.txt",
 					},
 				},
+				CipherBlobs: []types.BinaryCipherBlob{
+					{
+						EncType: types.BlobEncrytedTypeInline,
+						Inline: &types.BinaryBlobCompleted{
+							FileName: "abcd2",
+							FileSha:  "abcd2",
+							URL:      "a2.txt",
+						},
+					},
+					{
+						EncType: types.BlobEncrytedTypeVolume,
+						Volume: &types.BinaryBlobVolumeRef{
+							FileName:         "abcd3a",
+							ImageName:        "abcd3b",
+							FileMetadata:     "abcd3c",
+							ArtifactMetadata: "abcd3d",
+							ImageID:          "abcd3e",
+						},
+					},
+				},
 			},
 		},
 	})
@@ -259,6 +279,23 @@ func TestRequestPatchEnvelopes(t *testing.T) {
 							ArtifactMetadata: "",
 							URL:              "http://169.254.169.254/eve/v1/patch/download/6ba7b810-9dad-11d1-80b4-111111111111/abcd",
 							Size:             0,
+						},
+						{
+							FileName:         "abcd2",
+							FileSha:          "abcd2",
+							FileMetadata:     "",
+							ArtifactMetadata: "",
+							URL:              "http://169.254.169.254/eve/v1/patch/download/6ba7b810-9dad-11d1-80b4-111111111111/abcd2",
+							Size:             0,
+						},
+					},
+					VolumeRefs: []types.BinaryBlobVolumeRef{
+						{
+							FileName:         "abcd3a",
+							ImageName:        "abcd3b",
+							FileMetadata:     "abcd3c",
+							ArtifactMetadata: "abcd3d",
+							ImageID:          "abcd3e",
 						},
 					},
 				},

pkg/pillar/cmd/msrv/patchenvelopes.go
@@ -4,13 +4,20 @@
 package msrv
 
 import (
+	"crypto/sha256"
+	"encoding/gob"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"os"
 	"sync"
 
 	uuid "github.com/satori/go.uuid"
 
+	zconfig "github.com/lf-edge/eve-api/go/config"
+	"github.com/lf-edge/eve-api/go/evecommon"
 	"github.com/lf-edge/eve/pkg/pillar/base"
+	"github.com/lf-edge/eve/pkg/pillar/cipher"
 	"github.com/lf-edge/eve/pkg/pillar/pubsub"
 	"github.com/lf-edge/eve/pkg/pillar/types"
 	"github.com/lf-edge/eve/pkg/pillar/utils/generics"
@@ -65,6 +72,8 @@ type PatchEnvelopes struct {
 	pubSub                *pubsub.PubSub
 	log                   *base.LogObject
 	pubPatchEnvelopeState pubsub.Publication
+
+	msrv *Msrv
 }
 
 // UpdateStateNotificationCh return update channel to send notifications to update currentState
@@ -75,7 +84,7 @@ func (pes *PatchEnvelopes) UpdateStateNotificationCh() chan<- struct{} {
 // NewPatchEnvelopes returns PatchEnvelopes structure and starts goroutine
 // to process notifications from channel. Note that we create buffered channel
 // to avoid unbounded processing time in writing to channel
-func NewPatchEnvelopes(log *base.LogObject, ps *pubsub.PubSub) *PatchEnvelopes {
+func NewPatchEnvelopes(msrv *Msrv) *PatchEnvelopes {
 	pe := &PatchEnvelopes{
 
 		updateStateNotificationCh: make(chan struct{}, 1),
@@ -87,8 +96,9 @@ func NewPatchEnvelopes(log *base.LogObject, ps *pubsub.PubSub) *PatchEnvelopes {
 		completedVolumes:  generics.NewLockedMap[uuid.UUID, types.VolumeStatus](),
 		contentTreeStatus: generics.NewLockedMap[uuid.UUID, types.ContentTreeStatus](),
 
-		log:    log,
-		pubSub: ps,
+		log:    msrv.Log,
+		pubSub: msrv.PubSub,
+		msrv:   msrv,
 	}
 
 	var err error
@@ -135,6 +145,22 @@ func (pes *PatchEnvelopes) updateState() {
 			peState := types.PatchEnvelopeStateActive
 			for _, volRef := range pe.VolumeRefs {
 				if blob, blobState := pes.blobFromVolumeRef(volRef); blob != nil {
+					if blobState < peState {
+						peState = blobState
+					}
+					if idx := types.CompletedBinaryBlobIdxByName(pe.BinaryBlobs, blob.FileName); idx != -1 {
+						pe.BinaryBlobs[idx] = *blob
+					} else {
+						pe.BinaryBlobs = append(pe.BinaryBlobs, *blob)
+					}
+				}
+			}
+			// Check also the cipher blobs
+			for _, cipher := range pe.CipherBlobs {
+				if cipher.Volume == nil {
+					continue
+				}
+				if blob, blobState := pes.blobFromVolumeRef(*cipher.Volume); blob != nil {
 					if blobState < peState {
 						peState = blobState
 					}
@@ -273,6 +299,7 @@ func (pes *PatchEnvelopes) UpdateEnvelopes(peInfo []types.PatchEnvelopeInfo) {
 		if err != nil {
 			pes.log.Errorf("Failed to Update Envelopes :%v", err)
 		}
+		pes.checkAndExpandCipherBlobs(&pe)
 		envelopes.Store(peUUID, pe)
 	}
 
@@ -282,6 +309,146 @@ func (pes *PatchEnvelopes) UpdateEnvelopes(peInfo []types.PatchEnvelopeInfo) {
 	}
 
 	pes.envelopes = envelopes
+}
+
+func (pes *PatchEnvelopes) checkAndExpandCipherBlobs(pe *types.PatchEnvelopeInfo) {
+
+	for i := range pe.BinaryBlobs {
+		blob := &pe.BinaryBlobs[i]
+		if !blob.EncArtifactMeta.IsCipher {
+			continue
+		}
+		aMeta := pes.GetArtifactMetaData(blob.EncArtifactMeta)
+		if aMeta == "" {
+			pes.log.Errorf("checkAndExpandCipherBlobs: Failed to get artifact metadata for %v", blob.FileName)
+			continue
+		}
+		blob.ArtifactMetadata = aMeta
+		pes.log.Functionf("checkAndExpandCipherBlobs: got binaryBlobs (%d)", i)
+	}
+
+	for i := range pe.VolumeRefs {
+		volRef := &pe.VolumeRefs[i]
+		if !volRef.EncArtifactMeta.IsCipher {
+			continue
+		}
+		aMeta := pes.GetArtifactMetaData(volRef.EncArtifactMeta)
+		if aMeta == "" {
+			pes.log.Errorf("checkAndExpandCipherBlobs: Failed to get artifact metadata for %v", volRef.FileName)
+			continue
+		}
+		volRef.ArtifactMetadata = aMeta
+		pes.log.Functionf("checkAndExpandCipherBlobs: got binary volumeref (%d)", i)
+	}
+
+	// Either the inline blob or the volume ref is encrypted, the artifactMetadata attached
+	// may or may not be encrypted
+	for i := range pe.CipherBlobs {
+		cBlob := &pe.CipherBlobs[i]
+		if cBlob.EncArtifactMeta.IsCipher {
+			aMeta := pes.GetArtifactMetaData(cBlob.EncArtifactMeta)
+			if aMeta == "" {
+				pes.log.Errorf("checkAndExpandCipherBlobs: Failed to get artifact metadata for cipher blob")
+				continue
+			}
+			cBlob.ArtifactMetaData = aMeta
+		}
+		_, err := pes.msrv.PopulateBinaryBlobFromCipher(cBlob, true)
+		if err != nil {
+			pes.log.Errorf("checkAndExpandCipherBlobs: Failed to populate binary blob from cipher blob")
+		}
+	}
+}
+
+// GetArtifactMetaData returns artifact metadata for the given cipher block status
+func (pes *PatchEnvelopes) GetArtifactMetaData(c types.CipherBlockStatus) string {
+	status, decBlock, err := cipher.GetCipherCredentials(&pes.msrv.decryptCipherContext, c)
+	if pes.msrv.pubCipherBlockStatus != nil {
+		_ = pes.msrv.pubCipherBlockStatus.Publish(status.Key(), status)
+		if err != nil {
+			return ""
+		}
+	}
+	if decBlock.User == evecommon.EncryptionBlockUser_ENCRYPTION_BLOCK_USER_BINARY_ARTIFACT_METADATA {
+		return decBlock.EncryptedData
+	}
+	return ""
+}
+
+// PopulateBinaryBlobFromCipher populates BinaryBlobCompleted or BinaryBlobVolumeRef
+func (msrv *Msrv) PopulateBinaryBlobFromCipher(cBlob *types.BinaryCipherBlob, download bool) (string, error) {
+	if cBlob == nil {
+		return "", fmt.Errorf("cBlob is nil")
+	}
+	EncBinaryArtifact, err := loadCipherBlockStatusFromFile(cBlob.EncURL)
+	if err != nil {
+		return "", fmt.Errorf("failed to load cipher block status from file: %v", err)
+	}
+	status, clearBytes, err := cipher.GetCipherMarshalledData(&msrv.decryptCipherContext, EncBinaryArtifact)
+	if msrv.pubCipherBlockStatus != nil {
+		_ = msrv.pubCipherBlockStatus.Publish(status.Key(), status)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	switch cBlob.EncType {
+	case types.BlobEncrytedTypeInline:
+		var inline zconfig.InlineOpaqueBase64Data
+		err = cipher.UnmarshalCipherData(&msrv.decryptCipherContext, clearBytes, &inline)
+		if err != nil {
+			return "", fmt.Errorf("failed to unmarshal BinaryBlobCompleted: %v", err)
+		}
+		data := inline.GetBase64Data()
+		if download {
+			return data, nil
+		}
+		shaBytes := sha256.Sum256([]byte(data))
+		inlineBlob := &types.BinaryBlobCompleted{
+			FileName:     inline.GetFileNameToUse(),
+			FileSha:      hex.EncodeToString(shaBytes[:]),
+			FileMetadata: inline.GetBase64MetaData(),
+			URL:          "", //data, // this URL now is not used for download, but for decrypted data saved
+			Size:         int64(len(data)),
+		}
+		cBlob.Inline = inlineBlob
+		msrv.Log.Functionf("PopulateBinaryBlobFromCipher: inlineBlob %v", inlineBlob)
+	case types.BlobEncrytedTypeVolume:
+		var volume zconfig.ExternalOpaqueBinaryBlob
+		err = cipher.UnmarshalCipherData(&msrv.decryptCipherContext, clearBytes, &volume)
+		if err != nil {
+			return "", fmt.Errorf("failed to unmarshal BinaryBlobCompleted: %v", err)
+		}
+		volumeBlob := &types.BinaryBlobVolumeRef{
+			ImageName:    volume.GetImageName(),
+			FileName:     volume.GetFileNameToUse(),
+			FileMetadata: volume.GetBlobMetaData(),
+			ImageID:      volume.GetImageId(),
+		}
+		cBlob.Volume = volumeBlob
+		msrv.Log.Functionf("PopulateBinaryBlobFromCipher: volumeBlob %v", volumeBlob)
+	default:
+		return "", fmt.Errorf("unknown encryption type: %v", cBlob.EncType)
+	}
+	return "", nil
+}
+
+// loadCipherBlockStatusFromFile loads CipherBlockStatus from EncURL file path,
+// and Gob decodes it into CipherBlockStatus structure
+func loadCipherBlockStatusFromFile(encURL string) (types.CipherBlockStatus, error) {
+	var status types.CipherBlockStatus
+	file, err := os.Open(encURL)
+	if err != nil {
+		return status, fmt.Errorf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	decoder := gob.NewDecoder(file)
+	if err := decoder.Decode(&status); err != nil {
+		return status, fmt.Errorf("failed to decode CipherBlockStatus: %v", err)
+	}
+
+	return status, nil
 }
 
 // UpdateContentTree adds or removes ContentTreeStatus from PatchEnvelopes structure
@@ -329,24 +496,60 @@ func patchEnvelopesJSONForAppInstance(pe types.PatchEnvelopeInfoList) ([]byte, e
 	toDisplay := make([]PeInfoToDisplay, len(pe.Envelopes))
 
 	for i, envelope := range pe.Envelopes {
-
-		var binaryBlobs []types.BinaryBlobCompleted
-		binaryBlobs = nil
-		if envelope.BinaryBlobs != nil {
-			binaryBlobs = make([]types.BinaryBlobCompleted, len(envelope.BinaryBlobs))
-			copy(binaryBlobs, envelope.BinaryBlobs)
-		}
+		// Create copies of the slices
+		binaryBlobs := make([]types.BinaryBlobCompleted, len(envelope.BinaryBlobs))
+		copy(binaryBlobs, envelope.BinaryBlobs)
 
 		for j := range binaryBlobs {
 			url := fmt.Sprintf("http://%s%sdownload/%s/%s", MetaDataServerIP, PatchEnvelopeURLPath, envelope.PatchID, binaryBlobs[j].FileName)
 			binaryBlobs[j].URL = url
+			binaryBlobs[j].EncArtifactMeta = types.CipherBlockStatus{}
+		}
+
+		// Create copies of the cipher blobs
+		for _, cipher := range envelope.CipherBlobs {
+			if cipher.EncType == types.BlobEncrytedTypeInline && cipher.Inline != nil {
+				inline := *cipher.Inline
+				url := fmt.Sprintf("http://%s%sdownload/%s/%s", MetaDataServerIP, PatchEnvelopeURLPath, envelope.PatchID, inline.FileName)
+				inline.URL = url
+				inline.EncArtifactMeta = types.CipherBlockStatus{}
+				binaryBlobs = append(binaryBlobs, inline)
+			}
+		}
+
+		// Set binaryBlobs to nil if empty
+		if len(binaryBlobs) == 0 {
+			binaryBlobs = nil
+		}
+
+		// Create copies of the volumeRefs
+		volumeRefs := make([]types.BinaryBlobVolumeRef, len(envelope.VolumeRefs))
+		copy(volumeRefs, envelope.VolumeRefs)
+
+		for j := range volumeRefs {
+			volRef := &volumeRefs[j]
+			volRef.EncArtifactMeta = types.CipherBlockStatus{}
+		}
+
+		// Create copies of the cipher blobs for volume blobs
+		for _, cipher := range envelope.CipherBlobs {
+			if cipher.EncType == types.BlobEncrytedTypeVolume && cipher.Volume != nil {
+				volume := *cipher.Volume
+				volume.EncArtifactMeta = types.CipherBlockStatus{}
+				volumeRefs = append(volumeRefs, volume)
+			}
+		}
+
+		// Set volumeRefs to nil if empty
+		if len(volumeRefs) == 0 {
+			volumeRefs = nil
 		}
 
 		toDisplay[i] = PeInfoToDisplay{
 			PatchID:     envelope.PatchID,
 			Version:     envelope.Version,
 			BinaryBlobs: binaryBlobs,
-			VolumeRefs:  envelope.VolumeRefs,
+			VolumeRefs:  volumeRefs,
 		}
 	}
 

pkg/pillar/cmd/msrv/patchenvelopes_test.go
@@ -27,7 +27,12 @@ func TestPatchEnvelopes(t *testing.T) {
 	logger := logrus.StandardLogger()
 	log := base.NewSourceLogObject(logger, "petypes", 1234)
 	ps := pubsub.New(&pubsub.EmptyDriver{}, logger, log)
-	peStore := msrv.NewPatchEnvelopes(log, ps)
+	srv := &msrv.Msrv{
+		Log:    log,
+		PubSub: ps,
+		Logger: logger,
+	}
+	peStore := msrv.NewPatchEnvelopes(srv)
 
 	patch1UUID := "6ba7b810-9dad-11d1-80b4-000000000000"
 	app1UUID := "6ba7b810-9dad-11d1-80b4-00c04fd430c8"

commit f8bf4570f3dfb3d7e92b9369f2c72f249b37e2d1

cipher: report a failed decryption as a failure
GetCipherMarshalledData passed a literal nil where handleCipherBlockCredError
expects the error that just occurred. That helper only sets the object error
when its err argument is non-nil, and it returns that argument, so a failed
DecryptCipherBlock produced no error and no error state. GetCipherCredentials
then unmarshalled a nil payload, which proto.Unmarshal accepts as an empty
message, recorded a success, and handed the caller a zero EncryptionBlock.

A decrypt failure therefore looked like a decrypted empty secret. An
application with encrypted cloud-init user data reached RUNNING with an empty
environment and no error anywhere in the API, and the failure also incremented
SuccessCount. Only the cipher metrics hinted at it, because RecordFailure runs
before the guard.

This is a regression from 786a54885, which split GetCipherMarshalledData out of
GetCipherCredentials and changed this branch from passing err to passing nil;
the error was forwarded correctly before that. It is present in 14.5-stable,
16.0-stable and 17.0-stable.

Passing err through also lets the callers reach the fallbacks they already
implement: every one of them records CleartextFallback or MissingFallback and
decides whether to proceed on cleartext, and all of that was being skipped
because the error never arrived. The visible change is for an application whose
user data is encrypted with no cleartext to fall back to - it now surfaces an
error instead of booting with an empty environment.

Forwarding the error also means GetCipherCredentials sees it, and it used to
record a failure of its own on top of the one already recorded, so a single
failed decrypt would have counted twice. It now only propagates, and the
failure is recorded once by GetCipherMarshalledData as DecryptFailed rather
than Invalid. That also stops a block which arrived already in error from being
counted as a decryption failure, since it never reached decryption; it is

pkg/pillar/cipher/cipher.go
@@ -41,8 +41,8 @@ func GetCipherCredentials(ctx *DecryptCipherContext,
 	var decBlock types.EncryptionBlock
 	cipherBlock, clearBytes, err := GetCipherMarshalledData(ctx, status)
 	if err != nil {
-		return handleCipherBlockCredError(ctx, &cipherBlock,
-			decBlock, err, types.DecryptFailed)
+		// Already counted and marked in-error by GetCipherMarshalledData.
+		return cipherBlock, decBlock, err
 	}
 
 	var zconfigDecBlock zcommon.EncryptionBlock
@@ -91,7 +91,7 @@ func GetCipherMarshalledData(ctx *DecryptCipherContext,
 		ctx.Log.Errorf("%s, cipherblock decryption failed, %v\n",
 			cipherBlock.Key(), err)
 		cblock, _, err := handleCipherBlockCredError(ctx, cipherBlock,
-			decBlock, nil, types.Invalid)
+			decBlock, err, types.DecryptFailed)
 		return cblock, nil, err
 	}
 

pkg/pillar/cipher/cipher_test.go
@@ -1,0 +1,121 @@
+// Copyright (c) 2026 Zededa, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package cipher
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/lf-edge/eve/pkg/pillar/base"
+	"github.com/lf-edge/eve/pkg/pillar/types"
+	"github.com/sirupsen/logrus"
+)
+
+func testDecryptContext() *DecryptCipherContext {
+	return &DecryptCipherContext{
+		Log:          base.NewSourceLogObject(logrus.StandardLogger(), "test", 0),
+		AgentName:    "test",
+		AgentMetrics: NewAgentMetrics("test"),
+	}
+}
+
+// undecryptableBlock fails inside DecryptCipherBlock on the empty-payload check,
+// before any controller/edge-node cert lookup, so no subscriptions are needed.
+func undecryptableBlock() types.CipherBlockStatus {
+	return types.CipherBlockStatus{
+		CipherBlockID:   "test-block",
+		CipherContextID: "test-context",
+		IsCipher:        true,
+	}
+}
+
+func TestGetCipherMarshalledDataFailsClosed(t *testing.T) {
+	ctx := testDecryptContext()
+
+	status, clearBytes, err := GetCipherMarshalledData(ctx, undecryptableBlock())
+	if err == nil {
+		t.Fatalf("decryption failure reported as success (clearBytes=%v)", clearBytes)
+	}
+	if !status.HasError() {
+		t.Fatal("decryption failure left the cipher block status un-errored")
+	}
+}
+
+func TestGetCipherCredentialsFailsClosed(t *testing.T) {
+	ctx := testDecryptContext()
+
+	status, decBlock, err := GetCipherCredentials(ctx, undecryptableBlock())
+	if err == nil {
+		t.Fatalf("decryption failure reported as success (decBlock=%+v)", decBlock)
+	}
+	if !status.HasError() {
+		t.Fatal("decryption failure left the cipher block status un-errored")
+	}
+	if !reflect.DeepEqual(decBlock, types.EncryptionBlock{}) {
+		t.Fatalf("expected no plaintext on failure, got %+v", decBlock)
+	}
+}
+
+// The !IsCipher branch is a misuse guard, not a decryption failure: callers are
+// expected to check IsCipher and supply their own cleartext, so it stays
+// non-fatal.
+func TestGetCipherMarshalledDataNotCipher(t *testing.T) {
+	ctx := testDecryptContext()
+
+	status := types.CipherBlockStatus{CipherBlockID: "test-block"}
+	outStatus, _, err := GetCipherMarshalledData(ctx, status)
+	if err != nil {
+		t.Fatalf("expected no error for a non-cipher block, got %v", err)
+	}
+	if outStatus.HasError() {
+		t.Fatal("non-cipher block should not be marked in-error")
+	}
+}
+
+// A failed decrypt must be counted once: GetCipherMarshalledData records it and
+// GetCipherCredentials only propagates the error.
+func TestGetCipherCredentialsCountsFailureOnce(t *testing.T) {
+	ctx := testDecryptContext()
+
+	if _, _, err := GetCipherCredentials(ctx, undecryptableBlock()); err == nil {
+		t.Fatal("decryption failure reported as success")
+	}
+
+	m := ctx.AgentMetrics.metrics
+	if m.FailureCount != 1 {
+		t.Errorf("FailureCount = %d, want 1", m.FailureCount)
+	}
+	if m.SuccessCount != 0 {
+		t.Errorf("SuccessCount = %d, want 0", m.SuccessCount)
+	}
+	if got := m.TypeCounters[types.DecryptFailed]; got != 1 {
+		t.Errorf("TypeCounters[DecryptFailed] = %d, want 1", got)
+	}
+	if got := m.TypeCounters[types.Invalid]; got != 0 {
+		t.Errorf("TypeCounters[Invalid] = %d, want 0", got)
+	}
+}
+
+// A block that arrived already in error never reached decryption, so it counts
+// as NotReady and not as a decryption failure.
+func TestGetCipherCredentialsNotReadyCountedAsNotReady(t *testing.T) {
+	ctx := testDecryptContext()
+	status := undecryptableBlock()
+	status.SetErrorNow("cipher context not found")
+
+	if _, _, err := GetCipherCredentials(ctx, status); err == nil {
+		t.Fatal("not-ready cipher block reported as success")
+	}
+
+	m := ctx.AgentMetrics.metrics
+	if m.FailureCount != 1 {
+		t.Errorf("FailureCount = %d, want 1", m.FailureCount)
+	}
+	if got := m.TypeCounters[types.NotReady]; got != 1 {
+		t.Errorf("TypeCounters[NotReady] = %d, want 1", got)
+	}
+	if got := m.TypeCounters[types.DecryptFailed]; got != 0 {
+		t.Errorf("TypeCounters[DecryptFailed] = %d, want 0", got)
+	}
+}